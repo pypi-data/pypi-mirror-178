50cbdc390f5b5f045666eb3ad5813d6ab1bc62d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:54 +0000

    Add package mext/1.1.6/mext-1.1.6.tar.gz

---
+++ packages/mext/mext-1.1.6.tar.gz/mext-1.1.6/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+README = open('README.md').read()
+
+setuptools.setup(
+    name='mext',
+    version='1.1.6',
+    url='https://github.com/modbender/manga-extractor',
+    description='A simple manga extractor. Extracts any comic info, chapter list and chapter pages. In development.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Yashas H R',
+    author_email='rameshmamathayashas@gmail.com',
+    packages=setuptools.find_packages(),
+    package_dir={'mext': 'mext'},
+    package_data={
+        'mext': ['data/*.json'],
+    },
+    install_requires=[
+        'lxml>=4.8.0',
+        'beautifulsoup4>=4.10.0',
+        'undetected-chromedriver>=3.1.5.post1',
+    ],
+    python_requires='>=3.5',
+    platforms=['any'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
