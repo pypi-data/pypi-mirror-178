fa9f35b6f560ca89b7d36e9de3a3d80a28d12a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:34:42 +0000

    Add package Mycol/0.0.3/Mycol-0.0.3.tar.gz

---
+++ packages/Mycol/Mycol-0.0.3.tar.gz/Mycol-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+__name__ = "Mycol"
+__version__ = "0.0.3"
+__description__ = "Collection of random functions"
+__author__ = "Matti"
+__email__ = "877cmatt@gmail.com"
+__homepage__ = "https://github.com/Mattimated/my"
+__bug_tracker__ = "https://github.com/Mattimated/my/issues"
+
+setuptools.setup(
+    name=__name__, 
+    version=__version__, 
+    author=__author__, 
+    author_email=__email__, 
+    description=__description__, 
+    url=__homepage__, 
+    packages=setuptools.find_packages(), 
+    classifiers=[
+        "Programming Language : Python :: 3", 
+        "License :: OSI Approved :: MIT License", 
+        "Operating System :: OS Independent"
+    ]
+)
