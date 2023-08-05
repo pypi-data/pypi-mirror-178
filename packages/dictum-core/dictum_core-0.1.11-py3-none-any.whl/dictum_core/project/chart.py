2a6b3f39eb6ac3612dab595c200e6372a0b1c497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:35 +0000

    Add package dictum-core/0.1.11/dictum_core-0.1.11-py3-none-any.whl

---
+++ packages/dictum-core/dictum_core-0.1.11-py3-none-any.whl/dictum_core/project/chart.py
@@ -0,0 +1,32 @@
+from altair import Chart
+
+import dictum_core.project
+from dictum_core.project.altair.data import DictumData
+from dictum_core.ql.transformer import compile_dimension, compile_metric
+
+
+class ProjectChart:
+    def __init__(self, project: "dictum_core.project.Project"):
+        self.project = project
+        self.data = DictumData(project, [])
+
+    def __call__(self, *metrics):
+        self.data = DictumData(self.project, metrics)
+        return self
+
+    @property
+    def chart(self) -> Chart:
+        return Chart(self.data)
+
+    def where(self, *filters) -> Chart:
+        self.data.filters.extend(compile_dimension(str(f)) for f in filters)
+        return self
+
+    def limit(self, *limits) -> Chart:
+        self.data.limits.extend(compile_metric(str(lim)) for lim in limits)
+        return self
+
+    def __getattr__(self, name: str):
+        if name.startswith("_repr_"):
+            raise AttributeError
+        return getattr(self.chart, name)
