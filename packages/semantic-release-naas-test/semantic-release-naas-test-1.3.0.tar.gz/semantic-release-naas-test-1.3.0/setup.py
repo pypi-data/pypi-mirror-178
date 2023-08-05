fbc2309b1ebd7095e65eaef01dbe1c5436fbc3a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:52 +0000

    Add package semantic-release-naas-test/1.3.0/semantic-release-naas-test-1.3.0.tar.gz

---
+++ packages/semantic-release-naas-test/semantic-release-naas-test-1.3.0.tar.gz/semantic-release-naas-test-1.3.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='semantic-release-naas-test',
+    description="Test semantic release",
+    long_description="Simply test semantic release with pypi",
+    long_description_content_type="text/x-rst",
+    author="Maxime Jublou",
+    author_email="maxime@naas.ai",
+    maintainer="Maxime Jublou",
+    maintainer_email="maxime@naas.ai",
+    url="",
+    keywords=["test", "semantic-release"],
+    packages=find_packages(exclude=["tests"]),
+    extras_require={
+        'deploy': [
+            "twine~=4.0"
+        ]
+    }
+)
