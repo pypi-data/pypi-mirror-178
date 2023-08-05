7d7efe5a751e76365dd834f7815669da03ca8a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:26 +0000

    Add package cubicweb-celerytask/2.0.0/cubicweb_celerytask-2.0.0-py3-none-any.whl

---
+++ packages/cubicweb-celerytask/cubicweb_celerytask-2.0.0-py3-none-any.whl/cubicweb_celerytask/__init__.py
@@ -0,0 +1,23 @@
+"""cubicweb-celerytask application package
+
+Run and monitor celery tasks
+"""
+
+
+class _States(object):
+    """ Helper class to create customizable state "lists". Each state is
+    accessible as an attribute and the list may be customized by patching the
+    resulting object.
+    """
+    def __init__(self, *args):
+        super(_States, self).__init__()
+        for arg in args:
+            setattr(self, arg, arg)
+
+    def __contains__(self, item):
+        return hasattr(self, item)
+
+
+STATES = _States('PENDING', 'STARTED', 'SUCCESS', 'FAILURE', 'REVOKED')
+
+FINAL_STATES = _States('SUCCESS', 'FAILURE', 'REVOKED')
