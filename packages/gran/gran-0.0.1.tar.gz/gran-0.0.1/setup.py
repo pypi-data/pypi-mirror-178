7b25f6872c7ab0c2ee4d3d064cf3a000f23979b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:52 +0000

    Add package gran/0.0.1/gran-0.0.1.tar.gz

---
+++ packages/gran/gran-0.0.1.tar.gz/gran-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gran',
+    version='0.0.1',
+    description='Grads, Rands & Nets',
+    long_description='',
+    url='https://github.com/MaximilienLC/gran',
+    author='Maximilien Le Clei',
+    author_email='maximilienleclei@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
