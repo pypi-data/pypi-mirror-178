7e6880b085786f22203da4f4c3f3d2b53a9bed92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:44 +0000

    Add package vern/1.2.7b0/vern-1.2.7b0.tar.gz

---
+++ packages/vern/vern-1.2.7b0.tar.gz/vern-1.2.7b0/vern/parse_mat.py
@@ -0,0 +1,19 @@
+# profilometer
+import numpy as np
+import pandas as pd
+from .misc import *
+import mat73
+import scipy.io
+__all__ = ['parse_mat']
+
+def read_files(input_path, txt_path):
+    try:
+        mat = mat73.loadmat(input_path)
+    except:
+        mat = scipy.io.loadmat(input_path)
+    print(mat)
+    df = pd.DataFrame.from_dict(mat)
+    df.to_csv(txt_path, index=False)
+
+def parse_mat(**kwargs):
+    read_files(kwargs["input_path"], kwargs["txt_path"])
