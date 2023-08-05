8e0f45af04b70587cd2d98d797058912b817fc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:44 +0000

    Add package optunizer/0.1.10/optunizer-0.1.10-py3-none-any.whl

---
+++ packages/optunizer/optunizer-0.1.10-py3-none-any.whl/optunizer/__main__.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+from dotenv import load_dotenv, dotenv_values
+import yaml
+
+from optunizer.factory import factory
+
+
+if __name__ == '__main__':
+  load_dotenv('.env')
+  if len(sys.argv) > 1:
+    c = sys.argv[1]
+  else:
+    c = os.getenv('OPTUNA_CONFIG', 'optunizer.yaml')
+  with open(c) as f:
+    kwargs = yaml.safe_load(f)
+  s = os.getenv('OPTUNA_SECRET', '.env.secret')
+  s = dotenv_values(s)
+  s = {k.lower(): v for k, v in s.items()}
+  kwargs['secret'] = s
+  c, kwargs = factory(kwargs)
+  c(**kwargs)
