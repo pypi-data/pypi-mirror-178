d263b64df6ab2b5ac398ec07e089a6d8a8f9162c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:47 +0000

    Add package gparms/0.0.3/gparms-0.0.3.tar.gz

---
+++ packages/gparms/gparms-0.0.3.tar.gz/gparms-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Get URL Parameters'
+LONG_DESCRIPTION = 'A tools that takes URLs from stdin and outputs URLs with parameters.'
+
+# Setting up
+setup(
+    name="gparms",
+    version=VERSION,
+    author="Shelled",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'bug bounty', 'pentesting', 'url'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    entry_points={
+        'console_scripts': [
+            'gparms = gparms.gparms:main'
+        ]
+    }
+)
