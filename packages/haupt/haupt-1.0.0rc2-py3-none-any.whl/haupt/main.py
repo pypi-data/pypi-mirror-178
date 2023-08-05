14997ae7171df2afd52277dbaa3b3b7043f9a789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:10 +0000

    Add package haupt/1.0.0rc2/haupt-1.0.0rc2-py3-none-any.whl

---
+++ packages/haupt/haupt-1.0.0rc2-py3-none-any.whl/haupt/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#
+# Copyright 2018-2022 Polyaxon, Inc.
+# This file and its contents are licensed under the AGPLv3 License.
+# Please see the included NOTICE for copyright information and
+# LICENSE-AGPL for a copy of the license.
+import os
+
+import click
+
+from haupt.cli.proxies import proxy
+from haupt.cli.sandbox import sandbox
+from haupt.cli.server import server
+from haupt.cli.streams import streams
+from polyaxon.logger import clean_outputs, configure_logger
+
+
+@click.group()
+@click.option(
+    "-v", "--verbose", is_flag=True, default=False, help="Turn on debug logging"
+)
+@clean_outputs
+def cli(verbose):
+    """Haupt -  Lineage metadata API, artifacts streams, sandbox, ML-API, and spaces for Polyaxon.
+
+    Check the help available for each command listed below by appending `-h`.
+    """
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "haupt.polyconf.settings")
+    configure_logger(verbose)
+
+
+cli.add_command(sandbox)
+cli.add_command(server)
+cli.add_command(streams)
+cli.add_command(proxy)
+
+
+def main():
+    cli(auto_envvar_prefix="HAUPT_CLI")
