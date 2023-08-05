93ff4b2ca73b44bef439b911f047d8538ac1876d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:33:13 +0000

    Add package Mycol/0.0.4a0/Mycol-0.0.4a0.tar.gz

---
+++ packages/Mycol/Mycol-0.0.4a0.tar.gz/Mycol-0.0.4a0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+__name__ = "Mycol"
+__version__ = "0.0.4a"
+__description__ = "Collection of random functions"
+__author__ = "Matti"
+__homepage__ = "https://github.com/Mattimated/my"
+__bug_tracker__ = "https://github.com/Mattimated/my/issues"
+
+setuptools.setup(
+    name=__name__, 
+    version=__version__, 
+    author=__author__, 
+    description=__description__, 
+    url=__homepage__, 
+    packages=setuptools.find_packages(), 
+    classifiers=[
+        "Programming Language : Python :: 3", 
+        "License :: OSI Approved :: MIT License", 
+        "Operating System :: OS Independent"
+    ]
+)
