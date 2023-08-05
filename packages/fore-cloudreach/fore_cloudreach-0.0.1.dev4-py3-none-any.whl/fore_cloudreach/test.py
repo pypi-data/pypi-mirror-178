92795c0770207658b1e20bc485514f0c10c3e3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:07 +0000

    Add package fore-cloudreach/0.0.1.dev4/fore_cloudreach-0.0.1.dev4-py3-none-any.whl

---
+++ packages/fore-cloudreach/fore_cloudreach-0.0.1.dev4-py3-none-any.whl/fore_cloudreach/test.py
@@ -0,0 +1,50 @@
+
+from __future__ import print_function
+
+import os.path
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import fore_cloudreach as fc 
+
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1Ni5B7zolPKD2J0CXQSaLec31AXw8HpcdM_xPJKPILRw' #'1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+SAMPLE_RANGE_NAME = 'Rechnung!F5:G8'
+
+def main():
+    """ Shw basic usage of the Sheets API.
+        Prints values from a sample spreadsheet
+    """
+    authet = fc.Auth()
+    
+    creds = authet.get()
+
+    print("currentDir: ", format(os.getcwd()))
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        
+        print('result:', result)
+
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[1], row[0]))
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
