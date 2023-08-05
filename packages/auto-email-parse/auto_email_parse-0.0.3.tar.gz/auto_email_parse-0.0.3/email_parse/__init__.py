70b3381f8f08fff667fe8bb3928b3cfb1a31b77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:22 +0000

    Add package auto-email-parse/0.0.3/auto_email_parse-0.0.3.tar.gz

---
+++ packages/auto-email-parse/auto_email_parse-0.0.3.tar.gz/auto_email_parse-0.0.3/email_parse/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/11/23 下午3:08
+# @Author  : Xsu
+# @File    : __init__.py
+
+name = "auto_email_parse"
+
+from .email_parse import MailParse
---
+++ packages/auto-email-parse/auto_email_parse-0.0.3.tar.gz/auto_email_parse-0.0.3/email_parse/example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/11/24 下午2:15
+# @Author  : Xsu
+# @File    : example.py
+
+# print(MailParse("outlook").login("",""))
+# print(MailParse("outlook").parse(["Inbox","Junk"],"community@minima.global","","",'Welcome to the Minima Incentive program',
+#                                      r"<a.+?href=\"(.+?)\".*>",re_idx=1))
