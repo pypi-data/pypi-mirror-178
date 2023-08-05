28e52e38e4ac4f30e949e7abe259e1707b8f22dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:30 +0000

    Add package klyqa-ctl/1.0.12/klyqa_ctl-1.0.12.tar.gz

---
+++ packages/klyqa-ctl/klyqa_ctl-1.0.12.tar.gz/klyqa_ctl-1.0.12/klyqa_ctl/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+###################################################################
+#
+# Interactive Klyqa Control commandline client package
+#
+# Company: QConnex GmbH / Klyqa
+# Author: Frederick Stallmeyer
+#
+# E-Mail: frederick.stallmeyer@gmx.de
+#
+###################################################################
+
+__version__ = "1.0.12"
+__author__ = "Frederick Stallmeyer <fraizy@gmx.de>"
+__license__ = "MIT"
+
+from .klyqa_ctl import *
+from .general import *
