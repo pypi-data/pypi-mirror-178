61cbec18652ea95b490ced2ba5f227879bb009e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:51 +0000

    Add package cubicweb-compound/0.10.0/cubicweb_compound-0.10.0-py3-none-any.whl

---
+++ packages/cubicweb-compound/cubicweb_compound-0.10.0-py3-none-any.whl/cubicweb_compound/__pkginfo__.py
@@ -0,0 +1,27 @@
+# pylint: disable=W0622
+"""cubicweb-compound application packaging information"""
+
+distname = 'cubicweb-compound'
+modname = 'cubicweb_coumpound'
+
+numversion = (0, 10, 0)
+version = '.'.join(str(num) for num in numversion)
+
+license = 'LGPL'
+author = 'LOGILAB S.A. (Paris, FRANCE)'
+author_email = 'contact@logilab.fr'
+description = 'Library cube to handle assemblies of composite entities'
+web = 'https://forge.extranet.logilab.fr/cubicweb/cubes/%s' % distname
+
+__depends__ = {
+    'cubicweb': ">= 3.38.0, < 3.39.0",
+}
+
+__recommends__ = {}
+
+classifiers = [
+    'Environment :: Web Environment',
+    'Framework :: CubicWeb',
+    'Programming Language :: Python',
+    'Programming Language :: JavaScript',
+]
