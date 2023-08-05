3b1d0780415018dab391eafcbaeb21cc2351760e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:11 +0000

    Add package wavinfo/2.0.0/wavinfo-2.0.0-py3-none-any.whl

---
+++ packages/wavinfo/wavinfo-2.0.0-py3-none-any.whl/wavinfo/__init__.py
@@ -0,0 +1,9 @@
+"""
+methods to probe a WAV file for various kinds of production metadata.
+
+Go to the documentation for wavinfo.WavInfoReader for more information.
+"""
+
+from .wave_reader import WavInfoReader
+from .riff_parser import WavInfoEOFError
+
