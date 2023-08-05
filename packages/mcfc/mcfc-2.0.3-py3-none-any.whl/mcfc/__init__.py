be400acd0177bba7ee52ceafe59803e7271eedc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:22:09 +0000

    Add package mcfc/2.0.3/mcfc-2.0.3-py3-none-any.whl

---
+++ packages/mcfc/mcfc-2.0.3-py3-none-any.whl/mcfc/__init__.py
@@ -0,0 +1,10 @@
+"""
+## MCFC
+Text formatting using Minecraft color codes.
+
+---
+:copyright: Copyright 2022 (c) woidzero. All rights reserved.
+:license: Licensed under the MIT license.
+"""
+
+from .main import *
