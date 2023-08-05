f53670505632a1c0349340660b430f134a879b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:49 +0000

    Add package TPython/0.5/TPython-0.5.tar.gz

---
+++ packages/TPython/TPython-0.5.tar.gz/TPython-0.5/README.md
@@ -0,0 +1,30 @@
+# TPython
+TPython a better python REPL.
+
+## Features
+- built-in clear/cls
+- colors
+
+## Future Updates
+- Tab completion
+- More flexibility
+- Custom colors
+- Break Points
+- Better look
+
+## Installation
+```
+$ pip install TPython
+```
+
+## Usage
+### run
+```
+$ tpy
+```
+### built-in commands
+| Command | Function |
+| :-------: | :--------: |
+| version | tells version |
+| exit/quit/close | exits the REPL |
+| cls/clear | clears the terminal |
---
+++ packages/TPython/TPython-0.5.tar.gz/TPython-0.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    LD = f.read()
+VERSION = '0.5'
+
+if __name__ == '__main__':
+    setup(
+    name='TPython',
+    version=VERSION,
+    description='A better python REPL',
+    long_description=LD,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Techlord210/TPython',
+    author='Techlord210',
+    author_email='techlord210@gmail.com',
+    license='MIT',
+    classifiers=[
+            "Development Status :: 4 - Beta",
+            'Environment :: Console',
+            "Intended Audience :: Developers",
+            "Intended Audience :: Education",
+            "Intended Audience :: Other Audience",
+            "Programming Language :: Python :: 3.6",
+            "License :: OSI Approved :: MIT License"
+        ],
+    install_requires=[
+        'colorama',
+        'requests',
+        ],
+    entry_points={
+        'console_scripts':[
+            'tpy = main:main'
+            ]
+        }
+    )
