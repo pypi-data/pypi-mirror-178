0b346cb0e7cbdd21978b15b7b2400d793f1495a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:33:23 +0000

    Add package galatasaray/0.1.0/galatasaray-0.1.0.tar.gz

---
+++ packages/galatasaray/galatasaray-0.1.0.tar.gz/galatasaray-0.1.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['galatasaray']
+install_requires = \
+['beautifulsoup4>=4.11.1,<5.0.0',
+ 'requests>=2.28.1,<3.0.0',
+ 'rich>=12.6.0,<13.0.0']
+
+entry_points = \
+{'console_scripts': ['start = galatasaray:main']}
+
+setup_kwargs = {
+    'name': 'galatasaray',
+    'version': '0.1.0',
+    'description': 'Everything about the Galatasaray from cli!',
+    'long_description': '# Galatasaray\n\nEverything about the Galatasaray from cli!\n\n## Installation\n\n```shell\n$ pip install galatasaray\n```\n\n# Usage\n\n```shell\n$ galatasaray\n```\n\n![galatasaray](./assets/1.png)\n',
+    'author': 'Ozcan Yarimdunya',
+    'author_email': 'ozcanyd@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ozcanyarimdunya/commander',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
