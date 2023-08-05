661058cc86e6e654a23df851f6e99002d3bd9739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:16 +0000

    Add package reaxiom/0.0.5/reaxiom-0.0.5.tar.gz

---
+++ packages/reaxiom/reaxiom-0.0.5.tar.gz/reaxiom-0.0.5/axiom/settings.py
@@ -0,0 +1,8 @@
+import os
+__home = os.environ['HOME']
+config = {
+    'home': __home,
+    'axiom_path': os.path.join(__home, 'reaxiom'),
+    'axiom_cache': os.path.join(__home, 'reaxiom', 'cache'),
+    'huggingface_cache': os.path.join(__home, 'reaxiom', 'huggingface_cache'),
+}
