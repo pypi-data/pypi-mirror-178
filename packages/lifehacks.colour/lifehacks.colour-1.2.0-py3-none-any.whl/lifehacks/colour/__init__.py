879efad6da714f0bcb92edc71ce092f1625722aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:55 +0000

    Add package lifehacks.colour/1.2.0/lifehacks.colour-1.2.0-py3-none-any.whl

---
+++ packages/lifehacks.colour/lifehacks.colour-1.2.0-py3-none-any.whl/lifehacks/colour/__init__.py
@@ -0,0 +1,13 @@
+'''	Copyright © 2022 mightbesimon.com
+	All rights reserved.
+
+	Material belonging to others may have been
+	used under Creative Commons Licence or with
+	explicit or implicit permission.
+'''
+
+from .abstract import Colour
+from .hsla import hsla
+from .rgba import rgba
+
+__all__ = ['Colour', 'hsla', 'rgba']
---
+++ packages/lifehacks.colour/lifehacks.colour-1.2.0-py3-none-any.whl/lifehacks/colour/palette/apple.py
@@ -0,0 +1,23 @@
+'''	Copyright © 2022 mightbesimon.com
+	All rights reserved.
+
+	Material belonging to others may have been
+	used under Creative Commons Licence or with
+	explicit or implicit permission.
+'''
+
+from lifehacks.metaclasses import enum
+from .. import Colour, rgba
+
+################################################################
+#######                     palette                      #######
+################################################################
+class Apple(metaclass=enum[Colour]):
+	'''	MacOS colours
+	'''
+	DARK_0 = rgba( 34,  33, 35)
+	DARK_1 = rgba( 57,  55, 54)
+	RED    = rgba(255,  91, 84)
+	YELLOW = rgba(231, 192, 59)
+	GREEN  = rgba( 77, 192, 57)
+	LUCK   = rgba(135, 188, 93)
