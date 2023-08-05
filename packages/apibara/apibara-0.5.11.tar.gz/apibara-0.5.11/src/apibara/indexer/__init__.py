052009d1695d710df313d036c95175f4c760f2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:30 +0000

    Add package apibara/0.5.11/apibara-0.5.11.tar.gz

---
+++ packages/apibara/apibara-0.5.11.tar.gz/apibara-0.5.11/src/apibara/indexer/__init__.py
@@ -0,0 +1,6 @@
+"""
+This classes implement the same API as the v0.4 SDK, but are
+compatible with the new Apibara Stream protocol.
+"""
+
+from .runner import IndexerRunner, IndexerRunnerConfiguration, Info
