a33127e9bf5a306d2fb14463b2041810b1f886b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:24 +0000

    Add package PyQuantimClient/1.0.0/PyQuantimClient-1.0.0-py3-none-any.whl

---
+++ packages/PyQuantimClient/PyQuantimClient-1.0.0-py3-none-any.whl/PyQuantimClient/portfolios.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import datetime as dt
+from .api import quantim
+
+class portfolios(quantim):
+    def __init__(self, username, password, secretpool, env="qa"):
+        super().__init__(username, password, secretpool, env)
+
+    def get_portfolios(self, ref_date=None, country=None, fields=None, port_names=None):
+        '''
+        Get portfolio
+        '''
+        if country is None and port_names is None:
+            raise ValueError('Either country or port_names must be different to None.')
+        data = {'date':ref_date, 'country':country, 'fields':fields, 'port_names':port_names}
+        resp = self.api_call('query_portfolios', method="post", data=data, verify=False)
+        ports_df = pd.DataFrame(resp)
+        return ports_df
