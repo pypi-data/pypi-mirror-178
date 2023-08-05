a27f7d48a36766cc5ad28475cd37eb2a91d35f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:22:01 +0000

    Add package IQM-Vis/0.2.5.16/IQM_VIS-0.2.5.16-py3-none-any.whl

---
+++ packages/IQM-Vis/IQM_VIS-0.2.5.16-py3-none-any.whl/IQM_VIS/utils/gui_utils.py
@@ -0,0 +1,79 @@
+'''
+Utils for PyQt6 image, text and graph widgets
+'''
+# Author: Matt Clifford <matt.clifford@bristol.ac.uk>
+import sys
+try:
+    from PyQt6.QtGui import QPixmap, QImage
+    import matplotlib; matplotlib.use('Qt5Agg')
+    from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+    from matplotlib.figure import Figure
+    HEADLESS = False
+except ImportError:
+    import warnings
+    warnings.warn('Can not load PyQt6 library - running IQM_VIS package in headless mode')
+    HEADLESS = True
+import numpy as np
+from skimage.util import img_as_ubyte
+from skimage.transform import resize
+import cv2
+
+'''
+image helper functions
+'''
+def resize_im_to(np_array, size):
+    down_im = resize(np_array, size)
+    return img_as_ubyte(down_im)
+
+def change_im(widget, im, resize=False):
+    '''
+    given a numpy image, changes the given widget Frame
+    '''
+    if im.shape[2] == 1:
+        im = np.concatenate([im, im, im], axis=2)
+    if resize:
+        im = resize_im_to(im, resize)
+    qimage = QImage(im,
+                    im.shape[1],
+                    im.shape[0],
+                    im.shape[1]*im.shape[2],
+                    QImage.Format.Format_RGB888)
+                    # QImage.Format_RGB888)  # PyQt5
+    pixmap = QPixmap(qimage)
+    widget.setPixmap(pixmap)
+    # QApplication.processEvents()   # force to change other UI wont respond
+
+'''
+text utils
+'''
+
+def str_to_len(string, length=5, append_char='0', plus=False):
+    # cut string to length, or append character to make to length
+    if string[0] !=  '-' and plus == True:
+        string = '+' + string
+    if len(string) > length:
+        string = string[:length]
+    elif len(string) < length:
+        string = string + append_char*(length-len(string))
+    return string
+
+def get_metric_image_name(metric, image_name):
+    return metric+get_image_pair_name(image_name)
+
+def get_transformed_image_name(image_name):
+    return 'T('+image_name+')'
+
+def get_image_pair_name(image_name):
+    return str((image_name, get_transformed_image_name(image_name)))
+
+
+'''
+matplotlib widget utils
+'''
+if not HEADLESS:
+    # Get a matplotlib canvas as a Qt Widget
+    class MplCanvas(FigureCanvasQTAgg):
+        def __init__(self, parent=None, width=5, height=3, dpi=100, polar=False):
+            self.figure = Figure(figsize=(width, height), dpi=dpi)
+            self.axes = self.figure.add_subplot(111, polar=polar)
+            super(MplCanvas, self).__init__(self.figure)
