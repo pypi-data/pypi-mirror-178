ccf4e237ff420d337680fe112867ccab31ed93c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:14 +0000

    Add package TGSessionsConverter/0.0.4/TGSessionsConverter-0.0.4.tar.gz

---
+++ packages/TGSessionsConverter/TGSessionsConverter-0.0.4.tar.gz/TGSessionsConverter-0.0.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+    "opentele==1.15.1",
+    "tgcrypto==1.2.3",
+    "telethon",
+    "pyrogram",
+    "stream-sqlite==0.0.38",
+    "nest_asyncio"
+]
+
+setuptools.setup(
+    name="TGSessionsConverter",
+    version="0.0.4",
+    author="watdev",
+    author_email="v.liubachevskyi@gmail.com",
+    description="This module is small util for converting Telegram sessions  to various formats (Telethon, Pyrogram, Tdata)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wathipol/TGSessionsCoverter",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
