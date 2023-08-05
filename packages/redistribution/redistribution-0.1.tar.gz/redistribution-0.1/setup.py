ef95d739360a7a85db8be1d339ede44cc8f6a908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package redistribution/0.1/redistribution-0.1.tar.gz

---
+++ packages/redistribution/redistribution-0.1.tar.gz/redistribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='redistribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['redistribution'],
+      zip_safe=False)
