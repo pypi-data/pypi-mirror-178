ad6b4dbf0cf049d381eff7deff7c878c92125b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:19:30 +0000

    Add package jptest2/2.0.19rc0/jptest2-2.0.19rc0.tar.gz

---
+++ packages/jptest2/jptest2-2.0.19rc0.tar.gz/jptest2-2.0.19rc0/src/jptest2/__init__.py
@@ -0,0 +1,5 @@
+from .JPSetup import JPSetup, JPTeardown
+from .JPTest import JPTest
+from .JPTestComparison import JPTestComparison
+from .JPTestGet import JPTestGet
+from .notebook import Notebook, PythonNotebook, SQLiteNotebook, NotebookReference
