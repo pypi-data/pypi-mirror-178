d01ec1e3d8822e1f6a9f14a90ec6fe829731aa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:53 +0000

    Add package lifehacks.metaclasses/1.2.3/lifehacks.metaclasses-1.2.3-py3-none-any.whl

---
+++ packages/lifehacks.metaclasses/lifehacks.metaclasses-1.2.3-py3-none-any.whl/lifehacks/metaclasses/__init__.py
@@ -0,0 +1,17 @@
+'''	Copyright © 2022 mightbesimon.com
+	All rights reserved.
+
+	Material belonging to others may have been
+	used under Creative Commons Licence or with
+	explicit or implicit permission.
+'''
+
+################################################################
+#######                     exports                      #######
+################################################################
+
+from .exceptions import EnumException
+from .meta import meta
+from .enum import enum
+
+__all__ = ['meta', 'enum', 'EnumException']
