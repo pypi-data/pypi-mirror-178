7246e26a93d347b439597dbd3b0a7ddbfdc1bd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:02 +0000

    Add package helloworldbasic5message/1.0.11/helloworldbasic5message-1.0.11-py3-none-any.whl

---
+++ packages/helloworldbasic5message/helloworldbasic5message-1.0.11-py3-none-any.whl/helloworldbasic5message/_previousinit/__init__.py
@@ -0,0 +1,29 @@
+# help line 1
+# help line 21
+
+
+
+
+
+import helloworldbasic4message  as hb4
+from helloworldbasic4message  import MarianoDocument as RainbowDocument
+from helloworldbasic4message  import FontSize
+from helloworldbasic4message  import FontColor
+
+
+
+
+#import numpy as np
+
+# also recommended imports specifics
+
+
+
+#from helloworldbasic4message.utility2 import somefunction
+
+#nparray=np.array([1,2])
+#print(nparray)
+#print(help (hb4) )
+print("hello helloworldbasic5 Nov 9, 2022")
+
+
---
+++ packages/helloworldbasic5message/helloworldbasic5message-1.0.11-py3-none-any.whl/helloworldbasic5message/mymessage.py
@@ -0,0 +1,7 @@
+def sayhello1(name):
+    result="version 5.01 hello "+name
+    print(result)
+    return result
+
+
+
