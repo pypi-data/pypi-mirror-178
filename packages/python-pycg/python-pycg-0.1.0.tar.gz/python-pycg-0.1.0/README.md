33480b2655f712f8a459f9c3c9a11755a5c4d9a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:21 +0000

    Add package python-pycg/0.1.0/python-pycg-0.1.0.tar.gz

---
+++ packages/python-pycg/python-pycg-0.1.0.tar.gz/python-pycg-0.1.0/README.md
@@ -0,0 +1,21 @@
+# PyCG: Toolbox for CG-related visualizations and computations
+
+I'm writing the document of this. Please do not use it unless you are someone internal.
+
+## Install
+
+Clone this repository and install it via:
+```shell
+git clone --recursive https://github.com/heiwang1997/pycg
+python setup.py develop
+```
+
+### Bonus
+
+A customized compilation of Open3D will give you better experience including multi-window camera/light synchronization, animation maker and visualizer, scalar analyzer, and many more. Please compile and install my Open3D:
+
+```
+git clone --recursive https://github.com/heiwang1997/Open3D
+python setup.py install
+```
+
---
+++ packages/python-pycg/python-pycg-0.1.0.tar.gz/python-pycg-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from pycg import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+print(find_packages())
+
+setup(
+    name='python-pycg',
+    version=__version__,
+    author='Jiahui Huang',
+    author_email='huangjh.work@outlook.com',
+    description='PyCG: Toolbox for CG-related visualizations and computations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/heiwang1997/pycg",
+    packages=find_packages(),
+    classifiers=[
+        "Operating System :: Unix",
+        "Topic :: Multimedia :: Graphics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only"
+    ],
+    keywords=['pycg', 'graphics', '3d', 'visualization'],
+    python_requires='>=3.6',
+    install_requires=[],
+    include_package_data=True,
+)
