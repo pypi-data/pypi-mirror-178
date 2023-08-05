ec4e9f5177d60f61eb0a6023c8fba0bea18db002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:32:15 +0000

    Add package vscodeskin/0.0.1/vscodeskin-0.0.1.tar.gz

---
+++ packages/vscodeskin/vscodeskin-0.0.1.tar.gz/vscodeskin-0.0.1/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name="vscodeskin", version="0.0.1", py_modules=["vscodeskin"], author="me", author_email="me@me.com", description="This is a placeholder package created by registerit.", url="http://www.github.com")
