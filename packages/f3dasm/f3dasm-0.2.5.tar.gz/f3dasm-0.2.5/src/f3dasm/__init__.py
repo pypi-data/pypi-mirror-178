f2aa2b987c5c8d6209399ef5719d76c8cb405064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:17 +0000

    Add package f3dasm/0.2.5/f3dasm-0.2.5.tar.gz

---
+++ packages/f3dasm/f3dasm-0.2.5.tar.gz/f3dasm-0.2.5/src/f3dasm/__init__.py
@@ -0,0 +1,35 @@
+"""
+F3DASM
+======
+
+This is the initalizer of the F3DASM package
+"""
+
+__version__ = "0.2.5"
+import logging
+
+# Configuration file structure
+# Import implementation modules in separate namespaces
+from . import config, functions, optimization, sampling, simulation
+from .base.data import *
+from .base.design import *
+from .base.function import *
+from .base.metaoptimizer import *
+from .base.optimization import *
+from .base.samplingmethod import *
+from .base.simulation import *
+
+# Import base class in main namespace
+from .base.space import *
+from .base.utils import *
+
+# Import main scripts
+from .run_optimization import *
+
+# __version__ = "0.2.4"
+
+
+# Logging things
+
+
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
