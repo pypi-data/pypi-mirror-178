4d9fae1a81cac3c508ab0095fed5134d054c531d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:32:16 +0000

    Add package deeptrack/1.5.1/deeptrack-1.5.1-py3-none-any.whl

---
+++ packages/deeptrack/deeptrack-1.5.1-py3-none-any.whl/deeptrack/datasets/__init__.py
@@ -0,0 +1,7 @@
+from deeptrack.datasets import (
+    detection_QuantumDots,
+    segmentation_ssTEM_drosophila,
+    regression_holography_nanoparticles,
+    segmentation_fluorescence_u2os,
+    detection_holography_nanoparticles,
+)
