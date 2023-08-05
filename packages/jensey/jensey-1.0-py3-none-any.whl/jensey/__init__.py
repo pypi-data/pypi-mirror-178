c382f6ed7021c1f2e83e42b3af6c282325c3451b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:22:05 +0000

    Add package jensey/1.0/jensey-1.0-py3-none-any.whl

---
+++ packages/jensey/jensey-1.0-py3-none-any.whl/jensey/__init__.py
@@ -0,0 +1,2 @@
+def hello_shakespeare():
+    print('To python or not to python that is the question.')
---
+++ packages/jensey/jensey-1.0-py3-none-any.whl/jensey/__main__.py
@@ -0,0 +1,4 @@
+from . import hello_shakespeare
+
+if __name__ == '__main__':
+    hello_shakespeare()
