773bbb568bdad15684ae773bdf4870b0263211c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:27 +0000

    Add package django-outbox-menu/1.0.11/django_outbox_menu-1.0.11-py3-none-any.whl

---
+++ packages/django-outbox-menu/django_outbox_menu-1.0.11-py3-none-any.whl/menu/views.py
@@ -0,0 +1,9 @@
+# from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+class TestMenuView(TemplateView):
+    template_name = 'test-menu.html'
