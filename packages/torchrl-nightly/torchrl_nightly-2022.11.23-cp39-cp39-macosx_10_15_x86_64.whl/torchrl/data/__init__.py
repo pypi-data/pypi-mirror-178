7b5a98f53b4b31f929111dd350a068ce7ae81896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:51 +0000

    Add package torchrl-nightly/2022.11.23/torchrl_nightly-2022.11.23-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/torchrl-nightly/torchrl_nightly-2022.11.23-cp39-cp39-macosx_10_15_x86_64.whl/torchrl/data/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .postprocs import MultiStep
+from .replay_buffers import (
+    ReplayBuffer,
+    PrioritizedReplayBuffer,
+    TensorDictReplayBuffer,
+    TensorDictPrioritizedReplayBuffer,
+    Storage,
+    ListStorage,
+    LazyMemmapStorage,
+    LazyTensorStorage,
+)
+from .tensor_specs import (
+    TensorSpec,
+    BoundedTensorSpec,
+    OneHotDiscreteTensorSpec,
+    UnboundedContinuousTensorSpec,
+    UnboundedDiscreteTensorSpec,
+    NdBoundedTensorSpec,
+    NdUnboundedContinuousTensorSpec,
+    NdUnboundedDiscreteTensorSpec,
+    BinaryDiscreteTensorSpec,
+    MultOneHotDiscreteTensorSpec,
+    DiscreteTensorSpec,
+    CompositeSpec,
+    DEVICE_TYPING,
+)
