c22c7feb7a08f9e0c6cb3542b4359abc9913f228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:54 +0000

    Add package cloudnetpy/1.42.2/cloudnetpy-1.42.2.tar.gz

---
+++ packages/cloudnetpy/cloudnetpy-1.42.2.tar.gz/cloudnetpy-1.42.2/setup.py
@@ -0,0 +1,55 @@
+from setuptools import find_packages, setup
+
+version: dict = {}
+with open("cloudnetpy/version.py", encoding="utf8") as f:
+    exec(f.read(), version)  # pylint: disable=W0122
+
+with open("README.md", encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name="cloudnetpy",
+    version=version["__version__"],
+    description="Python package for Cloudnet processing",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Finnish Meteorological Institute",
+    author_email="actris-cloudnet@fmi.fi",
+    url="https://github.com/actris-cloudnet/cloudnetpy",
+    license="MIT License",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8",
+    setup_requires=["wheel"],
+    install_requires=[
+        "scipy",
+        "netCDF4",
+        "matplotlib",
+        "requests",
+        "pytz",
+        "cloudnetpy_qc>=1.1.1",
+        "scikit-image",
+        "rpgpy>=0.12.1",
+    ],
+    extras_require={
+        "test": [
+            "pytest",
+            "pytest-flakefinder",
+            "pylint",
+            "mypy",
+            "types-requests",
+            "types-pytz",
+        ],
+        "dev": ["pre-commit"],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+    ],
+)
