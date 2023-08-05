e4c025ca383d5ea20d7f934c6f761f9919cd3cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:31 +0000

    Add package serial-for-controller/0.0.3/serial-for-controller-0.0.3.tar.gz

---
+++ packages/serial-for-controller/serial-for-controller-0.0.3.tar.gz/serial-for-controller-0.0.3/README.md
@@ -0,0 +1,52 @@
+# Serial-for-controller [![old api](https://img.shields.io/badge/old-api-blue?style=for-the-badge&logoColor=white)](https://github.com/Adam-Software) [![Platforms](https://img.shields.io/badge/Raspberry%20Pi-A22846?style=for-the-badge&logo=Raspberry%20Pi&logoColor=white)](https://img.shields.io/badge/Raspberry%20Pi-A22846?style=for-the-badge&logo=Raspberry%20Pi&logoColor=white) [![Language](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white)](https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white) [![adam package](https://img.shields.io/badge/adam_package-red?style=for-the-badge&logo=python&logoColor=white)](https://github.com/Adam-Software)
+
+
+[![License](https://img.shields.io/github/license/Adam-Software/Serial-for-controller)](https://img.shields.io/github/license/Adam-Software/Serial-for-controller)
+[![Activity](https://img.shields.io/github/commit-activity/m/Adam-Software/Serial-for-controller)](https://img.shields.io/github/commit-activity/m/Adam-Software/Serial-for-controller)
+[![LastStatus](https://img.shields.io/github/last-commit/Adam-Software/Serial-for-controller)](https://img.shields.io/github/last-commit/Adam-Software/Serial-for-controller)
+[![CodeSize](https://img.shields.io/github/languages/code-size/Adam-Software/Serial-for-controller)](https://img.shields.io/github/languages/code-size/Adam-Software/Serial-for-controller)
+[![Depencies](https://img.shields.io/librariesio/github/Adam-Software/Serial-for-controller)](https://img.shields.io/librariesio/github/Adam-Software/Serial-for-controller)
+
+[![PyPI version](https://badge.fury.io/py/serial-for-controller.svg)](https://badge.fury.io/py/serial-for-controller)
+[![PythonVersion](https://img.shields.io/pypi/pyversions/serial-for-controller)](https://img.shields.io/pypi/pyversions/serial-for-controller)
+[![Wheel](https://img.shields.io/pypi/wheel/serial-for-controller)](https://img.shields.io/pypi/wheel/serial-for-controller)
+[![Status](https://img.shields.io/pypi/status/serial-for-controller)](https://img.shields.io/pypi/status/serial-for-controller)
+[![Format](https://img.shields.io/pypi/format/serial-for-controller)](https://img.shields.io/pypi/format/serial-for-controller)
+### What the library can do?
+
+Python library for data exchange with STM32 controller.
+
+### How install?
+
+Old version package, with full name compatibility
+
+```commandline
+pip install Adam-Serial-for-controller
+```
+
+New version package, without name compatibility
+
+```commandline
+pip install serial-for-controller
+```
+
+### How import?
+
+Old version
+
+```commandline
+from Serial.serialPi import SerialU
+```
+
+New version
+
+```commandline
+from serial_for_controller.SerialPi import SerialPi
+```
+
+### Versions
+
+For old package Adam-Serial-for-controller last version 0.2.1
+For the new version Serial-for-controller, the countdown started from 0.0.1
+
+Thus, the functionality of version 0.2.1 corresponds to version 0.0.1 only with other names of functions and classes
---
+++ packages/serial-for-controller/serial-for-controller-0.0.3.tar.gz/serial-for-controller-0.0.3/serial_for_controller/SerialPi.py
@@ -0,0 +1,169 @@
+﻿#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+# https://docs.python.org/3/library/ctypes.html
+
+import sys
+import time
+import ctypes 
+import struct
+import os
+
+import numpy.ctypeslib as ctl
+import numpy as np
+from numpy.ctypeslib import ndpointer
+
+# checking the bit depth of the operating system
+is64bit = struct.calcsize('P') * 8 == 64
+
+# path
+path = os.path.dirname(os.path.realpath(__file__))
+
+# loading the library depending on the architecture of the operating system
+if os.name != 'nt':
+    if(is64bit):
+        serial = ctypes.CDLL(os.path.join(path, 'serial_aaarch64.so'))
+    else:
+        serial = ctypes.CDLL(os.path.join(path, 'serial_armv71.so'))
+ 
+
+if os.name != 'nt':
+    # func returned int
+    serial.serialOpen.restype = ctypes.c_int
+    serial.serialClose.restype = ctypes.c_void_p
+    serial.serialDataAvail.restype = ctypes.c_int
+    serial.send.restype = ctypes.c_void_p
+
+    # func returned *
+    serial.readfrom.restype = ctypes.c_char_p
+    serial.readdata.restype = ctypes.c_char_p
+    serial.serialPrint.restype = ctypes.c_void_p
+
+    # func returned arg
+    serial.serialOpen.argtypes = [ctypes.POINTER(ctypes.c_char),ctypes.c_int, ]
+    serial.serialClose.argtypes = [ctypes.c_int]
+    serial.serialDataAvail.argtypes = [ctypes.c_int]
+    serial.send.argtypes = [ctypes.c_int,ctypes.POINTER(ctypes.c_ubyte),ctypes.c_uint,]
+    serial.readfrom.argtypes = [ctypes.c_int,ctypes.c_char,ctypes.c_int,]
+    serial.readdata.argtypes = [ctypes.c_int,ctypes.c_char,ctypes.c_int,ctypes.c_int,]
+    serial.serialPrint.argtypes = [ctypes.c_int,ctypes.POINTER(ctypes.c_char) ]
+
+class MetaSingleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class SerialPi(metaclass=MetaSingleton):
+    _port = None
+    _devicename = None
+    _baudrate = None
+
+    def __init__(self,
+                 devicename: str = '/dev/ttyAMA0',
+                 baudrate: int = 115200):
+        self._devicename = devicename
+        self._baudrate = baudrate
+        self._InitPort()
+
+    def _InitPort(self):
+        if os.name == 'nt':
+            return
+
+        if self._port is None:
+            self._port = serial.serialOpen(self._devicename.encode('utf-8'), self._baudrate)
+
+    def Close(self):
+        if os.name == 'nt':
+            return
+
+        if self._port is not None:
+            serial.serialClose(self._port)
+            self._port = None
+
+    def Read(self, symbol, time, buffer):
+        if os.name == 'nt':
+            return
+
+        if self._port is not None:
+            return serial.readdata(self._port, symbol.encode('utf-8'), time, buffer).decode('utf-8')
+
+        self._InitPort()
+        return serial.readdata(self._port, symbol.encode('utf-8'), time, buffer).decode('utf-8')
+
+    def ReadByte(self, symbol, time, buffer):
+        if os.name == 'nt':
+            return
+
+        if self._port is not None:
+            return serial.readdata(self._port, symbol.encode('utf-8'), time, buffer)
+
+        self._InitPort()
+        return serial.readdata(self._port, symbol.encode('utf-8'), time, buffer)
+
+    def ReadFrom(self, symbol, time):
+        if os.name == 'nt':
+            return
+
+        if self._port is not None:
+            return serial.readfrom(self._port, symbol.encode('utf-8'), time).decode('utf-8')
+
+        self._InitPort()
+        return serial.readfrom(self._port, symbol.encode('utf-8'), time).decode('utf-8')
+
+    def Write(self, byted, size):
+        if os.name == 'nt':
+            return
+
+        if self._port is not None:
+            buffer = (ctypes.c_ubyte * size)()
+            for i in range(size):
+                buffer[i]=byted[i]
+            serial.send(self._port, buffer, size)
+            return
+
+        self._InitPort()
+        self.Write(byted, size)
+        return
+
+    def Print(self, byted, size):
+        if os.name == 'nt':
+            return
+
+        if self._port is not None:
+            serial.serialPrint(self._port, byted, size)
+            return
+
+        self._InitPort()
+        self.Print(byted, size)
+        return
+
+    def Avail(self):
+        if os.name == 'nt':
+            return
+
+        if self._port is not None:
+            return serial.serialDataAvail(self._port)
+
+        self._InitPort()
+        return serial.serialDataAvail(self._port)
+
+    @staticmethod
+    def CalculateCrc(arr):
+        st_byt = 0
+        crc = 0
+
+        while st_byt < len(arr):
+            dat = arr[st_byt]
+            for i in range(8):
+                fb = crc ^ dat
+                fb &= 1
+                crc >>= 1
+                dat >>= 1
+                if fb == 1:
+                    crc ^= 0x8c
+            st_byt += 1
+
+        return crc
