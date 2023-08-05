3da923d15056624f0dd5b20331bca193691231b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:23:56 +0000

    Add package competitions/0.1.1/competitions-0.1.1.tar.gz

---
+++ packages/competitions/competitions-0.1.1.tar.gz/competitions-0.1.1/competitions/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+from .info import CompetitionInfo
+
+
+__version__ = "0.1.1"
+
+MOONLANDING_URL = os.getenv("MOONLANDING_URL")
+COMPETITION_ID = os.getenv("COMPETITION_ID")
+AUTOTRAIN_USERNAME = os.getenv("AUTOTRAIN_USERNAME")
+AUTOTRAIN_TOKEN = os.getenv("AUTOTRAIN_TOKEN")
+AUTOTRAIN_BACKEND_API = os.getenv("AUTOTRAIN_BACKEND_API")
+
+competition_info = CompetitionInfo(competition_id=COMPETITION_ID, autotrain_token=AUTOTRAIN_TOKEN)
---
+++ packages/competitions/competitions-0.1.1.tar.gz/competitions-0.1.1/competitions/text.py
@@ -0,0 +1,17 @@
+SUBMISSION_TEXT = """You can make upto {} submissions per day.
+The test data has been divided into public and private splits.
+Your score on the public split will be shown on the leaderboard.
+Your final score will be based on your private split performance.
+The final rankings will be based on the private split performance.
+"""
+
+SUBMISSION_ERROR = """Submission is not in a proper format.
+Please check evaluation instructions for more details."""
+
+SUBMISSION_SELECTION_TEXT = """You can select upto {} submissions for private leaderboard."""
+
+NO_SUBMISSIONS = """You have not made any submissions yet."""
+
+SUBMISSION_SUCCESS = """Submission successful! You have {} submissions remaining today."""
+
+SUBMISSION_LIMIT_REACHED = """You have reached your submission limit for today. Please try again tomorrow."""
