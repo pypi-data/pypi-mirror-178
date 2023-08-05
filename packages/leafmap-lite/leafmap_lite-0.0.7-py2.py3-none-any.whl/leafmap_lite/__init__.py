fcb625f0c2c0a5a38fedbbc1b1443ebb1975d6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:57 +0000

    Add package leafmap-lite/0.0.7/leafmap_lite-0.0.7-py2.py3-none-any.whl

---
+++ packages/leafmap-lite/leafmap_lite-0.0.7-py2.py3-none-any.whl/leafmap_lite/__init__.py
@@ -0,0 +1,25 @@
+"""Top-level package for leafmap-lite."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = 'giswqs@gmail.com'
+__version__ = '0.0.7'
+
+import sys
+
+
+async def install_pkgs():
+    """Check if the current environment is JupyterLite."""
+
+    import piplite
+
+    await piplite.install('leafmap', deps=False)
+    await piplite.install('shapely', deps=False)
+    await piplite.install('pyproj', deps=False)
+    await piplite.install('geopandas', deps=False)
+
+    
+if "pyodide" in sys.modules:
+    # pyodide doesn't support numpy, so we need to install it separately
+    # and then import it.
+    import pyodide
+    pyodide.runPython("import piplite; piplite.install('leafmap', deps=False)")
