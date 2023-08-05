d1c53df1bdf10a1fd144680706eba6fb88109ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:38 +0000

    Add package CausalPy/0.0.4/CausalPy-0.0.4.tar.gz

---
+++ packages/CausalPy/CausalPy-0.0.4.tar.gz/CausalPy-0.0.4/setup.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+README_FILE = os.path.join(PROJECT_ROOT, "README.md")
+VERSION_FILE = os.path.join(PROJECT_ROOT, "bambi", "version.py")
+REQUIREMENTS_FILE = os.path.join(PROJECT_ROOT, "requirements.txt")
+
+
+def get_long_description():
+    with open(README_FILE, encoding="utf-8") as f:
+        return f.read()
+
+
+# get version
+# sys.path.insert(0, os.path.abspath("../"))
+# from causalpy.version import __version__
+exec(open("causalpy/version.py").read())
+
+with open(REQUIREMENTS_FILE) as f:
+    install_reqs = f.read().splitlines()
+
+setup(
+    name="CausalPy",
+    version=__version__,
+    description="Causal inference for quasi-experiments in Python",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    license="Apache License 2.0",
+    url="https://github.com/pymc-labs/CausalPy",
+    packages=find_packages(exclude=["tests", "test_*"]),
+    package_data={"causalpy": ["data/*.csv"]},
+    python_requires=">=3.8",
+    maintainer="Benjamin T. Vincent",
+    install_requires=install_reqs,
+    # tests_require=test_reqs,
+)
