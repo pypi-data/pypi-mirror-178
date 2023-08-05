248a13cd4e38f88f42df375b6ff1868afdecc49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:34:41 +0000

    Add package Mycol/0.0.2/Mycol-0.0.2.tar.gz

---
+++ packages/Mycol/Mycol-0.0.2.tar.gz/Mycol-0.0.2/mycol/numbers.py
@@ -0,0 +1,14 @@
+import typing
+
+def add_one_to(number: float | int, print_result: typing.Optional[bool] = False) -> None:
+    result = number + 1
+
+    if print_result:
+        print(result)
+
+    return result
+
+def check_equation() -> None:
+    raise NotImplementedError("Method is work in progress")
+
+add_one_to(6)
