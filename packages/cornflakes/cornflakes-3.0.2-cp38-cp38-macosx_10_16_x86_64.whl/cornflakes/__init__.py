6f121a1d9b1a2b71bb538df2054d6bf37aa6981a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:25 +0000

    Add package cornflakes/3.0.2/cornflakes-3.0.2-cp38-cp38-macosx_10_16_x86_64.whl

---
+++ packages/cornflakes/cornflakes-3.0.2-cp38-cp38-macosx_10_16_x86_64.whl/cornflakes/__init__.py
@@ -0,0 +1,48 @@
+"""cornflakes (Top-level package).
+____________________________________
+
+.. currentmodule:: cornflakes
+
+.. autosummary::
+   :toctree: _generate
+
+    click_cli
+    ini_load
+    eval_type
+    eval_datetime
+    eval_csv
+    extract_between
+    apply_match
+    config
+    config_group
+    add_slots
+    generate_group_module
+    yaml_load
+"""  # noqa: RST303 D205
+from _cornflakes import apply_match, eval_csv, eval_datetime, eval_type, extract_between, ini_load
+from cornflakes.decorator import config, config_group, config_field, add_slots, click_cli
+from cornflakes.builder import generate_group_module
+from cornflakes.parser import yaml_load
+from cornflakes.logging import attach_log, setup_logging
+
+__author__ = "Semjon Geist"
+__email__ = "semjon.geist@ionos.com"
+__version__ = "3.0.2"
+
+__all__ = [
+    "click_cli",
+    "ini_load",
+    "eval_type",
+    "eval_datetime",
+    "eval_csv",
+    "extract_between",
+    "apply_match",
+    "config",
+    "config_group",
+    "config_field",
+    "add_slots",
+    "generate_group_module",
+    "yaml_load",
+    "attach_log",
+    "setup_logging",
+]
