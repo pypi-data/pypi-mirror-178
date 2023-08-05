12731d5d2b5101778a0b7548c3472fb34efb9381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:22 +0000

    Add package vrc-movie-maker/0.2.7/vrc-movie-maker-0.2.7.tar.gz

---
+++ packages/vrc-movie-maker/vrc-movie-maker-0.2.7.tar.gz/vrc-movie-maker-0.2.7/movie_maker/browser/twitter_browser.py
@@ -0,0 +1,12 @@
+from movie_maker.browser import BaseBrowser
+import time
+
+
+class TwitterBrowser(BaseBrowser):
+
+    def wait(self) -> None:
+        """
+        Wait for the javascript rendering.
+        :return: None
+        """
+        pass
