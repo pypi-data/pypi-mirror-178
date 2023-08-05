b356e8a79a75d5d116cb13767b9c369c44d75625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:05 +0000

    Add package vern/1.2.8b0/vern-1.2.8b0.tar.gz

---
+++ packages/vern/vern-1.2.8b0.tar.gz/vern-1.2.8b0/vern/parse_oscilloscope_optical.py
@@ -0,0 +1,22 @@
+# profilometer
+import numpy as np
+import pandas as pd
+from .misc import *
+import mat73
+import scipy.io
+__all__ = ['parse_oscilloscope_optical']
+
+def read_files(input_path, txt_path):
+    try:
+        mat = mat73.loadmat(input_path)
+    except:
+        mat = scipy.io.loadmat(input_path)
+    data = {
+        "Wavelength (µm)": mat["ref"][:,0],
+        "Transmittance (dB)": mat["ref"][:,1],
+    }
+    df = pd.DataFrame.from_dict(data)
+    df.to_csv(txt_path, index=False)
+
+def parse_mat(**kwargs):
+    read_files(kwargs["input_path"], kwargs["txt_path"])
