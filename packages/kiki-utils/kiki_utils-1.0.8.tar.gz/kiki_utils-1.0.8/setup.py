ba074ce2e07c94cf232fe3494f661bea7d515fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:04 +0000

    Add package kiki-utils/1.0.8/kiki_utils-1.0.8.tar.gz

---
+++ packages/kiki-utils/kiki_utils-1.0.8.tar.gz/kiki_utils-1.0.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='kiki_utils',
+    classifiers=[
+        'License :: Freely Distributable'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    version='1.0.8',
+    description='Utils functions without cv2 and pillow',
+    author='kiki-kanri',
+    author_email='a470666@gmail.com',
+    keywords=['Utils'],
+    install_requires=[
+        'aiodns',
+        'aiofiles',
+        'aiohttp',
+        'brotli',
+        'loguru',
+        'pycryptodomex',
+        'pyopenssl',
+        'python-magic;platform_system=="Linux"',
+        'python-magic-bin;platform_system=="Windows"',
+        'requests',
+        'sanic',
+        'validator'
+    ],
+    python_requires=">=3.6"
+)
