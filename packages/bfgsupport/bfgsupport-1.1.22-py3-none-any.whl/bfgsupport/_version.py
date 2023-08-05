7f5e19dfdc0cef106597a57be3aa07446d1199f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:22:47 +0000

    Add package bfgsupport/1.1.22/bfgsupport-1.1.22-py3-none-any.whl

---
+++ packages/bfgsupport/bfgsupport-1.1.22-py3-none-any.whl/bfgsupport/_version.py
@@ -0,0 +1,7 @@
+__version__ = '1.1.22'
+
+"""
+Version 1.1.22 24 Nov 2022:
+    board make dealer a property and tidy up json functions
+    overcaller rebid - check for double or redouble before two passes
+"""
