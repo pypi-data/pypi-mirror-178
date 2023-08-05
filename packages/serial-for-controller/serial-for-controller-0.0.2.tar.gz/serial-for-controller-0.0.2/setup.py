e54895481bbc98f2c49f5f39b240e5bdf604bf4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:20 +0000

    Add package serial-for-controller/0.0.2/serial-for-controller-0.0.2.tar.gz

---
+++ packages/serial-for-controller/serial-for-controller-0.0.2.tar.gz/serial-for-controller-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='serial-for-controller',
+    version='0.0.2',
+    packages=['serial_for_controller'],
+    url='https://github.com/Adam-Software/Serial-for-controller',
+    license='MIT',
+    author='vertigra',
+    author_email='a@nesterof.com',
+    description='Python library for data exchange with adam controller',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_data={'serial_for_controller': ['*.so']},
+    include_package_data=True,
+    install_requires=['numpy'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Other',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 3'
+    ]
+)
