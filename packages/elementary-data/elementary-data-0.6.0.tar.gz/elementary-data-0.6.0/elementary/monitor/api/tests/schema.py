ac27994bfe74fa5230832c649e4a1a0a4f57d1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:31:55 +0000

    Add package elementary-data/0.6.0/elementary-data-0.6.0.tar.gz

---
+++ packages/elementary-data/elementary-data-0.6.0.tar.gz/elementary-data-0.6.0/elementary/monitor/api/tests/schema.py
@@ -0,0 +1,67 @@
+import json
+from typing import List, Optional
+from pydantic import BaseModel, validator
+
+from elementary.utils.time import convert_partial_iso_format_to_full_iso_format
+
+
+TestUniqueIdType = str
+ModelUniqueIdType = str
+
+
+class TestMetadataSchema(BaseModel):
+    id: str
+    model_unique_id: Optional[ModelUniqueIdType] = None
+    test_unique_id: TestUniqueIdType
+    detected_at: str
+    database_name: str = None
+    schema_name: str
+    table_name: Optional[str] = None
+    column_name: Optional[str]
+    test_type: str
+    test_sub_type: str
+    test_results_description: Optional[str]
+    owners: Optional[str]
+    tags: Optional[str]
+    meta: Optional[dict]
+    test_results_query: Optional[str] = None
+    other: Optional[str]
+    test_name: str
+    test_params: Optional[str]
+    severity: str
+    status: str
+    test_created_at: Optional[str] = None
+    days_diff: float
+
+    @validator("detected_at", pre=True)
+    def format_detected_at(cls, detected_at):
+        return convert_partial_iso_format_to_full_iso_format(detected_at)
+
+    @validator("meta", pre=True)
+    def load_meta(cls, meta):
+        return json.loads(meta) if meta else {}
+
+
+class InvocationSchema(BaseModel):
+    affected_rows: Optional[int]
+    time_utc: str
+    id: str
+    status: str
+
+    @validator("time_utc", pre=True)
+    def format_time_utc(cls, time_utc):
+        return convert_partial_iso_format_to_full_iso_format(time_utc)
+
+
+class TotalsInvocationsSchema(BaseModel):
+    errors: Optional[int] = 0
+    warnings: Optional[int] = 0
+    passed: Optional[int] = 0
+    resolved: Optional[int] = 0
+
+
+class InvocationsSchema(BaseModel):
+    fail_rate: float
+    totals: TotalsInvocationsSchema
+    invocations: List[InvocationSchema]
+    description: str
