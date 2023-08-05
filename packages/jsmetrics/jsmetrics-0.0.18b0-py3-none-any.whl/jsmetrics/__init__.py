b26854684bb5be8dec9574911764769027109721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:57 +0000

    Add package jsmetrics/0.0.18b0/jsmetrics-0.0.18b0-py3-none-any.whl

---
+++ packages/jsmetrics/jsmetrics-0.0.18b0-py3-none-any.whl/jsmetrics/__init__.py
@@ -0,0 +1,15 @@
+"""Top-level package for jsmetrics."""
+
+__author__ = """Tom Keel"""
+__email__ = "thomas.keel.18@ucl.ac.uk"
+__version__ = "0.0.18-beta"
+
+from . import (
+    data_utils,
+    jetstream_metrics,
+    jetstream_algorithms,
+    jetstream_metrics_components,
+    jetstream_algorithms_components,
+    windspeed_utils,
+    spatial_utils,
+)
