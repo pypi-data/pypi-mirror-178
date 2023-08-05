63008191f7e099043807e6a1caf63d1e49c9596d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:55 +0000

    Add package dsx/0.9.10a2/dsx-0.9.10a2.tar.gz

---
+++ packages/dsx/dsx-0.9.10a2.tar.gz/dsx-0.9.10a2/dsx/logging.py
@@ -0,0 +1,36 @@
+# Setup Logger
+import logging
+
+
+def set_logging(logpath:str = "test.log", level=logging.DEBUG, debug:bool=True):
+    # create logger
+    logger = logging.getLogger(__name__)
+    # set log level for all handlers to debug
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(level)
+
+    # create file handler and set level to debug
+    fileHandler = logging.FileHandler(logpath)
+    fileHandler.setLevel(level)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to handlers
+    consoleHandler.setFormatter(formatter)
+    fileHandler.setFormatter(formatter)
+
+    # add handlers to logger
+    logger.addHandler(consoleHandler)
+    logger.addHandler(fileHandler)
+
+    # set log level for development
+    if debug:
+        logger.setLevel(logging.DEBUG)
+
+
+if __name__=='__main__':
+    set_logging()
