b0db9fa44a2dd4ed8a082159667e61f59cbf2eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:29 +0000

    Add package webssh-sh/22.11.4/webssh-sh-22.11.4.tar.gz

---
+++ packages/webssh-sh/webssh-sh-22.11.4.tar.gz/webssh-sh-22.11.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='webssh-sh',
+    version='22.11.4',
+    description='Shell Helpers about WebSSH',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/isontheline/webssh.sh',
+    author='Arnaud MENGUS',
+    license='MIT',
+    packages=['wsh'],
+    install_requires=[],
+    scripts=[],
+    entry_points={
+        'console_scripts': ['wshcopy=wsh.wshcopy:cli'],
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
---
+++ packages/webssh-sh/webssh-sh-22.11.4.tar.gz/webssh-sh-22.11.4/wsh/__init__.py
@@ -0,0 +1,10 @@
+"""
+wsh
+
+WebSSH Tools Library
+"""
+
+__author__ = 'Arnaud MENGUS'
+__credits__ = 'Arnaud MENGUS'
+__maintainer__ = 'Arnaud MENGUS'
+__license__ = 'MIT'
