ffde425912f59dad5b730ec697034001271b418a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:34:28 +0000

    Add package django-shopify-app/1.0.4/django_shopify_app-1.0.4.tar.gz

---
+++ packages/django-shopify-app/django_shopify_app-1.0.4.tar.gz/django_shopify_app-1.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='django_shopify_app',
+    version='1.0.4',
+    author='Moship',
+    author_email='hello@moship.io',
+    packages=find_packages(),
+    scripts=[],
+    url='http://pypi.python.org/pypi/django_shopify_app/',
+    license="MIT",
+    description='A django app with all the tools required to make a Shopify app',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "Django >= 4.0.0",
+        "pytest",
+        "shopifyapi",
+        "google-cloud-pubsub",
+        "google-auth",
+    ],
+)
