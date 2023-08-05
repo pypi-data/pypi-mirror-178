1410b4879b066b8cebe8b0d18e68abdad04c1e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:57 +0000

    Add package edc-model/0.3.25/edc-model-0.3.25.tar.gz

---
+++ packages/edc-model/edc-model-0.3.25.tar.gz/edc-model-0.3.25/runtests.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import logging
+import sys
+from os.path import abspath, dirname
+
+import django
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
+
+app_name = "edc_model"
+base_dir = dirname(abspath(__file__))
+
+DEFAULT_SETTINGS = DefaultTestSettings(
+    APP_NAME="edc_model",
+    BASE_DIR=base_dir,
+    ALLOWED_HOSTS=["localhost"],
+    ROOT_URLCONF=f"{app_name}.tests.urls",
+    STATIC_URL="/static/",
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "multisite",
+        "edc_dashboard.apps.AppConfig",
+        "edc_device.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
+        "edc_model.apps.AppConfig",
+    ],
+    add_dashboard_middleware=True,
+).settings
+
+
+def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    django.setup()
+    tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
+    failfast = True if [t for t in sys.argv if t == "--failfast"] else False
+    failures = DiscoverRunner(failfast=failfast, tags=tags).run_tests([f"{app_name}.tests"])
+    sys.exit(failures)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
