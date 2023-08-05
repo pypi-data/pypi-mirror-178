ac5de9f5f06b48ee8f461f36d4f3c8f2eafb52c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:31:02 +0000

    Add package daemon-application/0.5.8/daemon_application-0.5.8-py3-none-any.whl

---
+++ packages/daemon-application/daemon_application-0.5.8-py3-none-any.whl/daemon_application/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
+from zenutils.sixutils import *
+
+from .base import *
+from .app import DaemonApplication
+from .app import SimpleXmlRpcServer
+from .app import SimpleXmlRpcServerEngine
+from .app import SimpleRpcApplication
