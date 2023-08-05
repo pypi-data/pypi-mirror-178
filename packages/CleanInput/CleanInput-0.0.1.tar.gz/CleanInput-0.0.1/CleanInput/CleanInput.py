8a62a8452aafc338b82d706e41d3fd166270a9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:22:55 +0000

    Add package CleanInput/0.0.1/CleanInput-0.0.1.tar.gz

---
+++ packages/CleanInput/CleanInput-0.0.1.tar.gz/CleanInput-0.0.1/CleanInput/CleanInput.py
@@ -0,0 +1,2 @@
+def CleanInput(msg):
+    return input(msg).strip("'").strip("\"").strip(" ")
---
+++ packages/CleanInput/CleanInput-0.0.1.tar.gz/CleanInput-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CleanInput",
+    version="0.0.1",
+    keywords=["pip", "CleanInput"],
+    description="Auto del input noise.",
+    author="KurehaSHO",
+    packages=find_packages(),
+    platforms="any"
+)
