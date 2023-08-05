2464b9c17e683cd25c96d8b98e6f72469a7ba1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:17 +0000

    Add package taxonomy-connector/1.28.2/taxonomy_connector-1.28.2-py3-none-any.whl

---
+++ packages/taxonomy-connector/taxonomy_connector-1.28.2-py3-none-any.whl/taxonomy/validators/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Expose all the validators.
+"""
+
+from taxonomy.validators.course_metadata import CourseMetadataProviderValidator
+from taxonomy.validators.program_metadata import ProgramMetadataProviderValidator
