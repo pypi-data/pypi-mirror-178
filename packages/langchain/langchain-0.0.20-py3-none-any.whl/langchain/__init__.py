e1360d8822e9dab827a843cb015cd3e28b368539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:37 +0000

    Add package langchain/0.0.20/langchain-0.0.20-py3-none-any.whl

---
+++ packages/langchain/langchain-0.0.20-py3-none-any.whl/langchain/__init__.py
@@ -0,0 +1,49 @@
+"""Main entrypoint into package."""
+
+from pathlib import Path
+
+with open(Path(__file__).absolute().parents[0] / "VERSION") as _f:
+    __version__ = _f.read().strip()
+
+from langchain.agents import MRKLChain, ReActChain, SelfAskWithSearchChain
+from langchain.chains import (
+    LLMChain,
+    LLMMathChain,
+    PythonChain,
+    SerpAPIChain,
+    SQLDatabaseChain,
+    VectorDBQA,
+)
+from langchain.docstore import Wikipedia
+from langchain.llms import Cohere, HuggingFaceHub, OpenAI
+from langchain.prompts import (
+    BasePromptTemplate,
+    FewShotPromptTemplate,
+    Prompt,
+    PromptTemplate,
+)
+from langchain.sql_database import SQLDatabase
+from langchain.vectorstores import FAISS, ElasticVectorSearch
+
+__all__ = [
+    "LLMChain",
+    "LLMMathChain",
+    "PythonChain",
+    "SelfAskWithSearchChain",
+    "SerpAPIChain",
+    "Cohere",
+    "OpenAI",
+    "BasePromptTemplate",
+    "Prompt",
+    "FewShotPromptTemplate",
+    "PromptTemplate",
+    "ReActChain",
+    "Wikipedia",
+    "HuggingFaceHub",
+    "SQLDatabase",
+    "SQLDatabaseChain",
+    "FAISS",
+    "MRKLChain",
+    "VectorDBQA",
+    "ElasticVectorSearch",
+]
