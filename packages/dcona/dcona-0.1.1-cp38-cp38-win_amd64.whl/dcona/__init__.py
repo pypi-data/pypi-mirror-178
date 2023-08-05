8c05b8e9f9bbe8f2d964057809bbdad291a30b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:20:06 +0000

    Add package dcona/0.1.1/dcona-0.1.1-cp38-cp38-win_amd64.whl

---
+++ packages/dcona/dcona-0.1.1-cp38-cp38-win_amd64.whl/dcona/__init__.py
@@ -0,0 +1,30 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_1_2():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'dcona.libs'))
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    if sys.version_info[:2] >= (3, 8) and not os.path.exists(os.path.join(sys.base_prefix, 'conda-meta')) or sys.version_info[:2] >= (3, 10):
+        if not is_pyinstaller or os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        from ctypes import WinDLL
+        load_order_filepath = os.path.join(libs_dir, '.load-order-dcona-0.1.1')
+        if not is_pyinstaller or os.path.isfile(load_order_filepath):
+            with open(os.path.join(libs_dir, '.load-order-dcona-0.1.1')) as file:
+                load_order = file.read().split()
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if not is_pyinstaller or os.path.isfile(lib_path):
+                    WinDLL(lib_path)
+
+
+_delvewheel_init_patch_1_1_2()
+del _delvewheel_init_patch_1_1_2
+# end delvewheel patch
+
+from .lib.ztest import ztest
+from .lib.zscore import zscore
+from .lib.hypergeom import hypergeom
