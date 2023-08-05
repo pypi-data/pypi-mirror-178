7bf10cc5947b66f938c262e979a0ebd779f1b4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:34:38 +0000

    Add package pytigon/0.250/pytigon-0.250-py3-none-any.whl

---
+++ packages/pytigon/pytigon-0.250-py3-none-any.whl/pytigon/prj/scheditor/sched/__init__.py
@@ -0,0 +1,8 @@
+from django.utils.translation import gettext_lazy as _
+
+ModuleTitle = _("Editor")
+Title = _("Editor")
+Perms = False
+Index = ""
+Urls = (("save/?schtml=pscript", _("save"), None, """fa://paperclip.png"""),)
+UserParam = {}
