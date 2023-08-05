072e167594d61a680e9730c1dcc33542ad2523a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:58 +0000

    Add package converter-package/1.0/converter_package-1.0.tar.gz

---
+++ packages/converter-package/converter_package-1.0.tar.gz/converter_package-1.0/converter_package/ID.py
@@ -0,0 +1,13 @@
+import time
+
+
+def generate_k3s_pod_name(appinstanceinfo, component_name, minicloud_id):
+    running_component_id = time.time() * 1000
+    running_component_id_without_dot = stripped = str(running_component_id).split('.', 1)[0]
+    k3s_pod_name = appinstanceinfo + "-" + component_name + "-" + str(running_component_id_without_dot) + "-" + minicloud_id
+    return k3s_pod_name
+
+
+def genenerate_k3s_namespace(application_name, application_version, application_id):
+    k3s_namespace = "accordion-"+application_name + "-" + application_version + "-" + application_id
+    return k3s_namespace
