627f600c2c212f6ea3306cffee95771cffa2bb93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:07 +0000

    Add package dn3/1.0.2/dn3-1.0.2-py3-none-any.whl

---
+++ packages/dn3/dn3-1.0.2-py3-none-any.whl/dn3/pwn/deathnot3.py
@@ -0,0 +1,124 @@
+from pwn import ELF
+from dn3.misc.encoding import *
+from dn3.misc.utils import *
+from dn3 import l1ght
+from dn3.l1ght.context import context
+from logging import getLogger
+import os
+
+logger = getLogger(__name__)
+
+deathnote = None
+
+class DeathNot3():
+
+	def __init__(self,io=context.io,libc=context.libc):
+
+		global deathnote
+
+		if context.binary == None:
+			logger.warn("(context.binary) has not been set")
+
+		if not isinstance(io, (l1ght.proc, l1ght.sock, l1ght.debug)):
+			logger.error("Pipe provided isn't a l1ght entity")
+
+		if libc and not isinstance(libc, ELF):
+			logger.error("Provided libc isn't a pwnlib ELF")
+
+		self.binary = context.binary
+		self.io = io
+		self.libc = libc
+		deathnote = self
+
+		logger.info("deathnot3 has been initialized!")
+
+
+def re(n):
+	if dn3_exists(deathnote):
+		return deathnote.io.recv(n)
+
+def reu(s):
+	if dn3_exists(deathnote):
+		return deathnote.io.recvuntil(s)
+
+def rl():
+	if dn3_exists(deathnote):
+		return deathnote.io.recvline()
+
+def s(s):
+	if dn3_exists(deathnote):
+		return deathnote.io.send(s)
+
+def sl(s):
+	if dn3_exists(deathnote):
+		return deathnote.io.sendline(s)
+
+def sa(d,s):
+	if dn3_exists(deathnote):
+		return deathnote.io.sendafter(d,s)
+
+def sla(d,s):
+	if dn3_exists(deathnote):
+		return deathnote.io.sendlineafter(d,s)
+
+def interactive():
+	global deathnote
+	if dn3_exists(deathnote):
+		deathnote.io.interactive()
+		deathnote = None
+		return
+
+def intleak(length=14):
+	return int(x2str(deathnote.io.recv(length)),16)
+
+def byteleak(length=6):
+	if length > 8:
+		logger.error("Maximum leak size can only be 8 bytes!")
+	return upk(deathnote.io.recv(length))
+
+
+def libcleak(sym=0,length=6):
+
+	if dn3_libc_exists(deathnote):
+
+		leak = byteleak(length)
+		if isinstance(sym, str):
+			leak = leak - deathnote.libc.symbols[sym]
+		elif isinstance(sym, int):
+			leak = leak - sym
+		else:
+			logger.error("Invalid symbol/offset provided")
+			return
+
+		logger.info("Libc -> %s" % hex(leak))
+		return leak
+
+def intlibcleak(sym=0,length=14):
+
+	if dn3_libc_exists(deathnote):
+
+		leak = intleak(length)
+		if isinstance(sym, str):
+			leak = leak - deathnote.libc.symbols[sym]
+		elif isinstance(sym, int):
+			leak = leak - sym
+		else:
+			logger.error("Invalid symbol/offset provided")
+			return
+
+		logger.info("Libc -> %s" % hex(leak))
+		return leak
+
+def unhexdump(name,delim):
+
+	if dn3_exists(deathnote):
+
+		data = reu(delim)
+		f = open("/tmp/raw_hex","w+")
+		f.write(data)
+		f.close()
+		os.system(f"xxd -r /tmp/raw_hex > {name}", cwd=os.gewcwd())
+		exe = ELF(name)
+		context.binary = exe
+		deathnote.binary = exe
+		return exe
---
+++ packages/dn3/dn3-1.0.2-py3-none-any.whl/dn3/tools/cli.py
@@ -0,0 +1,43 @@
+from dn3.tools.linkerpatcher import *
+from dn3.tools.templategen import *
+from dn3.tools.config import *
+from dn3.pwn.cyclic import *
+from logging import getLogger
+from sys import argv
+import argparse
+
+logger = getLogger(__name__)
+
+
+class CLIHandler():
+
+	def argparse_handler(self):
+		parser = argparse.ArgumentParser(description='DeathNot3 CLI',formatter_class=argparse.RawTextHelpFormatter)
+		parser.add_argument("mode", help="""template/linker/interactive
+		template                ---> Automatic template generation
+		linker                  ---> Patch challenge binary with linker
+		interactive (or) i      ---> Open python interactive shell with dn3 imported
+		\n""")
+		parser.add_argument("binary", help="Path to challenge binary")
+		parser.add_argument("-l","--libc", help="Path to libc binary",metavar="")
+		parser.add_argument("-r","--remote", help="<remote_ip>:<port>",metavar="")
+		parser.add_argument("-V","--verbose", action="store_true", help="Detailed logging")
+		return parser.parse_args()
+
+
+	def __init__(self):
+
+		if argv and len(argv) > 1:
+			if argv[1] == "config":
+				return config.write(argv)
+			
+		args = self.argparse_handler()
+		
+		if args.mode == "template":
+			TemplateGenerator(args.binary,args.libc,args.remote)
+
+		elif args.mode == "linker":
+			LinkerPatcher(args.binary,args.libc)
+
+		elif args.mode == "cyclic":
+			CyclicHandler(args.binary)
