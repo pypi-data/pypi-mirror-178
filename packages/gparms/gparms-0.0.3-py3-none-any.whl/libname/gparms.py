7d2c9221101fc85c5bb82b10bee5150a0d77f209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:47 +0000

    Add package gparms/0.0.3/gparms-0.0.3-py3-none-any.whl

---
+++ packages/gparms/gparms-0.0.3-py3-none-any.whl/libname/gparms.py
@@ -0,0 +1,11 @@
+import sys
+from urllib.parse import urlparse
+
+def main():
+    for line in sys.stdin:
+        urlString = line.strip('\n')
+        url = urlparse(urlString)
+        if(url.query != ""):
+            print(line.strip('\n'))
+
+main()
