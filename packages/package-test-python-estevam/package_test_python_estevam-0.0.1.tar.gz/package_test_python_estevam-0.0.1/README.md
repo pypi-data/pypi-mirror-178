c3de6e18aef53a4c4f245e56a8cd975ad143a42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:25 +0000

    Add package package-test-python-estevam/0.0.1/package_test_python_estevam-0.0.1.tar.gz

---
+++ packages/package-test-python-estevam/package_test_python_estevam-0.0.1.tar.gz/package_test_python_estevam-0.0.1/README.md
@@ -0,0 +1,27 @@
+# package_name
+
+Description. 
+The package package_test_python_estevam is used to:
+	- 
+	-
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+
+```bash
+pip install package_test_python_estevam
+```
+
+## Usage
+
+```python
+from package_name import file1_name
+file1_name.my_function()
+```
+
+## Author
+Estevam
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/package-test-python-estevam/package_test_python_estevam-0.0.1.tar.gz/package_test_python_estevam-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_test_python_estevam",
+    version="0.0.1",
+    author="Estevam",
+    description="My short description",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+    setup_requires=['wheel'],
+)
