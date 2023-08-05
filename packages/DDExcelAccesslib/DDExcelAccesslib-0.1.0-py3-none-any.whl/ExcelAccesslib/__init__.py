253d5bd126ea35e99f252c338c90f27c056395ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:19 +0000

    Add package DDExcelAccesslib/0.1.0/DDExcelAccesslib-0.1.0-py3-none-any.whl

---
+++ packages/DDExcelAccesslib/DDExcelAccesslib-0.1.0-py3-none-any.whl/ExcelAccesslib/__init__.py
@@ -0,0 +1,18 @@
+from .excel_matrix_manage import get_linda_matrix, write_linda_matrix
+import logging
+
+logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+
+__author__ = "Jordi Arellano <jarellano@ifae.esm>"
+__status__ = "development"
+# The following module attributes are no longer updated.
+__version__ = "0.0.0.1"
+__date__ = "25 Oct 2022"
+
+__all__ = {
+    get_linda_matrix,
+    write_linda_matrix
+    }
