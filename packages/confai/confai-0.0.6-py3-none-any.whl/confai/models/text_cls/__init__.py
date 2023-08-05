eaf139f71d094df617499620cb11391bdde177ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:54 +0000

    Add package confai/0.0.6/confai-0.0.6-py3-none-any.whl

---
+++ packages/confai/confai-0.0.6-py3-none-any.whl/confai/models/text_cls/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+"""
+@author: chenhao
+@file: __init__.py.py
+@time: 2022/11/3 11:18
+"""
+from confai.models.text_cls.cls_token_cls import CLSTokenClsModel
+from confai.models.text_cls.mlm_cls import MLMCLSModel
