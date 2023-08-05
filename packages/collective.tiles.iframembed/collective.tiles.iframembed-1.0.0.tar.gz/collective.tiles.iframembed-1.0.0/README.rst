6c88dffa327e8baea2b69ae84df841e2a5974047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:48 +0000

    Add package collective.tiles.iframembed/1.0.0/collective.tiles.iframembed-1.0.0.tar.gz

---
+++ packages/collective.tiles.iframembed/collective.tiles.iframembed-1.0.0.tar.gz/collective.tiles.iframembed-1.0.0/README.rst
@@ -0,0 +1,50 @@
+.. This README is meant for consumption by humans and pypi. Pypi can render rst files so please do not use Sphinx features.
+   If you want to learn more about writing documentation, please check out: http://docs.plone.org/about/documentation_styleguide_addons.html
+   This text does not appear on pypi or github. It is a comment.
+
+==============================================================================
+collective.tiles.iframembed
+==============================================================================
+
+A tile which embeds external url inside an iframe
+
+Features
+--------
+
+- domains validation
+
+
+Installation
+------------
+
+Install collective.tiles.iframembed by adding it to your buildout::
+
+    [buildout]
+
+    ...
+
+    eggs =
+        collective.tiles.iframembed
+
+
+and then running ``bin/buildout``
+
+Compatibility
+-------------
+
+Plone >= 5.2 and Python3 from version 1.0.0.
+
+For previous Plone versions, use < 1.0.0
+
+
+Contribute
+----------
+
+- Issue Tracker: https://github.com/collective/collective.tiles.iframembed/issues
+- Source Code: https://github.com/collective/collective.tiles.iframembed
+
+
+License
+-------
+
+The project is licensed under the GPLv2.
---
+++ packages/collective.tiles.iframembed/collective.tiles.iframembed-1.0.0.tar.gz/collective.tiles.iframembed-1.0.0/src/collective/tiles/iframembed/tile.py
@@ -0,0 +1,35 @@
+#-*- encoding:utf-8 *-
+from plone import tiles
+from plone import api
+from plone.app.uuid.utils import uuidToObject
+from collective.tiles.iframembed.interfaces import IIFrameEmbedTilesSettings
+from urllib.parse import urlparse
+
+class FrameEmbedTile(tiles.PersistentTile):
+
+    def iframe_validation(self):
+        url_to_embed = self.data.get('url_to_embed')
+        if not url_to_embed:
+            return False
+
+        valid_domains = api.portal.get_registry_record(
+            'available_domains',
+            interface=IIFrameEmbedTilesSettings)
+
+        for domain in valid_domains:
+            if url_to_embed.find(domain) != -1:
+                return True
+
+        api.portal.show_message("L'url indicato non e' valido per i domini ammessi", self.request, 'error')
+        return False
+
+    def eval_query_string(self):
+        o = urlparse(self.request.HTTP_REFERER)
+        query_string = o.query
+        if query_string.startswith('_authenticator'):
+            return (self.data.get('url_to_embed')).rstrip('/')
+
+        query_string = query_string.replace('?', "/", 1)
+        query_string = query_string.replace('&', "?", 1)
+        result = '%s/%s' % (self.data.get('url_to_embed'), query_string)
+        return result.rstrip('/')
