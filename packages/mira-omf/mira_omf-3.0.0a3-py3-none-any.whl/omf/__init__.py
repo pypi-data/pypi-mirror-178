eb56bf2348de27aec0f714322fad6bd7862c7f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:46 +0000

    Add package mira-omf/3.0.0a3/mira_omf-3.0.0a3-py3-none-any.whl

---
+++ packages/mira-omf/mira_omf-3.0.0a3-py3-none-any.whl/omf/__init__.py
@@ -0,0 +1,32 @@
+"""omf: API library for Open Mining Format file interchange format"""
+
+from .base import Project
+from .data import (
+    ColorArray,
+    ColorData,
+    DateTimeArray,
+    DateTimeColormap,
+    DateTimeData,
+    Legend,
+    MappedData,
+    ScalarArray,
+    ScalarColormap,
+    ScalarData,
+    StringArray,
+    StringData,
+    Vector2Array,
+    Vector2Data,
+    Vector3Array,
+    Vector3Data,
+)
+from .fileio import GeoH5Writer, OMFReader, OMFWriter
+from .lineset import LineSetElement, LineSetGeometry
+from .pointset import PointSetElement, PointSetGeometry
+from .surface import SurfaceElement, SurfaceGeometry, SurfaceGridGeometry
+from .texture import ImageTexture
+from .volume import VolumeElement, VolumeGridGeometry
+
+__version__ = "3.0.0-alpha.3"
+__author__ = "Global Mining Standards and Guidelines Group"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2017 Global Mining Standards and Guidelines Group"
