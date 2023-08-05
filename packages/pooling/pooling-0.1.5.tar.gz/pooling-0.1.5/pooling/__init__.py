424b91667904704d905498978015e6bb5a953ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:30:53 +0000

    Add package pooling/0.1.5/pooling-0.1.5.tar.gz

---
+++ packages/pooling/pooling-0.1.5.tar.gz/pooling-0.1.5/pooling/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
+
+from .base import PoolBase
+from .base import Pool
