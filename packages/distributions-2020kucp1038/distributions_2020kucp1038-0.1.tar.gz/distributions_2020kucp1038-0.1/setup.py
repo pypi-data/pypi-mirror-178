6c2edb9af6df44d4729c8597ebb6b53a040f1700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:49 +0000

    Add package distributions-2020kucp1038/0.1/distributions_2020kucp1038-0.1.tar.gz

---
+++ packages/distributions-2020kucp1038/distributions_2020kucp1038-0.1.tar.gz/distributions_2020kucp1038-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_2020kucp1038',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Priyansh Jhalora',
+      author_email='2020kucp1038@iiitkota.ac.in',
+      zip_safe=False)
