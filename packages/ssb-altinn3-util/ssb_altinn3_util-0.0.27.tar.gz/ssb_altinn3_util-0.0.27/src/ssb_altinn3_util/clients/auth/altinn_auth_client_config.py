df837627d7102e45908193974ea9765cdda60b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:27 +0000

    Add package ssb-altinn3-util/0.0.27/ssb_altinn3_util-0.0.27.tar.gz

---
+++ packages/ssb-altinn3-util/ssb_altinn3_util-0.0.27.tar.gz/ssb_altinn3_util-0.0.27/src/ssb_altinn3_util/clients/auth/altinn_auth_client_config.py
@@ -0,0 +1,22 @@
+class AltinnAuthClientConfig:
+    """
+    Configuration class for AltinnAuthClient
+    """
+
+    def __init__(
+        self,
+        maskinport_client_id: str,
+        keycloak_secret_path: str,
+        platform_environment: str,
+        altinn_base_url: str,
+    ):
+        """
+        :param maskinport_client_id: A valid client id in Maskinporten
+        :param keycloak_secret_path: The path to the keycloak secret (in GCP secret manager)
+        :param platform_environment: Runtime environment for ssb subsystems (staging or prod)
+        :param altinn_base_url: Base url for the Altinn Authentication API
+        """
+        self.maskinport_client_id = maskinport_client_id
+        self.keycloak_secret_path = keycloak_secret_path
+        self.platform_environment = platform_environment
+        self.altinn_base_url = altinn_base_url
