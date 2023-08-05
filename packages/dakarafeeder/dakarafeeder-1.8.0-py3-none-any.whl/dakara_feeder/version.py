dd4c3b7e5a8e85edc2e561492a2f6b2134945fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:34 +0000

    Add package dakarafeeder/1.8.0/dakarafeeder-1.8.0-py3-none-any.whl

---
+++ packages/dakarafeeder/dakarafeeder-1.8.0-py3-none-any.whl/dakara_feeder/version.py
@@ -0,0 +1,21 @@
+"""Version of the feeder."""
+
+import logging
+
+from pkg_resources import parse_version
+
+__version__ = "1.8.0"
+__date__ = "2022-11-23"
+
+logger = logging.getLogger(__name__)
+
+
+def check_version():
+    """Display version number and check if on release."""
+    # log player versio
+    logger.info("Dakara feeder %s (%s)", __version__, __date__)
+
+    # check version is a release
+    version = parse_version(__version__)
+    if version.is_prerelease:
+        logger.warning("You are running a dev version, use it at your own risks!")
