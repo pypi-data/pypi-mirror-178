12523f5aeae146b32c1a57f6fa0899edbcc335b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:20:18 +0000

    Add package flowcontainer/4.3/flowcontainer-4.3.tar.gz

---
+++ packages/flowcontainer/flowcontainer-4.3.tar.gz/flowcontainer-4.3/setup.py
@@ -0,0 +1,30 @@
+#coding:utf8
+__author__ = 'dk'
+import setuptools
+long_desp = \
+'''
+A python lib to parse traffic flow information from pcaps.\n
+Homepage : https://github.com/jmhIcoding/flowcontainer.\n
+Fix bugs:\n
+\t set the default filter string to be `tcp or udp or gre`.\n
+\t update help information for errors. \n
+\t supports ipv6 parse. \n
+\t fix sperator bugs, replace sperator from '+' to '`'  \n
+'''
+
+setuptools.setup(
+    name="flowcontainer",
+    version="4.3",
+    author="Minghao Jiang",
+    author_email="jiangminghao@iie.ac.cn",
+    description="A python lib to parse traffic flow information from pcaps",
+    url="https://github.com/jmhIcoding/flowcontainer",
+    long_description=long_desp,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
