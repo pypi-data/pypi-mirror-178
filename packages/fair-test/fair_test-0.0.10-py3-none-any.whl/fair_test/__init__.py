a8f246a0f8fc363b154e06370e96812368877b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:32:04 +0000

    Add package fair-test/0.0.10/fair_test-0.0.10-py3-none-any.whl

---
+++ packages/fair-test/fair_test-0.0.10-py3-none-any.whl/fair_test/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.10"
+
+from .fair_test_api import FairTestAPI
+from .fair_test_evaluation import FairTestEvaluation
+from .fair_test import FairTest, MetricInput
+from .config import settings
