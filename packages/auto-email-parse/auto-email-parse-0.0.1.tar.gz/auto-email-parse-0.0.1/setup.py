8c572eba15cea890480edf11c05d2f3fe8b3c1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:25 +0000

    Add package auto-email-parse/0.0.1/auto-email-parse-0.0.1.tar.gz

---
+++ packages/auto-email-parse/auto-email-parse-0.0.1.tar.gz/auto-email-parse-0.0.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/11/24 下午2:36
+# @Author  : Xsu
+# @File    : setup.py
+
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="auto-email-parse",
+  version="0.0.1",
+  author="Xsu",
+  author_email="bbworktime@gmail.com",
+  description="A small example package for parse email",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/Web3-Base-Installation/auto-email-parse.git",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
