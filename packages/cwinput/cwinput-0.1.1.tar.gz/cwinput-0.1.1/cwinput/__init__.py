79f845e5db039873ce6edef4185c167f95171618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:05 +0000

    Add package cwinput/0.1.1/cwinput-0.1.1.tar.gz

---
+++ packages/cwinput/cwinput-0.1.1.tar.gz/cwinput-0.1.1/cwinput/__init__.py
@@ -0,0 +1,8 @@
+import platform
+
+__version__ = "0.1.1"
+
+if platform.system() != "Windows":
+    raise OSError
+
+from . import __main__
---
+++ packages/cwinput/cwinput-0.1.1.tar.gz/cwinput-0.1.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="cwinput",
+    version="0.1.1",
+    author="Zhilkaidarov Damir",
+    description="Listen to raw input from console",
+    packages=[
+        "cwinput",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha"
+    ]
+)
