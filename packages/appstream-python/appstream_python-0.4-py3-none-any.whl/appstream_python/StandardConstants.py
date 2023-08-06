52f865cfc5eb90cac75f9d02e39ddf88b32b71ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:13 +0000

    Add package appstream-python/0.4/appstream_python-0.4-py3-none-any.whl

---
+++ packages/appstream-python/appstream_python-0.4-py3-none-any.whl/appstream_python/StandardConstants.py
@@ -0,0 +1,76 @@
+"This module contains constansts for Appstream"
+
+URL_TYPES = [
+    "homepage",
+    "bugtracker",
+    "faq",
+    "help",
+    "donation",
+    "translate",
+    "contact",
+    "vcs-browser",
+    "contribute"
+]
+"All URL types"
+
+OARS_ATTRIBUTE_TYPES = [
+    "violence-cartoon",
+    "violence-fantasy",
+    "violence-realistic",
+    "violence-bloodshed",
+    "violence-sexual",
+    "violence-desecration",
+    "violence-slavery",
+    "drugs-alcohol",
+    "drugs-narcotics",
+    "drugs_tobacco",
+    "sex_nudity",
+    "sex-themes",
+    "language-profanity",
+    "language-humor",
+    "language-discrimination",
+    "money-advertising",
+    "money-gambling",
+    "money-purchasing",
+    "social-chat",
+    "social-audio",
+    "social-contacts",
+    "social-info",
+    "social-location"
+]
+"All aviable OARS attributes"
+
+OARS_VALUE_TYPES = [
+    "none",
+    "mild",
+    "moderate",
+    "intense"
+]
+"All ORAS value types"
+
+PROVIDES_TYPES = [
+    "mediatype",
+    "library",
+    "binary",
+    "font",
+    "modalias",
+    "firmware",
+    "python2",
+    "python3",
+    "dbus",
+    "id"
+]
+"The list with all types for provides"
+
+CONTROL_TYPES = [
+    "pointing",
+    "keyboard",
+    "console",
+    "tablet",
+    "touch",
+    "gamepad",
+    "tv-remote",
+    "voice",
+    "vision"
+]
+"The list with all possible values for control"
