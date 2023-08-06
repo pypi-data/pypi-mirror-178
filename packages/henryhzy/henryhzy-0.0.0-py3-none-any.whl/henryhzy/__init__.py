f52094e17c25aa7f97415d46cc65aa5dd641716b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:12 +0000

    Add package henryhzy/0.0.0/henryhzy-0.0.0-py3-none-any.whl

---
+++ packages/henryhzy/henryhzy-0.0.0-py3-none-any.whl/henryhzy/__init__.py
@@ -0,0 +1,3 @@
+def henryhzy():
+    # print("import henryhzy successful")
+    print('Hi, I am HenryHZY.\nJust look me up on my personal website: https://henryhzy.github.io/.\nThank you very much!')
