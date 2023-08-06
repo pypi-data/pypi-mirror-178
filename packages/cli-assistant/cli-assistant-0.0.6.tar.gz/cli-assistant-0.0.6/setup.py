39623997df0a779ad0241d8fb4f8f3dbc843d99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:58 +0000

    Add package cli-assistant/0.0.6/cli-assistant-0.0.6.tar.gz

---
+++ packages/cli-assistant/cli-assistant-0.0.6.tar.gz/cli-assistant-0.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("/home/william/code/willsketch/Helper/Description.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("/home/william/code/willsketch/Helper/requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+setup(
+ name= 'cli-assistant',
+ version= '0.0.6',
+ author= 'william mwine',
+ author_email= 'williammwine219@gmail.com',
+ license= 'MIT License',
+ description='guide you with terminal and git commands',
+ long_description=long_description,
+ url='https://github.com/willsketch/Helper',
+ py_modules=[ 'my_helper'],
+ packages= find_packages(),
+ install_requires = [requirements],
+ classifiers=[
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    ],
+ include_package_data=True,
+ package_data={'helper':['examples.txt']},
+ entry_points= {
+     'console_scripts':[
+         'helper = my_helper:cli',
+     ]
+ }
+)
