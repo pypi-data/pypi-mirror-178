7f8b4fcd6b99566672fe75d6afc3b8e62ae5d347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:44 +0000

    Add package neolinearalgebra/0.2.4/neolinearalgebra-0.2.4.tar.gz

---
+++ packages/neolinearalgebra/neolinearalgebra-0.2.4.tar.gz/neolinearalgebra-0.2.4/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+def read(fname):
+      return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+      name='neolinearalgebra',
+      version='0.2.4',
+      author='Sajid Sarker',
+      author_email='sajid.sarker@gmail.com',
+      description='NEO Linear Algebra package for Matrix Manipulation.',
+      license='MIT',
+      keywords='linear algebra matrix vector math',
+      packages=['neolinearalgebra'],
+      long_description=read('README.md'),
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Topic :: Utilities',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 3'
+      ],
+      zip_safe=False
+)
