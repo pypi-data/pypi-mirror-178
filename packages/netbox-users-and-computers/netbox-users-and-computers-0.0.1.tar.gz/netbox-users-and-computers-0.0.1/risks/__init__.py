3c0a89d5c9eb6b26dfbc92e653e0e971688d743d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:24 +0000

    Add package netbox-users-and-computers/0.0.1/netbox-users-and-computers-0.0.1.tar.gz

---
+++ packages/netbox-users-and-computers/netbox-users-and-computers-0.0.1.tar.gz/netbox-users-and-computers-0.0.1/risks/__init__.py
@@ -0,0 +1,14 @@
+from extras.plugins import PluginConfig
+
+
+class NetBoxRisksConfig(PluginConfig):
+    name = 'users_and_computers'
+    verbose_name = 'Users and Computers'
+    description = 'Manage AD Users and Workstations'
+    version = '0.1'
+    base_url = 'risks'
+    author = 'Artur Shamsiev'
+    author_email = 'me@z-lab.me'
+
+
+config = NetBoxRisksConfig
