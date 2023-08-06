0cf99844ed84905270531e73cde0cd5014678e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:12 +0000

    Add package mobile-dropdown/0.0.1/mobile_dropdown-0.0.1.tar.gz

---
+++ packages/mobile-dropdown/mobile_dropdown-0.0.1.tar.gz/mobile_dropdown-0.0.1/mobile_dropdown/MobileDropdown.py
@@ -0,0 +1,43 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class MobileDropdown(Component):
+    """A MobileDropdown component.
+ExampleComponent is an example component.
+It takes a property, `label`, and
+displays it.
+It renders an input with the property `value`
+which is editable by the user.
+
+Keyword arguments:
+
+- id (string; optional)
+
+- className (string; optional)
+
+- options (list; required)
+
+- value (string | number; optional)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'mobile_dropdown'
+    _type = 'MobileDropdown'
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, className=Component.UNDEFINED, options=Component.REQUIRED, value=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'className', 'options', 'value']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'className', 'options', 'value']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        for k in ['options']:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+
+        super(MobileDropdown, self).__init__(**args)
