6cc029ab6f992c064fcd4c414d13516748dc1676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:51 +0000

    Add package ftThanos/0.0.48/ftThanos-0.0.48.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.48.tar.gz/ftThanos-0.0.48/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.48",
+description="A client for 48 API",
+author="Thanos 48",
+author_email="thanos@48paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
