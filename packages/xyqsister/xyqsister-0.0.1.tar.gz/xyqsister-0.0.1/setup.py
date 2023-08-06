78f483964bdd0a93992bb62c20ddf6679fc103f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:29 +0000

    Add package xyqsister/0.0.1/xyqsister-0.0.1.tar.gz

---
+++ packages/xyqsister/xyqsister-0.0.1.tar.gz/xyqsister-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='xyqsister',
+    version='0.0.1',
+    author='ZHYCarge',
+    author_email='ZHYCarge@126.com',
+    long_description = '给谢妹妹写的一个专属小插件',
+    url='https://github.com/ZHYCarge/sister_xie',
+    description=u'写给妹妹的一个小软件（确信）',
+    packages=['xyqsister'],
+    install_requires=[],
+
+)
---
+++ packages/xyqsister/xyqsister-0.0.1.tar.gz/xyqsister-0.0.1/xyqsister/__init__.py
@@ -0,0 +1,11 @@
+print("XYq is my sister")
+
+def EN():
+    print('XYq is the only sister of intelligent science and technology, Xie sister')
+
+def CN():
+    print("XYq是智能科学与技术唯一的妹妹，谢妹妹")
+
+def all():
+    print('XYq is the only sister of intelligent science and technology, Xie sister')
+    print("XYq是智能科学与技术唯一的妹妹，谢妹妹")
