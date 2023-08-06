84d9458865541153c05100bbf65f3855fa77b5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:36 +0000

    Add package xknx/2.1.0/xknx-2.1.0.tar.gz

---
+++ packages/xknx/xknx-2.1.0.tar.gz/xknx-2.1.0/xknx/knxip/routing_indication.py
@@ -0,0 +1,54 @@
+"""
+Module for Serialization and Deserialization of KNX RoutingIndication frames.
+
+Routing indications are used to transport CEMI Messages.
+"""
+from __future__ import annotations
+
+import logging
+
+from xknx.exceptions import CouldNotParseKNXIP, UnsupportedCEMIMessage
+
+from .body import KNXIPBody
+from .cemi_frame import CEMIFrame
+from .knxip_enum import CEMIMessageCode, KNXIPServiceType
+
+logger = logging.getLogger("xknx.log")
+
+
+class RoutingIndication(KNXIPBody):
+    """Representation of a KNX RoutingIndication frame."""
+
+    SERVICE_TYPE = KNXIPServiceType.ROUTING_INDICATION
+
+    def __init__(self, cemi: CEMIFrame | None = None):
+        """Initialize RoutingIndication object."""
+        self.cemi: CEMIFrame | None = (
+            cemi if cemi is not None else CEMIFrame(code=CEMIMessageCode.L_DATA_IND)
+        )
+
+    def calculated_length(self) -> int:
+        """Get length of KNX/IP body."""
+        assert self.cemi is not None
+        return self.cemi.calculated_length()
+
+    def from_knx(self, raw: bytes) -> int:
+        """Parse/deserialize from KNX/IP raw data."""
+        assert self.cemi is not None
+
+        try:
+            return self.cemi.from_knx(raw)
+        except UnsupportedCEMIMessage as unsupported_cemi_err:
+            logger.warning("CEMI not supported: %s", unsupported_cemi_err)
+            self.cemi = None
+            return len(raw)
+
+    def to_knx(self) -> bytes:
+        """Serialize to KNX/IP raw data."""
+        if self.cemi is None:
+            raise CouldNotParseKNXIP("No CEMIFrame defined.")
+        return self.cemi.to_knx()
+
+    def __repr__(self) -> str:
+        """Return object as readable string."""
+        return f'<RoutingIndication cemi="{self.cemi}" />'
