923ca357a02e99b20be3470ce9d90d2c992f38a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:59 +0000

    Add package zeo-utils/0.0.2/zeo_utils-0.0.2.tar.gz

---
+++ packages/zeo-utils/zeo_utils-0.0.2.tar.gz/zeo_utils-0.0.2/zeo_utils/prop_parser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 11/24/2022 10:18 AM
+# @Author  : zhangbc0315@outlook.com
+# @File    : prop_parser.py
+# @Software: PyCharm
+
+
+class PropParser:
+
+    @classmethod
+    def parse_pdm(cls, pdm_str: str):
+        for line in pdm_str.split('\n'):
+            pass
+
+
+if __name__ == "__main__":
+    pass
