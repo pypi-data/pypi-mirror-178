f82562bbb3be574b30900a1dcaf17e682536e6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:16 +0000

    Add package microblocks-messaging-library/0.0.2/microblocks_messaging_library-0.0.2-py2.py3-none-any.whl

---
+++ packages/microblocks-messaging-library/microblocks_messaging_library-0.0.2-py2.py3-none-any.whl/microblocks_messaging_library/__init__.py
@@ -0,0 +1,100 @@
+# John Maloney, October 2022
+# Revised by Wenjie Wu, October 2022
+
+__version__ = "0.0.2"
+
+
+import serial
+
+
+class MicroblocksMessage:
+    def __init__(self, verbose=False):
+        self.ser = None
+        self._buffer = bytearray()
+        self._verbose = verbose  # verbose: Print various debugging information
+        self.on_message = None  # paho style:  https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php
+
+    def __del__(self):
+        self.disconnect()
+
+    def connect(self, port):
+        self.ser = serial.Serial(port, 115200)
+
+    def disconnect(self):
+        self.ser.close()
+
+    def sendBroadcast(self, aString):
+        if type(aString) != str:
+            raise TypeError("must be string")
+        utf8 = aString.encode("utf-8")
+        length = len(utf8) + 1
+        bytes = (
+            bytearray([251, 27, 0, length % 256, int(length / 256)]) + utf8 + b"\xfe"
+        )
+        self.ser.write(bytes)
+
+    def receiveBroadcasts(self):
+        result = []
+        data = self.ser.read()
+        if data:
+            self._buffer = self._buffer + data
+            for msgBytes in self._match(27):
+                result.append(msgBytes[4:].decode("utf-8").replace("\x00", ""))
+        if result == []:
+            return None
+        else:
+            return "".join(result)
+
+    def _match(self, filter="*"):
+        buf = self._buffer
+        result = []
+        bytesRemaining = None
+        cmd = None
+        msgLen = None
+        end = None
+        length = len(buf)
+        i = 0
+        while True:
+            while not ((i >= length) or (buf[i] == 250) or (buf[i] == 251)):
+                i += 1  # skip to start of next message
+            bytesRemaining = length - i
+            if bytesRemaining < 1:  # nothing to process
+                self._buffer = buf[i:]
+                return result
+            cmd = buf[i]
+            if (cmd == 250) and (bytesRemaining >= 3):  # short message (3 bytes)
+                if (filter == "*") or (filter == buf[i + 1]):
+                    result.append(buf[i : i + 3])
+                i += 3
+            elif (cmd == 251) and (bytesRemaining >= 5):  # long message (>= 5 bytes)
+                msgLen = (256 * buf[i + 4]) + buf[i + 3]
+                end = i + 5 + msgLen
+                if end > length:  # long message is not yet complete
+                    self._buffer = buf[i:]
+                    return result
+                if (filter == "*") or (filter == buf[i + 1]):
+                    result.append(buf[i:end])
+                i = end
+            else:
+                self._buffer = buf[i:]
+                return result
+
+    def _processReceiveBroadcasts(self):
+        while True:
+            message = self.receiveBroadcasts()
+            if callable(self.on_message):
+                self.on_message(message)
+
+    def loopForever(self):
+        # blocking
+        # self.on_message = on_message
+        self._processReceiveBroadcasts()
+
+    def loopStart(self):
+        # non-blocking
+        # https://stackoverflow.com/questions/70625801/threading-reading-a-serial-port-in-python-with-a-gui
+        """
+        x = threading.Thread(target=self._process_recv_messgae, args=())
+        x.start()
+        """
+        pass
