14db57ac3037d8a5f1528e150ffdc9ce9b6f7309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:13 +0000

    Add package k4sproff/1.0/k4sproff-1.0.tar.gz

---
+++ packages/k4sproff/k4sproff-1.0.tar.gz/k4sproff-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='k4sproff',
+    version='v1.0',
+    description='for profile',
+    py_modules=['k4sproff'],
+    author='upuuuuuu',
+    author_email='2226548059@qq.com',
+    url='https://github.com/upuuuuuu',
+    requires=['os', 'time', 'torch', 'logging', 'pandas', 'torchstat'],
+    license='MIT'
+)
