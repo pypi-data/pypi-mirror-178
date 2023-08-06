a53d80c9c431a5dda2a15485942a7525b662ba7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:58 +0000

    Add package my-timesunion-sdk/0.0.3/my-timesunion-sdk-0.0.3.tar.gz

---
+++ packages/my-timesunion-sdk/my-timesunion-sdk-0.0.3.tar.gz/my-timesunion-sdk-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="my-timesunion-sdk",
+    version="0.0.3",
+    author="xhh",
+    author_email="380472213@qq.com",
+    description="A commonly used tool library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/my-timesunion-sdk/",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    keywords=['my', 'times', 'union', 'sdk'],
+    install_requires=[
+        "requests",
+        "arrow"
+    ]
+)
