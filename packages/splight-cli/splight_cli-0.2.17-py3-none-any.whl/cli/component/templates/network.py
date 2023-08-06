cfbb42873b7ca68088f5dcfc69d572699f9d692f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:38 +0000

    Add package splight-cli/0.2.17/splight_cli-0.2.17-py3-none-any.whl

---
+++ packages/splight-cli/splight_cli-0.2.17-py3-none-any.whl/cli/component/templates/network.py
@@ -0,0 +1,9 @@
+from splight_lib.component.abstract import AbstractComponent
+
+class Main(AbstractComponent):
+
+    def __init__(self, *args, **kwargs):
+        super(Main, self).__init__(*args, **kwargs)
+    
+    def start(self):
+        pass
