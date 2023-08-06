3259fbe41c101d8b2eb97ea1cd500d446beb65ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:15 +0000

    Add package pygraph-01/0.1.0/pygraph_01-0.1.0.tar.gz

---
+++ packages/pygraph-01/pygraph_01-0.1.0.tar.gz/pygraph_01-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pygraph']
+setup_kwargs = {
+    'name': 'pygraph-01',
+    'version': '0.1.0',
+    'description': 'Simple Python Graph Library',
+    'long_description': 'None',
+    'author': 'None',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
