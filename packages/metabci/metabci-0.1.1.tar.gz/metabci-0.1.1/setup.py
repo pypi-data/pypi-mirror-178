11460523cd9e24d651f84f6be11db32f80d51a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:57 +0000

    Add package metabci/0.1.1/metabci-0.1.1.tar.gz

---
+++ packages/metabci/metabci-0.1.1.tar.gz/metabci-0.1.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r", errors="ignore", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="metabci",
+    version="0.1.1",
+    author="TBC-TJU",
+    author_email="TBC_TJU_2022@163.com",
+    description="A Library of Datasets, Algorithms, and Experiments workflow for Brain-Computer Interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
