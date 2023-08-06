e0632bed3379ed0155b219a0d872d70dbe4632c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:07 +0000

    Add package comets/1.1.1/comets-1.1.1-py3-none-any.whl

---
+++ packages/comets/comets-1.1.1-py3-none-any.whl/comets/experiments/optimization/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2021- 2022 Cosmo Tech
+# This document and all information contained herein is the exclusive property -
+# including all intellectual property rights pertaining thereto - of Cosmo Tech.
+# Any use, reproduction, translation, broadcasting, transmission, distribution,
+# etc., to any person is prohibited unless it has been previously and
+# specifically authorized by written means by Cosmo Tech.
+
+from .optimization import Optimization
+from .optimalgorithm import OptimizationAlgorithmRegistry
+from .nevergradalgorithm import NevergradOptimizationAlgorithm
+from .cmaes import CMAES
+from .space_optim import StandardizedSpace
+from .evolutionstrategies import OpenES
+from .bayesianoptimizer import BayesianOptimizer
+
+__all__ = [
+    "Optimization",
+    "OptimizationAlgorithmRegistry",
+    "NevergradOptimizationAlgorithm",
+    "CMAES",
+    "StandardizedSpace",
+    "OpenES",
+    "BayesianOptimizer",
+]
