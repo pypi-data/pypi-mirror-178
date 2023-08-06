298b7cc5d1677c00f3208ec9a74dd4e8a2fa741c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:18 +0000

    Add package ftThanos/0.0.71/ftThanos-0.0.71.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.71.tar.gz/ftThanos-0.0.71/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.71",
+description="A client for 71 API",
+author="Thanos 71",
+author_email="thanos@71paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
