cfdcf325903425bf122daa369197155a2eac0d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:00:30 +0000

    Add package ftThanos/0.0.76/ftThanos-0.0.76.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.76.tar.gz/ftThanos-0.0.76/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.76",
+description="A client for 76 API",
+author="Thanos 76",
+author_email="thanos@76paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
