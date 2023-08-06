f957eee90c0af55d9707370b0c4569df1847fdfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:23:41 +0000

    Add package ups-sdk/1.2.1/ups_sdk-1.2.1.tar.gz

---
+++ packages/ups-sdk/ups_sdk-1.2.1.tar.gz/ups_sdk-1.2.1/ups_sdk/models/TrackResponse.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from pydantic import BaseModel, Field
+from .Response import Response
+from .Shipment import Shipment
+from typing import Any, List
+
+class TrackResponse(BaseModel):
+    Response: Response
+    Shipments: List[Shipment] = Field(None, alias="Shipment")
+    
+    def __init__(__pydantic_self__, **data: Any) -> None:
+        if isinstance(data["Shipment"], dict):
+            data["Shipment"] = [data["Shipment"]]
+            
+        for shipment in data["Shipment"]:
+            if isinstance(shipment["Package"]["Activity"], dict):
+                shipment["Package"]["Activity"] = [shipment["Package"]["Activity"]]
+            if isinstance(shipment["ReferenceNumber"], dict):
+                shipment["ReferenceNumber"] = [shipment["ReferenceNumber"]]
+            
+        super().__init__(**data)
