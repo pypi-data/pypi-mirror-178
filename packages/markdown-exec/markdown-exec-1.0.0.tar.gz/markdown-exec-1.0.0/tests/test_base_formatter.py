7cc7ffd1b8c792169c853b4c8f716f8578289b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:57 +0000

    Add package markdown-exec/1.0.0/markdown-exec-1.0.0.tar.gz

---
+++ packages/markdown-exec/markdown-exec-1.0.0.tar.gz/markdown-exec-1.0.0/tests/test_base_formatter.py
@@ -0,0 +1,29 @@
+"""Tests for the base formatter."""
+
+import pytest
+from markdown import Markdown
+
+from markdown_exec.formatters.base import base_format
+
+
+def test_no_p_around_html(md: Markdown) -> None:
+    """Assert HTML isn't wrapped in a `p` tag.
+
+    Parameters:
+        md: A Markdown instance (fixture).
+    """
+    code = "<pre><code>hello</code></pre>"
+    html = base_format("whatever", lambda _: _, code, md, id="", html=True, source="", result="", tabs=("", ""))
+    assert html == code
+
+
+@pytest.mark.parametrize("html", [True, False])
+def test_render_source(md: Markdown, html: bool) -> None:
+    """Assert source is rendered.
+
+    Parameters:
+        md: A Markdown instance (fixture).
+        html: Whether output is HTML or not.
+    """
+    markup = base_format("python", lambda _: _, "hello", md, html, "tabbed-left", "", ("Source", "Output"), id="")
+    assert "Source" in markup
---
+++ packages/markdown-exec/markdown-exec-1.0.0.tar.gz/markdown-exec-1.0.0/tests/test_shell.py
@@ -0,0 +1,61 @@
+"""Tests for the shell formatters."""
+
+from textwrap import dedent
+
+from markdown import Markdown
+
+
+def test_output_markdown(md: Markdown) -> None:
+    """Assert Markdown is converted to HTML.
+
+    Parameters:
+        md: A Markdown instance (fixture).
+    """
+    html = md.convert(
+        dedent(
+            """
+            ```sh exec="yes"
+            echo "**Bold!**"
+            ```
+            """
+        )
+    )
+    assert html == "<p><strong>Bold!</strong></p>"
+
+
+def test_output_html(md: Markdown) -> None:
+    """Assert HTML is injected as is.
+
+    Parameters:
+        md: A Markdown instance (fixture).
+    """
+    html = md.convert(
+        dedent(
+            """
+            ```sh exec="yes" html="yes"
+            echo "**Bold!**"
+            ```
+            """
+        )
+    )
+    assert html == "<p>**Bold!**\n</p>"
+
+
+def test_error_raised(md: Markdown, caplog) -> None:
+    """Assert errors properly log a warning and return a formatted traceback.
+
+    Parameters:
+        md: A Markdown instance (fixture).
+        caplog: Pytest fixture to capture logs.
+    """
+    html = md.convert(
+        dedent(
+            """
+            ```sh exec="yes"
+            echo("wrong syntax")
+            ```
+            """
+        )
+    )
+    assert "error" in html
+    assert "Execution of sh code block exited with non-zero status" in caplog.text
