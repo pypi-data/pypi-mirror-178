153fd0751b7d43fc6ee393c857b4630b3a3d6a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:42 +0000

    Add package Wdatabase/2.2.0a2/Wdatabase-2.2.0a2-py3.8.egg

---
+++ packages/Wdatabase/Wdatabase-2.2.0a2-py3.8.egg/Wdata/plot_pic/__init__.py
@@ -0,0 +1,1 @@
+from .Line_chart import *
---
+++ packages/Wdatabase/Wdatabase-2.2.0a2-py3.8.egg/Wdata/read/read_main.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+"""
+Author: Wdataorg
+Date: 2022.06.17
+Project Name: Wdata
+"""
+
+from ..data_py import data
+
+
+def read_dict(fname: str) -> dict:
+    return data[fname]['datas']
+
+def read_dict_draw(fname:str) -> list:
+    file = data[fname]
+    return [file, file['Type']]
