601eb8b3103f23d241894efffda06feb5a7f11f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:26 +0000

    Add package bounden/1.0.0a2/bounden-1.0.0a2-py3-none-any.whl

---
+++ packages/bounden/bounden-1.0.0a2-py3-none-any.whl/bounden/__init__.py
@@ -0,0 +1,25 @@
+"""
+&copy; 2022 Cariad Eccleston and released under the MIT License.
+
+For usage and support, see https://github.com/cariad/bounden.
+"""
+
+from importlib.resources import files
+
+from bounden.points import Point, Point2
+
+
+def version() -> str:
+    """
+    Gets the package's version.
+    """
+
+    with files(__package__).joinpath("VERSION").open("r") as t:
+        return t.readline().strip()
+
+
+__all__ = [
+    "Point",
+    "Point2",
+    "version",
+]
