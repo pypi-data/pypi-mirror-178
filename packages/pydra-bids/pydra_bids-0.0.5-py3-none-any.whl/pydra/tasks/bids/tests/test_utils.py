3041fa1b608a5ef201400b54798f1d7be5d34596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:12 +0000

    Add package pydra-bids/0.0.5/pydra_bids-0.0.5-py3-none-any.whl

---
+++ packages/pydra-bids/pydra_bids-0.0.5-py3-none-any.whl/pydra/tasks/bids/tests/test_utils.py
@@ -0,0 +1,23 @@
+import pydra.tasks.bids.utils as utils
+
+
+def test_bfi_defaults():
+    task = utils.BIDSFileInfo().to_task()
+
+    assert "file_path" in task.input_names
+    assert {"entities", "suffix", "extension"} == set(task.output_names)
+
+
+def test_bdr_defaults():
+    task = utils.BIDSDatasetReader().to_task()
+
+    assert "dataset_path" in task.input_names
+    assert {"participant_id", "session_id"}.issubset(task.input_names)
+    assert "dataset_description" in task.output_names
+
+
+def test_bdr_with_output_query():
+    task = utils.BIDSDatasetReader(
+        output_query={"T1w": {"suffix": "T1w"}, "bold": {"suffix": "bold"}}
+    ).to_task()
+    assert {"T1w", "bold"}.issubset(task.output_names)
