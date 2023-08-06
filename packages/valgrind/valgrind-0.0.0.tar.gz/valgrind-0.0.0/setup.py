fdcd18226e04e2a1398106e5c203108e83ccde92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:32 +0000

    Add package valgrind/0.0.0/valgrind-0.0.0.tar.gz

---
+++ packages/valgrind/valgrind-0.0.0.tar.gz/valgrind-0.0.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup, Extension
+
+valgrind = Extension(
+    "valgrind",
+    include_dirs=["valgrind"],
+    sources=["valgrind.c"],
+)
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="valgrind",
+    version="0.0.0",
+    description="Control callgrind instrumentation from Python.",
+    author="Max Bernstein",
+    author_email="python@bernsteinbear.com",
+    url="https://github.com/tekknolagi/valgrind",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    ext_modules=[valgrind],
+)
