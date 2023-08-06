86412c31410a92bc796f7c23ef346d98575bf209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:01 +0000

    Add package stream-wars/0.1.0/stream_wars-0.1.0.tar.gz

---
+++ packages/stream-wars/stream_wars-0.1.0.tar.gz/stream_wars-0.1.0/stream_wars/frontend/build/precache-manifest.e169c48ee8a5b064e0ad02dd97cb7563.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c779871d9b6bebba76d5de7fb91b96",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dfe94b5aa901771dbbf4",
+    "url": "./static/css/main.e3314fdb.chunk.css"
+  },
+  {
+    "revision": "e4e8a301ccb7aad93e6f",
+    "url": "./static/js/2.29ebcd12.chunk.js"
+  },
+  {
+    "revision": "78c96c22aeb665cb55c9dc105844f80c",
+    "url": "./static/js/2.29ebcd12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfe94b5aa901771dbbf4",
+    "url": "./static/js/main.3e636c5f.chunk.js"
+  },
+  {
+    "revision": "6e0e1461711a4ec4b1dd",
+    "url": "./static/js/runtime-main.225e7569.js"
+  },
+  {
+    "revision": "6d9a2d0075fa8d5252ab7d08cf661136",
+    "url": "./static/media/StarJediHollow.6d9a2d00.woff"
+  },
+  {
+    "revision": "f36aaf0dc82131e21db43faa6db4095b",
+    "url": "./static/media/StarJediHollow.f36aaf0d.woff2"
+  },
+  {
+    "revision": "04238fc34d2403935761ff468bb8c5ed",
+    "url": "./static/media/volume_off.04238fc3.svg"
+  },
+  {
+    "revision": "ef7f87ab516ccf1eac5a5111815c427f",
+    "url": "./static/media/volume_on.ef7f87ab.svg"
+  }
+]);
