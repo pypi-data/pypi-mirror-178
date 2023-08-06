02d59f5fb952ba447e072083a9bb7732d4b5fa30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:47 +0000

    Add package testable-coresvc/0.0.6/testable_coresvc-0.0.6-py3-none-any.whl

---
+++ packages/testable-coresvc/testable_coresvc-0.0.6-py3-none-any.whl/testable_coresvc/settings.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, SecretStr
+
+
+class MongoSettings(BaseModel):
+    """
+    All Mongo settings, including connection settings
+    """
+    username: str
+    password: SecretStr
+    host: str
+    port: int = 27017
+    database: str = "testable"
+    params: dict[str, str] = {"ssl": "false"}
+
+    @property
+    def uri(self) -> str:
+        """Returns the formatted URI from the settings"""
+        uri = f"mongodb://{self.username}:{self.password.get_secret_value()}@{self.host}:{self.port}/"
+        if self.params:
+            uri += "?"
+
+        uri += "&".join(k + "=" + v for k, v in self.params.items())
+        return uri
