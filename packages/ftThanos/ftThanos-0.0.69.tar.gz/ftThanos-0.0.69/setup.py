9912042b5da678adde802c98317fbd4a190bd56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:09 +0000

    Add package ftThanos/0.0.69/ftThanos-0.0.69.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.69.tar.gz/ftThanos-0.0.69/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.69",
+description="A client for 69 API",
+author="Thanos 69",
+author_email="thanos@69paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
