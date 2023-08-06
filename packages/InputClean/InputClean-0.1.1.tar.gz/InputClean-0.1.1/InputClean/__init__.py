c6057ba737e3da33f5641c6972b26b63286204cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:42 +0000

    Add package InputClean/0.1.1/InputClean-0.1.1.tar.gz

---
+++ packages/InputClean/InputClean-0.1.1.tar.gz/InputClean-0.1.1/InputClean/__init__.py
@@ -0,0 +1,3 @@
+from InputClean import ci
+
+__all__ = ["ci"]
---
+++ packages/InputClean/InputClean-0.1.1.tar.gz/InputClean-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "InputClean",
+    version = "0.1.1",
+    keywords = ["pip", "InputClean"],
+    description = "Auto del input noise.",
+    long_description = "Auto del input noise.", 
+    author = "KurehaSHO",
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any" 
+)
