432d3bf3f77469d16b3857f601bed03e22af3197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:00 +0000

    Add package hifitime/3.7.0/hifitime-3.7.0.tar.gz

---
+++ packages/hifitime/hifitime-3.7.0.tar.gz/hifitime-3.7.0/src/lib.rs
@@ -0,0 +1,188 @@
+#![doc = include_str!("../README.md")]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/*
+ * Hifitime, part of the Nyx Space tools
+ * Copyright (C) 2022 Christopher Rabotin <christopher.rabotin@gmail.com> et al. (cf. AUTHORS.md)
+ * This Source Code Form is subject to the terms of the Apache
+ * v. 2.0. If a copy of the Apache License was not distributed with this
+ * file, You can obtain one at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Documentation: https://nyxspace.com/
+ */
+
+pub const J1900_NAIF: f64 = 2_415_020.0;
+pub const J2000_NAIF: f64 = 2_451_545.0;
+/// `J1900_OFFSET` determines the offset in julian days between 01 Jan 1900 at midnight and the
+/// Modified Julian Day at 17 November 1858.
+/// NOTE: Julian days "start" at noon so that astronomical observations throughout the night
+/// happen at the same Julian day. Note however that the Modified Julian Date (MJD) starts at
+/// midnight, not noon, cf. <http://tycho.usno.navy.mil/mjd.html>.
+pub const J1900_OFFSET: f64 = 15_020.0;
+/// `J2000_OFFSET` determines the offset in julian days between 01 Jan 2000 at **noon** and the
+/// Modified Julian Day at 17 November 1858.
+pub const J2000_OFFSET: f64 = 51_544.5;
+/// The Ephemeris Time epoch, in seconds
+pub const ET_EPOCH_S: i64 = 3_155_716_800;
+/// Modified Julian Date in seconds as defined [here](http://tycho.usno.navy.mil/mjd.html). MJD epoch is Modified Julian Day at 17 November 1858 at midnight.
+pub const MJD_OFFSET: f64 = 2_400_000.5;
+/// The JDE offset in days
+pub const JDE_OFFSET_DAYS: f64 = J1900_OFFSET + MJD_OFFSET;
+/// The JDE offset in seconds
+pub const JDE_OFFSET_SECONDS: f64 = JDE_OFFSET_DAYS * SECONDS_PER_DAY;
+/// `DAYS_PER_YEAR` corresponds to the number of days per year in the Julian calendar.
+pub const DAYS_PER_YEAR: f64 = 365.25;
+/// `DAYS_PER_YEAR_NLD` corresponds to the number of days per year **without leap days**.
+pub const DAYS_PER_YEAR_NLD: f64 = 365.0;
+/// `DAYS_PER_CENTURY` corresponds to the number of days per centuy in the Julian calendar.
+pub const DAYS_PER_CENTURY: f64 = 36525.0;
+pub const DAYS_PER_CENTURY_I64: i64 = 36525;
+/// `SECONDS_PER_MINUTE` defines the number of seconds per minute.
+pub const SECONDS_PER_MINUTE: f64 = 60.0;
+/// `SECONDS_PER_HOUR` defines the number of seconds per hour.
+pub const SECONDS_PER_HOUR: f64 = 3_600.0;
+/// `SECONDS_PER_DAY` defines the number of seconds per day.
+pub const SECONDS_PER_DAY: f64 = 86_400.0;
+pub const SECONDS_PER_DAY_I64: i64 = 86_400;
+/// `SECONDS_PER_CENTURY` defines the number of seconds per century.
+pub const SECONDS_PER_CENTURY: f64 = SECONDS_PER_DAY * DAYS_PER_CENTURY;
+/// `SECONDS_PER_YEAR` corresponds to the number of seconds per julian year from [NAIF SPICE](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/jyear_c.html).
+pub const SECONDS_PER_YEAR: f64 = 31_557_600.0;
+pub const SECONDS_PER_YEAR_I64: i64 = 31_557_600;
+/// `SECONDS_PER_TROPICAL_YEAR` corresponds to the number of seconds per tropical year from [NAIF SPICE](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/tyear_c.html).
+pub const SECONDS_PER_TROPICAL_YEAR: f64 = 31_556_925.974_7;
+/// `SECONDS_PER_SIDERAL_YEAR` corresponds to the number of seconds per sideral year from [NIST](https://www.nist.gov/pml/special-publication-811/nist-guide-si-appendix-b-conversion-factors/nist-guide-si-appendix-b9#TIME).
+pub const SECONDS_PER_SIDERAL_YEAR: f64 = 31_558_150.0;
+
+/// The duration between J2000 and J1900: one century **minus** twelve hours. J1900 starts at _noon_ but J2000 is at midnight.
+pub const J2000_TO_J1900_DURATION: Duration = Duration {
+    centuries: 0,
+    nanoseconds: 3_155_716_800_000_000_000,
+};
+
+mod parser;
+
+mod epoch;
+
+pub use epoch::*;
+
+mod duration;
+pub use duration::*;
+
+mod timescale;
+pub use timescale::*;
+
+mod timeunits;
+pub use timeunits::*;
+
+mod timeseries;
+pub use timeseries::*;
+
+/// This module defines all of the deprecated methods.
+mod deprecated;
+
+#[allow(deprecated)]
+pub mod prelude {
+    pub use crate::{
+        deprecated::TimeSystem, Duration, Epoch, Errors, Freq, Frequencies, TimeScale, TimeSeries,
+        TimeUnits, Unit,
+    };
+}
+
+#[cfg(feature = "asn1der")]
+pub mod asn1der;
+
+#[cfg(feature = "python")]
+pub mod python;
+
+#[cfg(feature = "std")]
+extern crate core;
+
+use core::convert;
+use core::fmt;
+use core::num::ParseIntError;
+
+#[cfg(feature = "std")]
+use std::error::Error;
+
+/// Errors handles all oddities which may occur in this library.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Errors {
+    /// Carry is returned when a provided function does not support time carry. For example,
+    /// if a call to `Datetime::new` receives 60 seconds and there are only 59 seconds in the provided
+    /// date time then a Carry Error is returned as the Result.
+    Carry,
+    /// ParseError is returned when a provided string could not be parsed and converted to the desired
+    /// struct (e.g. Datetime).
+    ParseError(ParsingErrors),
+    /// Raised when trying to initialize an Epoch or Duration from its hi and lo values, but these overlap
+    ConversionOverlapError(f64, f64),
+    /// Raised if an overflow occured
+    Overflow,
+    /// Raised if the initialization from system time failed
+    SystemTimeError,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum ParsingErrors {
+    ParseIntError,
+    ValueError,
+    TimeSystem,
+    ISO8601,
+    UnknownFormat,
+    UnknownOrMissingUnit,
+    UnsupportedTimeSystem,
+}
+
+impl fmt::Display for Errors {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Self::Carry => write!(f, "a carry error (e.g. 61 seconds)"),
+            Self::ParseError(kind) => write!(f, "ParseError: {:?}", kind),
+            Self::ConversionOverlapError(hi, lo) => {
+                write!(f, "hi and lo values overlap: {}, {}", hi, lo)
+            }
+            Self::Overflow => write!(
+                f,
+                "overflow occured when trying to convert Duration information"
+            ),
+            Self::SystemTimeError => write!(f, "std::time::SystemTime returned an error"),
+        }
+    }
+}
+
+impl convert::From<ParseIntError> for Errors {
+    fn from(_: ParseIntError) -> Self {
+        Errors::ParseError(ParsingErrors::ParseIntError)
+    }
+}
+
+#[cfg(feature = "std")]
+impl Error for Errors {}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Errors, ParsingErrors, TimeScale};
+
+    #[test]
+    fn enum_eq() {
+        // Check the equality compiles (if one compiles, then all asserts will work)
+        assert!(Errors::Carry == Errors::Carry);
+        assert!(ParsingErrors::ParseIntError == ParsingErrors::ParseIntError);
+        assert!(TimeScale::ET == TimeScale::ET);
+    }
+}
+
+#[test]
+fn test_ts() {
+    for ts_u8 in 0..u8::MAX {
+        let ts = TimeScale::from(ts_u8);
+        let ts_u8_back: u8 = ts.into();
+        // If the u8 is greater than 5, it isn't valid and necessarily encoded as TAI.
+        if ts_u8 < 8 {
+            assert_eq!(ts_u8_back, ts_u8, "got {ts_u8_back} want {ts_u8}");
+        } else {
+            assert_eq!(ts, TimeScale::TAI);
+        }
+    }
+}
---
+++ packages/hifitime/hifitime-3.7.0.tar.gz/hifitime-3.7.0/src/timescale.rs
@@ -0,0 +1,210 @@
+/*
+ * Hifitime, part of the Nyx Space tools
+ * Copyright (C) 2022 Christopher Rabotin <christopher.rabotin@gmail.com> et al. (cf. AUTHORS.md)
+ * This Source Code Form is subject to the terms of the Apache
+ * v. 2.0. If a copy of the Apache License was not distributed with this
+ * file, You can obtain one at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Documentation: https://nyxspace.com/
+ */
+
+#[cfg(feature = "python")]
+use pyo3::prelude::*;
+
+#[cfg(feature = "serde")]
+use serde_derive::{Deserialize, Serialize};
+
+use core::fmt;
+use core::str::FromStr;
+
+use crate::{Duration, Epoch, Errors, ParsingErrors, SECONDS_PER_DAY};
+
+/// GPS reference epoch is UTC midnight between 05 January and 06 January 1980; cf. <https://gssc.esa.int/navipedia/index.php/Time_References_in_GNSS#GPS_Time_.28GPST.29>.
+pub const GPST_REF_EPOCH: Epoch = Epoch::from_tai_duration(Duration {
+    centuries: 0,
+    nanoseconds: 2_524_953_619_000_000_000,
+});
+pub const SECONDS_GPS_TAI_OFFSET: f64 = 2_524_953_619.0;
+pub const SECONDS_GPS_TAI_OFFSET_I64: i64 = 2_524_953_619;
+pub const DAYS_GPS_TAI_OFFSET: f64 = SECONDS_GPS_TAI_OFFSET / SECONDS_PER_DAY;
+
+/// GST (Galileo) reference epoch is 13 seconds before 1999 August 21 UTC at midnight.
+pub const GST_REF_EPOCH: Epoch = Epoch::from_tai_duration(Duration {
+    centuries: 0,
+    nanoseconds: 3_144_268_819_000_000_000,
+});
+pub const SECONDS_GST_TAI_OFFSET: f64 = 3_144_268_819.0;
+pub const SECONDS_GST_TAI_OFFSET_I64: i64 = 3_144_268_819;
+
+/// BDT(BeiDou): 2005 Dec 31st Midnight
+/// BDT (BeiDou) reference epoch is 2005 December 31st UTC at midnight. **This time scale is synchronized with UTC.**
+pub const BDT_REF_EPOCH: Epoch = Epoch::from_tai_duration(Duration {
+    centuries: 1,
+    nanoseconds: 189_302_433_000_000_000,
+});
+pub const SECONDS_BDT_TAI_OFFSET: f64 = 3_345_062_433.0;
+pub const SECONDS_BDT_TAI_OFFSET_I64: i64 = 3_345_062_433;
+
+/// The UNIX reference epoch of 1970-01-01 in TAI duration, accounting only for IERS leap seconds.
+pub const UNIX_REF_EPOCH: Epoch = Epoch::from_tai_duration(Duration {
+    centuries: 0,
+    nanoseconds: 2_208_988_800_000_000_000,
+});
+
+/// Enum of the different time systems available
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "python", pyclass)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub enum TimeScale {
+    /// TAI is the representation of an Epoch internally
+    TAI,
+    /// Terrestrial Time (TT) (previously called Terrestrial Dynamical Time (TDT))
+    TT,
+    /// Ephemeris Time as defined by SPICE (slightly different from true TDB)
+    ET,
+    /// Dynamic Barycentric Time (TDB) (higher fidelity SPICE ephemeris time)
+    TDB,
+    /// Universal Coordinated Time
+    UTC,
+    /// GPST Time also applies to QZSS, IRNSS and GAL constellations
+    GPST,
+    /// Galileo Time scale
+    GST,
+    /// BeiDou Time scale
+    BDT,
+}
+
+impl Default for TimeScale {
+    /// Builds default TAI time scale
+    fn default() -> Self {
+        /*
+         * We use TAI as default Time scale,
+         * because `Epoch` is always defined with respect to TAI.
+         * Also, a default `Epoch` is then a null duration into TAI.
+         */
+        Self::TAI
+    }
+}
+
+impl TimeScale {
+    pub(crate) const fn formatted_len(&self) -> usize {
+        match &self {
+            Self::GPST => 4,
+            Self::TAI | Self::TDB | Self::UTC | Self::GST | Self::BDT => 3,
+            Self::ET | Self::TT => 2,
+        }
+    }
+
+    /// Returns true if Self is based off a GNSS constellation
+    pub const fn is_gnss(&self) -> bool {
+        matches!(self, Self::GPST | Self::GST | Self::BDT)
+    }
+}
+
+impl fmt::Display for TimeScale {
+    /// Prints given TimeScale
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::TAI => write!(f, "TAI"),
+            Self::TT => write!(f, "TT"),
+            Self::ET => write!(f, "ET"),
+            Self::TDB => write!(f, "TDB"),
+            Self::UTC => write!(f, "UTC"),
+            Self::GPST => write!(f, "GPST"),
+            Self::GST => write!(f, "GST"),
+            Self::BDT => write!(f, "BDT"),
+        }
+    }
+}
+
+impl fmt::LowerHex for TimeScale {
+    /// Prints given TimeScale in RINEX format
+    /// ie., standard GNSS constellation name is prefered when possible
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::GPST => write!(f, "GPS"),
+            Self::GST => write!(f, "GAL"),
+            Self::BDT => write!(f, "BDS"),
+            _ => write!(f, "{self}"),
+        }
+    }
+}
+
+#[cfg_attr(feature = "python", pymethods)]
+impl TimeScale {
+    /// Returns true if self takes leap seconds into account
+    pub const fn uses_leap_seconds(&self) -> bool {
+        matches!(self, Self::UTC)
+    }
+}
+
+/// Allows conversion of a TimeSystem into a u8
+/// Mapping: TAI: 0; TT: 1; ET: 2; TDB: 3; UTC: 4; GPST: 5; GST: 6; BDT: 7;
+impl From<TimeScale> for u8 {
+    fn from(ts: TimeScale) -> Self {
+        match ts {
+            TimeScale::TAI => 0,
+            TimeScale::TT => 1,
+            TimeScale::ET => 2,
+            TimeScale::TDB => 3,
+            TimeScale::UTC => 4,
+            TimeScale::GPST => 5,
+            TimeScale::GST => 6,
+            TimeScale::BDT => 7,
+        }
+    }
+}
+
+/// Allows conversion of a u8 into a TimeSystem.
+/// Mapping: 1: TT; 2: ET; 3: TDB; 4: UTC; 5: GPST; 6: GST; 7: BDT; anything else: TAI
+impl From<u8> for TimeScale {
+    fn from(val: u8) -> Self {
+        match val {
+            1 => Self::TT,
+            2 => Self::ET,
+            3 => Self::TDB,
+            4 => Self::UTC,
+            5 => Self::GPST,
+            6 => Self::GST,
+            7 => Self::BDT,
+            _ => Self::TAI,
+        }
+    }
+}
+
+impl FromStr for TimeScale {
+    type Err = Errors;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let val = s.trim();
+        if val == "UTC" {
+            Ok(Self::UTC)
+        } else if val == "TT" {
+            Ok(Self::TT)
+        } else if val == "TAI" {
+            Ok(Self::TAI)
+        } else if val == "TDB" {
+            Ok(Self::TDB)
+        } else if val == "ET" {
+            Ok(Self::ET)
+        } else if val == "GPST" || val == "GPS" {
+            Ok(Self::GPST)
+        } else if val == "GST" || val == "GAL" {
+            Ok(Self::GST)
+        } else if val == "BDT" || val == "BDS" {
+            Ok(Self::BDT)
+        } else {
+            Err(Errors::ParseError(ParsingErrors::TimeSystem))
+        }
+    }
+}
+
+#[test]
+#[cfg(feature = "serde")]
+fn test_serdes() {
+    let ts = TimeScale::UTC;
+    let content = "\"UTC\"";
+    assert_eq!(content, serde_json::to_string(&ts).unwrap());
+    let parsed: TimeScale = serde_json::from_str(content).unwrap();
+    assert_eq!(ts, parsed);
+}
