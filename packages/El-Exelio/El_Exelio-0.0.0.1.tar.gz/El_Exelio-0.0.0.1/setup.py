4dbb2315bf9bed134a3bd33ed9f95a770c6dc70f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:41 +0000

    Add package El-Exelio/0.0.0.1/El_Exelio-0.0.0.1.tar.gz

---
+++ packages/El-Exelio/El_Exelio-0.0.0.1.tar.gz/El_Exelio-0.0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='El_Exelio',
+	version="0.0.0.1",
+	author="Nikita_Khalitov",
+	author_email="nik1020031.nik@gmail.com",
+	description="A simple module will allow you to create Excel spreadsheets.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/TheYoungEngineers/SulfTaper",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3.8",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	# Требуемая версия Python.
+	python_requires='>=3.8'
+)
