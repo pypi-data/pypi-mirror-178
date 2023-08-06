08d7826f5163cf484f7e788a077fa8b8fa3c95e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:46 +0000

    Add package ciqreviews/0.1.1/ciqreviews-0.1.1.tar.gz

---
+++ packages/ciqreviews/ciqreviews-0.1.1.tar.gz/ciqreviews-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.1'
+DESCRIPTION = 'This package is used to analyse the downloads,reviews,ratings of some Garmin Connect IQ App'
+LONG_DESCRIPTION = 'Using Beautifulsoup4 to find the download and reviews statistics of some app'
+
+# 配置
+setup(
+    name="ciqreviews",
+    version=VERSION,
+    author="li2niu",
+    author_email="<chuanyi.work@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['connectiq', 'garmin', 'ciq'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
