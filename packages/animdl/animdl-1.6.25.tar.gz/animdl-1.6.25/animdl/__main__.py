2403b05a7d5b14cb1295e7df49ca4fcced5900b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:17 +0000

    Add package animdl/1.6.25/animdl-1.6.25.tar.gz

---
+++ packages/animdl/animdl-1.6.25.tar.gz/animdl-1.6.25/animdl/__main__.py
@@ -0,0 +1,32 @@
+"""
+A one cli for all the anime.
+"""
+
+import click
+
+from .core import __version__
+from .core.cli.commands import download, grab, schedule, search, stream, update
+from .core.cli.helpers.player import supported_streamers
+
+commands = {
+    "download": download.animdl_download,
+    "grab": grab.animdl_grab,
+    "schedule": schedule.animdl_schedule,
+    "search": search.animdl_search,
+    "update": update.animdl_update,
+}
+
+executable = list(supported_streamers())
+
+if executable:
+    commands.update({"stream": stream.animdl_stream})
+
+
+@click.group(commands=commands)
+@click.version_option(__version__.__core__, "-v")
+def __animdl_cli__():
+    pass
+
+
+if __name__ == "__main__":
+    __animdl_cli__()
