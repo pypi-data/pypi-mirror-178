419573021d1cb58f187fe628503659f2e15a9556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:41 +0000

    Add package smudge/1.0.0/smudge-1.0.0.tar.gz

---
+++ packages/smudge/smudge-1.0.0.tar.gz/smudge-1.0.0/setup.py
@@ -0,0 +1,35 @@
+"""
+SMUDGE setup.py
+"""
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='smudge',
+    version='1.0.0',
+    description='Passive OS detection with dynamic signatures.',
+    py_modules=["smudge"],
+    package_dir={'': 'smudge'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System :: Networking :: Monitoring",
+        "Natural Language :: English",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
+    install_requires=[
+        "scapy ~= 2.4.5",
+    ],
+    extras_require = {
+        "dev": [
+            "pylint>=2.15.0",
+        ],
+    },
+    url="https://github.com/activecm/smudge",
+    author="Dave Quartarolo",
+    author_email="david@activecountermeasures.com",
+)
