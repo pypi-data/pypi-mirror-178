a711884ab0de3ddc3762d2938e6c722e4018acd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:41 +0000

    Add package asvc/0.0.1/asvc-0.0.1.tar.gz

---
+++ packages/asvc/asvc-0.0.1.tar.gz/asvc-0.0.1/README.md
@@ -0,0 +1,100 @@
+# aio-services
+![CI](https://github.com/RaRhAeu/aio-services/workflows/CI/badge.svg)
+![Build](https://github.com/RaRhAeu/aio-services/workflows/build/badge.svg)
+![Code Coverage](https://codecov.io/gh/RaRhAeu/aio-services/branch/main/graph/badge.svg)
+![License](https://img.shields.io/github/license/RaRhAeu/aio-services)
+![Python](https://img.shields.io/pypi/pyversions/aio-services)
+![Format](https://img.shields.io/pypi/format/aio-services)
+![PyPi Status](https://img.shields.io/pypi/status/aio-services)
+
+*Event driven microservice framework for python*
+
+---
+
+## About
+
+The package utilizes `pydantic` as the only required dependency.
+For message format Cloud Events format is used.
+Service can be run as standalone processes, or included into starlette (e.g. FastAPI) applications.
+
+## Multiple broker support (in progress)
+
+- Stub (in memory using `asyncio.Queue` for PoC, local development and testing)
+- NATS (with JetStream)
+- Redis Pub/Sub
+- Kafka
+- Rabbitmq
+- Google Cloud PubSub
+
+## Optional Dependencies
+  - `cli` - `click` and `aiorun`
+  - `uvloop`
+  - broker of choice: `nats`, `kafka`, `rabbitmq`, `redis`, `pubsub`
+  - custom serializer `msgpack`, `orjson`
+  - `prometheus` - Metric exposure via `PrometheusMiddleware`
+  - `opentelemetry`
+
+
+## Motivation
+
+Python has many "worker-queue" libraries and frameworks, such as:
+    - Celery
+    - Dramatiq
+    - Huey
+    - arq
+
+However, those libraries don't provide a pub/sub pattern, useful for creating
+event driven and loosely coupled systems. Furthermore, the majority of those libraries
+do not support `asyncio`. This is why this project was born.
+
+## Basic usage
+
+
+```python
+
+import asyncio
+from aio_services import Service, CloudEvent
+from aio_services import Middleware
+from aio_services.backends.nats.broker import JetStreamBroker
+
+
+broker = JetStreamBroker(url="nats://localhost:4222")
+
+service = Service(name="example-service", broker=broker)
+
+
+class SendMessageMiddleware(Middleware):
+    async def after_service_start(self, broker, service: Service):
+        print(f"After service start, running with {broker}")
+        await asyncio.sleep(10)
+        for i in range(100):
+            await service.publish("test.topic", data={"counter": i})
+        print("Published event(s)")
+
+
+broker.add_middleware(SendMessageMiddleware())
+
+
+@service.subscribe("test.topic")
+async def example_run(message: CloudEvent):
+    print(f"Received Message {message.id} with data: {message.data}")
+
+```
+
+
+## Scaling
+
+Each message is load-balanced (depending on broker) between all service instances with the same `name`.
+To scale number of processes you can use containers (docker/k8s), [supervisor](http://supervisord.org/),
+or web server like gunicorn.
+
+
+## TODOS:
+
+- Automatic [Async Api](https://www.asyncapi.com/) docs generation from service definition.
+- More tests
+  - Integration tests with docker-compose and all backends
+- [OpenTelemetry](https://opentelemetry.io/) Middleware
+- More backends (zeromq, pulsar, solace?)
+- Pluggable logger interface (for third party integrations)
+- Docs + tutorials
---
+++ packages/asvc/asvc-0.0.1.tar.gz/asvc-0.0.1/setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['asvc',
+ 'asvc.backends',
+ 'asvc.backends.kafka',
+ 'asvc.backends.nats',
+ 'asvc.backends.rabbitmq',
+ 'asvc.backends.redis',
+ 'asvc.encoders',
+ 'asvc.middlewares',
+ 'asvc.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['async-timeout>=4.0.2,<5.0.0', 'pydantic>=1.9.1,<2.0.0']
+
+extras_require = \
+{'all': ['aiorun>=2022.4.1,<2023.0.0',
+         'click>=8.1.3,<9.0.0',
+         'orjson>=3.7.1,<4.0.0',
+         'ormsgpack>=1.2.3,<2.0.0',
+         'prometheus-client>=0.14.1,<0.15.0',
+         'opentelemetry-api>=1.13.0,<2.0.0',
+         'opentelemetry-sdk>=1.13.0,<2.0.0',
+         'nats-py>=2.1.3,<3.0.0',
+         'aioredis>=2.0.1,<3.0.0',
+         'aiokafka>=0.7.2,<0.8.0',
+         'aio-pika>=8.0.3,<9.0.0',
+         'gcloud-aio-pubsub>=5.0.0,<6.0.0'],
+ 'cli': ['aiorun>=2022.4.1,<2023.0.0', 'click>=8.1.3,<9.0.0'],
+ 'kafka': ['aiokafka>=0.7.2,<0.8.0'],
+ 'msgpack': ['ormsgpack>=1.2.3,<2.0.0'],
+ 'nats': ['nats-py>=2.1.3,<3.0.0'],
+ 'opentelemetry': ['opentelemetry-api>=1.13.0,<2.0.0',
+                   'opentelemetry-sdk>=1.13.0,<2.0.0'],
+ 'orjson': ['orjson>=3.7.1,<4.0.0'],
+ 'prometheus': ['prometheus-client>=0.14.1,<0.15.0'],
+ 'pubsub': ['gcloud-aio-pubsub>=5.0.0,<6.0.0'],
+ 'rabbitmq': ['aio-pika>=8.0.3,<9.0.0'],
+ 'redis': ['aioredis>=2.0.1,<3.0.0']}
+
+entry_points = \
+{'console_scripts': ['asvc = aio_services.cli:cli']}
+
+setup_kwargs = {
+    'name': 'asvc',
+    'version': '0.0.1',
+    'description': 'Event driven microservice framework for python',
+    'long_description': '# aio-services\n![CI](https://github.com/RaRhAeu/aio-services/workflows/CI/badge.svg)\n![Build](https://github.com/RaRhAeu/aio-services/workflows/build/badge.svg)\n![Code Coverage](https://codecov.io/gh/RaRhAeu/aio-services/branch/main/graph/badge.svg)\n![License](https://img.shields.io/github/license/RaRhAeu/aio-services)\n![Python](https://img.shields.io/pypi/pyversions/aio-services)\n![Format](https://img.shields.io/pypi/format/aio-services)\n![PyPi Status](https://img.shields.io/pypi/status/aio-services)\n\n*Event driven microservice framework for python*\n\n---\n\n## About\n\nThe package utilizes `pydantic` as the only required dependency.\nFor message format Cloud Events format is used.\nService can be run as standalone processes, or included into starlette (e.g. FastAPI) applications.\n\n## Multiple broker support (in progress)\n\n- Stub (in memory using `asyncio.Queue` for PoC, local development and testing)\n- NATS (with JetStream)\n- Redis Pub/Sub\n- Kafka\n- Rabbitmq\n- Google Cloud PubSub\n\n## Optional Dependencies\n  - `cli` - `click` and `aiorun`\n  - `uvloop`\n  - broker of choice: `nats`, `kafka`, `rabbitmq`, `redis`, `pubsub`\n  - custom serializer `msgpack`, `orjson`\n  - `prometheus` - Metric exposure via `PrometheusMiddleware`\n  - `opentelemetry`\n\n\n## Motivation\n\nPython has many "worker-queue" libraries and frameworks, such as:\n    - Celery\n    - Dramatiq\n    - Huey\n    - arq\n\nHowever, those libraries don\'t provide a pub/sub pattern, useful for creating\nevent driven and loosely coupled systems. Furthermore, the majority of those libraries\ndo not support `asyncio`. This is why this project was born.\n\n## Basic usage\n\n\n```python\n\nimport asyncio\nfrom aio_services import Service, CloudEvent\nfrom aio_services import Middleware\nfrom aio_services.backends.nats.broker import JetStreamBroker\n\n\nbroker = JetStreamBroker(url="nats://localhost:4222")\n\nservice = Service(name="example-service", broker=broker)\n\n\nclass SendMessageMiddleware(Middleware):\n    async def after_service_start(self, broker, service: Service):\n        print(f"After service start, running with {broker}")\n        await asyncio.sleep(10)\n        for i in range(100):\n            await service.publish("test.topic", data={"counter": i})\n        print("Published event(s)")\n\n\nbroker.add_middleware(SendMessageMiddleware())\n\n\n@service.subscribe("test.topic")\nasync def example_run(message: CloudEvent):\n    print(f"Received Message {message.id} with data: {message.data}")\n\n```\n\n\n## Scaling\n\nEach message is load-balanced (depending on broker) between all service instances with the same `name`.\nTo scale number of processes you can use containers (docker/k8s), [supervisor](http://supervisord.org/),\nor web server like gunicorn.\n\n\n## TODOS:\n\n- Automatic [Async Api](https://www.asyncapi.com/) docs generation from service definition.\n- More tests\n  - Integration tests with docker-compose and all backends\n- [OpenTelemetry](https://opentelemetry.io/) Middleware\n- More backends (zeromq, pulsar, solace?)\n- Pluggable logger interface (for third party integrations)\n- Docs + tutorials',
+    'author': 'Radzim Kowalow',
+    'author_email': 'radzim_ko@wp.pl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
