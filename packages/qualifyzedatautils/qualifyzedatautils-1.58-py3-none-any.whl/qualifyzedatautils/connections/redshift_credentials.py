468030d2e3077bd0f749bd568dc03fafdda9206a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:28 +0000

    Add package qualifyzedatautils/1.58/qualifyzedatautils-1.58-py3-none-any.whl

---
+++ packages/qualifyzedatautils/qualifyzedatautils-1.58-py3-none-any.whl/qualifyzedatautils/connections/redshift_credentials.py
@@ -0,0 +1,16 @@
+
+import redshift_connector
+from ._decode_secret import decode_secret
+from ._retrieve_awssecret import retrieve_awssecret
+
+def initialize_redshift_credentials():
+    redshift_secret = retrieve_awssecret("redshiftcluster_credentials", "eu-central-1")
+    redshift_decoded_secret = decode_secret(redshift_secret)
+    redshift_credentials = redshift_decoded_secret
+
+    return redshift_credentials
+
+
+print('loaded')
+
+
