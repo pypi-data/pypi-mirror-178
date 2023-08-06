123ccc001c86bf8a17a44b34d1efeedb060802be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:03 +0000

    Add package simplesheetsapi/0.0.4/simplesheetsapi-0.0.4.tar.gz

---
+++ packages/simplesheetsapi/simplesheetsapi-0.0.4.tar.gz/simplesheetsapi-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='simplesheetsapi',
+    version='0.0.4',
+    description='Simple lib to work with google sheets api',
+    long_description="Can read, write and find in spreadsheets",
+    url='',
+    author='Sheets Try',
+    author_email='sheetsapitry@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='spreadsheets',
+    packages=find_packages(),
+    install_requires=["setuptools>=42", "wheel", 'google-api-core', "google-api-python-client", "google-auth", "google-auth-httplib2", "google-auth-oauthlib", "googleapis-common-protos"]
+)
