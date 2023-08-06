e6d31fcaa9250c31626aa6a9cd4aebf2b409550c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:25 +0000

    Add package pyaon/1.0.0/pyaon-1.0.0-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyaon/pyaon-1.0.0-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyaon.pyi
@@ -0,0 +1,288 @@
+"""Interact with the aon (Andrezao Object Notation) file format"""
+import pyaon
+import typing
+
+__all__ = [
+    "dump",
+    "load",
+    "vector_float",
+    "vector_int",
+    "vector_string"
+]
+
+
+class vector_float():
+    def __bool__(self) -> bool: 
+        """
+        Check whether the list is nonempty
+        """
+    def __contains__(self, x: float) -> bool: 
+        """
+        Return true the container contains ``x``
+        """
+    @typing.overload
+    def __delitem__(self, arg0: int) -> None: 
+        """
+        Delete the list elements at index ``i``
+
+        Delete list elements using a slice object
+        """
+    @typing.overload
+    def __delitem__(self, arg0: slice) -> None: ...
+    def __eq__(self, arg0: vector_float) -> bool: ...
+    @typing.overload
+    def __getitem__(self, arg0: int) -> float: 
+        """
+        Retrieve list elements using a slice object
+        """
+    @typing.overload
+    def __getitem__(self, s: slice) -> vector_float: ...
+    @typing.overload
+    def __init__(self) -> None: 
+        """
+        Copy constructor
+        """
+    @typing.overload
+    def __init__(self, arg0: typing.Iterable) -> None: ...
+    @typing.overload
+    def __init__(self, arg0: vector_float) -> None: ...
+    def __iter__(self) -> typing.Iterator: ...
+    def __len__(self) -> int: ...
+    def __ne__(self, arg0: vector_float) -> bool: ...
+    def __repr__(self) -> str: 
+        """
+        Return the canonical string representation of this list.
+        """
+    @typing.overload
+    def __setitem__(self, arg0: int, arg1: float) -> None: 
+        """
+        Assign list elements using a slice object
+        """
+    @typing.overload
+    def __setitem__(self, arg0: slice, arg1: vector_float) -> None: ...
+    def append(self, x: float) -> None: 
+        """
+        Add an item to the end of the list
+        """
+    def clear(self) -> None: 
+        """
+        Clear the contents
+        """
+    def count(self, x: float) -> int: 
+        """
+        Return the number of times ``x`` appears in the list
+        """
+    @typing.overload
+    def extend(self, L: typing.Iterable) -> None: 
+        """
+        Extend the list by appending all the items in the given list
+
+        Extend the list by appending all the items in the given list
+        """
+    @typing.overload
+    def extend(self, L: vector_float) -> None: ...
+    def insert(self, i: int, x: float) -> None: 
+        """
+        Insert an item at a given position.
+        """
+    @typing.overload
+    def pop(self) -> float: 
+        """
+        Remove and return the last item
+
+        Remove and return the item at index ``i``
+        """
+    @typing.overload
+    def pop(self, i: int) -> float: ...
+    def remove(self, x: float) -> None: 
+        """
+        Remove the first item from the list whose value is x. It is an error if there is no such item.
+        """
+    __hash__ = None
+    pass
+class vector_int():
+    def __bool__(self) -> bool: 
+        """
+        Check whether the list is nonempty
+        """
+    def __contains__(self, x: int) -> bool: 
+        """
+        Return true the container contains ``x``
+        """
+    @typing.overload
+    def __delitem__(self, arg0: int) -> None: 
+        """
+        Delete the list elements at index ``i``
+
+        Delete list elements using a slice object
+        """
+    @typing.overload
+    def __delitem__(self, arg0: slice) -> None: ...
+    def __eq__(self, arg0: vector_int) -> bool: ...
+    @typing.overload
+    def __getitem__(self, arg0: int) -> int: 
+        """
+        Retrieve list elements using a slice object
+        """
+    @typing.overload
+    def __getitem__(self, s: slice) -> vector_int: ...
+    @typing.overload
+    def __init__(self) -> None: 
+        """
+        Copy constructor
+        """
+    @typing.overload
+    def __init__(self, arg0: typing.Iterable) -> None: ...
+    @typing.overload
+    def __init__(self, arg0: vector_int) -> None: ...
+    def __iter__(self) -> typing.Iterator: ...
+    def __len__(self) -> int: ...
+    def __ne__(self, arg0: vector_int) -> bool: ...
+    def __repr__(self) -> str: 
+        """
+        Return the canonical string representation of this list.
+        """
+    @typing.overload
+    def __setitem__(self, arg0: int, arg1: int) -> None: 
+        """
+        Assign list elements using a slice object
+        """
+    @typing.overload
+    def __setitem__(self, arg0: slice, arg1: vector_int) -> None: ...
+    def append(self, x: int) -> None: 
+        """
+        Add an item to the end of the list
+        """
+    def clear(self) -> None: 
+        """
+        Clear the contents
+        """
+    def count(self, x: int) -> int: 
+        """
+        Return the number of times ``x`` appears in the list
+        """
+    @typing.overload
+    def extend(self, L: typing.Iterable) -> None: 
+        """
+        Extend the list by appending all the items in the given list
+
+        Extend the list by appending all the items in the given list
+        """
+    @typing.overload
+    def extend(self, L: vector_int) -> None: ...
+    def insert(self, i: int, x: int) -> None: 
+        """
+        Insert an item at a given position.
+        """
+    @typing.overload
+    def pop(self) -> int: 
+        """
+        Remove and return the last item
+
+        Remove and return the item at index ``i``
+        """
+    @typing.overload
+    def pop(self, i: int) -> int: ...
+    def remove(self, x: int) -> None: 
+        """
+        Remove the first item from the list whose value is x. It is an error if there is no such item.
+        """
+    __hash__ = None
+    pass
+class vector_string():
+    def __bool__(self) -> bool: 
+        """
+        Check whether the list is nonempty
+        """
+    def __contains__(self, x: str) -> bool: 
+        """
+        Return true the container contains ``x``
+        """
+    @typing.overload
+    def __delitem__(self, arg0: int) -> None: 
+        """
+        Delete the list elements at index ``i``
+
+        Delete list elements using a slice object
+        """
+    @typing.overload
+    def __delitem__(self, arg0: slice) -> None: ...
+    def __eq__(self, arg0: vector_string) -> bool: ...
+    @typing.overload
+    def __getitem__(self, arg0: int) -> str: 
+        """
+        Retrieve list elements using a slice object
+        """
+    @typing.overload
+    def __getitem__(self, s: slice) -> vector_string: ...
+    @typing.overload
+    def __init__(self) -> None: 
+        """
+        Copy constructor
+        """
+    @typing.overload
+    def __init__(self, arg0: typing.Iterable) -> None: ...
+    @typing.overload
+    def __init__(self, arg0: vector_string) -> None: ...
+    def __iter__(self) -> typing.Iterator: ...
+    def __len__(self) -> int: ...
+    def __ne__(self, arg0: vector_string) -> bool: ...
+    def __repr__(self) -> str: 
+        """
+        Return the canonical string representation of this list.
+        """
+    @typing.overload
+    def __setitem__(self, arg0: int, arg1: str) -> None: 
+        """
+        Assign list elements using a slice object
+        """
+    @typing.overload
+    def __setitem__(self, arg0: slice, arg1: vector_string) -> None: ...
+    def append(self, x: str) -> None: 
+        """
+        Add an item to the end of the list
+        """
+    def clear(self) -> None: 
+        """
+        Clear the contents
+        """
+    def count(self, x: str) -> int: 
+        """
+        Return the number of times ``x`` appears in the list
+        """
+    @typing.overload
+    def extend(self, L: typing.Iterable) -> None: 
+        """
+        Extend the list by appending all the items in the given list
+
+        Extend the list by appending all the items in the given list
+        """
+    @typing.overload
+    def extend(self, L: vector_string) -> None: ...
+    def insert(self, i: int, x: str) -> None: 
+        """
+        Insert an item at a given position.
+        """
+    @typing.overload
+    def pop(self) -> str: 
+        """
+        Remove and return the last item
+
+        Remove and return the item at index ``i``
+        """
+    @typing.overload
+    def pop(self, i: int) -> str: ...
+    def remove(self, x: str) -> None: 
+        """
+        Remove the first item from the list whose value is x. It is an error if there is no such item.
+        """
+    __hash__ = None
+    pass
+def dump(arg0: std::vector<int, std::allocator<int> >, arg1: str) -> None:
+    """
+    Dump a vector of integers into an aon file
+    """
+def load(arg0: str) -> std::vector<int, std::allocator<int> >:
+    """
+    Load an aon file into a vector of integers
+    """
