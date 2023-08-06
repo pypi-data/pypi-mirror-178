fc6f4f85c6a567e533c0a0ea76fec1cf06d02e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:29 +0000

    Add package ditekmax/0.0.2/ditekmax-0.0.2.tar.gz

---
+++ packages/ditekmax/ditekmax-0.0.2.tar.gz/ditekmax-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.2'
+DESCRIPTION = 'PY LAB'
+
+
+# Setting up
+setup(
+    name="ditekmax",
+    version=VERSION,
+    author="DITEK",
+    author_email="<maks.ditkovskiy.05@mail.ru>",
+    description=DESCRIPTION,
+  
+    packages=['ditekmax'],
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
