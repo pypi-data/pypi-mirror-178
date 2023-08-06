6a30e344869071a42279c0d7c7a838a423ad5694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:55 +0000

    Add package oarepo-model-builder/1.0.0.dev44/oarepo-model-builder-1.0.0.dev44.tar.gz

---
+++ packages/oarepo-model-builder/oarepo-model-builder-1.0.0.dev44.tar.gz/oarepo-model-builder-1.0.0.dev44/oarepo_model_builder/model_preprocessors/elasticsearch.py
@@ -0,0 +1,17 @@
+from oarepo_model_builder.model_preprocessors import ModelPreprocessor
+from oarepo_model_builder.utils.deepmerge import deepmerge
+
+
+class ElasticsearchModelPreprocessor(ModelPreprocessor):
+    TYPE = "elasticsearch"
+
+    def transform(self, schema, settings):
+        deepmerge(
+            settings,
+            {
+                "elasticsearch": {
+                    "version": "v7",
+                    "templates": {"v7": {"mappings": {"properties": {}}}},
+                }
+            },
+        )
