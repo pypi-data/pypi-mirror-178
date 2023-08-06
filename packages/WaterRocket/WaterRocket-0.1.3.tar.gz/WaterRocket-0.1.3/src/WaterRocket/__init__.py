e5372a76aab458d9efe8a012bab073d57f638361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:33 +0000

    Add package WaterRocket/0.1.3/WaterRocket-0.1.3.tar.gz

---
+++ packages/WaterRocket/WaterRocket-0.1.3.tar.gz/WaterRocket-0.1.3/src/WaterRocket/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "Mohamed Nennouche"
+__copyright__ = "Copyright 20XX, WaterRocketPy Team"
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Mohamed Nennouche"
+__email__ = "moohaaameed.nennouche@gmail.com"
+__status__ = "Production"
+
+from .waterRocket import WaterRocket
