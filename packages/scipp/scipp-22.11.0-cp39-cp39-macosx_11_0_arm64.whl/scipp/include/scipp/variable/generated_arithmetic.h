ac8b3c7ecc3bfba8e12c95ae499287089a28818c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:22:27 +0000

    Add package scipp/22.11.0/scipp-22.11.0-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/scipp/scipp-22.11.0-cp39-cp39-macosx_11_0_arm64.whl/scipp/include/scipp/variable/generated_arithmetic.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#include "scipp/variable/negative.h"
+#include "scipp/variable/floor_divide.h"
+#include "scipp/variable/mod.h"
+#include "scipp/variable/mod_equals.h"
---
+++ packages/scipp/scipp-22.11.0-cp39-cp39-macosx_11_0_arm64.whl/scipp/include/scipp/variable/mod_equals.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#include "scipp-variable_export.h"
+#include "scipp/variable/variable.h"
+
+namespace scipp::variable {
+
+SCIPP_VARIABLE_EXPORT Variable &operator%=(Variable &lhs, const Variable &rhs);
+SCIPP_VARIABLE_EXPORT Variable operator%=(Variable &&lhs, const Variable &rhs);
+
+} // namespace scipp::variable
