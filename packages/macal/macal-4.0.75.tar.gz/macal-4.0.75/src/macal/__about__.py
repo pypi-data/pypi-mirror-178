87d6441c733c300bcf2c673cb221783785b9ecd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:43 +0000

    Add package macal/4.0.75/macal-4.0.75.tar.gz

---
+++ packages/macal/macal-4.0.75.tar.gz/macal-4.0.75/src/macal/__about__.py
@@ -0,0 +1,11 @@
+#
+# Product:   Macal
+# Author:    Marco Caspers
+# Date:      07-09-2022
+#
+
+__version__ = '4.0.74'
+__author__ = 'Marco Caspers'
+__author_email__ = 'marco.caspers@westcon.com'
+__credits__      = """Source used:
+   "The Dragon Book": Compilers: Principles, Techniques, and Tools"""
