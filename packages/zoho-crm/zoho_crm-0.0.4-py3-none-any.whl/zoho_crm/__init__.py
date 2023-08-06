d1b962c493dbe2662332de96b09e96f6ad2a9123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:43 +0000

    Add package zoho-crm/0.0.4/zoho_crm-0.0.4-py3-none-any.whl

---
+++ packages/zoho-crm/zoho_crm-0.0.4-py3-none-any.whl/zoho_crm/__init__.py
@@ -0,0 +1,9 @@
+"Unoficial Zoho CRN API client"
+
+__version__ = "0.0.4"
+
+from .api_client import ZohoAPIConfig as ZohoAPIConfig
+from .api_client import ZohoAuthConfig as ZohoAuthConfig
+from .api_client import ZohoClient as ZohoClient
+from .api_client import ZohoClientException as ZohoClientException
+from .api_client import ZohoEndpoint as ZohoEndpoint
