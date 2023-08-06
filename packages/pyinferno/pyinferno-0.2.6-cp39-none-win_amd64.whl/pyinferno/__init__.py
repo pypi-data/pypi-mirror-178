e59f0b5a9b960bf0ae671711c264d46df62cf76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:21 +0000

    Add package pyinferno/0.2.6/pyinferno-0.2.6-cp39-none-win_amd64.whl

---
+++ packages/pyinferno/pyinferno-0.2.6-cp39-none-win_amd64.whl/pyinferno/__init__.py
@@ -0,0 +1,7 @@
+from .pyinferno import InfernoError, flamegraph_from_lines
+from .renderer import InfernoRenderer as Renderer
+from .profiler import InfernoProfiler
+
+InfernoRenderer = Renderer
+
+__version__ = "0.2.6"
