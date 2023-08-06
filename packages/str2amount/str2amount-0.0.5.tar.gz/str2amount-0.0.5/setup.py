03311001892efae84838e0143b0b3cea81ac31e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:58 +0000

    Add package str2amount/0.0.5/str2amount-0.0.5.tar.gz

---
+++ packages/str2amount/str2amount-0.0.5.tar.gz/str2amount-0.0.5/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+packages=find_packages()
+print(packages)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+#     long_description = "\n" + fh.read()
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Convert String to Amount'
+LONG_DESCRIPTION = 'Will udpate later'
+
+# Setting up
+setup(
+    name="str2amount",
+    version=VERSION,
+    author="Nazmus Sakib",
+    author_email="<sakibkuet16@neuralnine.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    
+    install_requires=[],
+    keywords=['python', 'currency','amount'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
