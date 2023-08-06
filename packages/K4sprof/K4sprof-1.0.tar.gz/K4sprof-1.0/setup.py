ad90004c0a2567b00eb9ac30e11c154653b840d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:54 +0000

    Add package K4sprof/1.0/K4sprof-1.0.tar.gz

---
+++ packages/K4sprof/K4sprof-1.0.tar.gz/K4sprof-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='K4sprof',
+    version='v1.0',
+    description='for profile',
+    py_modules=['K4sprof'],
+    author='upuuuuuu',
+    author_email='2226548059@qq.com',
+    url='https://github.com/upuuuuuu',
+    requires=['os', 'time', 'torch', 'logging', 'pandas', 'torchstat'],
+    license='MIT'
+)
