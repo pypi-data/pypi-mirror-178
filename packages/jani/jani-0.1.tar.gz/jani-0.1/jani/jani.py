e49d51ae933670a1e2ed69143ce3ecdf1e3d1426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:49 +0000

    Add package jani/0.1/jani-0.1.tar.gz

---
+++ packages/jani/jani-0.1.tar.gz/jani-0.1/jani/jani.py
@@ -0,0 +1,183 @@
+
+class Jani:
+
+    def dataProcessing(self, dataFrame, job, arrivalTime, burstTime,timeSlice):
+        indexJob = dataFrame.columns.get_loc(job)
+        dataFrame[job] = dataFrame.iloc[:, indexJob].apply(lambda x: ord(x))
+        dataFrame = dataFrame.loc[:, [job, arrivalTime, burstTime]]
+        dataFrame = dataFrame.sort_values(by=dataFrame.columns[dataFrame.columns.get_loc(arrivalTime)])
+        indexJob2 = dataFrame.columns.get_loc(job)
+        dataFrame[job] = dataFrame.iloc[:, indexJob2].apply(lambda x: chr(x))
+        processedData = []
+        for i in range(dataFrame.shape[0]):
+            temp = []
+            job_id = dataFrame.iloc[i, dataFrame.columns.get_loc(job)]
+
+            arrival_time = int(dataFrame.iloc[i, dataFrame.columns.get_loc(arrivalTime)])
+
+            burst_time = int(dataFrame.iloc[i, dataFrame.columns.get_loc(burstTime)])
+
+            temp.extend([job_id, arrival_time, burst_time, 0, burst_time])
+          # 0 means process not executed 1 means process executed
+            processedData.append(temp)
+            print(f'Process {i}: {dataFrame.iloc[i, 0]}')
+        print(processedData)
+        Jani.schedulingProcess(self, processedData, timeSlice)
+
+    def schedulingProcess(self, process_data, time_slice=2):
+        start_time = []
+        exit_time = []
+        executed_process = []
+        ready_queue = []
+        s_time = 0
+        process_data.sort(key=lambda x: x[1])
+        '''
+        Sort processes according to the Arrival Time
+        '''
+        while 1:
+            normal_queue = []
+            temp = []
+            for i in range(len(process_data)):
+                if process_data[i][1] <= s_time and process_data[i][3] == 0:
+                    present = 0
+                    if len(ready_queue) != 0:
+                        for k in range(len(ready_queue)):
+                            if process_data[i][0] == ready_queue[k][0]:
+                                present = 1
+                    '''
+                    The above if loop checks that the next process is not a part of ready_queue
+                    '''
+                    if present == 0:
+                        temp.extend([process_data[i][0], process_data[i][1], process_data[i][2], process_data[i][4]])
+                        ready_queue.append(temp)
+                        temp = []
+                    '''
+                    The above if loop adds a process to the ready_queue only if it is not already present in it
+                    '''
+                    if len(ready_queue) != 0 and len(executed_process) != 0:
+                        for k in range(len(ready_queue)):
+                            if ready_queue[k][0] == executed_process[len(executed_process) - 1]:
+                                ready_queue.insert((len(ready_queue) - 1), ready_queue.pop(k))
+                    '''
+                    The above if loop makes sure that the recently executed process is appended at the end of ready_queue
+                    '''
+                elif process_data[i][3] == 0:
+                    temp.extend([process_data[i][0], process_data[i][1], process_data[i][2], process_data[i][4]])
+                    normal_queue.append(temp)
+                    temp = []
+            if len(ready_queue) == 0 and len(normal_queue) == 0:
+                break
+            if len(ready_queue) != 0:
+                if ready_queue[0][2] > time_slice:
+                    '''
+                    If process has remaining burst time greater than the time slice, it will execute for a time period equal to time slice and then switch
+                    '''
+                    start_time.append(s_time)
+                    s_time = s_time + time_slice
+                    e_time = s_time
+                    exit_time.append(e_time)
+                    executed_process.append(ready_queue[0][0])
+                    for j in range(len(process_data)):
+                        if process_data[j][0] == ready_queue[0][0]:
+                            break
+                    process_data[j][2] = process_data[j][2] - time_slice
+                    ready_queue.pop(0)
+                elif ready_queue[0][2] <= time_slice:
+                    '''
+                    If a process has a remaining burst time less than or equal to time slice, it will complete its execution
+                    '''
+                    start_time.append(s_time)
+                    s_time = s_time + ready_queue[0][2]
+                    e_time = s_time
+                    exit_time.append(e_time)
+                    executed_process.append(ready_queue[0][0])
+                    for j in range(len(process_data)):
+                        if process_data[j][0] == ready_queue[0][0]:
+                            break
+                    process_data[j][2] = 0
+                    process_data[j][3] = 1
+                    process_data[j].append(e_time)
+                    ready_queue.pop(0)
+            elif len(ready_queue) == 0:
+                if s_time < normal_queue[0][1]:
+                    s_time = normal_queue[0][1]
+                if normal_queue[0][2] > time_slice:
+                    '''
+                    If process has remaining burst time greater than the time slice, it will execute for a time period equal to time slice and then switch
+                    '''
+                    start_time.append(s_time)
+                    s_time = s_time + time_slice
+                    e_time = s_time
+                    exit_time.append(e_time)
+                    executed_process.append(normal_queue[0][0])
+                    for j in range(len(process_data)):
+                        if process_data[j][0] == normal_queue[0][0]:
+                            break
+                    process_data[j][2] = process_data[j][2] - time_slice
+                elif normal_queue[0][2] <= time_slice:
+                    '''
+                    If a process has a remaining burst time less than or equal to time slice, it will complete its execution
+                    '''
+                    start_time.append(s_time)
+                    s_time = s_time + normal_queue[0][2]
+                    e_time = s_time
+                    exit_time.append(e_time)
+                    executed_process.append(normal_queue[0][0])
+                    for j in range(len(process_data)):
+                        if process_data[j][0] == normal_queue[0][0]:
+                            break
+                    process_data[j][2] = 0
+                    process_data[j][3] = 1
+                    process_data[j].append(e_time)
+        t_time = Jani.calculateTurnaroundTime(self, process_data)
+        w_time = Jani.calculateWaitingTime(self, process_data)
+        Jani.printData(self, process_data, t_time, w_time, executed_process)
+
+    def calculateTurnaroundTime(self, process_data):
+        total_turnaround_time = 0
+        for i in range(len(process_data)):
+            turnaround_time = process_data[i][5] - process_data[i][1]
+            '''
+            turnaround_time = completion_time - arrival_time
+            '''
+            total_turnaround_time = total_turnaround_time + turnaround_time
+            process_data[i].append(turnaround_time)
+        average_turnaround_time = total_turnaround_time / len(process_data)
+        '''
+        average_turnaround_time = total_turnaround_time / no_of_processes
+        '''
+        return average_turnaround_time
+
+    def calculateWaitingTime(self, process_data):
+        total_waiting_time = 0
+        for i in range(len(process_data)):
+            waiting_time = process_data[i][6] - process_data[i][4]
+            '''
+            waiting_time = turnaround_time - burst_time
+            '''
+            total_waiting_time = total_waiting_time + waiting_time
+            process_data[i].append(waiting_time)
+        average_waiting_time = total_waiting_time / len(process_data)
+        '''
+        average_waiting_time = total_waiting_time / no_of_processes
+        '''
+        return average_waiting_time
+
+    def printData(self, process_data, average_turnaround_time, average_waiting_time, executed_process):
+        process_data.sort(key=lambda x: x[0])
+        '''
+        Sort processes according to the Process ID
+        '''
+        print(
+            "Process_ID  Arrival_Time  Rem_Burst_Time   Completed  Original_Burst_Time  Completion_Time  Turnaround_Time  Waiting_Time")
+        for i in range(len(process_data)):
+            for j in range(len(process_data[i])):
+                print(process_data[i][j], end="				")
+            print()
+
+        print(f'Average Turnaround Time: {average_turnaround_time}')
+
+        print(f'Average Waiting Time: {average_waiting_time}')
+
+        print(f'Sequence of Processes: {executed_process}')
+
