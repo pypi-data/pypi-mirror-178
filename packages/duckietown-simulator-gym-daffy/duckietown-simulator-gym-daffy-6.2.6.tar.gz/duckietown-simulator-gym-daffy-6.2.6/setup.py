9c2bea8b74305ca3686d23177659e5b3ca81f5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:43 +0000

    Add package duckietown-simulator-gym-daffy/6.2.6/duckietown-simulator-gym-daffy-6.2.6.tar.gz

---
+++ packages/duckietown-simulator-gym-daffy/duckietown-simulator-gym-daffy-6.2.6.tar.gz/duckietown-simulator-gym-daffy-6.2.6/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+
+def get_version(filename: str):
+    import ast
+
+    version = None
+    with open(filename) as f:
+        for line in f:
+            if line.startswith("__version__"):
+                version = ast.parse(line).body[0].value.s
+                break
+        else:
+            raise ValueError("No version found in %r." % filename)
+    if version is None:
+        raise ValueError(filename)
+    return version
+
+
+version = get_version(filename="src/duckietown_simulator_gym/__init__.py")
+
+line = "daffy"
+install_requires = [
+    f"duckietown-gym-{line}",
+    f"aido-agents-{line}",
+    f"aido-protocols-{line}",
+    "PyGeometry-z6",
+    "zuper-nodes-z6",
+    "zuper-commons-z6",
+    "opencv-python",
+    "PyYAML",
+    "numpy",
+    f"duckietown-world-{line}",
+    f"duckietown-utils-{line}",
+]
+
+setup(
+    name=f"duckietown-simulator-gym-{line}",
+    version=version,
+    keywords="",
+    package_dir={"": "src"},
+    packages=["duckietown_simulator_gym"],
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [],
+    },
+)
