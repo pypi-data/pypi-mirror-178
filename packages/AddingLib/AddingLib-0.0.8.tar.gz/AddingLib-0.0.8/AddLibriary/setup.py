3813a28c88e75680937b51d00586302511e39d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:00 +0000

    Add package AddingLib/0.0.8/AddingLib-0.0.8.tar.gz

---
+++ packages/AddingLib/AddingLib-0.0.8.tar.gz/AddingLib-0.0.8/AddLibriary/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='resources',
+    version='0.0.8',
+    package_dir={'fonts': 'fonts','images':'images','js':'js','maps':'maps','qml':'qml','sounds':'sounds','tr':'tr'},
+    packages=['fonts','images','js','maps','qml','sounds','tr'],
+    
+)
