2502b34d77d00ff8b534136b47adc51ef4127d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:03 +0000

    Add package pycdi/0.1.dev1669730582/pycdi-0.1.dev1669730582.tar.gz

---
+++ packages/pycdi/pycdi-0.1.dev1669730582.tar.gz/pycdi-0.1.dev1669730582/setup.py
@@ -0,0 +1,43 @@
+__version__ = '0.1.dev1669730582'
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+
+setup(
+    name='pycdi',
+    version=__version__,
+    description='Python Code Dependency Injection',
+    long_description=read('README.rst'),
+    url='https://github.com/ettoreleandrotognoli/python-cdi',
+    download_url='https://github.com/ettoreleandrotognoli/python-cdi/tree/%s/' % __version__,
+    license='BSD',
+    author='Éttore Leandro Tognoli',
+    author_email='ettore.leandro.tognoli@gmail.com',
+    packages=find_packages(exclude=['tests', 'examples']),
+    include_package_data=True,
+    keywords=[
+        'cdi',
+        'di',
+        'code dependency injection',
+        'dependency injection'
+    ],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: BSD License',
+    ],
+    install_requires=[
+    ],
+    tests_require=[
+    ],
+)
