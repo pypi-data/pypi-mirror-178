8eaf8ea57af1eea64382ab9b17a19aaecbaac73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:03 +0000

    Add package dsocli/1.0.196.dev0/dsocli-1.0.196.dev0.tar.gz

---
+++ packages/dsocli/dsocli-1.0.196.dev0.tar.gz/dsocli-1.0.196.dev0/src/dsocli/provider/parameter/aws/ssm/v1/main.py
@@ -0,0 +1,180 @@
+import boto3
+from dsocli.exceptions import DSOException
+from dsocli.logger import Logger
+from dsocli.providers import Providers
+from dsocli.parameters import ParameterProvider
+from dsocli.constants import *
+from dsocli.aws_ssm_utils import *
+from dsocli.configs import Config, ContextMode
+
+
+__default_spec = {
+    'pathPrefix': '/dso/parameter/',
+}
+
+def get_default_spec():
+    return __default_spec.copy()
+
+
+class AwsSsmParameterProvider(ParameterProvider):
+
+    def __init__(self):
+        super().__init__('parameter/aws/ssm/v1')
+
+
+    def get_path_prefix(self):
+        return Config.parameter_spec('pathPrefix')
+
+
+    def list(self, uninherited=False, filter=None):
+        Logger.debug(f"Listing SSM parameters: namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        parameters = load_context_ssm_parameters(parameter_type='String', path_prefix=self.get_path_prefix(), uninherited=uninherited, filter=filter)
+        result = []
+        for key, details in parameters.items():
+            item = {
+                'Key': key,
+                'RevisionId': str(details['Version']),
+            }
+            item.update(details)
+            result.append(item)
+
+        return result
+
+    def edit(self, key):
+        Logger.debug(f"Editing SSM parameter: namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        parameters = load_context_ssm_parameters(parameter_type='String', path_prefix=self.get_path_prefix(), uninherited=True, filter=f"^{key}$")
+        if len(parameters) > 1:
+            raise DSOException(f"Mutiple parameters found with the same key in the given context.")
+        result = {}
+        if parameters:
+            result['Key'] = list(parameters.keys())[0]
+            result.update(list(parameters.values())[0])
+
+        return result
+
+    def get(self, key, revision=None, uninherited=False, rendered=True):
+        Logger.debug(f"Locating SSM parameter '{key}': namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        found = locate_ssm_parameter_in_context_hierachy(key=key, path_prefix=self.get_path_prefix(), uninherited=uninherited)
+        if not found:
+            raise DSOException(f"Parameter '{key}' not found in the given context: namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        else:
+            if not found['Type'] == 'String':
+                raise DSOException(f"Parameter '{key}' not found in the given context: namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        Logger.debug(f"Getting SSM parameter: path={found['Name']}")
+        response = get_ssm_parameter_history(found['Name'])
+        parameters = sorted(response['Parameters'], key=lambda x: int(x['Version']), reverse=True)
+        if revision is None:
+            ### get the latest revision
+            result = {
+                    'RevisionId': str(parameters[0]['Version']),
+                    'Date': parameters[0]['LastModifiedDate'].strftime('%Y/%m/%d-%H:%M:%S'),
+                    'Key': key, 
+                    'Value': parameters[0]['Value'],
+                    'Scope': found['Scope'],
+                    'Context': found['Context'],
+                    'Path': found['Name'],
+                    'User': parameters[0]['LastModifiedUser'],
+                    }
+                
+        else:
+            ### get specific revision
+            parameters = [x for x in parameters if str(x['Version']) == revision]
+            if not parameters:
+                raise DSOException(f"Revision '{revision}' not found for parameter '{key}' in the given context: namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+            result = {
+                    'RevisionId':str(parameters[0]['Version']),
+                    'Date': parameters[0]['LastModifiedDate'].strftime('%Y/%m/%d-%H:%M:%S'),
+                    'Key': key, 
+                    'Value': parameters[0]['Value'],
+                    'Scope': found['Scope'],
+                    'Context': found['Context'],
+                    'Path': found['Name'],
+                    'User': parameters[0]['LastModifiedUser'],
+                    }
+
+        return result
+
+
+    def add(self, key, value):
+        Logger.debug(f"Checking SSM parameter overwrites '{key}': namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        assert_ssm_parameter_no_namespace_overwrites(key=key, path_prefix=self.get_path_prefix())
+        Logger.debug(f"Locating SSM parameter '{key}': namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        found = locate_ssm_parameter_in_context_hierachy(key=key, path_prefix=self.get_path_prefix(), uninherited=True)
+        if found and not found['Type'] == 'String':
+            raise DSOException(f"Failed to add parameter '{key}' becasue the key is not available in the given context: namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        path = get_ssm_path(context=Config.context, key=key, path_prefix=self.get_path_prefix())
+        Logger.debug(f"Adding SSM parameter: path={path}")
+        response = add_ssm_paramater(path, value)
+        result = {
+                'RevisionId': str(response['Version']),
+                'Key': key, 
+                'Value': value,
+                'Stage': Config.short_stage,
+                'Scope': Config.context.scope_translation,
+                'Context': {
+                    'Namespace': Config.namespace,
+                    'Application': Config.application,
+                    'Stage': Config.stage,
+                },
+                'Path': path,
+            }
+        result.update(response)
+        return result
+
+
+
+
+
+    def history(self, key):
+        Logger.debug(f"Locating SSM parameter '{key}': namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        found = locate_ssm_parameter_in_context_hierachy(key=key, path_prefix=self.get_path_prefix(), uninherited=True)
+        if not found:
+            raise DSOException(f"Parameter '{key}' not found in the given context: namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        else:
+            if not found['Type'] == 'String':
+                raise DSOException(f"Parameter '{key}' not found in the given context: namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        Logger.debug(f"Getting SSM parameter: path={found['Name']}")
+        response = get_ssm_parameter_history(found['Name'])
+        parameters = sorted(response['Parameters'], key=lambda x: int(x['Version']), reverse=True)
+        result = { "Revisions":
+            [{
+                'RevisionId': str(parameter['Version']),
+                'Date': parameter['LastModifiedDate'].strftime('%Y/%m/%d-%H:%M:%S'),
+                'Key': key,
+                'Value': parameter['Value'],
+                # 'Scope': found['Scope'],
+                # 'Context': found['Context'],
+                'User': parameter['LastModifiedUser'],
+                # 'Path': found['Name'],
+            } for parameter in parameters]
+        }
+
+        return result
+
+
+
+    def delete(self, key):
+        Logger.debug(f"Locating SSM parameter '{key}': namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        ### only parameters owned by the context can be deleted, hence uninherited=True
+        found = locate_ssm_parameter_in_context_hierachy(key=key, path_prefix=self.get_path_prefix(), uninherited=True)
+        if not found:
+            raise DSOException(f"Parameter '{key}' not found in the given context: namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        else:
+            # if len(found) > 1:
+            #     Logger.warn(f"More than one parameter found at '{found['Name']}'. The first one taken, and the rest were discarded.")
+            if not found['Type'] == 'String':
+                raise DSOException(f"Parameter '{key}' not found in the given context: namespace={Config.get_namespace(ContextMode.Target)}, application={Config.get_application(ContextMode.Target)}, stage={Config.get_stage(ContextMode.Target)}, scope={Config.scope}")
+        Logger.debug(f"Deleting SSM parameter: path={found['Name']}")
+        delete_ssm_parameter(found['Name'])
+        result = {
+                'Key': key,
+                'Stage': found['Stage'],
+                'Scope': found['Scope'],
+                'Context': found['Context'],
+                'Path': found['Name'],
+                }
+        return result
+
+
+def register():
+    Providers.register(AwsSsmParameterProvider())
---
+++ packages/dsocli/dsocli-1.0.196.dev0.tar.gz/dsocli-1.0.196.dev0/src/dsocli/provider/parameter/shell/v1/__init__.py
@@ -0,0 +1,3 @@
+__package__ = 'dsocli.provider.parameter.shell.v1'
+from .main import get_default_spec
+from .main import ShellParameterProvider, register
