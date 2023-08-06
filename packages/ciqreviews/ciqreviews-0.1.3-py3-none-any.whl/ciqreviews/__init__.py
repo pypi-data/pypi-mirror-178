6ecb1a70f0dc2b6e2ebae6c48ec0ef25b304d2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:03 +0000

    Add package ciqreviews/0.1.3/ciqreviews-0.1.3-py3-none-any.whl

---
+++ packages/ciqreviews/ciqreviews-0.1.3-py3-none-any.whl/ciqreviews/__init__.py
@@ -0,0 +1,1 @@
+from .connectiq import get_app_download_info, get_html_text_from_url, analyse_local_reviews_data, get_single_page_review_json_using_bs4, analyse_remote_reviews_data, analyse_local_reviews_data, get_multi_page_review_json_using_bs4
