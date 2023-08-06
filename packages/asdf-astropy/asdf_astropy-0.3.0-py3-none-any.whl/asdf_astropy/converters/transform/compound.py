781ffa3619d60f625afd0267b3d68203d73e22ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:12 +0000

    Add package asdf-astropy/0.3.0/asdf_astropy-0.3.0-py3-none-any.whl

---
+++ packages/asdf-astropy/asdf_astropy-0.3.0-py3-none-any.whl/asdf_astropy/converters/transform/compound.py
@@ -0,0 +1,89 @@
+from ..helpers import get_tag_name
+from .core import TransformConverterBase
+
+__all__ = ["CompoundConverter"]
+
+
+_OPERATOR_TO_TAG_NAME = {
+    "+": "add",
+    "-": "subtract",
+    "*": "multiply",
+    "/": "divide",
+    "**": "power",
+    "|": "compose",
+    "&": "concatenate",
+    "fix_inputs": "fix_inputs",
+}
+
+
+_TAG_NAME_TO_MODEL_METHOD = {
+    "add": "__add__",
+    "subtract": "__sub__",
+    "multiply": "__mul__",
+    "divide": "__truediv__",
+    "power": "__pow__",
+    "compose": "__or__",
+    "concatenate": "__and__",
+    "fix_inputs": "fix_inputs",
+}
+
+
+class CompoundConverter(TransformConverterBase):
+    """
+    ASDF serialization support for CompoundModel.
+    """
+
+    tags = [
+        "tag:stsci.edu:asdf/transform/add-*",
+        "tag:stsci.edu:asdf/transform/subtract-*",
+        "tag:stsci.edu:asdf/transform/multiply-*",
+        "tag:stsci.edu:asdf/transform/divide-*",
+        "tag:stsci.edu:asdf/transform/power-*",
+        "tag:stsci.edu:asdf/transform/compose-*",
+        "tag:stsci.edu:asdf/transform/concatenate-*",
+        "tag:stsci.edu:asdf/transform/fix_inputs-*",
+    ]
+
+    types = ["astropy.modeling.core.CompoundModel"]
+
+    def select_tag(self, model, tags, ctx):
+        tag_name = _OPERATOR_TO_TAG_NAME[model.op]
+
+        # The extension will never include two tags with the
+        # same name but different version, so we can just
+        # return the first matching tag that we discover in
+        # the list:
+        return next(t for t in tags if get_tag_name(t) == tag_name)
+
+    def to_yaml_tree_transform(self, model, tag, ctx):
+        left = model.left
+
+        if isinstance(model.right, dict):
+            right = {"keys": list(model.right.keys()), "values": list(model.right.values())}
+        else:
+            right = model.right
+
+        return {"forward": [left, right]}
+
+    def from_yaml_tree_transform(self, node, tag, ctx):
+        from astropy.modeling.core import CompoundModel, Model
+
+        oper = _TAG_NAME_TO_MODEL_METHOD[get_tag_name(tag)]
+
+        left = node["forward"][0]
+        if not isinstance(left, Model):
+            raise TypeError(f"Unknown left model type '{node['forward'][0]._tag}'")
+
+        right = node["forward"][1]
+        if (oper == "fix_inputs" and not isinstance(right, dict)) or (
+            oper != "fix_inputs" and not isinstance(right, Model)
+        ):
+            raise TypeError(f"Unknown right model type '{node['forward'][1]._tag}'")
+
+        if oper == "fix_inputs":
+            right = dict(zip(right["keys"], right["values"]))
+            model = CompoundModel("fix_inputs", left, right)
+        else:
+            model = getattr(left, oper)(right)
+
+        return model
---
+++ packages/asdf-astropy/asdf_astropy-0.3.0-py3-none-any.whl/asdf_astropy/testing/helpers.py
@@ -0,0 +1,124 @@
+from numpy.testing import assert_array_almost_equal, assert_array_equal
+
+
+def assert_earth_location_equal(a, b):
+    __tracebackhide__ = True
+
+    assert (a == b).all()
+
+
+def assert_representation_equal(a, b):
+    from astropy import units
+
+    __tracebackhide__ = True
+
+    assert type(a) is type(b)
+    assert a.components == b.components
+    for component in a.components:
+        assert units.allclose(getattr(a, component), getattr(b, component))
+
+
+def assert_sky_coord_equal(a, b):
+    __tracebackhide__ = True
+
+    assert a.is_equivalent_frame(b)
+    assert a.representation_type is b.representation_type
+    assert a.shape == b.shape
+
+    assert_representation_equal(a.data, b.data)
+
+
+def assert_frame_equal(a, b):
+    __tracebackhide__ = True
+
+    assert type(a) is type(b)
+
+    if a is None:
+        return
+
+    if a.has_data:
+        assert b.has_data
+        assert_representation_equal(a.data, b.data)
+    else:
+        return
+
+
+def assert_spectral_coord_equal(a, b):
+    from astropy.tests.helper import assert_quantity_allclose
+
+    __tracebackhide__ = True
+
+    assert type(a) is type(b)
+    assert_quantity_allclose(a.quantity, b.quantity)
+    assert_frame_equal(a.observer, b.observer)
+    assert_frame_equal(a.target, b.target)
+
+
+def assert_time_equal(a, b):
+    from astropy.coordinates import EarthLocation
+
+    assert a.format == b.format
+    assert a.scale == b.scale
+
+    assert isinstance(a.location, type(b.location))
+    if isinstance(a.location, EarthLocation):
+        assert_earth_location_equal(a.location, b.location)
+    else:
+        assert a.location == b.location
+
+    if a.format == "plot_date":
+        assert_array_almost_equal(a.value, b.value)
+    else:
+        assert_array_equal(a, b)
+
+
+def assert_time_delta_equal(a, b):
+    assert_array_equal(a.jd, b.jd)
+    assert_array_equal(a.jd2, b.jd2)
+    assert_array_equal(a.sec, b.sec)
+
+
+def assert_hdu_list_equal(a, b):
+    assert len(a) == len(b)
+    for hdu_a, hdu_b in zip(a, b):
+        assert_array_equal(hdu_a.data, hdu_b.data)
+        assert len(hdu_a.header.cards) == len(hdu_b.header.cards)
+        for card_a, card_b in zip(hdu_a.header.cards, hdu_b.header.cards):
+            assert tuple(card_a) == tuple(card_b)
+
+
+def assert_model_equal(a, b):
+    """
+    Assert that two model instances are equivalent.
+    """
+    if a is None and b is None:
+        return
+
+    assert a.__class__ == b.__class__
+
+    assert a.name == b.name
+    assert a.inputs == b.inputs
+    assert a.input_units == b.input_units
+    assert a.outputs == b.outputs
+    assert a.input_units_allow_dimensionless == b.input_units_allow_dimensionless
+    assert a.input_units_equivalencies == b.input_units_equivalencies
+
+    assert_array_equal(a.parameters, b.parameters)
+
+    assert a._user_bounding_box == b._user_bounding_box
+    try:
+        a_bounding_box = a.bounding_box
+    except NotImplementedError:
+        a_bounding_box = None
+
+    try:
+        b_bounding_box = b.bounding_box
+    except NotImplementedError:
+        b_bounding_box = None
+
+    assert a_bounding_box == b_bounding_box
+
+    assert a.fixed == b.fixed
+    assert a.bounds == b.bounds
+
+    assert_model_equal(a._user_inverse, b._user_inverse)
