db9a173a8ddb3e97397737c4d684ee0503df70fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:16 +0000

    Add package aibotweb/1.1/aibotweb-1.1.tar.gz

---
+++ packages/aibotweb/aibotweb-1.1.tar.gz/aibotweb-1.1/aibotweb/__init__.py
@@ -0,0 +1,3 @@
+from .aibotweb import webBot
+__all__ = ["webBot"]
+__version__="1.1"
---
+++ packages/aibotweb/aibotweb-1.1.tar.gz/aibotweb-1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aibotweb",  # 模块名称
+    version="1.1",  # 当前版本
+    author="chenyuegui",  # 作者
+    author_email="lnj0@foxmail.com",  # 作者邮箱
+    description="用tcp命令驱动ai-bot，控制浏览器",  # 模块简介
+    long_description=long_description,  # 模块详细介绍
+    long_description_content_type="text/markdown",  # 模块详细介绍格式
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+    # 模块相关的元数据
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    # 依赖模块
+    install_requires=[],
+    python_requires='>=3',
+)
