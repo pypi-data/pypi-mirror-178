cf7c0edab05436a4e7d9a1986f71d6f82c874432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:29 +0000

    Add package picsexl/0.0.2/picsexl-0.0.2.tar.gz

---
+++ packages/picsexl/picsexl-0.0.2.tar.gz/picsexl-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_namespace_packages, setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+with open("requirements.txt", "r") as requirements:
+    install_requires = [line.rstrip() for line in requirements.readlines()]
+
+setup(
+    name="picsexl",
+    version="0.0.2",
+    author="pog7x",
+    author_email="poluningm@gmail.com",
+    url="https://github.com/pog7x/picsexl",
+    license="MIT",
+    keywords="python calendar ics excel recurring",
+    description="Converter from ics format to xls",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_namespace_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=install_requires,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
