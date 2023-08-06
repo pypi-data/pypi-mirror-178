50b21f29b05ca03cd06bb64481f77933e9054585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:35 +0000

    Add package princess/0.6/princess-0.6.tar.gz

---
+++ packages/princess/princess-0.6.tar.gz/princess-0.6/princess.py
@@ -0,0 +1,166 @@
+"""Please Run IPython Notebook in the Current Environment with Stdout & Stderr
+"""
+import os
+import sys
+from argparse import ArgumentParser
+from contextlib import asynccontextmanager
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Optional
+
+from ipykernel.kernelspec import RESOURCES, get_kernel_dict
+from jupyter_client import AsyncKernelClient, AsyncKernelManager
+from jupyter_client.kernelspec import KernelSpecManager
+from nbclient import NotebookClient
+from nbclient.exceptions import CellExecutionError
+import nbformat
+
+__version__ = '0.6'
+
+
+class CurrentEnvKernelSpecManager(KernelSpecManager):
+    """Always launch a Python kernel with the current Python interpreter.
+
+    Ignores the kernel name (from the notebook)
+    """
+    def get_kernel_spec(self, kernel_name):
+        return self.kernel_spec_class(resource_dir=RESOURCES, **get_kernel_dict())
+
+
+class PrincessNotebookClient(NotebookClient):
+    __sockets_tmpdir = None
+
+    # nbclient allows these to be their non-async equivalents, but princess
+    # always uses the async variants (the default for nbclient anyway)
+    km: Optional[AsyncKernelManager] = None
+    kc: Optional[AsyncKernelClient] = None
+
+    def __init__(self, nb, run_before=None, **kwargs):
+        super().__init__(nb, **kwargs)
+        self.run_before = run_before
+
+    def create_kernel_manager(self):
+        kwargs = {}
+        # Use ipc transport (Unix domain sockets) with a temp dir if possible;
+        # something else may claim TCP sockets while the kernel is starting.
+        if os.name == 'posix':
+            self.__sockets_tmpdir = TemporaryDirectory(prefix="jupyter-kernel-")
+            kwargs['transport'] = 'ipc'
+            kwargs['ip'] = os.path.join(self.__sockets_tmpdir.name, 'socket')
+
+        return AsyncKernelManager(
+            kernel_spec_manager=CurrentEnvKernelSpecManager(),
+            **kwargs,
+        )
+
+    async def _async_cleanup_kernel(self):
+        await super()._async_cleanup_kernel()
+        if self.__sockets_tmpdir is not None:
+            self.__sockets_tmpdir.cleanup()
+
+    @asynccontextmanager
+    async def async_setup_kernel(self, **kwargs):
+        async with super().async_setup_kernel(**kwargs):
+            assert self.kc is not None
+            if self.run_before:
+                await self.kc.execute(self.run_before, silent=True, reply=True)
+            yield
+
+    def output(self, outs, msg, display_id, cell_index):
+        """Display text output, as well as saving it in the notebook"""
+        msg_type = msg['msg_type']
+        content = msg['content']
+        stream = sys.stdout
+        if msg_type == 'stream':
+            if content.get('name') == 'stderr':
+                stream = sys.stderr
+            text = content.get('text')
+        elif msg_type == 'display_data':
+            text = content.get('data', {}).get('text/plain')
+            if text:
+                text += '\n'
+        elif msg_type == 'error':
+            ename, evalue = content.get('ename'), content.get('evalue', '')
+            if ename == 'SystemExit' and evalue.isdigit():
+                text = None  # Exit code handled via CellExecutionError
+            else:
+                text = '\n'.join(content.get('traceback', [])) + '\n'
+        else:
+            text = None
+
+        if text:
+            print(text, file=stream, end='', flush=True)
+
+        return super().output(outs, msg, display_id, cell_index)
+
+
+def main(argv=None):
+    ap = ArgumentParser(
+        prog='python -m princess',
+        description="Run a Python notebook in the current environment, showing stdout & stderr."
+    )
+    ap.add_argument('notebook', help='Notebook file to run')
+
+    save_opts = ap.add_mutually_exclusive_group()
+    save_opts.add_argument(
+        '--save', action='store_true',
+        help="Save the notebook with output in the original file"
+    )
+    save_opts.add_argument(
+        '--save-as', help="Save the notebook with output to another file"
+    )
+    ap.add_argument(
+        '--discard-on-error', action='store_true',
+        help="Don't save the notebook after an error. Use with --save/--save-as."
+    )
+    ap.add_argument(
+        '--on-error-resume-next', action='store_true',
+        help="Execute remaining cells after an error"
+    )
+    ap.add_argument(
+        '--run-before', metavar='FILE', type=Path,
+        help="A file of Python code to execute in the kernel before the notebook"
+    )
+
+    args = ap.parse_args(argv)
+
+    out_filename = args.notebook if args.save else args.save_as
+    if args.discard_on_error and not out_filename:
+        print("--discard-on-error requires --save or --save-as", file=sys.stderr)
+        return 2
+
+    if args.discard_on_error and args.on_error_resume_next:
+        print("--discard-on-error doesn't work with --on-error-resume-next", file=sys.stderr)
+        return 2
+
+    run_before = args.run_before.read_text() if args.run_before else ''
+
+    nb = nbformat.read(args.notebook, as_version=4)
+
+    # Remove any existing ouput before executing
+    for cell in nb.cells:
+        if 'outputs' in cell:
+            cell.outputs = []
+
+    exit_code = 0
+    try:
+        PrincessNotebookClient(
+            nb, run_before=run_before, allow_errors=args.on_error_resume_next,
+        ).execute()
+    except CellExecutionError as e:
+        if e.ename == 'SystemExit' and e.evalue.isdigit():
+            # From e.g. sys.exit(0) in the notebook - don't display an error.
+            exit_code = int(e.evalue)
+        else:
+            print('\n\n' + '─' * 80)
+            print(e, file=sys.stderr)
+            exit_code = 1
+
+    if out_filename and ((exit_code == 0) or not args.discard_on_error):
+        nbformat.write(nb, out_filename)
+
+    return exit_code
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/princess/princess-0.6.tar.gz/princess-0.6/tests/test_princess.py
@@ -0,0 +1,69 @@
+import nbformat
+from pathlib import Path
+import shutil
+
+from princess import main
+
+this_dir = Path(__file__).parent
+
+def test_output(capsys):
+    main([str(this_dir / 'Sample.ipynb')])
+
+    captured = capsys.readouterr()
+    assert 'Print to stdout\n' in captured.out
+    assert 'Print to stderr\n' in captured.err
+    assert 'Display text' in captured.out
+    assert 'ZeroDivisionError' in captured.out
+
+
+def test_save(tmp_path):
+    nb_orig = nbformat.read(this_dir / 'Sample.ipynb', as_version=4)
+    assert nb_orig.cells[0].outputs == []
+
+    shutil.copy2(this_dir / 'Sample.ipynb', tmp_path)
+    main([str(tmp_path / 'Sample.ipynb'), '--save'])
+
+    nb_saved = nbformat.read(tmp_path / 'Sample.ipynb', as_version=4)
+    assert nb_saved.cells[0].outputs[0].text == 'Print to stdout\n'
+
+
+def test_save_as(tmp_path):
+    output_path = tmp_path / 'Executed.ipynb'
+    main([str(this_dir / 'Sample.ipynb'), '--save-as', str(output_path)])
+
+    nb_orig = nbformat.read(this_dir / 'Sample.ipynb', as_version=4)
+    assert nb_orig.cells[0].outputs == []
+
+    nb_saved = nbformat.read(output_path, as_version=4)
+    assert nb_saved.cells[0].outputs[0].text == 'Print to stdout\n'
+
+
+def test_error():
+    assert main([str(this_dir / 'Error.ipynb')]) == 1
+    assert main([str(this_dir / 'Error.ipynb'), '--on-error-resume-next']) == 0
+
+
+def test_error_save(tmp_path):
+    shutil.copy2(this_dir / 'Error.ipynb', tmp_path)
+    assert main([str(tmp_path / 'Error.ipynb'), '--save']) == 1
+
+    nb_saved = nbformat.read(tmp_path / 'Error.ipynb', as_version=4)
+    err_output = nb_saved.cells[0].outputs[0]
+    assert err_output.output_type == 'error'
+    assert err_output.ename == 'ZeroDivisionError'
+
+    new_path = tmp_path / 'Error2.ipynb'
+    assert main([
+        str(tmp_path / 'Error.ipynb'), '--save-as', str(new_path), '--discard-on-error'
+    ]) == 1
+    assert not new_path.exists()
+
+
+def test_run_before(tmp_path, monkeypatch):
+    pre_path = tmp_path / 'pre.py'
+    pre_path.write_text("open('foo.touch', 'w')\n")
+
+    monkeypatch.chdir(tmp_path)
+    main([str(this_dir / 'Sample.ipynb'), '--run-before', str(pre_path)])
+
+    assert (tmp_path / 'foo.touch').is_file()
