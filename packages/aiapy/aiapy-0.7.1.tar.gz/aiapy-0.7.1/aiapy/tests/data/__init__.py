557c31e31b8baf371c8399e4600d2c02b1ef25e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:38 +0000

    Add package aiapy/0.7.1/aiapy-0.7.1.tar.gz

---
+++ packages/aiapy/aiapy-0.7.1.tar.gz/aiapy-0.7.1/aiapy/tests/data/__init__.py
@@ -0,0 +1,21 @@
+from importlib import resources
+
+__all__ = ["get_test_filepath"]
+
+
+def get_test_filepath(filename, **kwargs):
+    """
+    Return the full path to a test file in the ``data/test`` directory.
+
+    Parameters
+    ----------
+    filename : `str`
+        The name of the file inside the ``data/test`` directory.
+
+    Return
+    ------
+    filepath : `str`
+        The full path to the file.
+    """
+    with resources.path("aiapy.tests.data", filename) as path:
+        return path
