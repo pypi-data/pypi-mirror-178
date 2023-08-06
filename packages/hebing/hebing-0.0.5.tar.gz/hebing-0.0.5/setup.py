abdca4ee5cb65f01d087d161fddb538c3fbd7a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:35 +0000

    Add package hebing/0.0.5/hebing-0.0.5.tar.gz

---
+++ packages/hebing/hebing-0.0.5.tar.gz/hebing-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="hebing",
+    version="0.0.5",
+    author="WangXueJie",
+    author_email="hajiew@163.com",
+    description="merge multiple python files into one python file",
+    packages=["hebing"],
+    python_requires='>=3.6',
+    install_requires=[
+        "chardet~=5.0.0",
+        "gmssl~=3.2.1"
+    ],
+    entry_points={'console_scripts': ['hebing=hebing:execute']}
+)
