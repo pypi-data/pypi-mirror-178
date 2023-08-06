74c9d1382d39b556a0d504095a5db208c2e43bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:23:05 +0000

    Add package MSOIoofs/1.0/MSOIoofs-1.0.tar.gz

---
+++ packages/MSOIoofs/MSOIoofs-1.0.tar.gz/MSOIoofs-1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension, find_packages
+try:
+    import pypandoc
+    long_descriptions = pypandoc.convert('README.md', 'rst')
+except:
+    long_descriptions = ""
+setup(
+      name='MSOIoofs',
+      version='1.0',
+      description='Flood Risk Analysis Tool',
+      author='ADS project Team Nene',
+      url = 'https://github.com/ese-msc-2022/ads-deluge-Nene',
+      packages= find_packages(),
+      long_description = long_descriptions
+      #packages=['flood_tool','flood_tool.fizz ']
+      )
