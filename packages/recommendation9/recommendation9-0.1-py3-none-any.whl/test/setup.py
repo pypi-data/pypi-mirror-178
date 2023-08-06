53e535a4e2b2bea52ac06278c181b7bdebf274f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:22:19 +0000

    Add package recommendation9/0.1/recommendation9-0.1-py3-none-any.whl

---
+++ packages/recommendation9/recommendation9-0.1-py3-none-any.whl/test/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name='recommendation9',
+    version='0.1',
+    author="Lohitha",
+    author_email="lohitha.m003@gmail.com",
+    description="Food Recommendation system",
+    packages=setuptools.find_packages(),
+    Classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+
+    ],
+)
