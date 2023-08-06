68612cab0abefe43a54d6fba6eebfac1d21765c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:28 +0000

    Add package pricehist/1.4.3/pricehist-1.4.3-py3-none-any.whl

---
+++ packages/pricehist/pricehist-1.4.3-py3-none-any.whl/pricehist/sources/__init__.py
@@ -0,0 +1,28 @@
+from .alphavantage import AlphaVantage
+from .bankofcanada import BankOfCanada
+from .coinbasepro import CoinbasePro
+from .coindesk import CoinDesk
+from .coinmarketcap import CoinMarketCap
+from .ecb import ECB
+from .exchangeratehost import ExchangeRateHost
+from .yahoo import Yahoo
+
+by_id = {
+    source.id(): source
+    for source in [
+        AlphaVantage(),
+        BankOfCanada(),
+        CoinbasePro(),
+        CoinDesk(),
+        CoinMarketCap(),
+        ECB(),
+        ExchangeRateHost(),
+        Yahoo(),
+    ]
+}
+
+
+def formatted():
+    width = max([len(k) for k, v in by_id.items()])
+    lines = [k.ljust(width + 4) + v.name() for k, v in by_id.items()]
+    return "\n".join(lines)
