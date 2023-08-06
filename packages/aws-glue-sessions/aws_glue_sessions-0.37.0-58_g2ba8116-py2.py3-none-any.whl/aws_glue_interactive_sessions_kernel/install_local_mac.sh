472a6664a336a65cf597599e59172b6ecc17d56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:25 +0000

    Add package aws-glue-sessions/0.37.0/aws_glue_sessions-0.37.0-58_g2ba8116-py2.py3-none-any.whl

---
+++ packages/aws-glue-sessions/aws_glue_sessions-0.37.0-58_g2ba8116-py2.py3-none-any.whl/aws_glue_interactive_sessions_kernel/install_local_mac.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Move kernels to shared folder
+shared_juptyer_path="$HOME/Library/Jupyter/kernels"
+mkdir -p ${shared_juptyer_path}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+rm -rf "${shared_juptyer_path}/glue_pyspark"
+rm -rf "${shared_juptyer_path}/glue_spark"
+rm -rf "${shared_juptyer_path}/glue_kernel_utils"
+cp -a "${DIR}/glue_pyspark" "${shared_juptyer_path}/"
+cp -a "${DIR}/glue_spark" "${shared_juptyer_path}/"
+cp -a "${DIR}/glue_kernel_utils" "${shared_juptyer_path}/"
+cp -a "${DIR}/logos/*" "${shared_juptyer_path}/glue_pyspark/"
+cp -a "${DIR}/logos/*" "${shared_juptyer_path}/glue_spark/"
+# enable jupyter widgets
+#jupyter nbextension enable --py widgetsnbextension
+#jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
