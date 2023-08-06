e45ede69811345b8794184970246692fcaa399df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:25 +0000

    Add package strongsort/0.1.8/strongsort-0.1.8.tar.gz

---
+++ packages/strongsort/strongsort-0.1.8.tar.gz/strongsort-0.1.8/strongsort/__init__.py
@@ -0,0 +1,6 @@
+from strongsort.strong_sort import StrongSORT
+from strongsort.deep.models import *
+from strongsort.deep import reid_model_factory
+from strongsort.utils import *
+
+__version__ = "0.1.8"
