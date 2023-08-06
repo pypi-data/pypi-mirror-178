c56abc979e66925875d08c2650e4b016f62f501b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:32 +0000

    Add package polars-lts-cpu/0.15.1/polars_lts_cpu-0.15.1.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.15.1.tar.gz/polars_lts_cpu-0.15.1/local_dependencies/polars-pipe/src/executors/sinks/groupby/aggregates/mod.rs
@@ -0,0 +1,12 @@
+mod convert;
+mod count;
+mod first;
+mod interface;
+mod last;
+mod mean;
+mod null;
+mod sum;
+
+pub use convert::*;
+pub(crate) use interface::{AggregateFn, AggregateFunction};
+pub(crate) use sum::SumAgg;
