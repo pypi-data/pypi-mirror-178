99e70d3007a20e2717fe19f608c26953a7b9d609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:26 +0000

    Add package testable-coresvc/0.2.2/testable_coresvc-0.2.2.tar.gz

---
+++ packages/testable-coresvc/testable_coresvc-0.2.2.tar.gz/testable_coresvc-0.2.2/testable_coresvc/app.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+from pymongo.database import Database
+from typing import Any, TypeVar
+from pydantic import BaseModel
+
+from settings import BaseServiceSettings, MongoSettings
+
+
+class WithMongo(BaseModel):
+    mongo: MongoClient
+    database: Database
+
+
+class TestableApp(FastAPI):
+
+    __S = TypeVar("__S", bound=BaseServiceSettings)
+
+    def __init__(self, settings: __S, **extra: Any):
+        super().__init__(**extra)
+        self.settings = settings
+
+    __T = TypeVar("__T")
+
+    def with_mongo(self: __T, settings: MongoSettings) -> __T | WithMongo:
+        """
+        Initializes a Mongo connection and binds the client and the Testable database to the app instance.
+
+        Parameters
+        ----------
+        settings : MongoSettings
+            Mongo Settings to use to initialize the connection
+
+        Returns
+        -------
+        app : __T | WithMongo
+            Will return the current app with the Mongo mixin
+        """
+        setattr(self, "mongo", MongoClient(settings.uri, uuidRepresentation="standard"))
+        setattr(self, "database", getattr(self, "mongo")[settings.database])
+        app = self
+        return app
