6c5167165797c41c6f0510b51c195c37cbec7739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:22 +0000

    Add package alicebot-adapter-dingtalk/0.6.0/alicebot_adapter_dingtalk-0.6.0.tar.gz

---
+++ packages/alicebot-adapter-dingtalk/alicebot_adapter_dingtalk-0.6.0.tar.gz/alicebot_adapter_dingtalk-0.6.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['alicebot', 'alicebot.adapter.dingtalk']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['alicebot>=0.6.0,<0.7.0']
+
+setup_kwargs = {
+    'name': 'alicebot-adapter-dingtalk',
+    'version': '0.6.0',
+    'description': 'DingTalk adapter for AliceBot.',
+    'long_description': '<div align="center">\n  <a href="https://docs.alicebot.dev/"><img src="https://raw.githubusercontent.com/st1020/alicebot/master/docs/public/logo.png" width="200" height="200" alt="logo"></a>\n\n# AliceBot-Adapter-DingTalk\n\n**钉钉协议适配**\n\n</div>\n',
+    'author': 'st1020',
+    'author_email': 'stone_1020@qq.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://docs.alicebot.dev/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
