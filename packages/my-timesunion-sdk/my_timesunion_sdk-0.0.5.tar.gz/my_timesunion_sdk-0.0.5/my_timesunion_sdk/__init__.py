078b44b86a427f6161c3437cfb1d87bfa3ef97f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:36 +0000

    Add package my-timesunion-sdk/0.0.5/my_timesunion_sdk-0.0.5.tar.gz

---
+++ packages/my-timesunion-sdk/my_timesunion_sdk-0.0.5.tar.gz/my_timesunion_sdk-0.0.5/my_timesunion_sdk/__init__.py
@@ -0,0 +1,1 @@
+from .my_timesunion_sdk import MyTMSDK
---
+++ packages/my-timesunion-sdk/my_timesunion_sdk-0.0.5.tar.gz/my_timesunion_sdk-0.0.5/my_timesunion_sdk/my_timesunion_sdk.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022-11-29 20:45
+# @Author  : MR.
+# @Software: PyCharm
+
+
+class MyTMSDK():
+    def __init__(self):
+        pass
+
+    def getsome(self):
+        print("你好，这是一个测试包")
+
+
+if __name__ == '__main__':
+    runapp = MyTMSDK()
+    runapp.getsome()
