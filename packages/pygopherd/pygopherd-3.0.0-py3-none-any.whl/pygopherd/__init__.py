f3217385b12d2a56b0a6abba96a9f094396261d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:55 +0000

    Add package pygopherd/3.0.0/pygopherd-3.0.0-py3-none-any.whl

---
+++ packages/pygopherd/pygopherd-3.0.0-py3-none-any.whl/pygopherd/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "3.0.0"
+
+__all__ = [
+    "handlers",
+    "protocols",
+    "GopherExceptions",
+    "gopherentry",
+    "logger",
+    "fileext",
+    "initialization",
+    "testutil",
+    "server",
+]
