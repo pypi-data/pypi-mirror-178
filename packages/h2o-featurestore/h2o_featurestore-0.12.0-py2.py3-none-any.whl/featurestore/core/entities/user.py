3c891aa1e5370a4f2a1e10c1db14630d61d4d878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:55 +0000

    Add package h2o-featurestore/0.12.0/h2o_featurestore-0.12.0-py2.py3-none-any.whl

---
+++ packages/h2o-featurestore/h2o_featurestore-0.12.0-py2.py3-none-any.whl/featurestore/core/entities/user.py
@@ -0,0 +1,21 @@
+from ..utils import Utils
+
+
+class User:
+    def __init__(self, user):
+        self._user = user
+
+    @property
+    def id(self):
+        return self._user.id
+
+    @property
+    def name(self):
+        return self._user.name
+
+    @property
+    def email(self):
+        return self._user.email
+
+    def __repr__(self):
+        return Utils.pretty_print_proto(self._user)
