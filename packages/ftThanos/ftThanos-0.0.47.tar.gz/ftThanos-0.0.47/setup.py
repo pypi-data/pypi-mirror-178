24941b58683144ffde2aadf1f50c797ad77bbab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:35 +0000

    Add package ftThanos/0.0.47/ftThanos-0.0.47.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.47.tar.gz/ftThanos-0.0.47/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.47",
+description="A client for 47 API",
+author="Thanos 47",
+author_email="thanos@47paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
