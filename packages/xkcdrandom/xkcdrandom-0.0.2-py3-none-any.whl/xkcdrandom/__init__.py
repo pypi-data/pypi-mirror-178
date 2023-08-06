f98560b004bce8e3f1f93ac539a0ec593652021d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:18 +0000

    Add package xkcdrandom/0.0.2/xkcdrandom-0.0.2-py3-none-any.whl

---
+++ packages/xkcdrandom/xkcdrandom-0.0.2-py3-none-any.whl/xkcdrandom/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2022-present Patrick Armengol <patrickarmengol@protonmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from xkcdrandom.grn import get_random_number
+
+__all__ = [
+    'get_random_number'
+]
---
+++ packages/xkcdrandom/xkcdrandom-0.0.2-py3-none-any.whl/xkcdrandom/grn.py
@@ -0,0 +1,2 @@
+def get_random_number() -> int:
+    return 4  # chosen by fair dice roll. guaranteed to be random.
