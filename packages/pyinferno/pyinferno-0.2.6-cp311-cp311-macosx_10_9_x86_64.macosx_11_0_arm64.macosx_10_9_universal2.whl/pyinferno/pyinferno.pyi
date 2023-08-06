028c23057b6852905cfbfa6963b8bf5c3dde494e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:19 +0000

    Add package pyinferno/0.2.6/pyinferno-0.2.6-cp311-cp311-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl

---
+++ packages/pyinferno/pyinferno-0.2.6-cp311-cp311-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl/pyinferno/pyinferno.pyi
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from typing import List
+
+def flamegraph_from_lines(lines: List[str], title: str | None = None) -> str: ...
+
+class InfernoError(Exception): ...
