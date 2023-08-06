19f4fadf6782ec718459a958b114469c06357dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:48 +0000

    Add package MSOIKK/1.0/MSOIKK-1.0.tar.gz

---
+++ packages/MSOIKK/MSOIKK-1.0.tar.gz/MSOIKK-1.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+
+setup(name='MSOIKK',
+      version='1.0',
+      description='Flood Risk Analysis Tool',
+      author='ADS project Team Nene',
+      url = 'https://github.com/ese-msc-2022/ads-deluge-Nene',
+      packages=['flood_tool'])
