60a8cad7e1c03b85432d8a1ba02f638bfc24c28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:20:04 +0000

    Add package splight-lib/0.5.10/splight-lib-0.5.10.tar.gz

---
+++ packages/splight-lib/splight-lib-0.5.10.tar.gz/splight-lib-0.5.10/splight_lib/component/__init__.py
@@ -0,0 +1,12 @@
+from .algorithms import AbstractAlgorithmComponent
+from .network import AbstractNetworkComponent
+from .abstract import AbstractComponent
+from .system import AbstractSystemComponent
+
+__all__ = [
+    'AbstractComponent',
+    # TODO: Remove all below
+    'AbstractNetworkComponent',
+    'AbstractAlgorithmComponent',
+    'AbstractSystemComponent',
+]
