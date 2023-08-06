8bdce408ccbb28de1a45c1fbce8db9dd5789d0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:52 +0000

    Add package randomlib/4.1/randomlib-4.1.tar.gz

---
+++ packages/randomlib/randomlib-4.1.tar.gz/randomlib-4.1/randomlib/config.py
@@ -0,0 +1,15 @@
+paths = { 
+    "autocomplete": {"marathi-gpt": "l3cube-pune/marathi-gpt"},
+
+    "hate": {"mahahate-bert": "l3cube-pune/mahahate-bert",
+             "mahahate-multi-roberta": "l3cube-pune/mahahate-multi-roberta"},
+    
+    "maskFill": {"marathi-bert-v2": "l3cube-pune/marathi-bert-v2",
+                 "marathi-roberta": "l3cube-pune/marathi-roberta",
+                 "marathi-albert": "l3cube-pune/marathi-albert"},
+    
+    "sentiment": {"MarathiSentiment": "l3cube-pune/MarathiSentiment"},
+   
+    "tagger": {"marathi-ner": "l3cube-pune/marathi-ner"}
+    
+}
