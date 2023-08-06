8dd7045bcb1634be0198d7859018c6188badce44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:02 +0000

    Add package cytopath/0.1.9/cytopath-0.1.9.tar.gz

---
+++ packages/cytopath/cytopath-0.1.9.tar.gz/cytopath-0.1.9/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='cytopath',
+  version='0.1.9',
+  description='Simulation based inference of differentiation trajectories from RNA velocity fields.',
+  license='BSD 3-Clause License',
+  packages=find_packages(),
+  author = 'Revant Gupta',
+  author_email = 'revant.gupta.93@gmail.com',
+  url = 'https://github.com/aron0093/cytopath',
+  download_url = 'https://github.com/aron0093/cytopath/archive/v_019.tar.gz',
+  keywords = ['Trajectory Inference', 'single-cell RNA sequencing', 'RNA velocity'],
+  install_requires=[
+          'numpy>=1.20.0',
+          'scipy',
+          'anndata',
+          'scvelo>=0.1.25',
+          'joblib',
+          'fastdtw',
+          'hausdorff',
+          'tqdm',
+	  'networkit',
+	  'tabulate',
+	  'numba'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ])
