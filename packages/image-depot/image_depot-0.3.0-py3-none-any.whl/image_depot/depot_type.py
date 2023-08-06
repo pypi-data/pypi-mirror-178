3b73348070cb9a549ba057aa084b18510efefde3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:25 +0000

    Add package image-depot/0.3.0/image_depot-0.3.0-py3-none-any.whl

---
+++ packages/image-depot/image_depot-0.3.0-py3-none-any.whl/image_depot/depot_type.py
@@ -0,0 +1,31 @@
+"""
+@author hujing
+"""
+from enum import Enum, auto
+
+
+class DepotType(Enum):
+    CatBox = auto()
+    """
+    图床
+    url: https://catbox.moe/
+    """
+    PngCm = auto()
+    """
+    图床
+    url: https://png.cm/
+    """
+    Riyugo = auto()
+    """
+    链接存在有效期
+    url: https://riyugo.com/
+    """
+    NiuPic = auto()
+    """
+    url: https://www.niupic.com/
+    """
+    Ai58 = auto()
+    """
+    58同城客服, 在线上传图片
+    url: https://ai.58.com
+    """
