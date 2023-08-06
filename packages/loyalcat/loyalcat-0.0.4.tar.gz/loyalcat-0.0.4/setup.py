d8d58f1106d814be753e9adb861f3603df8a73c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:49 +0000

    Add package loyalcat/0.0.4/loyalcat-0.0.4.tar.gz

---
+++ packages/loyalcat/loyalcat-0.0.4.tar.gz/loyalcat-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+
+packages = ['loyalcat.types', 'loyalcat']
+
+setup(
+    name='loyalcat',
+    version='0.0.4',
+    install_requires=['aiohttp'],
+    author='yupix',
+    author_email='yupi0982@outlook.jp',
+    license='MIT',
+    python_requires='>=3.10, <4.0',
+    packages=packages,
+    entry_points={'console_scripts': ['loyalcat = loyalcat.cli:main']},
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.10',
+        'Natural Language :: Japanese',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
+
