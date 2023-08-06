40cc831db5f9e4a8d8e2b57c4a895ebd4959f166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:07 +0000

    Add package timelinetestpack/1.1/timelinetestpack-1.1.tar.gz

---
+++ packages/timelinetestpack/timelinetestpack-1.1.tar.gz/timelinetestpack-1.1/function/test_t.py
@@ -0,0 +1,9 @@
+class test_t:
+    def __init__(self) -> None:
+        self.name = "ab"
+    
+    def getName(self):
+        return self.name + self.name
+
+    def getA(self):
+        return "a"
