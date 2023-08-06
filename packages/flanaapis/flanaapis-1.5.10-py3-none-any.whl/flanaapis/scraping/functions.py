0f75976868bee800b1200810dd7f449808a1bdcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:47 +0000

    Add package flanaapis/1.5.10/flanaapis-1.5.10-py3-none-any.whl

---
+++ packages/flanaapis/flanaapis-1.5.10-py3-none-any.whl/flanaapis/scraping/functions.py
@@ -0,0 +1,26 @@
+from typing import Iterable
+
+import flanautils
+from flanautils import Media, MediaType, OrderedSet
+
+
+async def filter_audios(medias: Iterable[Media]) -> OrderedSet[Media]:
+    filtered_medias = OrderedSet()
+
+    for media in medias:
+        if not media.content:
+            continue
+
+        try:
+            bytes_ = await flanautils.to_mp3(media.bytes_ or await flanautils.get_request(media.url))
+        except ValueError:
+            pass
+        else:
+            new_media = media.deep_copy()
+            new_media.url = None
+            new_media.bytes_ = bytes_
+            new_media.type_ = MediaType.AUDIO
+            new_media.extension = 'mp3'
+            filtered_medias.add(new_media)
+
+    return filtered_medias
