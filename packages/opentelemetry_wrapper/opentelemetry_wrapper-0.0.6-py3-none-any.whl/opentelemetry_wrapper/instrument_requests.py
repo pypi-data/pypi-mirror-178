6337c021a1f53856fc5eebdcd634cbffc6b60a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:26 +0000

    Add package opentelemetry_wrapper/0.0.6/opentelemetry_wrapper-0.0.6-py3-none-any.whl

---
+++ packages/opentelemetry_wrapper/opentelemetry_wrapper-0.0.6-py3-none-any.whl/opentelemetry_wrapper/instrument_requests.py
@@ -0,0 +1,15 @@
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
+from opentelemetry_wrapper.instrument_decorator import instrument_decorate
+
+
+@instrument_decorate
+def instrument_requests():
+    """
+    this function is idempotent; calling it multiple times has no additional side effects
+
+    :return:
+    """
+    _instrumentor = RequestsInstrumentor()
+    if not _instrumentor.is_instrumented_by_opentelemetry:
+        _instrumentor.instrument()
