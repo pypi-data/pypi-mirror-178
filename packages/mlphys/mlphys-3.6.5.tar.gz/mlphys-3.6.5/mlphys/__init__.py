3c5b00b2c26763dbf559f39bd7b33045485c33c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:22 +0000

    Add package mlphys/3.6.5/mlphys-3.6.5.tar.gz

---
+++ packages/mlphys/mlphys-3.6.5.tar.gz/mlphys-3.6.5/mlphys/__init__.py
@@ -0,0 +1,15 @@
+"""
+Please keep these lines. Thank you!
+"""
+__author__ = 'Sanjaya Lohani'
+__email__ = 'slohani@mlphys_nightly.com'
+__licence__ = 'Apache 2.0'
+__website__ = "sanjayalohani.com"
+
+from deepqis.simulator import *
+from deepqis.utils import *
+from deepqis.network import *
+from deepqis.optimization import *
+from fso.lgoam.source import *
+from fso.lgoam.utils import *
+from fso.turbulence import *
