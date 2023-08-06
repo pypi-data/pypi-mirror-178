cfa0e3d1373bb85284f3e3e041cbffe2a69aeb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:11 +0000

    Add package dtreeviz/1.4.1/dtreeviz-1.4.1.tar.gz

---
+++ packages/dtreeviz/dtreeviz-1.4.1.tar.gz/dtreeviz-1.4.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# python setup.py sdist upload
+
+setup(
+    name='dtreeviz',
+    version='1.4.1',
+    url='https://github.com/parrt/dtreeviz',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['graphviz>=0.9','pandas','numpy','scikit-learn',
+                        'matplotlib','colour', 'pytest'],
+    extras_require={'xgboost': ['xgboost'], 'pyspark':['pyspark'], 'lightgbm':['lightgbm']},
+    python_requires='>=3.6',
+    author='Terence Parr, Tudor Lapusan, and Prince Grover',
+    author_email='parrt@cs.usfca.edu',
+    description='A Python 3 library for sci-kit learn, XGBoost, LightGBM, and Spark decision tree visualization',
+    long_description='README.md',
+    keywords='machine-learning data structures trees visualization',
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Intended Audience :: Developers']
+)
