8010bd80f2f394f3e54064e9929d06efc752cc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:21 +0000

    Add package corefy/0.0.3/corefy-0.0.3.tar.gz

---
+++ packages/corefy/corefy-0.0.3.tar.gz/corefy-0.0.3/src/corefy.py
@@ -0,0 +1,7 @@
+def add_one(number):
+    print('Your input number is {},and output number is {}'.format(number, number + 1))
+    return number + 2
+
+
+if __name__ == "__main__":
+    print("ccc")
