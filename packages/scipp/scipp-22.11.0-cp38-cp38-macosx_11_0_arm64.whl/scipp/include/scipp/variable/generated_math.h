7621a4b9f09ae183b6eb8a47d28345d0cefe669a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:22:20 +0000

    Add package scipp/22.11.0/scipp-22.11.0-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/scipp/scipp-22.11.0-cp38-cp38-macosx_11_0_arm64.whl/scipp/include/scipp/variable/generated_math.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#include "scipp/variable/abs.h"
+#include "scipp/variable/exp.h"
+#include "scipp/variable/log.h"
+#include "scipp/variable/log10.h"
+#include "scipp/variable/reciprocal.h"
+#include "scipp/variable/sqrt.h"
+#include "scipp/variable/norm.h"
+#include "scipp/variable/floor.h"
+#include "scipp/variable/ceil.h"
+#include "scipp/variable/rint.h"
+#include "scipp/variable/erf.h"
+#include "scipp/variable/erfc.h"
+#include "scipp/variable/dot.h"
+#include "scipp/variable/cross.h"
