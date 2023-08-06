3c5a15fe449df6328ba14bd3fe6f14d77fc5e192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:58 +0000

    Add package pycsspeechtts/1.0.8/pycsspeechtts-1.0.8-py3-none-any.whl

---
+++ packages/pycsspeechtts/pycsspeechtts-1.0.8-py3-none-any.whl/pycsspeechtts/pycsspeechtts.py
@@ -0,0 +1,88 @@
+"""
+Python wrapper for Microsoft Cognitive Services Text-to-speech translator
+"""
+import requests
+import json
+from xml.etree import ElementTree
+import logging
+import sys
+
+_LOGGER = logging.getLogger(__name__)
+
+AccessTokenUrlTemplate = "https://{}.api.cognitive.microsoft.com/sts/v1.0/issueToken"
+SpeechUrlTemplate = "https://{}.tts.speech.microsoft.com/cognitiveservices/v1"
+
+class TTSTranslator(object):
+    """
+    Interface class for the Microsoft Cognitive Services Text-to-speech translator
+    """
+
+    def __init__(self, apiKey, region="eastus", isCustom=False, customEndpoint=None):
+        self._apiKey = apiKey
+        self._geoLocation = region
+        self._isCustom = isCustom
+        self._customEndpoint=customEndpoint
+
+        headers = {"Ocp-Apim-Subscription-Key": self._apiKey}
+        if not self._isCustom:
+            response = requests.post(AccessTokenUrlTemplate.format(
+                self._geoLocation), headers=headers)
+            response.raise_for_status()
+            self._accesstoken = str(response.text)
+        _LOGGER.debug("Connection Initialized OK")
+        
+
+    def speak(self, language="en-us", gender="Female", voiceType="JessaNeural",
+              output="riff-24khz-16bit-mono-pcm", rate="+0.00%", volume="+0.00%",
+              pitch="default", contour="(0%,+0%) (100%,+0%)", text=None):
+        def name_lang(language):
+            lang1,lang2 = language.split("-")
+            return "{}-{}".format(lang1,lang2.upper())
+        
+        body = ElementTree.Element('speak', version='1.0')
+        body.set('{http://www.w3.org/XML/1998/namespace}lang', language)
+        body.set('xmlns','http://www.w3.org/2001/10/synthesis')
+        body.set('xmlns:mstts', 'http://www.w3.org/2001/mstts')
+
+        voice = ElementTree.SubElement(body, 'voice')
+        voice.set('{http://www.w3.org/XML/1998/namespace}lang', language)
+        voice.set('{http://www.w3.org/XML/1998/namespace}gender', gender)
+        voice.set(
+            'name', 'Microsoft Server Speech Text to Speech Voice ('+name_lang(language)+', '+voiceType+')')
+
+        endpoint = None
+        if self._isCustom:
+            # this is a custom voice
+            endpoint = self._customEndpoint
+            headers = {"Content-Type": "application/ssml+xml",
+                "X-Microsoft-OutputFormat": output,
+                "Ocp-Apim-Subscription-Key": self._apiKey,
+                "User-Agent": "PYCSSpeechTTS"
+                }
+            voice.text = text
+        else: 
+            # not a custom voice, generate the endpoint
+            endpoint = SpeechUrlTemplate.format(self._geoLocation)
+            headers = {"Content-Type": "application/ssml+xml",
+                "X-Microsoft-OutputFormat": output,
+                "Authorization": "Bearer " + self._accesstoken,
+                "X-Search-AppId": "07D3234E49CE426DAA29772419F436CA",
+                "X-Search-ClientID": "1ECFAE91408841A480F00935DC390960",
+                "User-Agent": "PYCSSpeechTTS"
+                }
+            voice.append(ElementTree.XML('<prosody>'+text+'</prosody>'))
+            prosody = voice.find('prosody')
+            prosody.set('rate', rate)
+            prosody.set('volume', volume)
+            prosody.set('pitch', pitch)
+            prosody.set('contour', contour)
+
+        response = requests.post(
+            endpoint, ElementTree.tostring(body), headers=headers)
+        if response.status_code == requests.codes.ok:
+            _LOGGER.debug("Text synthesis OK")
+            return response.content
+        else:
+            _LOGGER.error("Text synthesis failed, statuscode " +
+                          str(response.status_code)+", reason: "+response.text)
+            return None
---
+++ packages/pycsspeechtts/pycsspeechtts-1.0.8-py3-none-any.whl/pycsspeechtts/test.py
@@ -0,0 +1,36 @@
+from pycsspeechtts import TTSTranslator
+useCustom = True
+api_key = "YOUR_API_KEY"
+custom_endpoint = "custom_endpoint"
+region = "westus"
+
+if not useCustom:
+    t = TTSTranslator(api_key, region=region)
+    # Speaking with default language of english US and default Female voice
+    data = t.speak(text='This is a test')
+    # Adding a pause to test XML support
+    data = t.speak(text='This is a test with a <break time="2s" />long pause')
+    # Change speed with -50%
+    data = t.speak(text="This is a test", rate="-50%")
+    # Change pitch to high
+    data = t.speak(text="This is a test", pitch="high")
+    # Change volume to +20%
+    data = t.speak(text="This is a test", volume="+20%")
+    # See https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support for the accepted values of the parameters below
+    data = t.speak('en-GB', 'Male', 'George, Apollo',
+                'riff-16khz-16bit-mono-pcm', text='I am Max')
+    data = t.speak('cs-CZ', 'Male', 'Jakub', text='Pojďme vyzkoušet klasickou českou testovací větu. Příliš žluťoučký kůň úpěl ďábelské ódy.')
+    # Using contour to change pitch from normal at 0% of speech and +100% at 100% of speech
+    data = t.speak(text="The Wall Street Journal - which says it's spoken to people close to the ongoing investigation - says the information it has paints a picture of a catastrophic failure that quickly overwhelmed the flight crew",
+                contour="(0%,+0%) (100%,+100%)")
+else:
+    # Test custom voice
+    t = TTSTranslator(api_key, region=region, isCustom=True, customEndpoint=custom_endpoint)
+    data = t.speak(language='en-gb',gender='Male',voiceType="ArchieNeural",text="This is a test for custom voice")
+
+if data == None:
+    print("An error occurred")
+else:
+    with open("file.wav", "wb") as f:
+        f.write(data)
+    print("Success! Open file.wav to hear the results")
