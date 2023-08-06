a6ae12a31e420269442ca54a5624becc1aba05ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:03 +0000

    Add package murasaki/1.0.1/murasaki-1.0.1-py3-none-any.whl

---
+++ packages/murasaki/murasaki-1.0.1-py3-none-any.whl/murasaki/__init__.py
@@ -0,0 +1,1 @@
+from .murasaki import Murasaki
---
+++ packages/murasaki/murasaki-1.0.1-py3-none-any.whl/murasaki/murasaki.py
@@ -0,0 +1,53 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+
+class Murasaki:
+    """Murasaki instance"""
+
+    def __init__(self, API_endpoint, language=None, timezone=None, user=None, password=None):
+        if not user:
+            user = os.environ.get("MURASAKI_USER")
+        if not password:
+            password = os.environ.get("MURASAKI_PWD")
+
+        self._endpoint = API_endpoint
+        self._language = language
+        self._timezone = timezone
+        self._user = user
+        self._password = password
+
+    def _call(self, engine, context, template):
+        ep = self._endpoint.strip("/") + "/html"
+        params = {}
+        if self._language:
+            params["lang"] = self._language
+        if self._timezone:
+            params["timezone"] = self._timezone
+
+        r = requests.post(
+            ep,
+            params=params,
+            json={
+                'template': template,
+                'data': context,
+                'engine': engine,
+            },
+            auth=HTTPBasicAuth(self._user, self._password)
+        )
+        return r.text
+
+    def pug(self, context, template):
+        return self._call("pug", context, template)
+
+    def mustache(self, context, template):
+        return self._call("mustache", context, template)
+
+    def javascript(self, context, template):
+        return self._call("javascript", context, template)
+
+    def __str__(self):
+        str = f"Murasaki: {self._endpoint}"
+        if self._language:
+            str += f" ({self._language})"
+        return str
