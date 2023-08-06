238d396ed2bfcff97b9213215ccdaa70b35f4bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:42 +0000

    Add package gh-utils/0.2.3/gh-utils-0.2.3.tar.gz

---
+++ packages/gh-utils/gh-utils-0.2.3.tar.gz/gh-utils-0.2.3/gh_utils/gh_create_repo_and_add_to_remote.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+"""
+Author : Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
+Date   : 2022-03-24
+Purpose: gh_create_repo_and_add_to_remote
+"""
+
+import argparse
+from pathlib import Path
+import subprocess
+from . import __version__, NotFoundError
+from .utils import (
+    gh_config_yaml_get_user_info,
+    gh_config_yaml_get_user_info_first,
+    gh_config_yaml_get_first_hostname_and_username,
+    hostname_to_user,
+)
+
+__app_name__ = 'ghcrar'
+
+
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        prog=__app_name__,
+        description='Create a GitHub repo with gh and add it as a remote',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        '-a',
+        '--append',
+        help='String to append to the repo name',
+        metavar='SUFFIX',
+        type=str,
+        default=None,
+    )
+
+    parser.add_argument(
+        '-n',
+        '--name',
+        help='The string to use as GitHub repo name',
+        metavar='GITHUB REPO NAME',
+        type=str,
+        default=None,
+    )
+
+    parser.add_argument(
+        '--public',
+        help='Create a public repository',
+        action='store_true',
+        default=False,
+    )
+
+    parser.add_argument(
+        '--overwrite-remote-origin',
+        '--force',
+        help='Overwrites remote origin if exists',
+        action='store_true',
+        default=False,
+    )
+
+    parser.add_argument(
+        '-H',
+        '--hostname',
+        help='GitHub hostname, default to use the first entry in hosts.yml',
+        default=gh_config_yaml_get_first_hostname_and_username()[0],
+        choices=['ssh', 'https'],
+    )
+
+    parser.add_argument('-p', '--protocol', help='git protocol', default='ssh')
+    parser.add_argument(
+        '-V', '--version', action='version', version=f'%(prog)s {__version__}'
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    append = args.append
+    name = args.name
+    public = args.public
+    overwrite_remote_origin = args.overwrite_remote_origin
+    hostname = args.hostname
+    protocol = args.protocol
+
+    curr_path_name = Path.cwd().name
+    if name is not None:
+        repo_name = name
+    else:
+        repo_name = curr_path_name if append is None else f'{curr_path_name}-{append}'
+    visibility_flag = '--public' if public else '--private'
+
+    # (re) init the repo
+    subprocess.run(['git', 'init'])
+
+    # add to remote
+    remotes_p = subprocess.run(['git', 'remote'], capture_output=True)
+    remotes = remotes_p.stdout.splitlines()
+    # print(remotes)
+    remote_origin_exists = b'origin' in remotes
+    gh_username = hostname_to_user(hostname)
+    if remote_origin_exists:
+        print('Remote origin exists.')
+        if overwrite_remote_origin:
+            subprocess.run(['git', 'remote', 'remove', 'origin'])
+            print('Removed previous remote.')
+        else:
+            subprocess.run(['git', 'remote', 'rename', 'origin', 'upstream'])
+            print('Renamed previous remote to upstream.')
+    if protocol == 'ssh':
+        remote_url_prefix = f'git@{hostname}:'
+    elif protocol == 'https':
+        remote_url_prefix = f'https://{hostname}/'
+    else:
+        raise ValueError(f'Unknown protocol: {protocol}')
+    if '/' not in repo_name:
+        remote_url = f'{remote_url_prefix}{gh_username}/{repo_name}.git'
+    else:
+        remote_url = f'{remote_url_prefix}{repo_name}.git'
+    subprocess.run(['git', 'remote', 'add', 'origin', remote_url])
+    print(f'Added remote: {remote_url}')
+
+    # create a repo on github, may fail if already exists
+    subprocess.run(['gh', 'repo', 'create', repo_name, visibility_flag])
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/gh-utils/gh-utils-0.2.3.tar.gz/gh-utils-0.2.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gh_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0']
+
+entry_points = \
+{'console_scripts': ['ghcrar = gh_utils.gh_create_repo_and_add_to_remote:main']}
+
+setup_kwargs = {
+    'name': 'gh-utils',
+    'version': '0.2.3',
+    'description': 'GitHub CLI Utilities',
+    'long_description': "# GH Utilities\n\nA collection of useful utilities that work with GitHub CLI `gh`.\n\n- [GH Utilities](#gh-utilities)\n  - [Installation](#installation)\n    - [pipx](#pipx)\n    - [pip](#pip)\n  - [Utilities](#utilities)\n    - [ghcrar](#ghcrar)\n      - [Features](#features)\n      - [Usage](#usage)\n      - [Screenshots](#screenshots)\n\n## Installation\n\n### pipx\n\nThis is the recommended installation method.\n\n```\n$ pipx install gh-utils\n```\n\n### [pip](https://pypi.org/project/gh-utils/)\n\n```\n$ pip install gh-utils\n```\n\n## Utilities\n\n### ghcrar\n\n\n#### Features\n- Custom GitHub instance hostname support with `--hostname`\n- Supports both `ssh` and `https` protocols\n\n#### Usage\n\n```\n$ ghcrar --help\n\nusage: ghcrar [-h] [-a SUFFIX] [-n GITHUB REPO NAME] [--public] [--overwrite_remote_origin] [-H {ssh,https}] [-p PROTOCOL] [-V]\n\nCreate a GitHub repo with gh and add it as a remote\n\noptions:\n  -h, --help            show this help message and exit\n  -a SUFFIX, --append SUFFIX\n                        String to append to the repo name (default: None)\n  -n GITHUB REPO NAME, --name GITHUB REPO NAME\n                        The string to use as GitHub repo name (default: None)\n  --public              Create a public repository (default: False)\n  --overwrite-remote-origin, --force\n                        Overwrites remote origin if exists (default: False)\n  -H {ssh,https}, --hostname {ssh,https}\n                        GitHub hostname, default to use the first entry in hosts.yml (default: github.com)\n  -p PROTOCOL, --protocol PROTOCOL\n                        git protocol (default: ssh)\n  -V, --version         show program's version number and exit\n```\n\n#### Screenshots\n![ghcrar-public](images/ghcrar-public.png)\n\n\n\n",
+    'author': 'Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/tddschn/gh-utils',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
