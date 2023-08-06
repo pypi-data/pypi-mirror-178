28765425e238d8b77f3b5ab4226535bc1cbac48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:41 +0000

    Add package AddingLib/0.1.0/AddingLib-0.1.0.tar.gz

---
+++ packages/AddingLib/AddingLib-0.1.0.tar.gz/AddingLib-0.1.0/AddLibriary/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='resources',
+    version='0.1.0',
+    package_dir={'fonts': 'fonts','images':'images','js':'js','maps':'maps','qml':'qml','sounds':'sounds','tr':'tr'},
+    packages=['fonts','images','js','maps','qml','sounds','tr'],
+    
+)
---
+++ packages/AddingLib/AddingLib-0.1.0.tar.gz/AddingLib-0.1.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AddingLib",
+    version="0.1.0",
+    author="Yegor",
+    author_email="maystrenko.yegor.my@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yegormaystrenko/AddLibriary.git",
+ #  package_dir={
+# 'fonts': 'resources/fonts',
+# 'images': 'resources/images',
+# 'js': 'resources/js',
+# 'maps': 'resources/maps',
+# 'qml': 'resources/qml',
+# 'sounds': 'resources/sounds',
+# 'tr': 'resources/tr',
+# },
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    #packages=['fonts','images','js','maps','qml','sounds','tr'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
