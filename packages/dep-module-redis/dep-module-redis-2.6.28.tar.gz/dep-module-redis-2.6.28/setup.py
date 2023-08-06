61601a6e271d6804a5c525fff809a65260e54854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:08 +0000

    Add package dep-module-redis/2.6.28/dep-module-redis-2.6.28.tar.gz

---
+++ packages/dep-module-redis/dep-module-redis-2.6.28.tar.gz/dep-module-redis-2.6.28/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dep_redis', 'tests']
+
+package_data = \
+{'': ['*']}
+
+modules = \
+['pyproject']
+install_requires = \
+['aioredis[hiredis]>=2.0.1,<3.0.0', 'dep-spec>=2.2.28,<3.0.0', 'poetry==1.1.15']
+
+setup_kwargs = {
+    'name': 'dep-module-redis',
+    'version': '2.6.28',
+    'description': '',
+    'long_description': None,
+    'author': 'everhide',
+    'author_email': 'i.tolkachnikov@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9.0,<=3.9.13',
+}
+
+
+setup(**setup_kwargs)
