672e929160e59395643d0f88024d58258c5c0466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:43 +0000

    Add package pabutools/0.4/pabutools-0.4.tar.gz

---
+++ packages/pabutools/pabutools-0.4.tar.gz/pabutools-0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pabutools',
+      version='0.4',
+      description='Implementation of PB voting rules and tools for reading Pabulib datasets',
+      readme = "README.md",
+      packages=['pabutools'],
+      author_email='g.pierczynski@gmail.com',
+      zip_safe=False)
