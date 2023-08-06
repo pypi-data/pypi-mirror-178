69d6606781d686fac29a5937717f4ad5604d63ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:46 +0000

    Add package mastrs/0.0.1/mastrs-0.0.1-py3-none-any.whl

---
+++ packages/mastrs/mastrs-0.0.1-py3-none-any.whl/mastars/mastar.py
@@ -0,0 +1,98 @@
+def ms():
+    graph = {
+        'A':['B','C'],
+        'B':['D','E'],
+        'C':['F','G'],
+        'D':['H','I'],
+        'E':['J','K'],
+        'F':['L','M'],
+        'G':['N','O'],
+        'H':[],
+        'I':[],
+        'J':[],
+        'K':[],
+        'L':[],
+        'M':[],
+        'N':[],
+        'O':[]
+    }
+
+    def MA(start,goal,path,level,maxD):
+        print('\nCurrent level-->',level)
+        print('Goal node testing for',start)
+        path.append(start)
+        if start == goal:
+            print("Goal test successful")
+            return path
+        print('Goal node testing failed')
+        if level==maxD:
+            return False
+        print('\nExpanding the current node',start)
+        for child in graph[start]:
+            if MA(child,goal,path,level+1,maxD):
+                return path
+            path.pop()
+        return False
+    
+
+
+    start = 'A'
+    goal = input('Enter the goal node:-')
+    maxD = int(input("Enter the maximum depth limit:-"))
+    print()
+    path = list()
+    res = MA(start,goal,path,0,maxD)
+    if(res):
+        print("Path to goal node available")
+        print("Path",path)
+    else:
+        print("No path available for the goal node in given depth limit")
+def msop():
+        print('''graph = {
+        'A':['B','C'],
+        'B':['D','E'],
+        'C':['F','G'],
+        'D':['H','I'],
+        'E':['J','K'],
+        'F':['L','M'],
+        'G':['N','O'],
+        'H':[],
+        'I':[],
+        'J':[],
+        'K':[],
+        'L':[],
+        'M':[],
+        'N':[],
+        'O':[]
+    }
+
+def MA(start,goal,path,level,maxD):
+        print('\nCurrent level-->',level)
+        print('Goal node testing for',start)
+        path.append(start)
+        if start == goal:
+            print("Goal test successful")
+            return path
+        print('Goal node testing failed')
+        if level==maxD:
+            return False
+        print('\nExpanding the current node',start)
+        for child in graph[start]:
+            if MA(child,goal,path,level+1,maxD):
+                return path
+            path.pop()
+        return False
+    
+
+
+start = 'A'
+goal = input('Enter the goal node:-')
+maxD = int(input("Enter the maximum depth limit:-"))
+print()
+path = list()
+res = MA(start,goal,path,0,maxD)
+if(res):
+        print("Path to goal node available")
+        print("Path",path)
+else:
+        print("No path available for the goal node in given depth limit") ''')
