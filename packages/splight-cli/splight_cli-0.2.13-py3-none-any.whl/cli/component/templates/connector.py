532ee54522c0a10d44d96a027d8021efafbba34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:41 +0000

    Add package splight-cli/0.2.13/splight_cli-0.2.13-py3-none-any.whl

---
+++ packages/splight-cli/splight_cli-0.2.13-py3-none-any.whl/cli/component/templates/connector.py
@@ -0,0 +1,9 @@
+from splight_lib.component.io import AbstractIOComponent
+
+class Main(AbstractIOComponent):
+
+    def __init__(self, *args, **kwargs):
+        super(Main, self).__init__(*args, **kwargs)
+
+    def start(self):
+        pass
