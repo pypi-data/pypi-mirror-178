cf579aa7876ce0e940f5bffae073c3ce713460fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:56 +0000

    Add package lagoons-distributions/0.1/lagoons_distributions-0.1.tar.gz

---
+++ packages/lagoons-distributions/lagoons_distributions-0.1.tar.gz/lagoons_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lagoons_distributions',
+      version='0.1',
+      description='Probability distributions',
+      packages=['lagoons_distributions'],
+      author = "Harris Ramos Baroni",
+      author_email = "yuopres@gmail.com",
+      zip_safe=False)
