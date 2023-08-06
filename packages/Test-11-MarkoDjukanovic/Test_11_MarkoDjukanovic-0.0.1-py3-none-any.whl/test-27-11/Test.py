da823eae5786d97ae31b6aa9bc7c388bb2d9246f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:34 +0000

    Add package Test-11-MarkoDjukanovic/0.0.1/Test_11_MarkoDjukanovic-0.0.1-py3-none-any.whl

---
+++ packages/Test-11-MarkoDjukanovic/Test_11_MarkoDjukanovic-0.0.1-py3-none-any.whl/test-27-11/Test.py
@@ -0,0 +1,19 @@
+from cmath import inf
+import copy
+import time
+import math
+from random import Random, shuffle
+import copy
+
+#import warnings
+
+from .solution import Solution
+#warnings.filterwarnings("ignore")
+
+class Test:
+
+    def __init__(self, message):
+        self.message = message
+    def print(self):
+        print(self.message)
+
