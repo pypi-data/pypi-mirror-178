c7f37216fb5deeb053943954570f4187400b326f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:47 +0000

    Add package cldfviz/0.10.0/cldfviz-0.10.0-py2.py3-none-any.whl

---
+++ packages/cldfviz/cldfviz-0.10.0-py2.py3-none-any.whl/cldfviz/commands/treemap.py
@@ -0,0 +1,185 @@
+"""
+Plot values for a parameter of the dataset against a Glottolog family tree.
+
+FIXME:
+Make configurable:
+- tree (from file? from string? from phlorest phylogeny!)
+  - get the summary tree from a CLDF Phylogeny dataset
+  - match nodes by Glottocode
+- tree labels (which property from LanguageTable?)
+- value labels and order
+- config file?
+
+Make this work with
+- metadata-free datasets (i.e. values.csv only, metadata from Glottolog)
+
+"""
+import io
+import pathlib
+import webbrowser
+import collections
+
+import newick
+from pyglottolog.objects import Glottocode
+from pycldf.cli_util import add_dataset, get_dataset
+from cldfbench.cli_util import add_catalog_spec, IGNORE_MISSING
+
+try:
+    import pandas as pd
+    from Bio import Phylo
+    import yaml
+    import lingtreemaps
+except ImportError:
+    lingtreemaps = None
+
+
+class DF:
+    def __init__(self):
+        self.df = None
+        self.acc = collections.defaultdict(list)
+
+    def __enter__(self):
+        return self
+
+    def add(self, item):
+        if self.df is None:
+            self.df = pd.DataFrame(columns=list(item))
+        for k, v in item.items():
+            self.acc[k].append(v)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        for k, v in self.acc.items():
+            self.df[k] = v
+
+
+def df_from_dicts(dicts):
+    with DF() as df:
+        for d in dicts:
+            df.add(d)
+    return df.df
+
+
+def newick_safe_name(s):
+    return s.replace(' ', '_').replace('(', '').replace(')', '').replace("'", '')
+
+
+def yaml_type(s):
+    return yaml.load(io.StringIO(s), yaml.SafeLoader)
+
+
+def iter_ltm_options():
+    h = []
+    if lingtreemaps:
+        for line in pathlib.Path(lingtreemaps.__file__).parent\
+                .joinpath('data', 'default_config.yaml').read_text(encoding='utf8').split('\n'):
+            line = line.strip()
+            if line.startswith('#'):
+                h.append(line[1:].strip())
+            elif line:
+                opt, _, default = line.partition(':')
+                opt, val, help = opt.strip(), yaml_type(default.strip()), ' '.join(h)
+                if opt == 'filename':
+                    help = "The filename. If unspecified, the parameter ID will be used."
+                yield opt, val, help
+                h = []
+
+
+def register(parser):
+    add_dataset(parser)
+    add_catalog_spec(parser, 'glottolog', default=IGNORE_MISSING)
+    parser.add_argument('parameter')
+    parser.add_argument(
+        'tree',
+        help="Tree specified as Glottocode (interpreted as the root of the Glottolog tree), "
+             "Newick formatted string or path to a file containing the Newick formatted "
+             "tree.")
+    parser.add_argument(
+        '--tree-label-property',
+        help="Name of the language property used to identify languages in the tree.",
+        default='glottocode')
+    for opt, default, help in iter_ltm_options():
+        parser.add_argument(
+            '--ltm-{}'.format(opt.replace('_', '-')),
+            default=default,
+            type=yaml_type,
+            help=help)
+
+
+def run(args):
+    if lingtreemaps is None:
+        args.log.error(
+            'install cldfviz with lingtreemaps, running "pip install cldfviz[lingtreemaps]"')
+        return
+
+    ds = get_dataset(args)
+
+    # 1. Get the tree ...
+    if Glottocode.pattern.match(args.tree):
+        tree = args.glottolog.api.languoid(args.tree).newick_node(template="{l.id}")
+    elif pathlib.Path(args.tree).exists():
+        tree = newick.read(args.tree)[0]
+    else:
+        tree = newick.loads(args.tree)
+    # ... and its set of node labels.
+    nodes = {n.name for n in tree.walk()}
+
+    # 2. Get the set of matching languages in the dataset.
+    id2name = {}
+    treelabel2name = {}
+    languages = []
+    for lang in ds.iter_rows('LanguageTable', 'id', 'glottocode', 'name', 'latitude', 'longitude'):
+        treelabel = lang[args.tree_label_property]
+        if treelabel in nodes:  # The language is in the tree.
+            name = newick_safe_name(lang['name'])
+            languages.append(dict(ID=name, Latitude=lang['latitude'], Longitude=lang['longitude']))
+            id2name[lang['id']] = name
+            treelabel2name[treelabel] = name
+
+    # 3. Get the set of values.
+    if 'CodeTable' in ds:
+        codes = collections.OrderedDict([
+            (c['id'], c['name'])
+            for c in ds.iter_rows('CodeTable', 'id', 'name', 'parameterReference')
+            if c['parameterReference'] == args.parameter])
+    else:
+        codes = None
+    values = []
+    for val in ds.iter_rows('ValueTable', 'parameterReference', 'languageReference', 'codeReference', 'value'):
+        if val['parameterReference'] == args.parameter and val['languageReference'] in id2name:
+            values.append(dict(
+                Clade=id2name[val['languageReference']],
+                Value=val['value'] if not codes else codes[val['codeReference']]))
+    # Sort values by value/code. ltm will just use this order.
+    values = sorted(values, key=lambda v: v['Value'] if not codes else list(codes.values()).index(v['Value']))
+
+    # 4. Prune the tree to only the languages in the dataset.
+    tree.prune_by_names(list(treelabel2name), inverse=True)
+
+    # 5. Rename the tree nodes
+    def rename(n):
+        if n.name in treelabel2name:
+            n.name = treelabel2name[n.name]
+        return n
+    tree.visit(rename)
+
+    # We have to restrict languages and values to what is now a leaf node!
+    # I.e. when a language and one of its dialects is in the dataset, we remove the language.
+    nodes = set()
+    tree.visit(lambda n: nodes.add(n.name if n.is_leaf else None))
+    leafs = set(n for n in nodes if n is not None)
+    tree.prune_by_names(list(leafs - {v['Clade'] for v in values}))
+    languages = df_from_dicts([l for l in languages if l['ID'] in leafs])
+    values = df_from_dicts([v for v in values if v['Clade'] in leafs])
+
+    if 1:
+        print(tree)
+        print(languages)
+        print(values)
+
+    kwargs = {k.replace('ltm_', ''): v for k, v in args.__dict__.items() if k.startswith('ltm_')}
+    kwargs['filename'] = kwargs['filename'] or args.parameter
+    fname = pathlib.Path(
+        kwargs['filename'] if '.' in kwargs['filename'] else '{filename}.{file_format}'.format(**kwargs))
+    lingtreemaps.plot(languages, Phylo.read(io.StringIO(tree.newick), 'newick'), values, **kwargs)
+    args.log.info('Output written to: {}'.format(fname))
+    webbrowser.open(fname.resolve().as_uri())
---
+++ packages/cldfviz/cldfviz-0.10.0-py2.py3-none-any.whl/cldfviz/multiparameter.py
@@ -0,0 +1,216 @@
+import typing
+import decimal
+import functools
+import itertools
+import collections
+
+import attr
+import pycldf
+from pyglottolog.languoids import Languoid
+
+CONTINUOUS = 1
+CATEGORICAL = 2
+
+
+@attr.s
+class Language:
+    id = attr.ib()
+    name = attr.ib()
+    lat = attr.ib()
+    lon = attr.ib()
+
+    @classmethod
+    def from_object(cls, obj, glottolog=None):
+        lonlat = obj.lonlat
+        gcode = getattr(obj.cldf, 'glottocode', obj.id)
+        # FIXME: If a language is mapped to multiple glottocodes, we could try to take the midpoint
+        # of these as coordinate. (If longitudes have different signs, transform back and forth
+        # appropriately, i.e. lon < 0 => lon = 360 - abs(lon))
+        # shapely.geometry.MultiPoint([(0, 0), (1, 1)]).convex_hull.centroid
+        glang = None if isinstance(gcode, list) else (glottolog or {}).get(gcode)
+        if (not lonlat) and glang and glang.latitude is not None:
+            lonlat = (glang.longitude, glang.latitude)
+        if lonlat and lonlat[0] is not None:
+            return cls(
+                id=obj.id,
+                name=getattr(obj.cldf, 'name', obj.id),
+                lon=float(lonlat[0]),
+                lat=float(lonlat[1]))
+
+    @classmethod
+    def from_glottolog(cls, lid, glottolog):
+        if glottolog:
+            glang = glottolog.get(lid)
+            if glang and glang.latitude is not None:
+                return cls(id=glang.id, name=glang.name, lat=glang.latitude, lon=glang.longitude)
+
+
+@attr.s
+class Parameter:
+    id = attr.ib()
+    name = attr.ib()
+    type = attr.ib(default=CATEGORICAL)
+    domain = attr.ib(default=attr.Factory(dict))
+    value_to_code = attr.ib(default=attr.Factory(dict))
+
+    @classmethod
+    def from_object(cls, obj):
+        return cls(id=obj.id, name=getattr(obj.cldf, 'name', obj.id))
+
+
+@functools.total_ordering
+@attr.s(order=False, eq=False)
+class Value:
+    v = attr.ib()
+    pid = attr.ib()
+    lid = attr.ib()
+    code = attr.ib()
+    float = attr.ib(default=None)
+
+    def __attrs_post_init__(self):
+        try:
+            self.float = float(self.v)
+        except (ValueError, TypeError):
+            self.float = None
+
+    def __eq__(self, other):
+        return (self.lid, self.pid, self.v) == (other.lid, other.pid, other.v)
+
+    def __lt__(self, other):
+        return (self.lid, self.pid, self.v) < (other.lid, other.pid, other.v)
+
+    @classmethod
+    def from_row(cls, row, codes):
+        return cls(
+            v=row.get('codeReference') or row['value'],
+            lid=row['languageReference'],
+            pid=row['parameterReference'],
+            code=codes[row['parameterReference']][row['codeReference']]
+            if row['parameterReference'] in codes and row['codeReference'] else None,
+        )
+
+
+class MultiParameter:
+    """
+    Extracts relevant data about a set of parameters from a CLDF dataset.
+
+    :ivar parameters: `OrderedDict` mapping parameter IDs to :class:`Parameter` instances.
+    """
+    def __init__(self,
+                 ds: pycldf.Dataset,
+                 pids: typing.Iterable[str],
+                 datatypes: typing.Iterable[str] = None,
+                 include_missing: bool = False,
+                 glottolog: typing.Optional[typing.Dict[str, Languoid]] = None,
+                 language_properties: typing.Optional[typing.Iterable[str]] = None):
+        self.include_missing = include_missing
+        language_properties = language_properties or []
+        if 'LanguageTable' in ds:
+            langs = {lg.id: Language.from_object(lg, glottolog=glottolog)
+                     for lg in ds.objects('LanguageTable')}
+        else:
+            langs = {gc: Language.from_glottolog(gc, glottolog=glottolog)
+                     for gc in set(r['languageReference'] for r in
+                                   ds.iter_rows('ValueTable', 'languageReference'))}
+        langs = {k: v for k, v in langs.items() if v}
+        params = {p.id: Parameter.from_object(p)
+                  for p in ds.objects('ParameterTable')} if 'ParameterTable' in ds else {}
+        # For each pid, we add a parameter:
+        self.parameters = collections.OrderedDict(
+            [(pid, params.get(pid, Parameter(id=pid, name=pid))) for pid in pids])
+        # For each language-property we add a parameter:
+        for language_property in language_properties:
+            self.parameters[language_property] = Parameter(
+                id=language_property, name=language_property)
+        if not self.parameters:
+            # No parameters and no language property specified: Just plot language locations.
+            self.parameters['__language__'] = Parameter(id='__language__', name='language')
+
+        codes = collections.defaultdict(collections.OrderedDict)
+        if 'CodeTable' in ds:
+            for row in ds.iter_rows('CodeTable', 'id', 'parameterReference', 'name'):
+                if row['parameterReference'] in self.parameters:
+                    codes[row['parameterReference']][row['id']] = row['name']
+        language_rows = []
+        for i, language_property in enumerate(language_properties):
+            if i == 0:
+                language_rows = list(ds.iter_rows('LanguageTable', 'id', 'name'))
+            if not all(isinstance(v[language_property], (int, float, decimal.Decimal))
+                       for v in language_rows if v[language_property] is not None):
+                codes[language_property] = collections.OrderedDict([
+                    (p, p) for p in sorted(
+                        set(r[language_property] for r in language_rows
+                            if r[language_property] is not None))
+                ])
+        self.languages = collections.OrderedDict()
+        self.values = []
+        colmap = ['languageReference', 'parameterReference', 'value']
+        if codes:
+            colmap.append('codeReference')
+        if pids:
+            comp = 'ValueTable' if ds.module == 'StructureDataset' else 'FormTable'
+            for val in ds.iter_rows(comp, *colmap):
+                if ((val['value'] is not None) or self.include_missing) and \
+                        val['parameterReference'] in self.parameters:
+                    lang = langs.get(val['languageReference'])
+                    if lang:
+                        self.languages[val['languageReference']] = lang
+                        self.values.append(Value.from_row(val, codes))
+                        self.parameters[val['parameterReference']] \
+                            .value_to_code[str(val['value'])] = \
+                            val.get('codeReference') or val['Value']
+        for language_property in language_properties:
+            for lang in language_rows:
+                if (lang['id'] in langs) and lang[language_property] is not None:
+                    if lang['id'] in langs:
+                        self.languages.setdefault(lang['id'], langs[lang['id']])
+                        self.values.append(Value(
+                            v=lang[language_property],
+                            pid=language_property,
+                            lid=lang['id'],
+                            code=language_property))
+        if not self.values:
+            # No parameters and no language property specified: Just plot language locations.
+            if 'LanguageTable' not in ds:
+                for lid, lang in langs.items():
+                    self.languages.setdefault(lid, lang)
+                    self.values.append(Value(
+                        v='y',
+                        pid='__language__',
+                        lid=lid,
+                        code='language'))
+            else:
+                for lang in ds.iter_rows('LanguageTable', 'id', 'name'):
+                    if lang['id'] in langs:
+                        self.languages.setdefault(lang['id'], langs[lang['id']])
+                        self.values.append(Value(
+                            v='y',
+                            pid='__language__',
+                            lid=lang['id'],
+                            code='language'))
+
+        for i, p in enumerate(self.parameters.values()):
+            if p.id in codes:
+                p.domain = codes[p.id]
+            else:
+                vals = [v for v in self.values if v.pid == p.id]
+                if all(v.float is not None for v in vals) and \
+                        (len(set(v.v for v in vals)) > 8 or  # noqa: W504
+                         (datatypes and datatypes[i] == 'number')):
+                    p.type = CONTINUOUS
+                    p.domain = (min(v.float for v in vals), max(v.float for v in vals))
+                else:
+                    p.domain = collections.OrderedDict([
+                        (v, v) for v in sorted(set(vv.v for vv in vals), key=lambda vv: str(vv))])
+
+    def __str__(self):  # pragma: no cover
+        return str(self.parameters)
+
+    def iter_languages(self) \
+            -> typing.Iterator[typing.Tuple[Language, typing.Dict[str, typing.List[Value]]]]:
+        for lid, values in itertools.groupby(sorted(self.values), lambda v: v.lid):
+            values = {pid: list(vals) for pid, vals in itertools.groupby(values, lambda v: v.pid)}
+            values = collections.OrderedDict(
+                [(pid, values.get(pid, [])) for pid in self.parameters])
+            if self.include_missing or all(bool(v) for v in values.values()):
+                yield self.languages[lid], values
