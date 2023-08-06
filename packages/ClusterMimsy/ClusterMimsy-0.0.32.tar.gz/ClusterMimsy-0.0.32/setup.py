4a8ff913731628c1296f1c4124699678edf08ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:49 +0000

    Add package ClusterMimsy/0.0.32/ClusterMimsy-0.0.32.tar.gz

---
+++ packages/ClusterMimsy/ClusterMimsy-0.0.32.tar.gz/ClusterMimsy-0.0.32/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+VERSION = '0.0.32' 
+DESCRIPTION = 'A package for the ClusterMI algorithm'
+LONG_DESCRIPTION = 'A package for the ClusterMI algorithm'
+
+# Setting up
+setup(
+        name="ClusterMimsy", 
+        version=VERSION,
+        author="ClusterMimsyDev",
+        author_email="<ClusterMimsyDev@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=['ClusterMimsy'],
+        install_requires=[], # add any additional packages that 
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
