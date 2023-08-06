7b89b8f9095347983b44c6ca5228f50f4893e215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:56 +0000

    Add package jiale0709/0.0.2/jiale0709-0.0.2.tar.gz

---
+++ packages/jiale0709/jiale0709-0.0.2.tar.gz/jiale0709-0.0.2/jiale0709/src/jiale0709.py
@@ -0,0 +1,30 @@
+# Info, Version
+def info():
+    print("This is Jia Le's Python Library")
+    print('This Library is Developed by Jia Le')
+    
+def version():
+    print('Jia Le Version : v0.0.2') 
+    print('Release Date : 11/27/2022')
+
+# Math: Add, Plus (+) 
+def plus_numbers(num1, num2):
+    return num1 + num2    
+    
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Math: Substract, Minus (-)
+def substract_numbers(num1, num2):
+    return num1 - num2
+
+def minus_numbers(num1, num2):
+    return num1 - num2
+
+# Math: Multiply (x, *)
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+# Math: Divide (÷, /)
+def divide_numbers(num1, num2):
+    return num1 / num2
