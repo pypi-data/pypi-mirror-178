c292e06badbc2a70bfea002056db67e7201bcc79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:19 +0000

    Add package django-docs/0.3.3/django_docs-0.3.3-py3-none-any.whl

---
+++ packages/django-docs/django_docs-0.3.3-py3-none-any.whl/docs/urls.py
@@ -0,0 +1,13 @@
+from django.urls.conf import re_path
+from docs.views import DocsRootView, serve_docs, DOCS_DIRHTML
+
+urlpatterns = []
+
+if not DOCS_DIRHTML:
+    urlpatterns += [
+        re_path(r'^$', DocsRootView.as_view(permanent=True), name='docs_root'),
+    ]
+
+urlpatterns += [
+    re_path(r"^(?P<path>.*)$", serve_docs, name="docs_files"),
+]
