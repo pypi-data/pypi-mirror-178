934cbde6735c43972c6577b8b01eeb11d524c0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:04 +0000

    Add package recommendation8/0.1/recommendation8-0.1-py2.py3-none-any.whl

---
+++ packages/recommendation8/recommendation8-0.1-py2.py3-none-any.whl/test/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name='recommendation8',
+    version='0.1',
+    author="Lohitha",
+    author_email="lohitha.m003@gmail.com",
+    description="Food Recommendation system",
+    packages=setuptools.find_packages(),
+    Classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+
+    ],
+)
