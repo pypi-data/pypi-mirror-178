2bb32a084c6c54f4b0437f88c3d2bbdcb5659cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:37 +0000

    Add package hitsave/0.0.2/hitsave-0.0.2.tar.gz

---
+++ packages/hitsave/hitsave-0.0.2.tar.gz/hitsave-0.0.2/src/hitsave/__main__.py
@@ -0,0 +1,8 @@
+
+import typer
+
+def thanks():
+    print("Thanks for downloading hitsave. We are working hard to release the library and CLI tool. Watch this space!")
+
+if __name__ == "__main__":
+    typer.run(thanks)
