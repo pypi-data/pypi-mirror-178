afbfc673490caa57abac9359227955207bab03d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:34 +0000

    Add package tc-pysh/0.4.1/tc_pysh-0.4.1-py3-none-any.whl

---
+++ packages/tc-pysh/tc_pysh-0.4.1-py3-none-any.whl/pysh/__main__.py
@@ -0,0 +1,36 @@
+import sys
+
+from typing import Union, Callable
+
+from . import *
+from .path import *
+from . import ls as _ls
+from . import cd as _cd
+from .path import cwd as _cwd
+from .command import Command
+from .interpreter import Interpreter
+
+ls = Command(_ls)
+cd = Command(_cd)
+cwd = Command(_cwd)
+
+
+def main():
+    interp.interact()
+
+
+def set_ps1(prompt: Union[str, Callable]):
+    if callable(prompt):
+        class P:
+            def __str__(self):
+                return prompt()
+        sys.ps1 = P()
+    else:
+        sys.ps1 = prompt
+
+
+interp = Interpreter(local=locals())
+
+
+if __name__ == "__main__":
+    main()
