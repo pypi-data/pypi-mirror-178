476d6ef141d18c6f35a492768dcd13e7f77987cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:01 +0000

    Add package king-paint/1.1/king-paint-1.1.tar.gz

---
+++ packages/king-paint/king-paint-1.1.tar.gz/king-paint-1.1/src/king/alg/test/test02.py
@@ -0,0 +1,6 @@
+from src import king
+
+
+c = king.conic([0, 1, 2, 0, 3, 0])
+# c = king.conic(p_list=king.point(p_list=[[3, 5], [-4, 2], [-4, 9], [0, 2], [5, 4]]))
+# c = king.conic(p_list=king.point(p_list=[[1, 0], [2, 0], [3, 0], [4, 0], [5, 0]]))
