696c053b5f4d35851b3cd16551739caf090c81e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:38 +0000

    Add package otrs-somconnexio/0.4.33/otrs_somconnexio-0.4.33-py2.py3-none-any.whl

---
+++ packages/otrs-somconnexio/otrs_somconnexio-0.4.33-py2.py3-none-any.whl/tests/otrs_models/test_provision_article.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+import unittest
+from mock import Mock, patch
+
+from otrs_somconnexio.otrs_models.provision_article import ProvisionArticle 
+
+
+class ProvisionArticleTestCase(unittest.TestCase):
+
+    @patch('otrs_somconnexio.otrs_models.abstract_article.Article')
+    def test_call_with_notes(self, MockArticle):
+
+        expected_article_arguments = {
+            "Subject": "Sol·licitud a b",
+            "Body": "c",
+            "ContentType": "text/plain; charset=utf8",
+            "IsVisibleForCustomer": "0",
+        }
+        ProvisionArticle(service_type='a', order_id='b', notes='c').call()
+        MockArticle.assert_called_once_with(expected_article_arguments)
+
+    @patch('otrs_somconnexio.otrs_models.abstract_article.Article')
+    def test_call_without_notes(self, MockArticle):
+
+        expected_article_arguments = {
+            "Subject": "Sol·licitud a b",
+            "Body": "",
+            "ContentType": "text/plain; charset=utf8",
+            "IsVisibleForCustomer": "1",
+        }
+        ProvisionArticle(service_type='a', order_id='b', notes='').call()
+        MockArticle.assert_called_once_with(expected_article_arguments)
