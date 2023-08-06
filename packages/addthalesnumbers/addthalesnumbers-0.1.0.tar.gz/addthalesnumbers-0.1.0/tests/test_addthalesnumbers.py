738cd4f2f432faaf30a37276eafd8d296ba689fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:53 +0000

    Add package addthalesnumbers/0.1.0/addthalesnumbers-0.1.0.tar.gz

---
+++ packages/addthalesnumbers/addthalesnumbers-0.1.0.tar.gz/addthalesnumbers-0.1.0/tests/test_addthalesnumbers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `addthalesnumbers` package."""
+
+
+import unittest
+
+from addthalesnumbers import addthalesnumbers
+
+
+class TestAddthalesnumbers(unittest.TestCase):
+    """Tests for `addthalesnumbers` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
