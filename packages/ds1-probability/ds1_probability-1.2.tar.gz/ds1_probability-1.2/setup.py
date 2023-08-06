3b897ca261115668a4c1560109ff4c37c1795147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:36 +0000

    Add package ds1-probability/1.2/ds1_probability-1.2.tar.gz

---
+++ packages/ds1-probability/ds1_probability-1.2.tar.gz/ds1_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ds1_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['ds1_probability'],
+      author = 'Jiawei Chen',
+      author_email = 'Jiawei1626navy@gmail.com',
+      zip_safe=False)
