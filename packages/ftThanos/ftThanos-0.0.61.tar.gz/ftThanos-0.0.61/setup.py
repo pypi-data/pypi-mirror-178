6783006c9dec7f526a786ead694332fbf1542953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:02:20 +0000

    Add package ftThanos/0.0.61/ftThanos-0.0.61.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.61.tar.gz/ftThanos-0.0.61/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.61",
+description="A client for 61 API",
+author="Thanos 61",
+author_email="thanos@61paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
