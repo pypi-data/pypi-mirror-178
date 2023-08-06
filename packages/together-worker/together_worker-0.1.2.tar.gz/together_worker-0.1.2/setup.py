8c9ff3910d41f5751b67bb2dbe5e22d399004926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:00 +0000

    Add package together-worker/0.1.2/together_worker-0.1.2.tar.gz

---
+++ packages/together-worker/together_worker-0.1.2.tar.gz/together_worker-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['together_worker', 'together_worker.profiler']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dacite>=1.6.0,<2.0.0',
+ 'influxdb-client>=1.34.0,<2.0.0',
+ 'netifaces>=0.11.0,<0.12.0',
+ 'pynvml>=11.4.1,<12.0.0',
+ 'together-web3>=0.1.0,<0.2.0']
+
+setup_kwargs = {
+    'name': 'together-worker',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': '# together_worker\n',
+    'author': 'together',
+    'author_email': 'together@together.xyz',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/together-computer/together_worker',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.2,<3.11',
+}
+
+
+setup(**setup_kwargs)
