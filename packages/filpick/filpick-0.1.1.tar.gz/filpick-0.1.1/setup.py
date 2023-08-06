bf81d2fbeccf96e91af70f314b47eee46d0ad3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:07 +0000

    Add package filpick/0.1.1/filpick-0.1.1.tar.gz

---
+++ packages/filpick/filpick-0.1.1.tar.gz/filpick-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['filpick']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dacite>=1.6.0,<2.0.0', 'fire>=0.4.0,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['filpick = filpick:main']}
+
+setup_kwargs = {
+    'name': 'filpick',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': 'aoout',
+    'author_email': 'wuz66280@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
