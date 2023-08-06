e6dbb7c0a704fe4bf036393ee4fc3ef16904136e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:22:04 +0000

    Add package scipp/22.11.0/scipp-22.11.0-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/scipp/scipp-22.11.0-cp310-cp310-macosx_10_15_x86_64.whl/scipp/include/scipp/dataset/generated_arithmetic.h
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#include "scipp/dataset/negative.h"
+#include "scipp/dataset/add.h"
+#include "scipp/dataset/subtract.h"
+#include "scipp/dataset/multiply.h"
+#include "scipp/dataset/divide.h"
+#include "scipp/dataset/floor_divide.h"
+#include "scipp/dataset/mod.h"
+#include "scipp/dataset/add_equals.h"
+#include "scipp/dataset/subtract_equals.h"
+#include "scipp/dataset/multiply_equals.h"
+#include "scipp/dataset/divide_equals.h"
+#include "scipp/dataset/mod_equals.h"
