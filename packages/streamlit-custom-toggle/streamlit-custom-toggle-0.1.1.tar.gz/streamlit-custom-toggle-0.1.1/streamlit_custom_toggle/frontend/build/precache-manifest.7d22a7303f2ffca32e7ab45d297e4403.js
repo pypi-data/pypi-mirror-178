7a6e81a8d2aa6478833669fcb0fde958fe0824b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:41 +0000

    Add package streamlit-custom-toggle/0.1.1/streamlit-custom-toggle-0.1.1.tar.gz

---
+++ packages/streamlit-custom-toggle/streamlit-custom-toggle-0.1.1.tar.gz/streamlit-custom-toggle-0.1.1/streamlit_custom_toggle/frontend/build/precache-manifest.7d22a7303f2ffca32e7ab45d297e4403.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e9e1b920eb81b37195b6dc38e09b8a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fcb76f2fdfc9347c1df8",
+    "url": "./static/js/2.7b19a51b.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.7b19a51b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd55007d87b65803a90",
+    "url": "./static/js/main.181af669.chunk.js"
+  },
+  {
+    "revision": "3c1f40f41587097ea834",
+    "url": "./static/js/runtime-main.3a596aa5.js"
+  }
+]);
