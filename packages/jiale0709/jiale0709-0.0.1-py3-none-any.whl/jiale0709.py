f7a0c7c96e7824fd6305ea402479220fad754948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:24 +0000

    Add package jiale0709/0.0.1/jiale0709-0.0.1-py3-none-any.whl

---
+++ packages/jiale0709/jiale0709-0.0.1-py3-none-any.whl/jiale0709.py
@@ -0,0 +1,27 @@
+# Info
+def info():
+    print('************************************************')
+    print('This Package is Developed by JiaLe')
+    print('Jia Le Version : v0.0.1')
+    print('************************************************')
+    return()
+
+# Basic Math Section
+    # Math: Add (+) 
+def add_numbers(num1, num2):
+    return num1 + num2
+
+    # Math: Substract,Minus (-)
+def substract_numbers(num1, num2):
+    return num1 - num2
+
+def minus_numbers(num1, num2):
+    return num1 - num2
+
+    # Math: Multiply (x,*)
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+    # Math: Divide (÷,/)
+def divide_numbers(num1, num2):
+    return num1 / num2
