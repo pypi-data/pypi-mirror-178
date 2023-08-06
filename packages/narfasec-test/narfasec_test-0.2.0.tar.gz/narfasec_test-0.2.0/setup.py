bb25faaf0b537be72e2f2b86eff4d46924c499a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:52 +0000

    Add package narfasec-test/0.2.0/narfasec_test-0.2.0.tar.gz

---
+++ packages/narfasec-test/narfasec_test-0.2.0.tar.gz/narfasec_test-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['example_package_narfasec']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['sherlog = src.example_package_narfasec.entry:main']}
+
+setup_kwargs = {
+    'name': 'narfasec-test',
+    'version': '0.2.0',
+    'description': 'Just a test',
+    'long_description': '# Example Package\n\nThis is a simple example package. You can use\n[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)\nto write your content.',
+    'author': 'narfasec',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
