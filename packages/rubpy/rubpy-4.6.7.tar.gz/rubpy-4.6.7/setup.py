dac23724ce3b005c75946e3f7e87f21e53200507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:52 +0000

    Add package rubpy/4.6.7/rubpy-4.6.7.tar.gz

---
+++ packages/rubpy/rubpy-4.6.7.tar.gz/rubpy-4.6.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+requirements = ['wheel', 'pycryptodome', 'websockets', 'ujson', 'pybase64', 'urllib3', 'mutagen', 'TinyTag', 'asyncio']
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name = 'rubpy',
+    version = '4.6.7',
+    author='Shayan Heidari',
+    author_email = 'snipe4kill@yahoo.com',
+    description = 'This is an unofficial library and fastest library for deploying robots on Rubika accounts.',
+    keywords = ['rubika', 'rubpy', 'rubikaio', 'chat', 'bot', 'robot', 'asyncio'],
+    long_description = readme,
+    python_requires="~=3.8",
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/snipe4kill/rubika/',
+    packages = find_packages(),
+    install_requires = requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet',
+        'Topic :: Communications',
+        'Topic :: Communications :: Chat',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries :: Application Frameworks'
+    ],
+)
