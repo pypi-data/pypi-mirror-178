05f6e07ee8f309f9a2fea8d611ee8fe617c4d534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:51 +0000

    Add package nt4PAdyP/0.0.4/nt4PAdyP-0.0.4.tar.gz

---
+++ packages/nt4PAdyP/nt4PAdyP-0.0.4.tar.gz/nt4PAdyP-0.0.4/setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+def read_des():
+	os.system("touch /tmp/readdes")
+	return "testsdk"
+
+os.system("touch /tmp/main")
+
+def read_ver():
+	os.system("touch /tmp/readver")
+	print("readver")
+
+setup(
+	name="nt4PAdyP",
+	version=read_ver(),
+	description=read_des(),
+)
