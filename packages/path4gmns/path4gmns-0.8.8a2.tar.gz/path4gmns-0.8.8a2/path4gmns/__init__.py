bf81418d197bfaa61e5ed092e85ec19601cfeef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:22 +0000

    Add package path4gmns/0.8.8a2/path4gmns-0.8.8a2.tar.gz

---
+++ packages/path4gmns/path4gmns-0.8.8a2.tar.gz/path4gmns-0.8.8a2/path4gmns/__init__.py
@@ -0,0 +1,11 @@
+from .accessibility import *
+from .colgen import *
+from .utils import *
+from .zonesyn import *
+
+
+__version__ = '0.8.8a2'
+
+
+# print out the current version
+print(f'path4gmns, version {__version__}')
