a822a6289d758e6477ec45f5b66cbf527a54ba71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:19 +0000

    Add package bloock/2.0.0/bloock-2.0.0-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/bloock/bloock-2.0.0-cp310-cp310-macosx_11_0_arm64.whl/bloock/client/entity/encrypter.py
@@ -0,0 +1,28 @@
+from abc import abstractmethod
+import bloock._bridge.proto.record_pb2 as proto
+
+
+class EncrypterArgs:
+    def __init__(self, password: str) -> None:
+        self.password = password
+
+    def to_proto(self) -> proto.EncrypterArgs:
+        return proto.EncrypterArgs(password=self.password)
+
+
+class Encrypter:
+    def __init__(self, alg: proto.EncryptionAlg.ValueType, args: EncrypterArgs) -> None:
+        self.alg = alg
+        self.args = args
+
+    @abstractmethod
+    def to_proto(self):
+        raise NotImplementedError
+
+
+class AesEncrypter(Encrypter):
+    def __init__(self, password: str) -> None:
+        super().__init__(alg=proto.A256GCM, args=EncrypterArgs(password))
+
+    def to_proto(self) -> proto.Encrypter:
+        return proto.Encrypter(alg=self.alg, args=self.args.to_proto())
