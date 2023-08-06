1a207ae0cda0eabea63cf1b57430c52b88136862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:26 +0000

    Add package opentelemetry_wrapper/0.0.6/opentelemetry_wrapper-0.0.6.tar.gz

---
+++ packages/opentelemetry_wrapper/opentelemetry_wrapper-0.0.6.tar.gz/opentelemetry_wrapper-0.0.6/opentelemetry_wrapper/__init__.py
@@ -0,0 +1,31 @@
+"""
+a wrapper around `opentelemetry` and `opentelemetry-instrumentation-*` to make life a bit easier
+"""
+
+__version__ = '0.0.6'
+
+from opentelemetry_wrapper.instrument_dataclasses import instrument_dataclasses
+from opentelemetry_wrapper.instrument_decorator import instrument_decorate
+from opentelemetry_wrapper.instrument_fastapi import instrument_fastapi
+from opentelemetry_wrapper.instrument_logging import instrument_logging
+from opentelemetry_wrapper.instrument_requests import instrument_requests
+
+
+@instrument_decorate
+def instrument_all():
+    # todo: accept kwargs to disable/enable some of these
+    instrument_dataclasses()
+    instrument_logging()
+    instrument_fastapi()
+    instrument_requests()
+
+
+__all__ = (
+    __version__,
+    instrument_decorate,
+    instrument_dataclasses,
+    instrument_logging,
+    instrument_fastapi,
+    instrument_requests,
+    instrument_all,
+)
