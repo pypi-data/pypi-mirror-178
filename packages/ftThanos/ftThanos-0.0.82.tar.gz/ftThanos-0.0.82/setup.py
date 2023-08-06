89ae67dcbbb34991aec8a79d49e8fb8bf580aa09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:00:57 +0000

    Add package ftThanos/0.0.82/ftThanos-0.0.82.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.82.tar.gz/ftThanos-0.0.82/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.82",
+description="A client for 82 API",
+author="Thanos 82",
+author_email="thanos@82paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
