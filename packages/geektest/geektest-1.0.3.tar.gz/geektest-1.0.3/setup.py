a97e26abed2ae406d19cfaaf99c8d5414bcb16c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:02:19 +0000

    Add package geektest/1.0.3/geektest-1.0.3.tar.gz

---
+++ packages/geektest/geektest-1.0.3.tar.gz/geektest-1.0.3/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="geektest", version='1.0.3', author='geek', description='simple tutorial package', long_description='simple python package test', packages=["modes"], install_requires=['cloudscraper'])
