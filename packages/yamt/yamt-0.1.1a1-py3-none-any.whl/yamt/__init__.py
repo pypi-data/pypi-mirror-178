42876edf9691d9960da6ddfa995a21a222a7299e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:42 +0000

    Add package yamt/0.1.1a1/yamt-0.1.1a1-py3-none-any.whl

---
+++ packages/yamt/yamt-0.1.1a1-py3-none-any.whl/yamt/__init__.py
@@ -0,0 +1,32 @@
+from .collections import (
+    ChainedSequence,
+    ChainedList,
+    ChainedDeque,
+)
+from .enum import (
+    DataRichEnum,
+    IntDataRichEnum,
+)
+from .asyncio_sync_primitives import (
+    StackLimitedLock,
+    SemaphorePerSecond,
+)
+from .misc import (
+    WordForm,
+    FormedWord,
+    Sentinel,
+    SingletonMeta,
+    recursive_base_attributes,
+    split_on_chuncks,
+    simple_chain,
+    mapdefault,
+    sentinel,
+)
+from .asyncio_misc import (
+    AsyncEventManager,
+    autogather,
+    amapdefault,
+)
+from .typing import (
+    SupportsRichComparison,
+)
