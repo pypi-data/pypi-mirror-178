b21d567695efd6953cd3788b944668626d98e1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:00:45 +0000

    Add package ftThanos/0.0.78/ftThanos-0.0.78.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.78.tar.gz/ftThanos-0.0.78/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.78",
+description="A client for 78 API",
+author="Thanos 78",
+author_email="thanos@78paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
