34bf01635c90969273c74d31e83944fae59e02b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:58 +0000

    Add package bits-lang/1.7/bits-lang-1.7.tar.gz

---
+++ packages/bits-lang/bits-lang-1.7.tar.gz/bits-lang-1.7/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+long_description = 'A very annoying language - read the docs at https://www.github.com/nayakrujul/bits'
+
+setup(
+  name = 'bits-lang',
+  version = '1.7',
+  license='Apache',
+  description = 'A very annoying language',
+  author = 'Rujul Nayak',
+  author_email = 'rujulnayak@outlook.com',
+  url = 'https://github.com/nayakrujul/bits',
+  download_url = 'https://github.com/nayakrujul/bits/archive/refs/tags/v16.tar.gz',
+  keywords = ['golfing', 'code-golf', 'language'],
+  install_requires=[
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+  long_description=long_description,
+  long_description_content_type='text/x-rst',
+  packages = find_packages(),
+  entry_points = {
+    'console_scripts': [
+      'bits = bits.main:from_cmdline'
+    ]
+  }
+)
