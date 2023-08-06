d046b104135d459ea1f5e4a807eef29cec8e3564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:41 +0000

    Add package agile-north-build-essentials/0.0.4/agile_north_build_essentials-0.0.4.tar.gz

---
+++ packages/agile-north-build-essentials/agile_north_build_essentials-0.0.4.tar.gz/agile_north_build_essentials-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+setup(
+    name='agile_north_build_essentials',
+    version='0.0.4',
+    description="Python scripts to assist in setting up project dependencies in order to build and run projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["agile_north_build_essentials"],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License"
+    ],
+    url="https://github.com/agile-north/build-essentails",
+    author="Stefan Venter",
+    author_email="stefan@nrth.com",
+    extras_require={
+        "dev": [
+            "pytest>=2.7"
+        ]
+    }
+)
