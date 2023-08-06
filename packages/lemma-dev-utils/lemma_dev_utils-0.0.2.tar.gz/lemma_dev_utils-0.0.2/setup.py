cbeb357a76ee886445977a77bc22464b1f4fb06d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:08 +0000

    Add package lemma-dev-utils/0.0.2/lemma_dev_utils-0.0.2.tar.gz

---
+++ packages/lemma-dev-utils/lemma_dev_utils-0.0.2.tar.gz/lemma_dev_utils-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lemma_dev_utils',
+    version='0.0.2',
+    license='MIT',
+    author="Ludovico Lemma",
+    author_email='lwdovico@protonmail.com',
+    packages=find_packages('source'),
+    package_dir={'': 'source'},
+    url='https://github.com/ludovicolemma/lemma-dev-utils',
+    keywords='Utils',
+    install_requires=[
+          'tqdm',
+      ],
+
+)
---
+++ packages/lemma-dev-utils/lemma_dev_utils-0.0.2.tar.gz/lemma_dev_utils-0.0.2/source/lemma_dev_utils/__init__.py
@@ -0,0 +1,5 @@
+"""Small package with some useful functions I developed. Author: Ludovico Lemma"""
+
+__version__ = '0.0.1'
+
+from .zip_downloader import download, unzip, download_unzip
