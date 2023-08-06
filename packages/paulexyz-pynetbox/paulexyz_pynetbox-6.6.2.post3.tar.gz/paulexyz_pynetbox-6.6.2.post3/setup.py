f9f62d39b0b75dc471f7c5564681c6c1c2377e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:07 +0000

    Add package paulexyz-pynetbox/6.6.2.post3/paulexyz_pynetbox-6.6.2.post3.tar.gz

---
+++ packages/paulexyz-pynetbox/paulexyz_pynetbox-6.6.2.post3.tar.gz/paulexyz_pynetbox-6.6.2.post3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name="paulexyz_pynetbox",
+    description="NetBox API client library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Zach Moody",
+    author_email="zmoody@do.co",
+    license="Apache2",
+    include_package_data=True,
+    version="6.6.2.post3",
+    setup_requires=["setuptools_scm"],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=[
+        "requests>=2.20.0,<3.0",
+    ],
+    zip_safe=False,
+    keywords=["netbox"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
