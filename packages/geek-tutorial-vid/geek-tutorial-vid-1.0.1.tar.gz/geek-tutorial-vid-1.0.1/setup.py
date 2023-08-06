66e80c5222d63d6760d818f3b1ce13ababd247f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:58 +0000

    Add package geek-tutorial-vid/1.0.1/geek-tutorial-vid-1.0.1.tar.gz

---
+++ packages/geek-tutorial-vid/geek-tutorial-vid-1.0.1.tar.gz/geek-tutorial-vid-1.0.1/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="geek-tutorial-vid", version='1.0.1', author='geek', description='simple tutorial package', long_description='simple python package test', packages=find_packages(), install_requires=['cloudscraper'])
