b93e7fdb6d13ab2b804655885289e2f80561b1a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:09 +0000

    Add package mlphys/3.6.6/mlphys-3.6.6.tar.gz

---
+++ packages/mlphys/mlphys-3.6.6.tar.gz/mlphys-3.6.6/mlphys/__init__.py
@@ -0,0 +1,23 @@
+"""
+Please keep these lines. Thank you!
+"""
+__author__ = 'Sanjaya Lohani'
+__email__ = 'slohani@mlphys.com'
+__licence__ = 'Apache 2.0'
+__website__ = "sanjayalohani.com"
+
+
+print("""
+                *-* Machine Learning for Physical Sciences *-*
+**********************************************************************************
+                    #    # #      #####  #    # #   #  ####  
+                    ##  ## #      #    # #    #  # #  #      
+                    # ## # #      #    # ######   #    ####  
+                    #    # #      #####  #    #   #        # 
+                    #    # #      #      #    #   #   #    # 
+                    #    # ###### #      #    #   #    ####  
+**********************************************************************************
+                                                            slohani@mlphys.com
+""")
+
+
