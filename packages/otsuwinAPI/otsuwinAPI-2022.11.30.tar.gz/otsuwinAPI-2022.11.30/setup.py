65b2ed94557199d44802f2a17a2ab103dbbf034f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:01 +0000

    Add package otsuwinAPI/2022.11.30/otsuwinAPI-2022.11.30.tar.gz

---
+++ packages/otsuwinAPI/otsuwinAPI-2022.11.30.tar.gz/otsuwinAPI-2022.11.30/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='otsuwinAPI',
+    version='2022.11.30',
+    url='https://github.com/Otsuhachi/otsuwinAPI',
+    description='WindowsAPIを追加していくライブラリ。',
+    author='Otsuhachi',
+    author_email='agequodagis.tufuiegoeris@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
