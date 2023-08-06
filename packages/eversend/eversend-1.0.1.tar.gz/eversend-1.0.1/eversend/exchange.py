38fea7eb7e92478bbeb784b202f71e87c249c487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:07 +0000

    Add package eversend/1.0.1/eversend-1.0.1.tar.gz

---
+++ packages/eversend/eversend-1.0.1.tar.gz/eversend-1.0.1/eversend/exchange.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+from eversend.utils.api import API
+
+class Exchange(API):
+    def __init__(self, client_id, client_secret, version):
+        super(Exchange, self).__init__(client_id, client_secret, version)
+    
+    def getQuotation(self, source=None, destination=None, amount=None):
+        if not source:
+            raise ValueError('method required')
+        if not destination:
+            raise ValueError('currency required')
+        if not amount:
+            raise ValueError('amount required')
+        payload = {'from': source, 'to': destination, 'amount': amount }
+        return self.call_api(path='/exchanges/quotation', method='post', data=payload)
+    
+    def exchange(
+            self,
+            token=None,
+            transactionRef=None,
+        ):
+        if not token:
+            raise ValueError('method required')
+        payload = {
+            'token': token
+        }
+        if transactionRef is not None:
+            payload['transactionRef'] = transactionRef
+        return self.call_api(path='/exchanges', method='post', data=payload)
---
+++ packages/eversend/eversend-1.0.1.tar.gz/eversend-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1' 
+DESCRIPTION = 'Eversend Python SDK'
+with open("README.md", "r") as fp:
+    LONG_DESCRIPTION = fp.read()
+
+# Setting up
+setup(
+    name="eversend", 
+    version=VERSION,
+    author="Eversend",
+    author_email="info@eversend.co",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eversend/eversend-python-sdk",
+    license="MIT",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    install_requires=[
+        'requests >= 2.20; python_version >= "3.0"',
+        'requests[security] >= 2.20; python_version < "3.0"',
+        'cachetools',
+
+    ],
+    keywords=['eversend api payments'],
+    classifiers= [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
