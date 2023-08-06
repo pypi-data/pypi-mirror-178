0cad916904b7b6510d75fbc94e649890a3cfa528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:24 +0000

    Add package carreralib/0.10.1/carreralib-0.10.1.tar.gz

---
+++ packages/carreralib/carreralib-0.10.1.tar.gz/carreralib-0.10.1/docs/conf.py
@@ -0,0 +1,30 @@
+import pathlib
+import sys
+
+basedir = pathlib.Path(__file__).parent.parent
+
+sys.path.insert(0, str((basedir / "src").resolve()))
+
+
+def get_version():
+    import configparser
+
+    cp = configparser.ConfigParser()
+    cp.read(basedir / "setup.cfg")
+    return cp["metadata"]["version"]
+
+
+project = "carreralib"
+copyright = "2015-2022 Thomas Kemmer"
+version = get_version()
+release = version
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+]
+exclude_patterns = ["_build"]
+master_doc = "index"
+html_theme = "default"
