aba811ae0efb2a12c43327c85228491e6494c448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:58 +0000

    Add package fgclustering/1.0.0/fgclustering-1.0.0.tar.gz

---
+++ packages/fgclustering/fgclustering-1.0.0.tar.gz/fgclustering-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='fgclustering',
+    version='1.0.0',
+    url='https://github.com/HelmholtzAI-Consultants-Munich/forest_guided_clustering',
+    author='Lisa Barros de Andrade e Sousa, Dominik Thalmeier, Helena Pelin, Marie Piraud',
+    author_email='lisa.barros.andrade.sousa@gmail.com, dominikthalmeier@googlemail.com, helena.pelin@helmholtz-muenchen.de',
+    packages=setuptools.find_packages(),
+    install_requires=['pandas','numpy','matplotlib','seaborn>=0.12','sklearn','kmedoids','scipy','tqdm', 'statsmodels>=0.13.5','Sphinx>=4.2.0', 'numexpr>=2.8.4'],
+    long_description=open('README.rst').read(),
+)
