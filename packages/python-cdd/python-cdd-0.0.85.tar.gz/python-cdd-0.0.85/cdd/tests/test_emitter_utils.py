9d24ebf5e3017cd15f61e826f927104a9cb57999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:25 +0000

    Add package python-cdd/0.0.85/python-cdd-0.0.85.tar.gz

---
+++ packages/python-cdd/python-cdd-0.0.85.tar.gz/python-cdd-0.0.85/cdd/tests/test_emitter_utils.py
@@ -0,0 +1,106 @@
+""" Tests for emitter_utils """
+
+from ast import Attribute, Call, Expr, Load, Name, Subscript, keyword
+from copy import deepcopy
+from unittest import TestCase
+
+from cdd.ast_utils import set_value
+from cdd.emit.emitter_utils import (
+    interpolate_defaults,
+    param_to_sqlalchemy_column_call,
+    parse_out_param,
+)
+from cdd.pure_utils import rpartial
+from cdd.tests.mocks.argparse import argparse_add_argument_ast, argparse_func_ast
+from cdd.tests.mocks.ir import intermediate_repr
+from cdd.tests.utils_for_tests import run_ast_test, unittest_main
+
+
+class TestEmitterUtils(TestCase):
+    """Test class for emitter_utils"""
+
+    def test_param_to_sqlalchemy_column_call_when_sql_constraints(self) -> None:
+        """Tests that with SQL constraints the SQLalchemy column is correctly generated"""
+        run_ast_test(
+            self,
+            param_to_sqlalchemy_column_call(
+                (
+                    "foo",
+                    {
+                        "doc": "",
+                        "typ": "str",
+                        "x_typ": {"sql": {"constraints": {"indexed": True}}},
+                    },
+                ),
+                include_name=False,
+            ),
+            gold=Call(
+                func=Name(id="Column", ctx=Load()),
+                args=[Name(id="String", ctx=Load())],
+                keywords=[keyword(arg="indexed", value=set_value(True))],
+            ),
+        )
+
+    def test_parse_out_param(self) -> None:
+        """Test that parse_out_param parses out the right dict"""
+        self.assertDictEqual(
+            parse_out_param(
+                next(filter(rpartial(isinstance, Expr), argparse_func_ast.body[::-1]))
+            )[1],
+            # Last element:
+            intermediate_repr["params"]["data_loader_kwargs"],
+        )
+
+    def test_parse_out_param_default(self) -> None:
+        """Test that parse_out_param sets default when required and unset"""
+
+        self.assertDictEqual(
+            parse_out_param(argparse_add_argument_ast)[1],
+            {"default": 0, "doc": None, "typ": "int"},
+        )
+
+    def test_parse_out_param_fails(self) -> None:
+        """Test that parse_out_param throws NotImplementedError when unsupported type given"""
+        self.assertRaises(
+            NotImplementedError,
+            lambda: parse_out_param(
+                Expr(
+                    Call(
+                        args=[set_value("--num")],
+                        func=Attribute(
+                            Name("argument_parser", Load()),
+                            "add_argument",
+                            Load(),
+                        ),
+                        keywords=[
+                            keyword(
+                                arg="type",
+                                value=Subscript(
+                                    expr_context_ctx=None,
+                                    expr_slice=None,
+                                    expr_value=None,
+                                ),
+                                identifier=None,
+                            ),
+                            keyword(
+                                arg="required",
+                                value=set_value(True),
+                                identifier=None,
+                            ),
+                        ],
+                        expr=None,
+                        expr_func=None,
+                    )
+                )
+            ),
+        )
+
+    def test_interpolate_defaults(self) -> None:
+        """Test that interpolate_defaults corrects sets the default property"""
+        param = "K", deepcopy(intermediate_repr["params"]["K"])
+        param_with_correct_default = deepcopy(param[1])
+        del param[1]["default"]
+        self.assertDictEqual(interpolate_defaults(param)[1], param_with_correct_default)
+
+
+unittest_main()
