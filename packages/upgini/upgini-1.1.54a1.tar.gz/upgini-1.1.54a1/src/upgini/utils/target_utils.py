8989b1942a67788ce0b46a61ae76698381212127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:56 +0000

    Add package upgini/1.1.54a1/upgini-1.1.54a1.tar.gz

---
+++ packages/upgini/upgini-1.1.54a1.tar.gz/upgini-1.1.54a1/src/upgini/utils/target_utils.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+import numpy as np
+import logging
+
+from upgini.metadata import ModelTaskType
+from upgini.errors import ValidationError
+from upgini.resource_bundle import bundle
+
+
+def define_task(y: pd.Series, logger: logging.Logger, silent: bool = False) -> ModelTaskType:
+    target = y.dropna()
+    if is_numeric_dtype(target):
+        target = target.loc[np.isfinite(target)]  # type: ignore
+    else:
+        target = target.loc[target != ""]
+    if len(target) == 0:
+        raise ValidationError(bundle.get("empty_target"))
+    target_items = target.nunique()
+    target_ratio = target_items / len(target)
+    if (target_items > 50 or (target_items > 2 and target_ratio > 0.2)) and is_numeric_dtype(target):
+        task = ModelTaskType.REGRESSION
+    elif target_items <= 2:
+        if is_numeric_dtype(target):
+            task = ModelTaskType.BINARY
+        else:
+            raise ValidationError(bundle.get("non_numeric_target"))
+    else:
+        task = ModelTaskType.MULTICLASS
+    logger.info(f"Detected task type: {task}")
+    if not silent:
+        print(bundle.get("target_type_detected").format(task))
+    return task
