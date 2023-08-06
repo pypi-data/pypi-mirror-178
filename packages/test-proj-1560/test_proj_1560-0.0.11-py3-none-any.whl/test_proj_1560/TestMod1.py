a399fc81ebaeef26a2ab5c0a8eea297d9e4f1ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:42 +0000

    Add package test-proj-1560/0.0.11/test_proj_1560-0.0.11-py3-none-any.whl

---
+++ packages/test-proj-1560/test_proj_1560-0.0.11-py3-none-any.whl/test_proj_1560/TestMod1.py
@@ -0,0 +1,19 @@
+import os
+
+ITEMLIST = []
+
+def test():
+    print("Good to go.")
+
+def test2():
+    print("Still good to go.")
+
+def get_items():
+    global ITEMLIST
+    path = os.path.join(os.path.dirname(__file__), "items.txt")
+
+    if os.path.exists(path) and os.path.isfile(path):
+        with open(path, "r") as file:
+            ITEMLIST = [item.strip() for item in file.readlines()]
+    else:
+        print("failed")
