91dd239ee4e1dc57bc68414281fa30049c601585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:59 +0000

    Add package my-timesunion-sdk/0.0.9/my_timesunion_sdk-0.0.9.tar.gz

---
+++ packages/my-timesunion-sdk/my_timesunion_sdk-0.0.9.tar.gz/my_timesunion_sdk-0.0.9/my_timesunion_sdk/my_timesunion_sdk.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022-11-29 20:45
+# @Author  : MR.
+# @Software: PyCharm
+
+
+def test111():
+    aa = 1
+    bb = 2
+
+
---
+++ packages/my-timesunion-sdk/my_timesunion_sdk-0.0.9.tar.gz/my_timesunion_sdk-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="my_timesunion_sdk",
+    version="0.0.9",
+    author="xhh",
+    author_email="380472213@qq.com",
+    description="A commonly used tool library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/my-timesunion-sdk/",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    keywords=['my', 'times', 'union', 'sdk'],
+    install_requires=[
+        "requests",
+        "arrow"
+    ]
+)
