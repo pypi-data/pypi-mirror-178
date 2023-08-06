0df34d469952077040f72d743e4bbd33443961bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:13 +0000

    Add package NET/0.1/NET-0.1.tar.gz

---
+++ packages/NET/NET-0.1.tar.gz/NET-0.1/README.txt
@@ -0,0 +1,12 @@
+NET Package (Lib)
+
+A Simple (For Now) Package For Creating Servers, ETC
+
+Basicly A Package For Networking!
+
+There Will Most Likely Be Future Updates For This Pkg!
+
+Example:
+print(https(url="https://example.com").statusCode())
+
+And Other Cool Things, Enjoy :)
---
+++ packages/NET/NET-0.1.tar.gz/NET-0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    packages=setuptools.find_packages(),
+    license="MIT",
+    description="A Python Package For Networking Such As Servers/LocalHost ETC",
+    author="Hunter Mikesell",
+    version="0.1",
+    name="NET",
+    long_description='NET Package (Lib)\nBasicly A Package For Networking!\nThere Will Most Likely Be Future Updates For This Pkg!\nExample:\nprint(https(url="https://example.com").statusCode())\nAnd Other Cool Things, Enjoy :)',
+    script_name='NET',
+    platforms=['Windows'],
+    author_email='huntermikesell84@gmail.com',
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+],
+)
