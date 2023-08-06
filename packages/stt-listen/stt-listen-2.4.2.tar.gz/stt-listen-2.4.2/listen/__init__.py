47bc3ce3c918d73d599b6efce621c6ae4c225c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:30 +0000

    Add package stt-listen/2.4.2/stt-listen-2.4.2.tar.gz

---
+++ packages/stt-listen/stt-listen-2.4.2.tar.gz/stt-listen-2.4.2/listen/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+__version__ = "2.4.2"
+
+I18N, L10N = (x for x in os.environ.get('LANG', "en_EN.UTF-8").split(".")[0].split("_"))
+
+USERNAME = os.environ.get("USERNAME", 'root')
+HOME = f"/home/{USERNAME}" if USERNAME != "root" else "/root"
+ASSISTANT_PATH = f"{HOME}/.assistant" if USERNAME != "root" else "/usr/share/assistant"
+CONFIG_PATH = f"{ASSISTANT_PATH}/stt.toml"
+
+MODELS_URL = "https://raw.githubusercontent.com/wasertech/stt-models-locals/main/models.json"
