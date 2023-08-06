6d269573016ea24b14b6004176e08ab5e0ae4cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:20:05 +0000

    Add package relations-pymysql/0.6.12/relations-pymysql-0.6.12.tar.gz

---
+++ packages/relations-pymysql/relations-pymysql-0.6.12.tar.gz/relations-pymysql-0.6.12/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="relations-pymysql",
+    version="0.6.12",
+    package_dir = {'': 'lib'},
+    py_modules = [
+        'relations_pymysql'
+    ],
+    install_requires=[
+        'PyMySQL==0.10.0',
+        'relations-dil>=0.6.12',
+        'relations-mysql>=0.6.3'
+    ],
+    url="https://github.com/relations-dil/python-relations-pymysql",
+    author="Gaffer Fitch",
+    author_email="relations@gaf3.com",
+    description="DB Modeling for MySQL using the PyMySQL library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license_files=('LICENSE.txt',),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
