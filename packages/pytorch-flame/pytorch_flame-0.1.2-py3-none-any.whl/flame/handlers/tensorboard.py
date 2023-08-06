e70e15a2d263f169c300d7f6fe557b532078b6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:18 +0000

    Add package pytorch-flame/0.1.2/pytorch_flame-0.1.2-py3-none-any.whl

---
+++ packages/pytorch-flame/pytorch_flame-0.1.2-py3-none-any.whl/flame/handlers/tensorboard.py
@@ -0,0 +1,46 @@
+import os
+from datetime import datetime
+from typing import Any, Dict, List
+
+from ignite.contrib.handlers import TensorboardLogger
+from ignite.engine import Events
+
+from .handler import Handler
+
+
+class Tensorboard(Handler):
+    def __init__(
+        self,
+        *,
+        log_dir: str = None,
+        logger_handlers: List[Dict[str, Any]],
+        **kwargs: Any,
+    ):
+        if log_dir is not None:
+            log_dir = os.path.join(log_dir, datetime.now().strftime('%y%m%d%H%M%S'))
+
+        self.logger = TensorboardLogger(log_dir=log_dir, **kwargs)
+
+        actions = []
+
+        for logger_handler in logger_handlers:
+            action = {}
+
+            if 'engine' in logger_handler:
+                action.update({
+                    'engine': logger_handler.pop('engine'),
+                })
+
+            action.update({
+                'event': None,
+                'func': self.logger.attach,
+                'akwargs': logger_handler,
+            })
+            actions.append(action)
+
+        actions.append({
+            'event': Events.EPOCH_COMPLETED,
+            'func': self.logger.writer.flush,
+        })
+
+        super(Tensorboard, self).__init__(actions=actions)
