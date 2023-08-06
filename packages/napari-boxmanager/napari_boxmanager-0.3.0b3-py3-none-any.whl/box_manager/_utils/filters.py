62ce666c4c934d16626c3d5a2f99a04dcb8342d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:27 +0000

    Add package napari-boxmanager/0.3.0b3/napari_boxmanager-0.3.0b3-py3-none-any.whl

---
+++ packages/napari-boxmanager/napari_boxmanager-0.3.0b3-py3-none-any.whl/box_manager/_utils/filters.py
@@ -0,0 +1,69 @@
+import numpy as np
+import numpy.typing as npt
+import scipy.fftpack as fft
+from scipy.ndimage import gaussian_filter
+
+def bandpass_filter(
+    input_data: npt.ArrayLike,
+    lp_filter_resolution_ang: float,
+    hp_filter_resolution_ang: float,
+    pixel_size: float,
+    log=print,
+):
+
+    if (
+        lp_filter_resolution_ang >= hp_filter_resolution_ang
+        and hp_filter_resolution_ang != 0
+    ):
+        log(
+            f"{lp_filter_resolution_ang} cannot be greater than {hp_filter_resolution_ang}"
+        )
+        return None
+
+    # input_data = np.array(input_data)
+
+    old_shape = input_data.shape
+    new_shape = np.max(old_shape)
+    box_range = np.arange(-new_shape // 2 + 1, 1 + new_shape // 2)
+
+    mesh_dist = np.sum(
+        np.array(
+            np.meshgrid(*([box_range] * len(old_shape)), sparse=True),
+            dtype=object,
+        )
+        ** 2,
+        axis=0,
+    )
+
+    mask = mesh_dist >= 0
+    if lp_filter_resolution_ang != 0:
+        lp_filter_frequency = pixel_size / lp_filter_resolution_ang
+        mask = (
+            mesh_dist <= (new_shape // 2 * 2 * lp_filter_frequency) ** 2
+        ) & mask
+
+    if hp_filter_resolution_ang != 0:
+        hp_filter_frequency = pixel_size / hp_filter_resolution_ang
+        mask = (
+            (new_shape // 2 * 2 * hp_filter_frequency) ** 2 <= mesh_dist
+        ) & mask
+
+    pad_list = []
+    for shape_i in old_shape:
+        diff_i = new_shape - shape_i
+        pad_list.append([diff_i // 2, diff_i // 2 + diff_i % 2])
+
+    pad_image = np.pad(input_data, pad_list, "symmetric")
+    pad_image = fft.fftn(pad_image)
+    pad_image = fft.fftshift(pad_image)
+
+    mask=gaussian_filter(mask.astype(float),sigma=4)
+    pad_image *= mask
+    pad_image = fft.ifftshift(pad_image)
+    pad_image = fft.ifftn(pad_image)
+
+    unpad_list = []
+    for pad_i, shape_i in zip(pad_list, old_shape):
+        unpad_list.append(np.s_[pad_i[0] : pad_i[0] + shape_i])
+
+    return pad_image.real[tuple(unpad_list)], mask
---
+++ packages/napari-boxmanager/napari_boxmanager-0.3.0b3-py3-none-any.whl/box_manager/io/coords.py
@@ -0,0 +1,112 @@
+import os
+import typing
+
+import numpy.typing as npt
+import pandas as pd
+
+from .interface import NapariLayerData
+from . import io_utils as coordsio
+
+
+class BoxFileNumberOfColumnsError(pd.errors.IntCastingNaNError):
+    pass
+
+
+class UnknownFormatException(Exception):
+    ...
+
+
+DEFAULT_BOXSIZE: int = 10
+
+
+def get_valid_extensions():
+
+    return ["coords"]
+
+
+def read(path: os.PathLike) -> pd.DataFrame:
+
+    names = ["x", "y", "z"]
+    box_data: pd.DataFrame = pd.read_csv(
+        path,
+        delim_whitespace=True,
+        index_col=False,
+        header=None,
+        dtype=float,
+        names=names,
+        usecols=range(len(names)),
+    )  # type: ignore
+    try:
+        box_data.astype(int)
+    except pd.errors.IntCastingNaNError:
+        raise BoxFileNumberOfColumnsError
+
+    return box_data
+
+
+def to_napari(
+    path: os.PathLike | list[os.PathLike],
+) -> "list[NapariLayerData]":
+
+    return coordsio.to_napari(
+        path=path,
+        read_func=read,
+        prepare_napari_func=_prepare_napari_coords,
+        meta_columns=[],
+        feature_columns=[],
+        valid_extensions=get_valid_extensions()
+    )
+
+
+def _prepare_napari_coords(
+    input_df: pd.DataFrame,
+) -> pd.DataFrame:
+    output_data: pd.DataFrame = pd.DataFrame(columns=["x", "y", "z"])
+
+    output_data["z"] = input_df["x"]
+    output_data["y"] = input_df["y"]
+    output_data["x"] = input_df["z"]
+    output_data["boxsize"] = DEFAULT_BOXSIZE
+
+    return output_data
+
+
+def _write_coords(path: os.PathLike, df: pd.DataFrame, **kwargs):
+    df[["x", "y", "z"]].to_csv(path, sep=" ", header=None, index=None)
+
+
+def _make_df_data(
+    coordinates: pd.DataFrame, box_size: npt.ArrayLike, features: pd.DataFrame
+) -> pd.DataFrame:
+    data = {"x": [], "y": [], "z": [], "boxsize": []}
+
+    is_filament_data = coordinates.shape[1] == 4
+
+
+    for coords, boxsize in zip(
+        coordinates,
+        box_size,
+    ):
+        if is_filament_data:
+            z, y, x, _ = coords
+        else:
+            z, y, x = coords
+
+        data["x"].append(x)
+        data["y"].append(y)
+        data["z"].append(z)
+        data["boxsize"].append(boxsize)
+    return pd.DataFrame(data)
+
+
+def from_napari(
+    path: os.PathLike, layer_data: list[NapariLayerData], suffix: str
+):
+    path = coordsio.from_napari(
+        path=path,
+        layer_data=layer_data,
+        write_func=_write_coords,
+        format_func=_make_df_data,
+        suffix=suffix
+    )
+    return path
