e0f0b455d2b0bf67c69b9494672faf15b2f1d9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:40 +0000

    Add package pyndns/0.0.1/pyndns-0.0.1.tar.gz

---
+++ packages/pyndns/pyndns-0.0.1.tar.gz/pyndns-0.0.1/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name="pyndns", version="0.0.1", py_modules=["pyndns"], author="me", author_email="me@me.com", description="This is a placeholder package created by registerit.", url="http://www.github.com")
