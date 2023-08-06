b551235d974b6203a16491fc665229d2b0221a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:59 +0000

    Add package sidewalkdemo/0.2/sidewalkdemo-0.2.tar.gz

---
+++ packages/sidewalkdemo/sidewalkdemo-0.2.tar.gz/sidewalkdemo-0.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='sidewalkdemo',
+    version='0.2',
+    author='Zhanzhi Jiang',
+    author_email='hnzjzz@gmail.com',
+    description='This is an implementation of sidewalk environment for reinforcement learning',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
