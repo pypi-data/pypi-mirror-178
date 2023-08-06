d14371a256b0b5e1e21f92130641e9664055d185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:40 +0000

    Add package gauss-distribs-m/0.1/gauss_distribs_m-0.1.tar.gz

---
+++ packages/gauss-distribs-m/gauss_distribs_m-0.1.tar.gz/gauss_distribs_m-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gauss_distribs_m',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gauss_distribs_m'],
+      zip_safe=False)
