98d0955817c9d7e0defd4813af1d406cbb767ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:49 +0000

    Add package bsb/4.0.0a43.dev0/bsb-4.0.0a43.dev0-py3-none-any.whl

---
+++ packages/bsb/bsb-4.0.0a43.dev0-py3-none-any.whl/bsb/simulation/cell.py
@@ -0,0 +1,22 @@
+from .component import SimulationComponent
+from .parameter import Parameter
+from .. import config
+from ..config import refs
+
+
+@config.node
+class CellModel(SimulationComponent):
+    """
+    Cell models are simulator specific representations of a cell type.
+    """
+
+    cell_type = config.ref(refs.cell_type_ref, key="name")
+    """
+    The cell type associated to this cell_model.
+    """
+    relay = config.attr(type=bool, default=False)
+    """
+    Whether this cell type is a relay type. Relay types, during simulation, instantly
+    transmit incoming spikes to their targets.
+    """
+    parameters = config.list(type=Parameter)
