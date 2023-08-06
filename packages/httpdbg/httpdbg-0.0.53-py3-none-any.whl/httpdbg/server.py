c2beef4c79d0b642d8c38715d7d01bab930e6086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:57 +0000

    Add package httpdbg/0.0.53/httpdbg-0.0.53-py3-none-any.whl

---
+++ packages/httpdbg/httpdbg-0.0.53-py3-none-any.whl/httpdbg/server.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from contextlib import contextmanager
+from http.server import HTTPServer
+import threading
+
+from httpdbg.hooks import unset_hook_for_requests, set_hook_for_requests
+from httpdbg.records import HTTPRecords
+from httpdbg.webapp import HttpbgHTTPRequestHandler
+
+
+@contextmanager
+def httpdbg(records=None):
+    if not records:
+        records = HTTPRecords()
+    try:
+        set_hook_for_requests(records)
+
+        yield records
+
+        unset_hook_for_requests(records)
+    except Exception as ex:
+        unset_hook_for_requests(records)
+        raise ex
+
+
+@contextmanager
+def httpdbg_srv(port):
+    server = None
+    records = HTTPRecords()
+    try:
+        server = ServerThread(port, records)
+        server.start()
+
+        yield records
+
+        server.shutdown()
+    except Exception as ex:
+        if server:
+            server.shutdown()
+        raise ex
+
+
+class ServerThread(threading.Thread):
+    def __init__(self, port, records):
+        threading.Thread.__init__(self)
+        self.port = port
+
+        def http_request_handler(*args, **kwargs):
+            HttpbgHTTPRequestHandler(records, *args, **kwargs)
+
+        self.srv = HTTPServer(("localhost", port), http_request_handler)
+
+    def run(self):
+        self.srv.serve_forever()
+
+    def shutdown(self):
+        self.srv.shutdown()
