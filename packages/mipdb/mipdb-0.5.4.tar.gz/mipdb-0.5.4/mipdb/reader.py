9cb102a326c63c73fa39ea26a3ec4fa7a12a2ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:28 +0000

    Add package mipdb/0.5.4/mipdb-0.5.4.tar.gz

---
+++ packages/mipdb/mipdb-0.5.4.tar.gz/mipdb-0.5.4/mipdb/reader.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+import json
+
+import pandas as pd
+
+from mipdb.exceptions import FileContentError
+
+
+class Reader(ABC):
+    @abstractmethod
+    def read(self):
+        pass
+
+
+class JsonFileReader(Reader):
+    def __init__(self, file) -> None:
+        self.file = file
+
+    def read(self):
+        with open(self.file, "r") as stream:
+            try:
+                return json.load(stream)
+            except json.JSONDecodeError as exc:
+                orig_msg = exc.args[0]
+                raise FileContentError(f"Unable to decode json file. {orig_msg}")
