02290713e994a58e808eb94a4e7045d5c77ebbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:04 +0000

    Add package ebcic/0.0.4/ebcic-0.0.4-py3-none-any.whl

---
+++ packages/ebcic/ebcic-0.0.4-py3-none-any.whl/ebcic/__main__.py
@@ -0,0 +1,12 @@
+""" EBCIC Command Line Interface
+
+This is part of https://github.com/KazKobara/ebcic/ .
+"""
+
+import ebcic
+# 'ebcic.' in ebcic.function can be removed by using below:
+# from ebcic import print_interval, exact, Params, __version__
+# from . import print_interval, exact, Params, __version__
+
+if __name__ == "__main__":
+    ebcic.cli()
