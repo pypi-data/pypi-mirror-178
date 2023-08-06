e8929e08568484bcba296e54d4564790ef8734fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:34 +0000

    Add package pymycode-packages/0.0.8/pymycode_packages-0.0.8-py3-none-any.whl

---
+++ packages/pymycode-packages/pymycode_packages-0.0.8-py3-none-any.whl/pymycode_packages/pdf.py
@@ -0,0 +1,13 @@
+from PyPDF2 import PdfWriter, PdfReader
+
+def remove_pages(input_file_path, pages, out_file_path):
+    pages_to_keep = pages # page numbering starts from 0
+    infile = PdfReader(input_file_path, 'rb')
+    output = PdfWriter()
+
+    for i in pages_to_keep:
+        p = infile.pages[i] 
+        output.add_page(p)
+
+    with open(out_file_path, 'wb') as f:
+        output.write(f)
