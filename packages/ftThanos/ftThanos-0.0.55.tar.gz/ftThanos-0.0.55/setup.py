e220212c970fe9eae2a7f689c000233221e1cca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:04 +0000

    Add package ftThanos/0.0.55/ftThanos-0.0.55.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.55.tar.gz/ftThanos-0.0.55/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.55",
+description="A client for 55 API",
+author="Thanos 55",
+author_email="thanos@55paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
