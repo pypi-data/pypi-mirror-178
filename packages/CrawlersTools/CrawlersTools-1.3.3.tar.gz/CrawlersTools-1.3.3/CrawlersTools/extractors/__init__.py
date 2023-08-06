0c6fdc477fc6a366e446805083dd30938cd0b7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:34 +0000

    Add package CrawlersTools/1.3.3/CrawlersTools-1.3.3.tar.gz

---
+++ packages/CrawlersTools/CrawlersTools-1.3.3.tar.gz/CrawlersTools-1.3.3/CrawlersTools/extractors/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Project : CrawlersTools
+# @Time    : 2022/8/15 11:29
+# @Author  : MuggleK
+# @File    : __init__.py
+
+from CrawlersTools.extractors.list_extractor import ListExtractor
+
+from CrawlersTools.extractors.title_extractor import TitleExtractor
+from CrawlersTools.extractors.time_extractor import TimeExtractor
+from CrawlersTools.extractors.content_extractor import ContentExtractor
+
+
+class PolicyExtractor(object):
+
+    @staticmethod
+    def extract(html, title_xpath: str = "", publish_time_xpath: str = "", content_xpath: str = "") -> dict:
+        title = TitleExtractor().extract(html, title_xpath=title_xpath)
+        publish_time = TimeExtractor().extract(html, publish_time_xpath=publish_time_xpath)
+        content = ContentExtractor().extract(html, content_xpath=content_xpath)
+
+        return {
+            "title": title,
+            "publish_time": publish_time,
+            "content": content,
+        }
---
+++ packages/CrawlersTools/CrawlersTools-1.3.3.tar.gz/CrawlersTools-1.3.3/CrawlersTools/requests/proxy.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Project : CrawlersTools
+# @Time    : 2022/6/21 17:06
+# @Author  : MuggleK
+# @File    : proxy.py
+
+import httpx
+
+from loguru import logger
+
+
+def get_proxies(proxy_type="random"):
+    """
+    qzd_proxy：亿牛云+星速云
+    @param proxy_type: 默认取随机
+    @return:
+    """
+    try:
+        protocol = 'http://'
+        proxy = httpx.get(f"http://192.168.9.3:5555/{proxy_type.lower()}").text.strip()
+        proxy = protocol + proxy
+        return {protocol: proxy, 'https://': proxy}
+    except Exception as err:
+        logger.error(f'获取代理失败：{err}')
