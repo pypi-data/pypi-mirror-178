fad19195a44ae9589fe43ff915bf381e8191ad26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:24:21 +0000

    Add package mini-six/0.1.10/mini_six-0.1.10.tar.gz

---
+++ packages/mini-six/mini_six-0.1.10.tar.gz/mini_six-0.1.10/mini_six/core/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+"""
+
+@Auther: niu
+@FileName: __init__.py.py
+@Introduction: 
+
+"""
+from .interface import Agent
+from .interface import Observer
+from .interface import SubscribeData
+from .interface import DataSourceType
+from .config import Config
---
+++ packages/mini-six/mini_six-0.1.10.tar.gz/mini_six-0.1.10/mini_six/portable/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+"""
+
+@Auther: niu
+@FileName: __init__.py.py
+@Introduction: 
+
+"""
+import sys
+
+__all__ = ["observer", "operation"]
+
+if sys.platform == "win32":
+    import mini_six.portable.win32.observer as observer
+    import mini_six.portable.win32.operation as operation
+    import mini_six.portable.win32.operation.key_map as key_map
