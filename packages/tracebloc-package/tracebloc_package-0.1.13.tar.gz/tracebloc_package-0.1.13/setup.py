7fcda8504df2bde8f36c4de49bfd3fe719aa24cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:53 +0000

    Add package tracebloc-package/0.1.13/tracebloc_package-0.1.13.tar.gz

---
+++ packages/tracebloc-package/tracebloc_package-0.1.13.tar.gz/tracebloc_package-0.1.13/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="tracebloc_package",
+    version="0.1.13",
+    description="Package required to run Tracebloc jupyter notebook to create experiment",
+    url="https://gitlab.com/tracebloc/tracebloc-py-package",
+    license="MIT",
+    python_requires=">=3",
+    packages=["tracebloc_package"],
+    author="Tracebloc",
+    author_email="lukas-wutke@tracebloc.io",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["requests", "termcolor", "rich"],
+    zip_safe=False,
+)
