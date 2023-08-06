ea528bafe291d5ffe0b887073eef30766661db87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:17 +0000

    Add package river-torch/0.1.2/river_torch-0.1.2-py3-none-any.whl

---
+++ packages/river-torch/river_torch-0.1.2-py3-none-any.whl/river_torch/regression/__init__.py
@@ -0,0 +1,7 @@
+from river_torch.regression.regressor import Regressor
+from river_torch.regression.rolling_regressor import RollingRegressor
+
+__all__ = [
+    "Regressor",
+    "RollingRegressor",
+]
