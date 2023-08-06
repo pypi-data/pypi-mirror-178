82400214427cc091e6237b9d53c8e9cf2a17ea86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:06 +0000

    Add package paddlelabel/0.5.0/paddlelabel-0.5.0.tar.gz

---
+++ packages/paddlelabel/paddlelabel-0.5.0.tar.gz/paddlelabel-0.5.0/paddlelabel/__main__.py
@@ -0,0 +1,104 @@
+import os
+import argparse
+import logging
+import webbrowser
+
+from paddlelabel.serve import connexion_app
+from paddlelabel.api.controller.sample import prep_samples
+from paddlelabel.util import pyVerGt, portInUse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="PP Label")
+    parser.add_argument(
+        "--lan",
+        "-l",
+        default=False,
+        action="store_true",
+        help="Whether to expose PaddleLabel to lan",
+    )
+    parser.add_argument(
+        "--port",
+        "-p",
+        default=17995,
+        type=int,
+        help="The port to use",
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        default=False,
+        action="store_true",
+        help="Output more log in command line, if not set, cmd will only output error",
+    )
+    parser.add_argument(
+        "--debug",
+        "-d",
+        default=False,
+        action="store_true",
+        help="Run in debug mode",
+    )
+
+    return parser.parse_args()
+
+
+pyVerWarning = """
+It's recommended to run PaddleLabel with Python>=3.9.0. Please consider creating a new virtual enviroment and run PaddleLabel with:
+
+conda create -y -n paddlelabel python=3.9
+conda activate paddlelabel
+pip install --upgrade paddlelabel
+paddlelabel
+
+"""
+
+
+def run():
+    args = parse_args()
+
+    # 1. ensuer port not in use
+    if not args.debug and not args.verbose and portInUse(args.port):
+        print(
+            f"Port {args.port} is currently in use. Please identify and stop that process using port {args.port} or specify a different port with: paddlelabel -p [Port other than {args.port}]."
+        )
+        exit()
+
+    # 2. warn if low py version
+    if not pyVerGt():
+        print(pyVerWarning)
+
+    # 3. create sample datasets
+    prep_samples()
+
+    host = "0.0.0.0" if args.lan else "127.0.0.1"
+
+    # 4. configure logger and logging levels
+    logger = logging.getLogger("PaddleLabel")
+    logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter("[%(levelname)s]%(module)s.%(lineno)d: %(message)s")
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    logging.getLogger("werkzeug").setLevel(logging.ERROR)
+    logging.getLogger("PaddleLabel").setLevel(logging.ERROR)
+
+    if args.verbose:
+        logging.getLogger("werkzeug").setLevel(logging.INFO)
+        logging.getLogger("PaddleLabel").setLevel(logging.DEBUG)
+
+    if args.debug:
+        logging.getLogger("werkzeug").setLevel(logging.ERROR)
+        logging.getLogger("PaddleLabel").setLevel(logging.DEBUG)
+
+    logger.info("App starting")
+
+    # 5. run
+    if not args.debug:
+        webbrowser.open(f"http://localhost:{args.port}")
+
+    print(f"PaddleLabel is running at http://localhost:{args.port}")
+    connexion_app.run(host=host, port=args.port, debug=args.debug)
+
+if __name__ == "__main__":
+    run()
---
+++ packages/paddlelabel/paddlelabel-0.5.0.tar.gz/paddlelabel-0.5.0/paddlelabel/api/controller/sample.py
@@ -0,0 +1,222 @@
+import os
+import os.path as osp
+import requests
+import tarfile
+import zipfile
+from pathlib import Path
+
+from tqdm import tqdm
+import connexion
+import flask
+
+import paddlelabel
+from paddlelabel.config import data_base_dir
+from paddlelabel.api.schema import ProjectSchema
+from paddlelabel.api.model import TaskCategory, Project
+from paddlelabel.api.util import abort
+from paddlelabel.config import basedir
+from paddlelabel.task.util.file import copy, copycontent
+
+
+def prep_samples(sample_dst: str = None):
+    if sample_dst is None:
+        sample_dst = osp.join(osp.expanduser("~"), ".paddlelabel", "sample")
+    sample_source = osp.join(basedir, "sample")
+    copycontent(sample_source, sample_dst)
+
+    dsts = [
+        "bear/placeholder/1/1.jpeg",
+        "bear/placeholder/2/2.jpeg",
+        "bear/placeholder/3/3.jpeg",
+        "bear/placeholder/4/4.jpeg",
+        "bear/classification/multiClass/image/1.jpeg",
+        "bear/classification/multiClass/image/2.jpeg",
+        "bear/classification/multiClass/image/3.jpeg",
+        "bear/classification/multiClass/image/4.jpeg",
+        "bear/classification/singleClass/1/1.jpeg",
+        "bear/classification/singleClass/2/2.jpeg",
+        "bear/classification/singleClass/3/3.jpeg",
+        "bear/classification/singleClass/4/4.jpeg",
+        "bear/detection/coco/JPEGImages/1.jpeg",
+        "bear/detection/coco/JPEGImages/2.jpeg",
+        "bear/detection/coco/JPEGImages/3.jpeg",
+        "bear/detection/coco/JPEGImages/4.jpeg",
+        "bear/detection/voc/JPEGImages/1.jpeg",
+        "bear/detection/voc/JPEGImages/2.jpeg",
+        "bear/detection/voc/JPEGImages/3.jpeg",
+        "bear/detection/voc/JPEGImages/4.jpeg",
+        "bear/detection/yolo/JPEGImages/1.jpeg",
+        "bear/detection/yolo/JPEGImages/2.jpeg",
+        "bear/detection/yolo/JPEGImages/3.jpeg",
+        "bear/detection/yolo/JPEGImages/4.jpeg",
+        "bear/img/1.jpeg",
+        "bear/img/2.jpeg",
+        "bear/img/3.jpeg",
+        "bear/img/4.jpeg",
+        "bear/instanceSegmentation/mask/JPEGImages/1.jpeg",
+        "bear/instanceSegmentation/mask/JPEGImages/2.jpeg",
+        "bear/instanceSegmentation/mask/JPEGImages/3.jpeg",
+        "bear/instanceSegmentation/mask/JPEGImages/4.jpeg",
+        "bear/instanceSegmentation/polygon/image/1.jpeg",
+        "bear/instanceSegmentation/polygon/image/2.jpeg",
+        "bear/instanceSegmentation/polygon/image/3.jpeg",
+        "bear/instanceSegmentation/polygon/image/4.jpeg",
+        "bear/semanticSegmentation/mask/JPEGImages/1.jpeg",
+        "bear/semanticSegmentation/mask/JPEGImages/2.jpeg",
+        "bear/semanticSegmentation/mask/JPEGImages/3.jpeg",
+        "bear/semanticSegmentation/mask/JPEGImages/4.jpeg",
+        "bear/semanticSegmentation/polygon/image/1.jpeg",
+        "bear/semanticSegmentation/polygon/image/2.jpeg",
+        "bear/semanticSegmentation/polygon/image/3.jpeg",
+        "bear/semanticSegmentation/polygon/image/4.jpeg",
+        "fruit/classification/multiClass/image/1.jpeg",
+        "fruit/classification/multiClass/image/2.jpeg",
+        "fruit/classification/multiClass/image/3.jpeg",
+        "fruit/classification/multiClass/image/4.jpeg",
+        "fruit/classification/multiClass/image/5.jpeg",
+        "fruit/placeholder/梨/2.jpeg",
+        "fruit/placeholder/梨/4.jpeg",
+        "fruit/placeholder/苹果/1.jpeg",
+        "fruit/placeholder/苹果/3.jpeg",
+        "fruit/classification/singleClass/梨/2.jpeg",
+        "fruit/classification/singleClass/梨/4.jpeg",
+        "fruit/classification/singleClass/苹果/1.jpeg",
+        "fruit/classification/singleClass/苹果/3.jpeg",
+        "fruit/classification/singleClass/5.jpeg",
+        "fruit/detection/coco/image/1.jpeg",
+        "fruit/detection/coco/image/2.jpeg",
+        "fruit/detection/coco/image/3.jpeg",
+        "fruit/detection/coco/image/4.jpeg",
+        "fruit/detection/coco/image/5.jpeg",
+        "fruit/detection/voc/JPEGImages/1.jpeg",
+        "fruit/detection/voc/JPEGImages/2.jpeg",
+        "fruit/detection/voc/JPEGImages/3.jpeg",
+        "fruit/detection/voc/JPEGImages/4.jpeg",
+        "fruit/detection/voc/JPEGImages/5.jpeg",
+        "fruit/detection/yolo/JPEGImages/1.jpeg",
+        "fruit/detection/yolo/JPEGImages/2.jpeg",
+        "fruit/detection/yolo/JPEGImages/3.jpeg",
+        "fruit/detection/yolo/JPEGImages/4.jpeg",
+        "fruit/detection/yolo/JPEGImages/5.jpeg",
+        "fruit/img/1.jpeg",
+        "fruit/img/2.jpeg",
+        "fruit/img/3.jpeg",
+        "fruit/img/4.jpeg",
+        "fruit/img/5.jpeg",
+        "fruit/instanceSegmentation/mask/JPEGImages/1.jpeg",
+        "fruit/instanceSegmentation/mask/JPEGImages/2.jpeg",
+        "fruit/instanceSegmentation/mask/JPEGImages/3.jpeg",
+        "fruit/instanceSegmentation/mask/JPEGImages/4.jpeg",
+        "fruit/instanceSegmentation/mask/JPEGImages/5.jpeg",
+        "fruit/instanceSegmentation/polygon/image/1.jpeg",
+        "fruit/instanceSegmentation/polygon/image/2.jpeg",
+        "fruit/instanceSegmentation/polygon/image/3.jpeg",
+        "fruit/instanceSegmentation/polygon/image/4.jpeg",
+        "fruit/instanceSegmentation/polygon/image/5.jpeg",
+        "fruit/semanticSegmentation/mask/JPEGImages/1.jpeg",
+        "fruit/semanticSegmentation/mask/JPEGImages/2.jpeg",
+        "fruit/semanticSegmentation/mask/JPEGImages/3.jpeg",
+        "fruit/semanticSegmentation/mask/JPEGImages/4.jpeg",
+        "fruit/semanticSegmentation/mask/JPEGImages/5.jpeg",
+        "fruit/semanticSegmentation/polygon/image/1.jpeg",
+        "fruit/semanticSegmentation/polygon/image/2.jpeg",
+        "fruit/semanticSegmentation/polygon/image/3.jpeg",
+        "fruit/semanticSegmentation/polygon/image/4.jpeg",
+        "fruit/semanticSegmentation/polygon/image/5.jpeg",
+    ]
+    for dst in dsts:
+        img_fdr = osp.join(sample_source, dst.split("/")[0], "img")
+        dst = osp.join(sample_dst, dst)
+        src = osp.join(img_fdr, osp.basename(dst))
+        copy(src, dst, make_dir=True)
+
+
+def load_sample(sample_family="bear"):
+    prep_samples()
+
+    task_category_id = connexion.request.json.get("task_category_id")
+
+    sample_folder = {
+        "classification": ["classification", "singleClass"],
+        "detection": ["detection", "voc"],
+        "semantic_segmentation": ["semanticSegmentation", "mask"],
+        "instance_segmentation": ["instanceSegmentation", "polygon"],
+    }
+    label_formats = {
+        "classification": "single_class",
+        "detection": "voc",
+        "semantic_segmentation": "mask",
+        "instance_segmentation": "polygon",
+    }
+    task_category = TaskCategory._get(task_category_id=task_category_id)
+    data_dir = osp.join(
+        osp.expanduser("~"), ".paddlelabel", "sample", sample_family, *sample_folder[task_category.name]
+    )
+    name = f"Sample Project - {task_category.name}"
+
+    curr_project = Project._get(data_dir=data_dir)
+    if curr_project is not None:
+        return {"project_id": curr_project.project_id}, 200
+
+    curr_project = Project._get(name=name)
+    if curr_project is not None:
+        return {"project_id": curr_project.project_id}, 200
+
+    project = {
+        "name": name,
+        "description": f"A built in {task_category.name} sample project",
+        "task_category_id": str(task_category_id),
+        "data_dir": data_dir,
+        "label_format": label_formats[task_category.name],
+    }
+    project = ProjectSchema().load(project)
+
+    if task_category is None:
+        handler = paddlelabel.task.BaseTask(project)
+    else:
+        handler = eval(task_category.handler)(project, data_dir=data_dir)
+
+    handler.importers[label_formats[task_category.name]]()
+
+    return {"project_id": handler.project.project_id}, 200
+
+
+def sample_folder_structure(path):
+    base_path = osp.join(osp.expanduser("~"), ".paddlelabel")
+    path.replace("/", osp.sep)
+    path = osp.join(base_path, path)
+
+    def dfs(path):
+        res = []
+        names = os.listdir(path)
+        for name in names:
+            if name == "paddlelabel.warning":
+                continue
+            temp = {}
+            full_path = osp.join(path, name)
+            if osp.isdir(full_path):
+                temp["title"] = name
+                temp["key"] = osp.relpath(full_path, base_path)
+                temp["children"] = dfs(full_path)
+                temp["isLeaf"] = False
+            else:
+                temp["title"] = name
+                temp["key"] = osp.relpath(full_path, base_path)
+                temp["isLeaf"] = True
+            res.append(temp)
+        res.sort(key=lambda v: v["isLeaf"], reverse=True)
+        return res
+
+    res = dfs(path)
+
+    return res, 200
+
+
+def serve_sample_file(path):
+    base_path = osp.join(osp.join(osp.expanduser("~"), ".paddlelabel"))
+    path.replace("/", osp.sep)
+    path = osp.join(base_path, path)
+
+    file_name = osp.basename(path)
+    folder = osp.dirname(path)
+    return flask.send_from_directory(folder, file_name)
