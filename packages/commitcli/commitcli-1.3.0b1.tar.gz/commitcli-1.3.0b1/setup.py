e2791d4e33cba551a4c6fba7320858391352cc6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:55 +0000

    Add package commitcli/1.3.0b1/commitcli-1.3.0b1.tar.gz

---
+++ packages/commitcli/commitcli-1.3.0b1.tar.gz/commitcli-1.3.0b1/setup.py
@@ -0,0 +1,45 @@
+"""
+@Author Marco A. Gallegos
+@Date   2020/12/31
+@update 2022/04/04
+@Description
+    archivo que describe el paquete distribuible.
+    how to use:
+    compile ->  python3 setup.py sdist bdist_wheel
+"""
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+    fh.close()
+
+setuptools.setup(
+    name="commitcli",
+    version="1.3.0-beta1",
+    author="Marco A. Gallegos",
+    author_email="ma_galeza@hotmail.com",
+    description="commit cli for git with some formats, by default short version of odoo format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marco-gallegos/commit-cli",
+    license="MIT",
+    keywords="cli,cc,commit,git,odoo,github,gitlab,bitbucket,conventional commits, semantic commits",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'inquirer<=2.8.0',
+        'click',
+        'loguru',
+        'pendulum',
+        'python-dotenv'
+    ],
+    # hacer que setup tools genere un comando cli
+    entry_points={
+        'console_scripts': ['commitcli=commitcli.commitcli:main'],
+    }
+)
