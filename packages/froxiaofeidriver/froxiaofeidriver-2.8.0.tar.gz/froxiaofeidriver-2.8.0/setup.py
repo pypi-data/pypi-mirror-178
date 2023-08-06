df4cb48f8f420985e1783ad1dd5c4b4a614454a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:45 +0000

    Add package froxiaofeidriver/2.8.0/froxiaofeidriver-2.8.0.tar.gz

---
+++ packages/froxiaofeidriver/froxiaofeidriver-2.8.0.tar.gz/froxiaofeidriver-2.8.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+import os.path
+from distutils.core import  setup
+
+
+packages = ['froxiaofeidriver']
+
+setup(name='froxiaofeidriver',version='2.8.0',author='feipip', packages=packages, package_dir={'requests': 'requests'},)
