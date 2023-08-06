648bcf8ce5646762f2aa24e9d5075897bdb6dc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:24:39 +0000

    Add package inventory-monitor/2.1.2/inventory-monitor-2.1.2.tar.gz

---
+++ packages/inventory-monitor/inventory-monitor-2.1.2.tar.gz/inventory-monitor-2.1.2/inventory_monitor/api/views.py
@@ -0,0 +1,29 @@
+from netbox.api.viewsets import NetBoxModelViewSet
+
+from .. import filtersets, models
+from .serializers import (ContractorSerializer, ContractSerializer,
+                          ProbeSerializer, InvoiceSerializer)
+
+
+class ProbeViewSet(NetBoxModelViewSet):
+    queryset = models.Probe.objects.prefetch_related('tags', 'device')
+    serializer_class = ProbeSerializer
+    filterset_class = filtersets.ProbeFilterSet
+
+
+class ContractorViewSet(NetBoxModelViewSet):
+    queryset = models.Contractor.objects.prefetch_related('tags')
+    serializer_class = ContractorSerializer
+    filterset_class = filtersets.ContractorFilterSet
+
+
+class ContractViewSet(NetBoxModelViewSet):
+    queryset = models.Contract.objects.prefetch_related('tags', 'contractor')
+    serializer_class = ContractSerializer
+    filterset_class = filtersets.ContractFilterSet
+
+
+class InvoiceViewSet(NetBoxModelViewSet):
+    queryset = models.Invoice.objects.prefetch_related('tags', 'contract')
+    serializer_class = InvoiceSerializer
+    filterset_class = filtersets.InvoiceFilterSet
