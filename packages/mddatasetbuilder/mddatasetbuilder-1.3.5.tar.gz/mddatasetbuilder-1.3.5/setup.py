fad6d9830bbb434e31064f41743086b4709857db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:37 +0000

    Add package mddatasetbuilder/1.3.5/mddatasetbuilder-1.3.5.tar.gz

---
+++ packages/mddatasetbuilder/mddatasetbuilder-1.3.5.tar.gz/mddatasetbuilder-1.3.5/setup.py
@@ -0,0 +1,20 @@
+"""Use 'pip install .' to install mddatasetbuilder."""
+
+
+import os
+
+from setuptools import setup, Extension
+
+if __name__ == '__main__':
+    define_macros = []
+    if os.environ.get("DEBUG", 0):
+        define_macros.extend(
+            (('CYTHON_TRACE', '1'), ('CYTHON_TRACE_NOGIL', '1')))
+    setup(
+          ext_modules=[
+              Extension("mddatasetbuilder.dps", sources=[
+                        "mddatasetbuilder/dps.pyx", "mddatasetbuilder/c_stack.cpp"], language="c++",
+                        define_macros=define_macros,
+                        ),
+          ],
+          )
