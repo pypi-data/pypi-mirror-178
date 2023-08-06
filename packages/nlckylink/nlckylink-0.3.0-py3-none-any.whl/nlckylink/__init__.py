6e4f31a315b72393e9884ae2b1c8c27bb0d8483d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:25 +0000

    Add package nlckylink/0.3.0/nlckylink-0.3.0-py3-none-any.whl

---
+++ packages/nlckylink/nlckylink-0.3.0-py3-none-any.whl/nlckylink/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.3.0'
+from nlckylink import link
+
+class link:
+  def link():
+    x = input("Do you have a nlcky id? Y or N if Y, enter your id number instead.")
+    if x == "N" or x == "n":
+      print("Ok! Loading repl...")
+    else:
+      print(x + " is your id number. Loading repl...")
