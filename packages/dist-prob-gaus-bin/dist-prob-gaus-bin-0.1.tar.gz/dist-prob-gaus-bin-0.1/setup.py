53b736baa09b6aa40b7b26403fa8de91cd566348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:18 +0000

    Add package dist-prob-gaus-bin/0.1/dist-prob-gaus-bin-0.1.tar.gz

---
+++ packages/dist-prob-gaus-bin/dist-prob-gaus-bin-0.1.tar.gz/dist-prob-gaus-bin-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist-prob-gaus-bin',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist-prob-gaus-bin'],
+      zip_safe=False)
