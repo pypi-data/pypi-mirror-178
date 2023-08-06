227bde581a48ca3136919c1827e6554c7e48459e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:22:16 +0000

    Add package audio-xblock/0.2.0/audio_xblock-0.2.0-py3-none-any.whl

---
+++ packages/audio-xblock/audio_xblock-0.2.0-py3-none-any.whl/audio/audio.py
@@ -0,0 +1,73 @@
+"""TO-DO: This XBlock will play an MP3 file as an HTML5 audio element. """
+
+
+import pkg_resources
+from xblock.core import XBlock
+from xblock.fields import Integer, Scope, String
+from xblock.fragment import Fragment
+
+
+class AudioXBlock(XBlock):
+    """
+    This XBlock will play an MP3 file as an HTML5 audio element. 
+    """
+
+    # Fields are defined on the class.  You can access them in your code as
+    # self.<fieldname>.
+    src = String(
+           scope = Scope.settings, 
+           help = "URL for MP3 file to play"
+        )
+
+    def resource_string(self, path):
+        """Handy helper for getting resources from our kit."""
+        data = pkg_resources.resource_string(__name__, path)
+        return data.decode("utf8")
+
+    # TO-DO: change this view to display your data your own way.
+    def student_view(self, context=None):
+        """
+        The primary view of the AudioXBlock, shown to students
+        when viewing courses.
+        """
+        html = self.resource_string("static/html/audio.html")
+        frag = Fragment(html.format(src = self.src))
+        frag.add_css(self.resource_string("static/css/audio.css"))
+        return frag
+
+    def studio_view(self, context):
+        """
+        The view for editing the AudioXBlock parameters inside Studio.
+        """
+        html = self.resource_string("static/html/audio_edit.html")
+        frag = Fragment(html.format(src=self.src))
+
+        js = self.resource_string("static/js/src/audio_edit.js")
+        frag.add_javascript(js)
+        frag.initialize_js('AudioEditBlock')
+
+        return frag
+
+    @XBlock.json_handler
+    def studio_submit(self, data, suffix=''):
+        """
+        Called when submitting the form in Studio.
+        """
+        self.src = data.get('src')
+
+        return {'result': 'success'}
+
+    # TO-DO: change this to create the scenarios you'd like to see in the
+    # workbench while developing your XBlock.
+    @staticmethod
+    def workbench_scenarios():
+        """A canned scenario for display in the workbench."""
+        return [
+            ("AudioXBlock",
+             """<vertical_demo>
+                  <audio src="http://localhost/Ikea.mp3"> </audio>
+                  <audio src="http://localhost/skull.mp3"> </audio>
+                  <audio src="http://localhost/monkey.mp3"> </audio>
+                </vertical_demo>
+             """),
+        ]
