f285751394f513006ff4330a577695db14a11056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:18 +0000

    Add package abcheat/0.0.1/abcheat-0.0.1.tar.gz

---
+++ packages/abcheat/abcheat-0.0.1.tar.gz/abcheat-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import pathlib
+from setuptools import setup
+README = "whatever"
+setup(
+  name="abcheat",
+  version="0.0.1",
+  description="",
+  long_description=README,
+  long_description_content_type="text/markdown",
+  author="",
+  author_email="",
+  license="MIT",
+  packages=["abcheat"],
+  zip_safe=False
+)
