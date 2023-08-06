252da7973b638526ea069aa7715c5f2402ec469a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:07 +0000

    Add package desmos2python/0.0.8.dev40/desmos2python-0.0.8.dev40-py2.py3-none-any.whl

---
+++ packages/desmos2python/desmos2python-0.0.8.dev40-py2.py3-none-any.whl/desmos2python/__init__.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import sys
+
+
+def get_rootpath(startpath=__file__):
+    #: package root path
+    rootpath = Path(startpath).parent
+    return rootpath
+
+
+#: top-level root path
+rootpath = get_rootpath()
+
+#: insert to sys.path if needed.
+if rootpath not in sys.path:
+    sys.path.insert(0, rootpath)
+
+#: other imports
+import desmos2python.utils as utils
+import desmos2python.latex as latex
+from desmos2python.latex import DesmosLatexParser
+import desmos2python.browser as browser
+from desmos2python.browser import DesmosWebSession
+
+
+__all__ = [
+    'rootpath',
+    'get_rootpath',
+    'DesmosLatexParser',
+    'DesmosWebSession',
+]
