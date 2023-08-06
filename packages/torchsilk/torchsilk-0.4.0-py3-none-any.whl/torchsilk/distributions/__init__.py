25f9e79a6f5779ae3320af80150bde57a58765a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:40 +0000

    Add package torchsilk/0.4.0/torchsilk-0.4.0-py3-none-any.whl

---
+++ packages/torchsilk/torchsilk-0.4.0-py3-none-any.whl/torchsilk/distributions/__init__.py
@@ -0,0 +1,3 @@
+"""Functional versions of torch.distributions."""
+from torchsilk.distributions.normal import Normal as Normal
+from torchsilk.distributions.normal import NormalDistParams as NormalDistParams
