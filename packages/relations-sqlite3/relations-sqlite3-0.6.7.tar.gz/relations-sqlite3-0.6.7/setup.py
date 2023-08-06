578fc69c43b45d188c9ea8be6c9337d9512bd418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:20:16 +0000

    Add package relations-sqlite3/0.6.7/relations-sqlite3-0.6.7.tar.gz

---
+++ packages/relations-sqlite3/relations-sqlite3-0.6.7.tar.gz/relations-sqlite3-0.6.7/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="relations-sqlite3",
+    version="0.6.7",
+    package_dir = {'': 'lib'},
+    py_modules = [
+        'relations_sqlite3'
+    ],
+    install_requires=[
+        'relations-dil>=0.6.12',
+        'relations-sqlite>=0.6.2'
+    ],
+    url="https://github.com/relations-dil/python-relations-sqlite3",
+    author="Gaffer Fitch",
+    author_email="relations@gaf3.com",
+    description="DB Modeling for SQLite using the sqlite3 library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license_files=('LICENSE.txt',),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
