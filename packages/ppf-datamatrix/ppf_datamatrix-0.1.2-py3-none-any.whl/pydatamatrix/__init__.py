fdeb2271c90915121c77c14e9c8c0c9591f4d9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:21:41 +0000

    Add package ppf-datamatrix/0.1.2/ppf_datamatrix-0.1.2-py3-none-any.whl

---
+++ packages/ppf-datamatrix/ppf_datamatrix-0.1.2-py3-none-any.whl/pydatamatrix/__init__.py
@@ -0,0 +1,13 @@
+"""
+Sample
+++++++
+
+Sample is a package demonstrating how to package Python code
+"""
+# flake8: noqa
+
+# import every function, class, etc. that should be visible in the package
+from .datamatrix import *
+
+del datamatrix
+del utils
