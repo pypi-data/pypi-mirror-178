46987d16827b08add08a0beea8c1e3c19b49949c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:32 +0000

    Add package djag-scheduler/2.0.0/djag_scheduler-2.0.0-py3-none-any.whl

---
+++ packages/djag-scheduler/djag_scheduler-2.0.0-py3-none-any.whl/djag_scheduler/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+__all__ = ['DjagSchedulerConfig']
+
+
+class DjagSchedulerConfig(AppConfig):
+    """Default configuration for Djag-Scheduler app."""
+
+    name = 'djag_scheduler'
+    label = 'djag_scheduler'
+    verbose_name = 'Djag Scheduler'
+    default_auto_field = 'django.db.models.AutoField'
