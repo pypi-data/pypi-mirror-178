d29aed161eb293fbb207a3e31a60907c2c606cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:17 +0000

    Add package timelinetesttest13/0.0/timelinetesttest13-0.0.tar.gz

---
+++ packages/timelinetesttest13/timelinetesttest13-0.0.tar.gz/timelinetesttest13-0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(name='timelinetesttest13',
+      version='0.0',
+      description='This is timelinetestpack0.0.',
+      url='https://github.com/F-Jirayut/create_p',
+      author='U test',
+      author_email='fayristh0268@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires= [],
+      python_requires='>=3.6',
+      zip_safe=False)
