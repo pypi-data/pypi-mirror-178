6cde7080ed5c14b6180d28e8a7ed6e2594e0cb67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:16 +0000

    Add package py-csv-xls/0.0.3/py_csv_xls-0.0.3-py3-none-any.whl

---
+++ packages/py-csv-xls/py_csv_xls-0.0.3-py3-none-any.whl/py_csv_xls/__init__.py
@@ -0,0 +1,6 @@
+__version__ = [0, 0, 3]
+
+
+from .csvsniffer import CSVSniffer  # noqa:F401
+from .excelworker import ExcelWorker  # noqa:F401
+from .exception import PyCsvXlsException  # noqa:F401
