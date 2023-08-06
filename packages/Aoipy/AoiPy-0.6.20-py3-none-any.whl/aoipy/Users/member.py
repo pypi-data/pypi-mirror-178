188198bba95b21609ccb7f355ea74e99f95de2c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:06 +0000

    Add package Aoipy/0.6.20/AoiPy-0.6.20-py3-none-any.whl

---
+++ packages/Aoipy/AoiPy-0.6.20-py3-none-any.whl/aoipy/Users/member.py
@@ -0,0 +1,28 @@
+import discord
+
+
+async def banMember(member: discord.Member, reason: str = None):
+    if reason is None:
+        return await member.ban()
+    else:
+        return await member.ban(reason=reason)
+
+
+def getNick(member: discord.Member):
+    return member.display_name
+
+
+def getRoles(member: discord.Member):
+    return member.roles
+
+
+def getJoinDate(member: discord.Member):
+    return member.joined_at
+
+
+def sendMessage(user: discord.User, message, embed: bool = False):
+    if embed:
+        user.send(embed=message)
+    else:
+        user.send(message)
+
