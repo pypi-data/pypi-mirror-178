6e0a5796206a8bf6fed42c723aebf605aa569148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:51 +0000

    Add package robotpy-ctre/2023.0.0b1/robotpy_ctre-2023.0.0b1-cp37-cp37m-win_amd64.whl

---
+++ packages/robotpy-ctre/robotpy_ctre-2023.0.0b1-cp37-cp37m-win_amd64.whl/ctre/_tools_sim/include/ctre/phoenixpro/Utils.hpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) CTR-Electronics
+ * Contact support@ctr-electronics for any questions
+ * including support, features, and licensing.
+ */
+#pragma once
+
+#include "ctre/phoenix/export.h"
+
+CTREXPORT double GetCurrentTimeSeconds();
