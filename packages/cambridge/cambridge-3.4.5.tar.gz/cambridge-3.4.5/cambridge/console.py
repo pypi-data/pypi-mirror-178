31073ccbad7d2ac10a695e1deb71b82c038282c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:18 +0000

    Add package cambridge/3.4.5/cambridge-3.4.5.tar.gz

---
+++ packages/cambridge/cambridge-3.4.5.tar.gz/cambridge-3.4.5/cambridge/console.py
@@ -0,0 +1,8 @@
+"""
+Rich is a Python library for rich text and beautiful formatting in the terminal.
+This script constructs rich console object.
+"""
+
+from rich.console import Console
+
+console = Console(color_system="truecolor", highlight=False)
