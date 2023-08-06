e0e35a52688f3474231d326bcc2b43f24aa1aa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:32 +0000

    Add package splight-lib/0.5.7/splight_lib-0.5.7-py3-none-any.whl

---
+++ packages/splight-lib/splight_lib-0.5.7-py3-none-any.whl/splight_lib/component/__init__.py
@@ -0,0 +1,14 @@
+from .io import AbstractIOComponent, AbstractClientComponent
+from .algorithms import AbstractAlgorithmComponent
+from .network import AbstractNetworkComponent
+from .abstract import AbstractComponent
+from .system import AbstractSystemComponent
+
+__all__ = [
+    'AbstractComponent',
+    'AbstractIOComponent',
+    'AbstractClientComponent',
+    'AbstractNetworkComponent',
+    'AbstractAlgorithmComponent',
+    'AbstractSystemComponent',
+]
---
+++ packages/splight-lib/splight_lib-0.5.7-py3-none-any.whl/splight_models/notification.py
@@ -0,0 +1,52 @@
+from pydantic import Field
+from datetime import datetime, timezone
+from typing import Optional
+from enum import Enum
+from splight_models.datalake import DatalakeModel
+from splight_models.severity import SeverityType
+
+
+SYSTEM = 'system'
+INFO = 'info'
+LOW = 'low'
+MEDIUM = 'medium'
+HIGH = 'high'
+CRITICAL = 'critical'
+
+GREATER_THAN = 'gt'
+GREATER_THAN_OR_EQUAL = 'ge'
+LOWER_THAN = 'lt'
+LOWER_THAN_OR_EQUAL = 'le'
+EQUAL = 'eq'
+
+SEVERITIES = (
+    (SYSTEM, 'system'),
+    (INFO, 'info'),
+    (LOW, 'low'),
+    (MEDIUM, 'medium'),
+    (HIGH, 'high'),
+    (CRITICAL, 'critical'),
+)
+
+SOURCE_TYPE = (
+    ('Algorithm', 'Algorithm'),
+    ('Network', 'Network'),
+    ('Connector', 'Connector'),
+)
+
+
+class SourceType(str, Enum):
+    algorithm = 'Algorithm'
+    network = 'Network'
+    connector = 'Connector'
+
+
+class Notification(DatalakeModel):
+    id: Optional[str]
+    title: str
+    message: str
+    seen: bool = False
+    timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    severity: SeverityType = SeverityType.info
+    source_id: Optional[str]
+    source_type: Optional[SourceType]
