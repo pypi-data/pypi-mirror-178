66b2d66ecf0411d7c235f24ec095f025da09eb1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:12 +0000

    Add package wcatapi/1.0.4/wcatapi-1.0.4.tar.gz

---
+++ packages/wcatapi/wcatapi-1.0.4.tar.gz/wcatapi-1.0.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+longdesc = """# CatApi
+
+random_cat() -> img: random cat
+
+## class img
+
+img.download(filename: str, encoding: str): download img
+
+img.as_bytes(): get bytes from img
+
+img.as_file(): get file from img
+
+img.url: url to img
+"""
+
+setuptools.setup(
+	name="wcatapi",
+	version="1.0.4",
+	author="WiSpace",
+	author_email="wiforumit@gmail.com",
+	description="CatApi",
+	long_description=longdesc,
+	long_description_content_type="text/markdown",
+	url="https://api.wispace.ru/cat",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3.10"
+	],
+	python_requires='>=3.6',
+)
