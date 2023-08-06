4877992658d60f985618bf652f7a462023d2d20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:29 +0000

    Add package SWTK/1.1/SWTK-1.1.tar.gz

---
+++ packages/SWTK/SWTK-1.1.tar.gz/SWTK-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SWTK',
+    version='1.1',
+    description='An experimental Unsupervised Learning Log Anomaly Detection toolkit. YOU ARE BEAUTIFUL!',
+    url='https://github.com/nileshkhetrapal/SWTK',
+    author='Nilesh Khetrapal',
+    packages=find_packages(),
+    scripts=['bin/SWTK'],
+)
+
