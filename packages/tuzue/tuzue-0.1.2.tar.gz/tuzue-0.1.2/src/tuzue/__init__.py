2e25fd5a83a948371c4935e297916801f243763b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:22 +0000

    Add package tuzue/0.1.2/tuzue-0.1.2.tar.gz

---
+++ packages/tuzue/tuzue-0.1.2.tar.gz/tuzue-0.1.2/src/tuzue/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2022 Leandro Lisboa Penz <lpenz@lpenz.org>
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE', which is part of this source code package.
+
+import tuzue.inspector
+import tuzue.ui.curses
+import tuzue.view
+
+__version__ = "0.1.2"
+
+
+def navigate(struct, title=""):
+    view = tuzue.view.View(items=struct, title=title)
+    done = None
+    with tuzue.ui.curses.context() as ui:
+        while not done:
+            ui.show(view)
+            done = ui.interact(view)
+    return view.selected_item()
+
+
+def inspect(*args, **kwargs):
+    return tuzue.inspector.inspect(*args, **kwargs)
