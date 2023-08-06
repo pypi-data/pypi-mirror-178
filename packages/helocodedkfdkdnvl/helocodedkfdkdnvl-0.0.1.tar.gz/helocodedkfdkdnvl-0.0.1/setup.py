902d4259ad61c0aa8f6112e610706c49583dcc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:01 +0000

    Add package helocodedkfdkdnvl/0.0.1/helocodedkfdkdnvl-0.0.1.tar.gz

---
+++ packages/helocodedkfdkdnvl/helocodedkfdkdnvl-0.0.1.tar.gz/helocodedkfdkdnvl-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'a basic hello package'
+LONG_DESCRIPTION = 'A package that calls sayhello function.'
+
+# Setting up
+setup(
+    name="helocodedkfdkdnvl",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
