5db219d75acbea6beab89b55bc32e7e1eba8c78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:24:20 +0000

    Add package more-polars/0.2.1/more_polars-0.2.1-py3-none-any.whl

---
+++ packages/more-polars/more_polars-0.2.1-py3-none-any.whl/more_polars/more_polars.py
@@ -0,0 +1,29 @@
+from composable import pipeable
+import polars as pl
+
+__all__ = ('get_dtype_dict', 'col')
+
+@pipeable
+def get_dtype_dict(df):
+    ''' Construct a `dict` of column name, dtype pairs.
+    
+        **Note.** Useful when defining/changing the schema.
+        
+        Args:
+            df - A polars.Dataframe
+        
+        Returns: A dict of column names: str, and polars.dataypes
+    '''
+    return  dict(zip(df.columns, df.dtypes))
+
+
+@pipeable
+def col(s: str) -> pl.Expr:
+    ''' Pipeable function for creating polars column expressions.
+    
+    Args:
+        - s: A column label
+             Can be a regular expression starting with ^ and ending with $
+    Returns: A polars column expression.
+    '''
+    return pl.col(s)
