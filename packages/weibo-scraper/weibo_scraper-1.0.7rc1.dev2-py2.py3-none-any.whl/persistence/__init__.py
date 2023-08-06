a5159c89e4f109651241d2a867707fa6ebbdcf27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:00 +0000

    Add package weibo-scraper/1.0.7rc1.dev2/weibo_scraper-1.0.7rc1.dev2-py2.py3-none-any.whl

---
+++ packages/weibo-scraper/weibo_scraper-1.0.7rc1.dev2-py2.py3-none-any.whl/persistence/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+"""
+ Verion: 1.0
+ Author: Helixcs
+ Site: https://github.com/Xarrow/weibo-scraper
+ File: __init__.py
+ Time: 6/9/18
+"""
+
+from .persistence import dispatch
