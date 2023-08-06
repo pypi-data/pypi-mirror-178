ffb1994751f377b5440256df4a1477107b6d9abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:01 +0000

    Add package pytest-check/1.2.0/pytest_check-1.2.0-py3-none-any.whl

---
+++ packages/pytest-check/pytest_check-1.2.0-py3-none-any.whl/pytest_check/check_log.py
@@ -0,0 +1,29 @@
+from .pseudo_traceback import _build_pseudo_trace_str
+from colorama import Fore
+
+should_use_color = False
+_failures = []
+
+
+def clear_failures():
+    global _failures
+    _failures = []
+
+
+def any_failures() -> bool:
+    return bool(get_failures())
+
+
+def get_failures():
+    return _failures
+
+
+def log_failure(msg=""):
+    __tracebackhide__ = True
+    msg = str(msg).strip()
+    pseudo_trace_str = _build_pseudo_trace_str()
+    msg_plus_trace = f"{msg}\n{pseudo_trace_str}"
+    if should_use_color:
+        msg_plus_trace = f"{Fore.RED}{msg_plus_trace}{Fore.RESET}"
+    entry = f"FAILURE: {msg_plus_trace}"
+    _failures.append(entry)
