f429f8430f6b2ddca49c1b9ad9358fbceb09d693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:36 +0000

    Add package dep-module-mongo/2.10.28/dep-module-mongo-2.10.28.tar.gz

---
+++ packages/dep-module-mongo/dep-module-mongo-2.10.28.tar.gz/dep-module-mongo-2.10.28/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dep_mongo']
+
+package_data = \
+{'': ['*']}
+
+modules = \
+['pyproject']
+install_requires = \
+['beanie>=1.11.9,<2.0.0', 'dep-spec>=2.2.28,<3.0.0', 'poetry==1.1.15']
+
+setup_kwargs = {
+    'name': 'dep-module-mongo',
+    'version': '2.10.28',
+    'description': '',
+    'long_description': None,
+    'author': 'everhide',
+    'author_email': 'i.tolkachnikov@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9.0,<=3.9.13',
+}
+
+
+setup(**setup_kwargs)
