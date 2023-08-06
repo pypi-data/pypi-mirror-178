62f8440e6793a8628fc5658b04f6aa0a23be3cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:17 +0000

    Add package eth-transfer/0.0.2/eth_transfer-0.0.2.tar.gz

---
+++ packages/eth-transfer/eth_transfer-0.0.2.tar.gz/eth_transfer-0.0.2/test/tests.py
@@ -0,0 +1,12 @@
+
+import unittest
+import time
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+#from eth_transfer.gas import *
+#from eth_transfer.transaction import *
+
+
+class TestEthTransfer(unittest.TestCase):
+    pass
