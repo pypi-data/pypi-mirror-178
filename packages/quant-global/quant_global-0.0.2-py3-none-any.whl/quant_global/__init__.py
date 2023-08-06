f7980294c4bc6f12f930edf1f08d51c1e981ecbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:12 +0000

    Add package quant-global/0.0.2/quant_global-0.0.2-py3-none-any.whl

---
+++ packages/quant-global/quant_global-0.0.2-py3-none-any.whl/quant_global/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 24 16:10:29 2022
+
+@author: Local User
+"""
+
+from quant_global import download
---
+++ packages/quant-global/quant_global-0.0.2-py3-none-any.whl/quant_global/quant_global.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 24 15:55:23 2022
+
+@author: Local User
+"""
+
+import pandas as pd
+import requests
+import json
+
+def download(key, strategy, underlying, from_date, end_date = None):
+    
+    try:
+    
+        data_request = requests.get(f"https://qgserver.pythonanywhere.com/download?key={key}&strategy={strategy}&underlying={underlying}&from_date={from_date}&end_date={end_date}")
+        # data_request = requests.get(f"http://127.0.0.1:5000/download?key={key}&strategy={strategy}&underlying={underlying}&from_date={from_date}&end_date={end_date}")
+    
+        if strategy == 'pt_extended':
+            
+            try:
+                formatted_data = pd.DataFrame.from_dict(json.loads(data_request.content))
+                formatted_data['datetime'] = pd.to_datetime(formatted_data['datetime'], unit = 'ms')
+                formatted_data = formatted_data.set_index('datetime')
+            
+            except:
+                return data_request.text
+            
+        elif strategy == 'pt' or strategy == 'dca':
+            
+            try:
+                formatted_data = pd.DataFrame.from_dict(json.loads(data_request.content)).set_index('datetime')
+            
+            except:
+                return data_request.text
+            
+        return formatted_data    
+
+    except:
+        return 'Internal server error, please wait or modify parameters. If the error persists, contact: services@qg-indices.com along with your relevant code.'
