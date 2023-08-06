43d033d1a6c1028f4dcf4f62a10152c3b3547383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:35 +0000

    Add package ftThanos/0.0.45/ftThanos-0.0.45.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.45.tar.gz/ftThanos-0.0.45/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.45",
+description="A client for 45 API",
+author="Thanos 45",
+author_email="thanos@45paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
