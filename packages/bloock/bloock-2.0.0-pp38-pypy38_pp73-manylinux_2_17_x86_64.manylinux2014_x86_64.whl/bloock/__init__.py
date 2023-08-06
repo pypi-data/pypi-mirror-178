b0920e2eefa265185107063a68e969e090d10b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:08 +0000

    Add package bloock/2.0.0/bloock-2.0.0-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/bloock/bloock-2.0.0-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/bloock/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "2.0.0"
+
+from typing import Dict
+
+import bloock
+from bloock._bridge.proto.config_pb2 import NetworkConfig as NetworkConfigProto
+from bloock.client.entity.network import Network
+from bloock.client.entity.network_config import NetworkConfig
+
+api_key = ""
+api_host = ""
+network_config: Dict[int, NetworkConfigProto] = {}
+
+
+def set_network_config(network: Network, config: NetworkConfig):
+    bloock.network_config[int(network)] = NetworkConfigProto(
+        ContractAddress=config.contract_address,
+        ContractAbi=config.contract_abi,
+        HttpProvider=config.http_provider,
+    )
