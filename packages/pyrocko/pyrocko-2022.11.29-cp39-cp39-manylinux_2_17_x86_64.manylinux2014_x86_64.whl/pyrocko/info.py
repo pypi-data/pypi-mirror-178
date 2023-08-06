fcd9e5eb81714b536dfc39ffd2bb4490cfcb6b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:34 +0000

    Add package pyrocko/2022.11.29/pyrocko-2022.11.29-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyrocko/pyrocko-2022.11.29-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrocko/info.py
@@ -0,0 +1,6 @@
+# This module is automatically created from setup.py
+git_sha1 = '173b74e9b72e7ccf490085c8e2de4f903d441b74'
+local_modifications = True
+version = '2022.11.29'
+long_version = 'pyrocko-2022.11.29-173b74e9b72e7ccf490085c8e2de4f903d441b74-modified-2022-11-29_10:43:17'  # noqa
+installed_date = '2022-11-29_10:43:17'
