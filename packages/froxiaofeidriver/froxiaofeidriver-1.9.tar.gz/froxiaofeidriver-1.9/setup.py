ee1570def7aee9f89150ee930cb670dfa708aa9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:20 +0000

    Add package froxiaofeidriver/1.9/froxiaofeidriver-1.9.tar.gz

---
+++ packages/froxiaofeidriver/froxiaofeidriver-1.9.tar.gz/froxiaofeidriver-1.9/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import os.path
+from distutils.core import  setup
+
+
+packages = ['froxiaofeidriver']
+
+setup(name='froxiaofeidriver',version='1.9',author='feipip', packages=packages, package_dir={'requests': 'requests'},)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
