27f627c70c36a3bab2a7a0c0cb963a06eb35fa42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:22:32 +0000

    Add package jj/2.7.0/jj-2.7.0-py3-none-any.whl

---
+++ packages/jj/jj-2.7.0-py3-none-any.whl/jj/mock/__init__.py
@@ -0,0 +1,49 @@
+import os
+from distutils.util import strtobool
+from typing import Optional, Union
+
+from jj.expiration_policy import ExpirationPolicy
+from jj.matchers import LogicalMatcher, RequestMatcher
+
+from ._history import (
+    HistoryAdapterType,
+    HistoryItem,
+    HistoryRepository,
+    HistoryRequest,
+    HistoryResponse,
+    default_history_adapter,
+)
+from ._mock import Mock
+from ._mocked import Mocked
+from ._remote_handler import RemoteHandler
+from ._remote_mock import RemoteMock
+from ._remote_response import RemoteResponseType
+from ._system_log_filter import SystemLogFilter
+
+_REMOTE_MOCK_URL = os.environ.get("JJ_REMOTE_MOCK_URL", "http://localhost:8080")
+_REMOTE_MOCK_DISPOSABLE = os.environ.get("JJ_REMOTE_MOCK_DISPOSABLE", "True")
+
+
+def mocked(matcher: Union[RequestMatcher, LogicalMatcher],
+           response: RemoteResponseType,
+           expiration_policy: Optional[ExpirationPolicy] = None,
+           *,
+           disposable: Optional[bool] = None,
+           prefetch_history: bool = True,
+           history_adapter: Optional[HistoryAdapterType] = default_history_adapter) -> "Mocked":
+    if disposable is None:
+        disposable = bool(strtobool(_REMOTE_MOCK_DISPOSABLE))
+
+    handler = RemoteMock(_REMOTE_MOCK_URL).create_handler(
+        matcher,
+        response,
+        expiration_policy,
+        history_adapter=history_adapter
+    )
+    return Mocked(handler, disposable=disposable, prefetch_history=prefetch_history)
+
+
+__all__ = ("Mock", "mocked", "RemoteMock", "RemoteHandler", "Mocked",
+           "HistoryRepository", "HistoryRequest", "HistoryResponse", "HistoryItem",
+           "SystemLogFilter", "RemoteResponseType", "HistoryAdapterType",
+           "default_history_adapter",)
