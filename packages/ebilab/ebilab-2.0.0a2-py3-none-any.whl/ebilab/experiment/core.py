db53fea17a56ef77c2441aea54f954906e56675e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:16 +0000

    Add package ebilab/2.0.0a2/ebilab-2.0.0a2-py3-none-any.whl

---
+++ packages/ebilab/ebilab-2.0.0a2-py3-none-any.whl/ebilab/experiment/core.py
@@ -0,0 +1,5 @@
+print("Deprecated warning: importing ebilab.experiment.core is deprecated. Use `from ebilab.experiment import Experiment, Plotter` instead.")
+
+from ._core import Experiment, Plotter
+
+
