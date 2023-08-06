897462262f3ce5f381a69594fac4efbaab3e5346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:16 +0000

    Add package pydata-master/0.0.7/pydata_master-0.0.7-py3-none-any.whl

---
+++ packages/pydata-master/pydata_master-0.0.7-py3-none-any.whl/pydata_master/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2022 Thinh Vu @ GitHub
+# See LICENSE for details.
+
+__author__ = "Thinh Vu @thinh-vu in GitHub"
+__version__ = "0.0.7"
+
+from ur_gadget import *
+from .database_connect import *
+from .messaging_api import *
+from .google_service import *
+from .visualization import *
+from .crm_system import *
