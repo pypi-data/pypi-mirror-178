955ed01a5f224de333bcf9b7fd3f51c5d52b52e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:29 +0000

    Add package cstack/0.1.0/cstack-0.1.0.tar.gz

---
+++ packages/cstack/cstack-0.1.0.tar.gz/cstack-0.1.0/cstack/cli/generators/model.py
@@ -0,0 +1,34 @@
+"""
+generators/model.py
+Ian Kollipara
+2022.11.26
+
+Tortoise Model Generator
+"""
+
+# Imports
+from os import curdir
+from pathlib import Path
+from tomllib import loads
+from cstack._internal import templater
+
+PROJECT = loads(Path("pyproject.toml").read_text("utf-8"))
+
+
+class ModelGenerator:
+    def __init__(self, model_name: str) -> None:
+        self.model_path = (
+            Path(curdir)
+            / PROJECT["tool"]["poetry"]["name"]
+            / Path("api")
+            / "models"
+            / f"{model_name}.py"
+        )
+
+    def __call__(self):
+        self.model_path.write_text(
+            templater.render(
+                "api_model.py.j2",
+                {"model_name": self.model_path.name.removesuffix(".py").title()},
+            )
+        )
