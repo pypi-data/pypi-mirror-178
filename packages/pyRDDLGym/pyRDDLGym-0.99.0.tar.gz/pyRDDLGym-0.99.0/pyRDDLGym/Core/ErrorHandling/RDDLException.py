f0bf98d5f64a43003e84d24be2e991ac05edaed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:14 +0000

    Add package pyRDDLGym/0.99.0/pyRDDLGym-0.99.0.tar.gz

---
+++ packages/pyRDDLGym/pyRDDLGym-0.99.0.tar.gz/pyRDDLGym-0.99.0/pyRDDLGym/Core/ErrorHandling/RDDLException.py
@@ -0,0 +1,56 @@
+
+
+class RDDLActionPreconditionNotSatisfiedError(ValueError):
+    pass
+
+
+class RDDLInvalidDependencyInCPFError(SyntaxError):
+    pass
+
+
+class RDDLInvalidExpressionError(SyntaxError):
+    pass
+
+
+class RDDLInvalidNumberOfArgumentsError(SyntaxError):
+    pass
+
+
+class RDDLMissingCPFDefinitionError(SyntaxError):
+    pass
+
+
+class RDDLNotImplementedError(NotImplementedError):
+    pass
+
+
+class RDDLParseError(SyntaxError):
+    pass
+
+
+class RDDLStateInvariantNotSatisfiedError(ValueError):
+    pass
+
+
+class RDDLTypeError(TypeError):
+    pass
+
+
+class RDDLUndefinedCPFError(SyntaxError):
+    pass
+
+
+class RDDLUndefinedVariableError(SyntaxError):
+    pass
+
+
+class RDDLValueOutOfRangeError(ValueError):
+    pass
+
+
+class RDDLEnvironmentNotExist(ValueError):
+    pass
+
+
+class RDDLInstanceNotExist(ValueError):
+    pass
