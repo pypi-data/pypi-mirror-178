e78ec9b815d06e62c6f13633a26e6c1fdd99876d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:33 +0000

    Add package WES-Calculation/2.3.8/WES_Calculation-2.3.8.tar.gz

---
+++ packages/WES-Calculation/WES_Calculation-2.3.8.tar.gz/WES_Calculation-2.3.8/WES_Calculation/__init__.py
@@ -0,0 +1,15 @@
+'''
+Author: BDFD
+Date: 2021-10-27 18:42:50
+LastEditTime: 2022-05-13 10:52:05
+LastEditors: BDFD
+Description: In User Settings Edit
+FilePath: \5.2-PyPi-WES_Calculation\WES_Calculation\__init__.py
+'''
+
+from WES_Calculation.templateproj import add_one
+from WES_Calculation.templateproj import add_two
+from WES_Calculation.gumbel import gumbel
+from WES_Calculation.greenampt import greenampt
+from WES_Calculation.wavespectra import wave
+
