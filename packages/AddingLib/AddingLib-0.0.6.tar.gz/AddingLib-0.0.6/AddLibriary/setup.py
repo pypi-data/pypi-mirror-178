be035a2bc27b694d2ecfcea82e8ac4c7e8ae0f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:29 +0000

    Add package AddingLib/0.0.6/AddingLib-0.0.6.tar.gz

---
+++ packages/AddingLib/AddingLib-0.0.6.tar.gz/AddingLib-0.0.6/AddLibriary/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='resources',
+    version='0.0.6',
+    package_dir={'fonts': 'fonts','images':'images','js':'js','maps':'maps','qml':'qml','sounds':'sounds','tr':'tr'},
+    packages=['fonts','images','js','maps','qml','sounds','tr'],
+)
