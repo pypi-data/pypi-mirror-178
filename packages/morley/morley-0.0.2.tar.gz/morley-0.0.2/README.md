b117de5c24042320c462f06794c241a6fc0ded4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:07 +0000

    Add package morley/0.0.2/morley-0.0.2.tar.gz

---
+++ packages/morley/morley-0.0.2.tar.gz/morley-0.0.2/README.md
@@ -0,0 +1,5 @@
+# Morley
+
+Morphometry of germinated plants in Python
+
+Morley is open-sourse software for plants morphometry: measuring sprouts, roots length.
---
+++ packages/morley/morley-0.0.2.tar.gz/morley-0.0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name                 = 'morley',
+    version              = '0.0.2',
+    description          = '''GUI software for plant morphometry''',
+    long_description     = None,
+    # long_description_content_type="text/markdown",
+    author               = 'Daria Emekeeva & Lev Levitsky',
+    author_email         = 'dashabezik65@gmail.com',
+    install_requires     = ['pyteomics', 'seaborn', 'scipy', 'scikit-learn', 'opencv-python', 'imutils'],
+    classifiers          = ['Intended Audience :: Science/Research',
+                            'Programming Language :: Python :: 3',
+                            'Topic :: Scientific/Engineering :: Bio-Informatics',
+                            'Intended Audience :: End Users/Desktop'],
+    license              = 'License :: OSI Approved :: Apache Software License',
+    packages             = find_packages(),
+    package_data         = {'morley': ['ethalon.png']},
+    entry_points         = {'console_scripts': ['morley=morley.main:main']},
+    )
