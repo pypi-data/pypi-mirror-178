4d016b5fadb0ee9b27a5a1752084e2547566557f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:00:23 +0000

    Add package stringcalc/0.1.0.dev0/stringcalc-0.1.0.dev0.tar.gz

---
+++ packages/stringcalc/stringcalc-0.1.0.dev0.tar.gz/stringcalc-0.1.0.dev0/data/extract-daddario.py
@@ -0,0 +1,19 @@
+"""
+Extract data from the D'Addario string tension chart PDF
+into a more useful format.
+
+The chart is available at:
+https://www.daddario.com/globalassets/pdfs/accessories/tension_chart_13934.pdf
+"""
+import PyPDF2
+
+f = open("tension_chart_13934.pdf", "rb")
+pdf = PyPDF2.PdfFileReader(f)
+
+assert pdf.numPages == 14
+
+page = pdf.getPage(4)
+print(page.extractText())
+
+
+f.close()
