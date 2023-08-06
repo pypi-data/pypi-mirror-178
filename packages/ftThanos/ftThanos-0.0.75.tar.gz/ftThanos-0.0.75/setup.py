33625daca6bc57971cb3376fc75b6879f0f70e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:00:30 +0000

    Add package ftThanos/0.0.75/ftThanos-0.0.75.tar.gz

---
+++ packages/ftThanos/ftThanos-0.0.75.tar.gz/ftThanos-0.0.75/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ftThanos',
+version="0.0.75",
+description="A client for 75 API",
+author="Thanos 75",
+author_email="thanos@75paris.fr",
+packages=["ftThanos"],
+install_requires=["requests","python-dotenv"],
+)
