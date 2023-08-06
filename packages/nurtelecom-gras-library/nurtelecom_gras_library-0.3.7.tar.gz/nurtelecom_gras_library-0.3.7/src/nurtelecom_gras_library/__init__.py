91273cef342e279667943820f3264fe3bc955973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:13 +0000

    Add package nurtelecom-gras-library/0.3.7/nurtelecom_gras_library-0.3.7.tar.gz

---
+++ packages/nurtelecom-gras-library/nurtelecom_gras_library-0.3.7.tar.gz/nurtelecom_gras_library-0.3.7/src/nurtelecom_gras_library/__init__.py
@@ -0,0 +1,7 @@
+# from PLSQL_data_importer import PLSQL_data_importer
+# from additional_functions import *
+# from PLSQL_geodata_importer import PLSQL_geodata_importer
+from importlib.metadata import version
+__version__ = version("nurtelecom_gras_library")
+
+from core.additional_functions import *
