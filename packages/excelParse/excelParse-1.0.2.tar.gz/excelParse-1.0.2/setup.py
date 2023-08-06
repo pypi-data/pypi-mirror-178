cd9bd380493ff88d89ca1f50a85588a6209f8f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:11 +0000

    Add package excelParse/1.0.2/excelParse-1.0.2.tar.gz

---
+++ packages/excelParse/excelParse-1.0.2.tar.gz/excelParse-1.0.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "excelParse",
+    version = "1.0.2",
+    keywords= ["pip","excelParse"],
+    description= "Used for KCmetadata.xls Parsing",
+    
+    url = "https://gitee.com/ganjun87/excel-parse.git",
+    author = "ganjun",
+    author_email= "ganjun@alumni.hust.edu.cn",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+)
