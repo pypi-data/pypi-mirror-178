fa9a3d14fd2ca73d8a08bbb7ba4ad43825c56819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:40 +0000

    Add package ndnsimgraph/0.2.8/ndnsimgraph-0.2.8.tar.gz

---
+++ packages/ndnsimgraph/ndnsimgraph-0.2.8.tar.gz/ndnsimgraph-0.2.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ndnsimgraph",
+    version="0.2.8",
+    author="SunnyQjm",
+    author_email="qjm253@pku.edu.cn",
+    description="A small graph package used to draw image for ndnsim metrics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SunnyQjm/ndnsim-graph",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'openpyxl',
+        'matplotlib==3.5.3',
+        'pandas'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
