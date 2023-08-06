48246c69700ee03b2255455fac7c6e014761b447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:52 +0000

    Add package stream-wars/0.0.4/stream_wars-0.0.4.tar.gz

---
+++ packages/stream-wars/stream_wars-0.0.4.tar.gz/stream_wars-0.0.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['stream_wars']
+
+package_data = \
+{'': ['*'], 'stream_wars': ['frontend/build/*']}
+
+install_requires = \
+['streamlit>=1.12.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'stream-wars',
+    'version': '0.0.4',
+    'description': 'Streamlit component to generate the Star Wars crawl text.',
+    'long_description': '<p align="center">\n  <a href="https://github.com/murilo-cunha/stream-wars"><img width="50%" alt="logo" src="https://raw.githubusercontent.com/murilo-cunha/stream-wars/main/images/stream-wars-logo.png"></a>\n</p>\n<p align="center">\n  <a href="https://pypi.org/project/stream-wars/"><img alt="Python versions" src="https://img.shields.io/pypi/pyversions/stream-wars" /></a>\n  <a href="https://pypi.org/project/stream-wars/"><img alt="PiPy" src="https://img.shields.io/pypi/v/stream-wars" /></a>\n  <a href="https://pepy.tech/project/stream-wars"><img alt="Downloads" src="https://pepy.tech/badge/stream-wars" /></a>\n  <a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg" /></a>\n  <a href="http://mypy-lang.org/"><img alt="Mypy checked" src="https://img.shields.io/badge/mypy-checked-1f5082.svg" /></a>\n</p>\n\n> A Star Wars crawl component for Streamlit apps!\n\nA wrapper around [`star-wars-crawl-greensock-with-hooks`](https://github.com/mandiwise/star-wars-crawl-greensock-with-hooks)\nby [@mandiwise](https://github.com/mandiwise), using the\n[`streamlit-component-template-react-hooks`](https://github.com/whitphx/streamlit-component-template-react-hooks)\nby [@whitphx](https://github.com/whitphx) (credits to them! 👏👏).\n\n## Details\n\nThis is a small project putting together a "Star Wars" crawl component written in React and a React hooks-based template for Streamlit apps.\n\nWhy? The official documentation lists the class based components for React. React has released the hook-based API, which is arguably simpler and generally preferred over the class based components. Personally, I also prefer hook-based components over class-based ones. The project also uses `poetry` over the official documentation\'s `setup.py` and `MANIFEST.in` files - another personal decision.\n\n## Features\n\n- [x] Custom button\n- [x] Custom intro\n- [x] Custom episode text\n- [x] Custom episode title\n- [x] Custom crawl content\n- [ ] Available on `wide` layout\n',
+    'author': 'Murilo Cunha',
+    'author_email': 'murilo@dataroots.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://murilo-cunha-stream-wars-stream-warsdocs-slmccu.streamlit.app/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8, !=2.7.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, !=3.7.*',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/stream-wars/stream_wars-0.0.4.tar.gz/stream_wars-0.0.4/stream_wars/frontend/build/precache-manifest.0f035c538e0c3e45cbc258cee93da590.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ed9de3792a49545dee57736da522b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68e19cba8c488ace970e",
+    "url": "/static/css/main.8c36efda.chunk.css"
+  },
+  {
+    "revision": "480d9c457005aa374268",
+    "url": "/static/js/2.ee335c75.chunk.js"
+  },
+  {
+    "revision": "78c96c22aeb665cb55c9dc105844f80c",
+    "url": "/static/js/2.ee335c75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68e19cba8c488ace970e",
+    "url": "/static/js/main.e0207066.chunk.js"
+  },
+  {
+    "revision": "bf12a9f5cb9a024688c1",
+    "url": "/static/js/runtime-main.cc04dae6.js"
+  },
+  {
+    "revision": "6d9a2d0075fa8d5252ab7d08cf661136",
+    "url": "/static/media/StarJediHollow.6d9a2d00.woff"
+  },
+  {
+    "revision": "f36aaf0dc82131e21db43faa6db4095b",
+    "url": "/static/media/StarJediHollow.f36aaf0d.woff2"
+  },
+  {
+    "revision": "04238fc34d2403935761ff468bb8c5ed",
+    "url": "/static/media/volume_off.04238fc3.svg"
+  },
+  {
+    "revision": "ef7f87ab516ccf1eac5a5111815c427f",
+    "url": "/static/media/volume_on.ef7f87ab.svg"
+  }
+]);
