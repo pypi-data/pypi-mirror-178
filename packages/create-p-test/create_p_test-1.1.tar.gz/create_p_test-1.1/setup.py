0dbfa0b4d7a64036480653ebdf16fb7036e6316f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:07 +0000

    Add package create-p-test/1.1/create_p_test-1.1.tar.gz

---
+++ packages/create-p-test/create_p_test-1.1.tar.gz/create_p_test-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(name='create_p_test',
+      version='1.1',
+      description='This is create_p_test.',
+      url='https://github.com/F-Jirayut/create_p',
+      author='U test',
+      author_email='fayristh0268@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires= [],
+      python_requires='>=3.6',
+      zip_safe=False)
