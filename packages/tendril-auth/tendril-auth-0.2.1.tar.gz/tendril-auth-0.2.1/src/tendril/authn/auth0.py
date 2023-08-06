93c622154ca1046115531886593f7e5af86c8b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:53 +0000

    Add package tendril-auth/0.2.1/tendril-auth-0.2.1.tar.gz

---
+++ packages/tendril-auth/tendril-auth-0.2.1.tar.gz/tendril-auth-0.2.1/src/tendril/authn/auth0.py
@@ -0,0 +1,35 @@
+
+
+import os
+from tendril.config import AUTH0_NAMESPACE
+os.environ['AUTH0_RULE_NAMESPACE'] = AUTH0_NAMESPACE
+
+from fastapi import Security
+from fastapi_auth0 import Auth0, Auth0User
+from tendril.config import AUTH0_DOMAIN
+from tendril.config import AUTH0_AUDIENCE
+
+from tendril.authz import scopes
+from tendril.utils import log
+logger = log.get_logger(__name__, log.DEBUG)
+
+
+logger.info("Using auth0 parameters:\n"
+            "  - domain    {} \n"
+            "  - audience  {} \n"
+            "  - namespace {} ".format(AUTH0_DOMAIN, AUTH0_AUDIENCE, AUTH0_NAMESPACE))
+
+auth = Auth0(domain=AUTH0_DOMAIN,
+             api_audience=AUTH0_AUDIENCE,
+             scopes=scopes.scopes)
+
+
+authn_dependency = auth.implicit_scheme
+AuthUserModel = Auth0User
+
+
+def auth_spec(scopes=None):
+    kwargs = {}
+    if scopes:
+        kwargs['scopes'] = scopes
+    return Security(auth.get_user, **kwargs)
---
+++ packages/tendril-auth/tendril-auth-0.2.1.tar.gz/tendril-auth-0.2.1/src/tendril/config/auth.py
@@ -0,0 +1,45 @@
+
+
+from tendril.utils.config import ConfigOption
+from tendril.utils import log
+logger = log.get_logger(__name__, log.DEFAULT)
+
+depends = ['tendril.config.core']
+
+
+config_elements_auth = [
+    ConfigOption(
+        'AUTH_PROVIDER',
+        "'auth0'",
+        "Auth Provider"
+    ),
+]
+
+
+config_elements_auth0 = [
+    ConfigOption(
+        'AUTH0_DOMAIN',
+        "None",
+        "Auth0 Domain"
+    ),
+    ConfigOption(
+        'AUTH0_AUDIENCE',
+        "None",
+        "Auth0 Audience"
+    ),
+    ConfigOption(
+        'AUTH0_NAMESPACE',
+        "'https://tendril.link/schema/auth0'",
+        "Auth0 Namespace for Token Contents"
+    )
+]
+
+
+def load(manager):
+    logger.debug("Loading {0}".format(__name__))
+    manager.load_elements(config_elements_auth,
+                          doc="Authentication Configuration")
+    if manager.AUTH_PROVIDER == "auth0":
+        manager.load_elements(config_elements_auth0,
+                              doc="Auth0 Configuration")
+
