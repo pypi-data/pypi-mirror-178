4f7482c921d0423ae14ff0786fd5b338a8e36693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:23:43 +0000

    Add package splight-lib/0.5.8/splight-lib-0.5.8.tar.gz

---
+++ packages/splight-lib/splight-lib-0.5.8.tar.gz/splight-lib-0.5.8/remote_splight_lib/database/classmap.py
@@ -0,0 +1,85 @@
+from splight_models import (
+    Algorithm,
+    Asset,
+    Attribute,
+    Connector,
+    File,
+    System,
+    Mapping,
+    Edge,
+    Graph,
+    Network,
+    Notification,
+    Query,
+    Node,
+    Tag,
+    BlockchainContract,
+    BlockchainContractSubscription,
+    Component,
+    ComponentObject,
+    ComponentCommand,
+)
+
+
+CLASSMAP = {
+    Algorithm: {
+        "path": "algorithm"
+    },
+    Asset: {
+        "path": "asset"
+    },
+    Attribute: {
+        "path": "attribute"
+    },
+    Connector: {
+        "path": "connector"
+    },
+    Mapping: {
+        "path": "mapping"
+    },
+    Edge: {
+        "path": "graph/edge"
+    },
+    File: {
+        "path": "file"
+    },
+    Graph: {
+        "path": "graph/graph"
+    },
+    # Namespace: {
+    #     "path": "asset"
+    # },
+    Network: {
+        "path": "network"
+    },
+    Notification: {
+        "path": "notification"
+    },
+    Node: {
+        "path": "graph/node"
+    },
+    Tag: {
+        "path": "tag"
+    },
+    BlockchainContract: {
+        "path": "blockchain/contract"
+    },
+    BlockchainContractSubscription: {
+        "path": "blockchain/subscription"
+    },
+    System: {
+        "path": "system"
+    },
+    Component: {
+        "path": "components"
+    },
+    ComponentObject: {
+        "path": "component-objects"
+    },
+    ComponentCommand: {
+        "path": "component-commands"
+    },
+    Query: {
+        "path": "query"
+    },
+}
