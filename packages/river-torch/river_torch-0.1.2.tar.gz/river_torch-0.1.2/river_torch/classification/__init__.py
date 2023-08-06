ca2e3c9a5cf0f82a43c23b236ae90e6c7adae95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:17 +0000

    Add package river-torch/0.1.2/river_torch-0.1.2.tar.gz

---
+++ packages/river-torch/river_torch-0.1.2.tar.gz/river_torch-0.1.2/river_torch/classification/__init__.py
@@ -0,0 +1,7 @@
+from river_torch.classification.classifier import Classifier
+from river_torch.classification.rolling_classifier import RollingClassifier
+
+__all__ = [
+    "Classifier",
+    "RollingClassifier",
+]
