2a0c84c04291e4212c55b3531e3d1c4efa78f564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:52 +0000

    Add package pylond/0.0.2/pylond-0.0.2.tar.gz

---
+++ packages/pylond/pylond-0.0.2.tar.gz/pylond-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name = 'pylond',
+    version = '0.0.2',
+    description = 'Python 3 package that provides Icelandic translations of countries, nationalities and languages',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/thrkll/pylond',
+    author = 'Þorkell Einarsson',
+    author_email = 'thorkell@thorkell.lv',
+    classifiers=[
+         'Development Status :: 3 - Alpha',
+         'Intended Audience :: Developers',
+         'Natural Language :: Icelandic',
+         'License :: OSI Approved :: MIT License',
+         'Programming Language :: Python :: 3',
+    ],
+    keywords='iceland, icelandic-language, localization',
+    license = 'MIT',
+    packages=['pylond'],
+    install_requires=['numpy'],
+    extras_require={'dev': ['pytest']}
+    )
