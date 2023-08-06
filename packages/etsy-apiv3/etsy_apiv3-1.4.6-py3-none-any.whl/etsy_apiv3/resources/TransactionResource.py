e77e326489c3239915d0ecb4a3edf4049e33af9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:32 +0000

    Add package etsy-apiv3/1.4.6/etsy_apiv3-1.4.6-py3-none-any.whl

---
+++ packages/etsy-apiv3/etsy_apiv3-1.4.6-py3-none-any.whl/etsy_apiv3/resources/TransactionResource.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from ..utils import EtsySession, Response
+from ..models import Transaction
+
+
+@dataclass
+class TransactionResource():
+    session: EtsySession
+    __endpoint = "shops/{shop_id}"
+    
+    def get_transactions_by_listing_id(self, shop_id: int, listing_id: int, limit=25, offset=0) -> Response[Transaction]:
+        url = f"{self.__endpoint.format(shop_id=shop_id)}/listings/{listing_id}/transactions"
+
+        json = self.session.request(url, params={"limit":limit, "offset":offset})
+        return Response[Transaction](**json)
+    
+    def get_transactions_by_receipt_id(self, shop_id: int, receipt_id: int) -> Response[Transaction]:
+        url = self.__endpoint.format(shop_id=shop_id) + f"/receipts/{receipt_id}/transactions"
+        json = self.session.request(url)
+        
+        return Response[Transaction](**json)
+
+    def find_one(self, shop_id: int, transaction_id: int) -> Transaction:
+        url = self.__endpoint.format(shop_id=shop_id) + f"/transactions/{transaction_id}"
+        json = self.session.request(url)
+        return Transaction(**json)
+    
+    def find(self, shop_id: int):
+        url = self.__endpoint.format(shop_id=shop_id) + f"/transactions"
+        json = self.session.request(url)
+        return Response[Transaction](**json)
---
+++ packages/etsy-apiv3/etsy_apiv3-1.4.6-py3-none-any.whl/etsy_apiv3/utils/APIV3.py
@@ -0,0 +1,83 @@
+from typing import Optional, Callable
+from requests_oauthlib import OAuth2Session
+from async_oauthlib import OAuth2Session as AsyncOAuth2Session
+import requests
+from .RequestException import EtsyRequestException
+
+
+
+class EtsySession:
+    
+    def __token_updater(token):
+        return token
+    
+    def __init__(self,
+                 client_key: str,
+                 client_secret: str,
+                 token: dict,
+                 refresh_url="https://api.etsy.com/v3/public/oauth/token",
+                 headers: Optional[dict] = None,
+                 token_updater: Callable = __token_updater
+    ):
+        self.CLIENT_KEY = client_key
+        self.CLIENT_SECRET = client_secret
+        self.TOKEN = token
+        self.REFRESH_URL = refresh_url
+        self.headers = headers or {"x-api-key": self.CLIENT_KEY}
+        self.refresh_kwargs = {
+            'client_id': self.CLIENT_KEY,
+            'client_secret': self.CLIENT_SECRET,
+        }
+        
+        self.__base_endpoint = "https://openapi.etsy.com/v3/application/"
+        self.__oauth2_session = OAuth2Session(
+            self.CLIENT_KEY,
+            token=self.TOKEN,
+            auto_refresh_kwargs=self.refresh_kwargs,
+            auto_refresh_url=self.REFRESH_URL,
+            token_updater=token_updater)
+    @property
+    def me(self) -> dict:
+        session = self.get_or_create_sync_session()
+        return session.get(f"{self.__base_endpoint}/users/me", headers=self.headers).json()
+
+    @staticmethod
+    def create_response(response: requests.Response):
+        json: dict = response.json()
+        
+        if "error" in json.keys():
+            raise EtsyRequestException(response.status_code, json["error"])
+        
+        return json
+    
+    def request(self, endpoint: str, method="GET", *args, **kwargs) -> dict:
+        """ 
+        Send Request to target endpoint by method
+        
+        Args:
+            endpoint (str): Api Endpoint Url
+            method (str, optional): HTTP Methods [GET, POST, PUT, DELETE, UPDATE]. Defaults to "GET".
+
+        Raises:
+            EtsyRequestException: EtsyRequestException(status_code, message)
+
+        Returns:
+            json: Json From Request Response
+        """
+        url = f"{self.__base_endpoint}{endpoint}"
+        req: requests.Response = self.__oauth2_session.request(method, url, headers=self.headers, *args, **kwargs)
+        
+        return self.create_response(req)
+        
+    
+    async def async_request(self, session: AsyncOAuth2Session, endpoint: str, method="GET", *args, **kwargs) -> dict:
+            url = f"{self.__base_endpoint}{endpoint}"
+            req: requests.Response = await session.get(url=url, headers=self.headers, *args, **kwargs)
+            print(req.text)
+            json: dict = await req.json()
+            
+            if "error" in json.keys():
+                raise EtsyRequestException(req.status_code, json["error"])
+        
+            return json
+            
