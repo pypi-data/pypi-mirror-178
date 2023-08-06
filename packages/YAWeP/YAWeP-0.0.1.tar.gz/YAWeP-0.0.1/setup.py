85b265a793eb699416088ad05652b6552b2e7aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:36 +0000

    Add package YAWeP/0.0.1/YAWeP-0.0.1.tar.gz

---
+++ packages/YAWeP/YAWeP-0.0.1.tar.gz/YAWeP-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name = "YAWeP",
+    packages = ['YAWeP'],
+    version = "0.0.1",
+    author = "Leihaorambam Abhijit Singh",
+    author_email = "labhijitsingh13@gmail.com",
+    description = "Yet another weather package api",
+    url="https://gitlab.com/AbhijitL/yawp",
+    keywords=['weather','forecast','openwatherapi'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = ">=3.6"
+)
