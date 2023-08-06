dc9e083b3ef09acdec67778c7257843fabcdc558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:24:24 +0000

    Add package pkgexamplesubmodules/0.0.2/pkgexamplesubmodules-0.0.2.tar.gz

---
+++ packages/pkgexamplesubmodules/pkgexamplesubmodules-0.0.2.tar.gz/pkgexamplesubmodules-0.0.2/src/pkgexamplesubmodules/rexsubmodule2/src/rexsubmodule2/sayhello.py
@@ -0,0 +1,6 @@
+class SayHello:
+    def __init__(self):
+        self.a_simple_message = "Submodule 2 says hello!"
+
+    def sayhello(self):
+        print(self.a_simple_message)
---
+++ packages/pkgexamplesubmodules/pkgexamplesubmodules-0.0.2.tar.gz/pkgexamplesubmodules-0.0.2/src/rexsubmodule1/src/rexsubmodule1/sayhello.py
@@ -0,0 +1,6 @@
+class SayHello:
+    def __init__(self):
+        self.a_simple_message = "Submodule 1 says hello!"
+
+    def sayhello(self):
+        print(self.a_simple_message)
