bb2a25f16955f28cfa78ee3f2bc8308a4cb609ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:30 +0000

    Add package mpltoolbox/0.3.0/mpltoolbox-0.3.0-py3-none-any.whl

---
+++ packages/mpltoolbox/mpltoolbox-0.3.0-py3-none-any.whl/mpltoolbox/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Mpltoolbox contributors (https://github.com/mpltoolbox)
+
+# flake8: noqa
+
+from .lines import Lines
+from .rectangles import Rectangles
+from .ellipses import Ellipses
+from .points import Points
+from .polygons import Polygons
+from .hspans import Hspans
+from .vspans import Vspans
