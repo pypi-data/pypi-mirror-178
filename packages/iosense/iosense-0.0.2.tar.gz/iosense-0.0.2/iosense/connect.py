222218af1423986aa99c31390a3ba81f0895c7d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:11 +0000

    Add package iosense/0.0.2/iosense-0.0.2.tar.gz

---
+++ packages/iosense/iosense-0.0.2.tar.gz/iosense-0.0.2/iosense/connect.py
@@ -0,0 +1,13 @@
+"""
+This module implements the main data access functionalities of I/O Sense
+Author: Rishi Sharma from Faclon Labs
+"""
+
+__author__ = "Rishi Sharma, Faclon Labs Private Limited"
+__email__ = "rishi.sharma@faclon.com"
+__status__ = "development"
+
+
+class lastdp:
+    def testFunc():
+        return ("This works")
