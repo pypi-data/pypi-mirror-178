1b69396060dbe9b87e3ffb97818206115cc16189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:35 +0000

    Add package cortexlib/0.0.5/cortexlib-0.0.5.tar.gz

---
+++ packages/cortexlib/cortexlib-0.0.5.tar.gz/cortexlib-0.0.5/src/cortexlib/panda_helpers.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+d = {'col1': [1, 2], 'col2': [3, 4]}
+df = pd.DataFrame(data=d)
+
+def add_blank_column(dataframe,column_name):
+    dataframe[f'{column_name}'] = ''
+    return dataframe
