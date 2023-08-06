8648b096a9c8947e0881e2ab8a5e9d0bf240d3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:20 +0000

    Add package vodesfunc/1.0.1/vodesfunc-1.0.1.zip

---
+++ packages/vodesfunc/vodesfunc-1.0.1.zip/vodesfunc-1.0.1/vodesfunc/__init__.py
@@ -0,0 +1,13 @@
+"""
+    Oh god no
+"""
+
+# flake8: noqa
+
+from . import automation, misc, noise, scale, util
+from .automation import *
+from .auto.convert import *
+from .misc import *
+from .noise import *
+from .scale import *
+from .util import *
