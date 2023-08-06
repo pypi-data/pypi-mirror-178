5e74d9d0508e91fae3e31ca631d7a46e4fc7d17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:22:08 +0000

    Add package scipp/22.11.0/scipp-22.11.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/scipp/scipp-22.11.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/scipp/include/scipp/dataset/generated_bins.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#include "scipp/dataset/bin_sizes.h"
+#include "scipp/dataset/bins_mean.h"
+#include "scipp/dataset/bins_nanmean.h"
+#include "scipp/dataset/bins_sum.h"
+#include "scipp/dataset/bins_nansum.h"
+#include "scipp/dataset/bins_max.h"
+#include "scipp/dataset/bins_nanmax.h"
+#include "scipp/dataset/bins_min.h"
+#include "scipp/dataset/bins_nanmin.h"
+#include "scipp/dataset/bins_all.h"
+#include "scipp/dataset/bins_any.h"
