ef94263c5ad694618d85cad52a065f99315449d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:10 +0000

    Add package JJTrainer/1.0.3/JJTrainer-1.0.3.tar.gz

---
+++ packages/JJTrainer/JJTrainer-1.0.3.tar.gz/JJTrainer-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import  setup
+import setuptools
+packages = ['JJTrainer']# 唯一的包名，自己取名
+setup(name='JJTrainer',
+    version='1.0.3',
+    author='RenjieYu',
+    author_email='770819952@qq.com',
+    packages=packages,
+    install_requires=[
+        'numpy',
+        'torch',
+        'tqdm',
+        'sklearn',
+    ],
+    package_dir={'requests': 'requests'})
