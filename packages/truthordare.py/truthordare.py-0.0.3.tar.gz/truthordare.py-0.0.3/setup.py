496f22936e83d963a779e492e074c851a95caa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:11 +0000

    Add package truthordare.py/0.0.3/truthordare.py-0.0.3.tar.gz

---
+++ packages/truthordare.py/truthordare.py-0.0.3.tar.gz/truthordare.py-0.0.3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+readme = ""
+with open("./doc/README.rst") as f:
+    readme = f.read()
+
+VERSION = "0.0.3"
+DESCRIPTION = "A Python Package that enables you to fetch data from Truth or Dare API"
+LONG_DESCRIPTION = "Please visit https://github.com/iamd4rk/truthordare.py#readme for docs"
+
+setup(
+    name="truthordare.py",
+    version=VERSION,
+    author="d4rkpoison",
+    author_email="ashutoshdas2004@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[requirements],
+    keywords=["python", "truth", "dare", "truthordare"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+)
