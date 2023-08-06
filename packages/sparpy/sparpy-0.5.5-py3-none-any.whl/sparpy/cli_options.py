cb27cdef6dd4e9759b741e1db01cedfe93b72c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:47 +0000

    Add package sparpy/0.5.5/sparpy-0.5.5-py3-none-any.whl

---
+++ packages/sparpy/sparpy-0.5.5-py3-none-any.whl/sparpy/cli_options.py
@@ -0,0 +1,295 @@
+from configparser import ConfigParser
+from functools import update_wrapper
+from pathlib import Path
+
+import click
+
+from . import __version__
+from .config import load_user_config
+
+
+class EnvValue(click.types.StringParamType):
+    name = 'env_var=value'
+
+    def convert(self, value, param, ctx):
+        if isinstance(value, tuple):
+            return value
+
+        value = super(EnvValue, self).convert(value, param, ctx)
+        value = tuple(value.split('=', 1))
+        if len(value) == 1:
+            value = (value[0], '')
+        return value
+
+    def __repr__(self):
+        return "ENV_VAR=VALUE"
+
+
+def apply_decorators(func, *args):
+    fn = func
+    for opt in args:
+        fn = opt(fn)
+
+    return update_wrapper(fn, func)
+
+
+def plugins_options(func=None):
+    def inner(fn):
+        return apply_decorators(
+            fn,
+            click.option(
+                '--plugin', '-p',
+                type=str,
+                multiple=True,
+                envvar='SPARPY_PLUGINS',
+                help='Download plugin'
+            ),
+            click.option(
+                '--requirements-file', '-r',
+                type=click.Path(),
+                multiple=True,
+                envvar='SPARPY_REQUIREMENT_FILES',
+                help='Plugins requirements file'
+            ),
+            click.option(
+                '--constraint', '-c',
+                type=click.Path(),
+                multiple=True,
+                envvar='SPARPY_CONSTRAINTS',
+                help='Package constraints'
+            ),
+            click.option(
+                '--exclude-python-package', '-x',
+                type=click.Path(),
+                multiple=True,
+                envvar='SPARPY_EXCLUDE_PYTHON_PACKAGES',
+                help='Exclude Python packages'
+            ),
+            click.option(
+                '--no-index',
+                is_flag=True,
+                type=bool,
+                default=None,
+                envvar='SPARPY_NO_INDEX',
+                help='Ignore package index (only looking at --find-links URLs instead).'
+            ),
+            click.option(
+                '--extra-index-url', '-e',
+                type=str,
+                multiple=True,
+                envvar='SPARPY_EXTRA_INDEX_URLS',
+                help='Extra repository url'
+            ),
+            click.option(
+                '--find-links', '-f',
+                type=str,
+                multiple=True,
+                envvar='SPARPY_FIND_LINKS',
+                help='If a URL or path to an html file, then parse for links to archives such as sdist (.tar.gz)'
+                     ' or wheel (.whl) files. If a local path or file:// URL that\'s a directory,  then look for'
+                     ' archives in the directory listing. Links to VCS project URLs are not supported.'
+            ),
+            click.option(
+                '--no-self',
+                is_flag=True,
+                type=bool,
+                default=None,
+                envvar='SPARPY_NO_SELF',
+                help='No include Sparpy itself as requirement'
+            ),
+            click.option(
+                '--force-download',
+                is_flag=True,
+                type=bool,
+                default=None,
+                envvar='SPARPY_FORCE_DOWNLOAD',
+                help='Avoid cache and download all packages'
+            ),
+            click.option(
+                '--pre',
+                is_flag=True,
+                type=bool,
+                default=None,
+                envvar='SPARPY_PRE_RELEASES',
+                help='Include pre-release and development versions. By default, sparpy only finds stable versions.'
+            ),
+            click.option(
+                '--proxy',
+                is_flag=True,
+                type=str,
+                default=None,
+                envvar='SPARPY_PROXY',
+                help='Proxy to pass through to Python repositories.'
+            ),
+            click.option(
+                '--plugin-env',
+                type=EnvValue(),
+                multiple=True,
+                envvar='SPARPY_PLUGIN_ENVVARS',
+                help='Environment variables values for plugin download process'
+            ),
+        )
+
+    if func:
+        return inner(func)
+    return inner
+
+
+def common_spark_options(func=None):
+    def inner(fn):
+        return apply_decorators(
+            fn,
+            click.option(
+                '--master',
+                type=str,
+                envvar='SPARPY_MASTER',
+                help='The master URL for the cluster'
+            ),
+            click.option(
+                '--deploy-mode',
+                type=str,
+                envvar='SPARPY_DEPLOY_MODE',
+                help='Whether to deploy your driver on the worker nodes (cluster) '
+                     'or locally as an external client (client)'
+            ),
+            click.option(
+                '--queue',
+                type=str,
+                envvar='SPARPY_QUEUE',
+                help='The name of the YARN queue to which the application is submitted.'
+            ),
+            click.option(
+                '--conf',
+                type=str,
+                multiple=True,
+                envvar='SPARPY_CONF',
+                help='Arbitrary Spark configuration property in key=value format. '
+                     'For values that contain spaces wrap “key=value” in quotes.'
+            ),
+            click.option(
+                '--packages',
+                type=str,
+                envvar='SPARPY_PACKAGES',
+                help='Comma-delimited list of Maven coordinates'
+            ),
+            click.option(
+                '--exclude-packages',
+                type=str,
+                envvar='SPARPY_EXCLUDE_PACKAGES',
+                help='Comma-delimited list of Maven coordinates'
+            ),
+            click.option(
+                '--repositories',
+                type=str,
+                envvar='SPARPY_REPOSITORIES',
+                help='Comma-delimited list of Maven repositories'
+            ),
+            click.option(
+                '--env',
+                type=EnvValue(),
+                multiple=True,
+                envvar='SPARPY_ENVVARS',
+                help='Environment variables values'
+            ),
+            click.option(
+                '--properties-file',
+                type=str,
+                envvar='SPARPY_PROPERTIES_FILE',
+                help="Path to a file from which to load extra properties. If not"
+                     "specified, this will look for conf/spark-defaults.conf."
+            ),
+            click.option(
+                '--klass', '--class',
+                type=str,
+                envvar='SPARPY_CLASS',
+                help='Your application\'s main class (for Java / Scala apps).'
+            ),
+        )
+
+    if func:
+        return inner(func)
+    return inner
+
+
+def spark_submit_options(func=None):
+    def inner(fn):
+        return apply_decorators(
+            fn,
+            click.option(
+                '--spark-submit-executable',
+                type=str,
+                help='Spark submit executable'
+            ),
+            click.argument(
+                'job_args',
+                nargs=-1,
+                type=click.UNPROCESSED
+            )
+        )
+
+    if func:
+        return inner(func)
+    return inner
+
+
+def spark_interactive_options(func=None):
+    def inner(fn):
+        return apply_decorators(
+            fn,
+            click.option(
+                '--pyspark-executable',
+                type=str,
+                help='PySpark executable'
+            ),
+            click.option(
+                '--python-interactive-driver',
+                type=str,
+                help='Python interactive driver'
+            )
+        )
+
+    if func:
+        return inner(func)
+    return inner
+
+
+class Config(click.ParamType):
+    name = 'configfile'
+
+    def __call__(self, value, param=None, ctx=None):
+        return self.convert(value, param, ctx)
+
+    def convert(self, value, param, ctx):
+        if isinstance(value, ConfigParser):
+            return value
+
+        if value:
+            value = Path(value)
+        return load_user_config(value)
+
+
+def general_options(func=None):
+    def inner(fn):
+        return apply_decorators(
+            fn,
+            click.option(
+                '--config',
+                type=Config(),
+                default=load_user_config(),
+                envvar='SPARPY_CONFIG',
+                help='Path to configuration file'
+            ),
+            click.option(
+                '--debug', '-d',
+                type=bool,
+                default=False,
+                is_flag=True,
+                envvar='SPARPY_DEBUG',
+                help='Debug mode'
+            ),
+            click.version_option(version=__version__)
+        )
+
+    if func:
+        return inner(func)
+    return inner
---
+++ packages/sparpy/sparpy-0.5.5-py3-none-any.whl/sparpy/plugins.py
@@ -0,0 +1,255 @@
+import os
+import re
+import sys
+from itertools import chain
+from logging import Logger, getLogger
+from pathlib import Path
+from pkgutil import iter_modules
+from tempfile import mkdtemp
+from typing import Dict, Iterable
+from urllib.parse import urlparse
+from zipimport import zipimporter
+
+import click
+from pkg_resources import find_distributions, iter_entry_points, working_set
+
+from .config import ConfigParser
+from .processor import ProcessManager
+
+PLUGIN_REGEX = re.compile(r'([^[,]+(?:\[[^]]+])?(?:(?:[><~=]?=|[><~])[^,]+)?)')
+
+
+class DynamicGroup(click.Group):
+    PLUGINS_ENTRY_POINT = 'sparpy.cli_plugins'
+
+    def __init__(self, *args, **kwargs):
+        super(DynamicGroup, self).__init__(*args, **kwargs)
+
+        ensure_plugin_distribution()
+
+    def iter_plugins(self):
+        yield from iter_entry_points(self.PLUGINS_ENTRY_POINT)
+
+    def get_plugin(self, name):
+        for plugin in self.iter_plugins():
+            if plugin.name == name:
+                return plugin
+
+        raise RuntimeError(f'Plugin {name} does not exist')
+
+    def list_commands(self, ctx):
+        rv = sorted([plugin.name for plugin in self.iter_plugins()])
+        return rv
+
+    def get_command(self, ctx, name):
+        try:
+            plugin = self.get_plugin(name=name)
+        except IndexError:
+            return None
+
+        command = plugin.resolve()
+
+        if isinstance(command, click.BaseCommand):
+            return command
+
+        @click.command(name=name, context_settings={'ignore_unknown_options': True})
+        @click.argument(
+            'job_args',
+            nargs=-1,
+            type=click.UNPROCESSED
+        )
+        def wrapper(job_args):
+            ctx.exit(command(job_args))
+
+        return wrapper
+
+
+def ensure_plugin_distribution():
+    for module_info in iter_modules():
+        if not isinstance(module_info.module_finder, zipimporter):
+            continue
+
+        for dist in find_distributions(module_info.module_finder.archive):
+            working_set.add(dist)
+
+
+def _convert_to_zip(f: Path):
+    new_path = f.with_suffix('.zip')
+    f.rename(new_path)
+    return new_path
+
+
+class DownloadPlugins:
+
+    def __init__(self,
+                 config=None,
+                 plugins: Iterable[str] = None,
+                 requirements_files: Iterable[str] = None,
+                 constraints: Iterable[str] = None,
+                 exclude_packages: Iterable[str] = None,
+                 extra_index_urls: Iterable[str] = None,
+                 find_links: Iterable[str] = None,
+                 no_index: bool = None,
+                 no_self: bool = None,
+                 force_download: bool = None,
+                 pre: bool = None,
+                 proxy: str = None,
+                 logger: Logger = None,
+                 download_dir: str = None,
+                 convert_to_zip: bool = True,
+                 env: Dict[str, str] = None):
+        try:
+            plugin_config = config['plugins']
+        except (KeyError, TypeError):
+            config = ConfigParser(default_sections=('plugins', 'plugin-env'))
+            plugin_config = config['plugins']
+
+        try:
+            env_config = config['plugin-env']
+        except KeyError:
+            config.add_section('plugin-env')
+            env_config = config['plugin-env']
+
+        self.logger = logger or getLogger(__name__)
+
+        self.extra_index_urls = plugin_config.getlist('extra-index-urls', fallback=[])
+        self.find_links = plugin_config.getlist('find-links', fallback=[])
+        self.no_index = plugin_config.getboolean('no-index', fallback=False)
+        self.cache_dir = plugin_config.getpath('cache-dir')
+
+        self.plugins = plugin_config.getlist('plugins', fallback=[])
+        self.requirements_files = plugin_config.getpathlist('requirements-files', fallback=[])
+        self.constraints = plugin_config.getpathlist('constraints', fallback=[])
+        self.exclude_packages = plugin_config.getlist('exclude-packages', fallback=[])
+
+        self.download_dir_prefix = plugin_config.get('download-dir-prefix', fallback='sparpy_')
+
+        self.no_self = plugin_config.getboolean('no-self', fallback=False)
+        self.force_download = plugin_config.getboolean('force-download', fallback=False)
+        self.pre = plugin_config.getboolean('pre', fallback=False)
+        self.proxy = plugin_config.get('proxy')
+
+        self.env = dict(env_config)
+
+        if plugins:
+            self.plugins.extend(plugins)
+
+        if requirements_files:
+            self.requirements_files.extend([Path(p) for p in requirements_files])
+
+        if constraints:
+            self.constraints.extend([Path(p) for p in constraints])
+
+        if exclude_packages:
+            self.exclude_packages.extend(exclude_packages)
+
+        if extra_index_urls:
+            self.extra_index_urls.extend(extra_index_urls)
+
+        if find_links:
+            self.find_links.extend(find_links)
+
+        if no_index is not None:
+            self.no_index = no_index
+
+        if no_self is not None:
+            self.no_self = no_self
+
+        if force_download is not None:
+            self.force_download = force_download
+
+        if pre is not None:
+            self.pre = pre
+
+        if proxy is not None:
+            self.proxy = proxy
+
+        if env is not None:
+            self.env.update(env)
+
+        if download_dir:
+            self.reqs_path = download_dir
+        else:
+            self.reqs_path = mkdtemp(prefix=self.download_dir_prefix)
+
+        self.convert_to_zip = convert_to_zip
+
+    def build_command(self):
+        pip_exec_params = [sys.executable, '-m', 'pip', 'download']
+        pip_exec_params.extend(['-d', self.reqs_path])
+
+        if self.force_download:
+            pip_exec_params.append('--no-cache-dir')
+        elif self.cache_dir:
+            pip_exec_params.extend(['--cache-dir', str(self.cache_dir)])
+
+        if self.pre:
+            pip_exec_params.append('--pre')
+
+        if self.proxy:
+            pip_exec_params.extend(['--proxy', self.proxy])
+
+        pip_exec_params.extend(chain.from_iterable([['--extra-index-url',
+                                                     u,
+                                                     '--trusted-host',
+                                                     urlparse(u).hostname]
+                                                    for u in self.extra_index_urls]))
+
+        pip_exec_params.extend(chain.from_iterable([['--find-links', str(r)] for r in self.find_links]))
+        if self.no_index:
+            pip_exec_params.append('--no-index')
+
+        if not self.no_self:
+            from . import __version__
+            pip_exec_params.append(f'sparpy=={__version__}')
+        if len(self.plugins):
+            pip_exec_params.extend(chain.from_iterable([PLUGIN_REGEX.findall(p)
+                                                        if ',' in p else [p, ] for p in self.plugins]))
+        if len(self.requirements_files):
+            pip_exec_params.extend(chain.from_iterable([['-r', str(r)] for r in self.requirements_files]))
+        if len(self.constraints):
+            pip_exec_params.extend(chain.from_iterable([['-c', str(c)] for c in self.constraints]))
+
+        pip_exec_params.extend(['--exists-action', 'i'])
+
+        return pip_exec_params
+
+    def download(self, debug=False):
+        if self.no_self and not len(self.plugins) and not len(self.requirements_files):
+            return None
+
+        pip_exec_params = self.build_command()
+
+        self.logger.info('Downloading python plugins...')
+        self.logger.debug(' '.join(pip_exec_params))
+
+        env = os.environ.copy()
+        if self.env:
+            env.update(self.env)
+
+        process = ProcessManager(pip_exec_params, pass_through=debug, env=env)
+        process.start_process()
+        process.wait()
+
+        if process.returncode != 0:
+            raise RuntimeError('Download packages failed')
+
+        [p.unlink()
+         for p in Path(self.reqs_path).glob('*.whl')
+         if p.is_file() and self.is_exclude(p)]
+
+        if self.convert_to_zip:
+            [_convert_to_zip(p.resolve())
+             for p in Path(self.reqs_path).glob('*.whl')
+             if p.is_file()]
+
+        return self.reqs_path
+
+    def is_exclude(self, package_file: Path) -> bool:
+        from pkginfo import Wheel
+        if len(self.exclude_packages) == 0:
+            return False
+
+        w = Wheel(str(package_file))
+
+        return w.name in self.exclude_packages
