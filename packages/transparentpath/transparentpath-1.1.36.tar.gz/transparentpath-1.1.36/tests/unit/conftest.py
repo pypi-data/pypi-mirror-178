adc216f5d572e91465027a9075e71af40dea60c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:54 +0000

    Add package transparentpath/1.1.36/transparentpath-1.1.36.tar.gz

---
+++ packages/transparentpath/transparentpath-1.1.36.tar.gz/transparentpath-1.1.36/tests/unit/conftest.py
@@ -0,0 +1,50 @@
+import pytest
+import os
+from .functions import before_init, reinit
+from pathlib import Path
+from transparentpath import TransparentPath
+
+
+def process_token(token, token_file):
+    if token is not None:
+        if not token_file.is_file():
+            with open(str(token_file), "w") as ofile:
+                ofile.write(token)
+        if "2" in str(token_file):
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS_2"] = str(token_file)
+        else:
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(token_file)
+
+
+@pytest.fixture
+def clean(pytestconfig):
+    token_file = Path("cred.json")
+    token_file2 = Path("cred2.json")
+    token = pytestconfig.getoption("token")
+    token2 = pytestconfig.getoption("token2")
+    process_token(token, token_file)
+    process_token(token2, token_file2)
+
+    TransparentPath._do_update_cache = True
+    TransparentPath._do_check = True
+    before_init()
+    yield
+    TransparentPath._do_update_cache = False
+    TransparentPath._do_check = False
+    path1 = TransparentPath("chien")
+    path2 = TransparentPath("chien2")
+    suffixes = ["", ".zip", ".txt", ".json", ".csv", ".parquet", ".hdf5", ".xlsx", ".joblib"]
+    for suffix in suffixes:
+        path1.with_suffix(suffix).rm(recursive=True, ignore_kind=True, absent="ignore")
+        path1.with_suffix(suffix).rm(recursive=True, ignore_kind=True, absent="ignore")
+        path2.with_suffix(suffix).rm(recursive=True, ignore_kind=True, absent="ignore")
+        path2.with_suffix(suffix).rm(recursive=True, ignore_kind=True, absent="ignore")
+    TransparentPath("chat chat").rm(recursive=True, ignore_kind=True, absent="ignore")
+    TransparentPath._do_update_cache = True
+    TransparentPath._do_check = True
+    reinit()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--token", action="store", default=None)
+    parser.addoption("--token2", action="store", default=None)
---
+++ packages/transparentpath/transparentpath-1.1.36.tar.gz/transparentpath-1.1.36/tests/unit/vanilla/test_fs_initialisation.py
@@ -0,0 +1,40 @@
+import gcsfs
+import pytest
+from fsspec.implementations.local import LocalFileSystem
+from transparentpath import TransparentPath
+from ..functions import init, skip_gcs, reinit
+
+
+# noinspection PyUnusedLocal
+@pytest.mark.parametrize(
+    "fs_kind, bucket, expected_fs_name, expected_fs_kind, expected_fs_type",
+    [
+        ("local", None, "local", "local", LocalFileSystem),
+        ("gcs", "code_tests_sand", "gcs_sandbox-281209", "gcs", gcsfs.GCSFileSystem),
+        ("gcs", None, "gcs_sandbox-281209", "gcs", gcsfs.GCSFileSystem),
+    ],
+)
+def test_init(clean, fs_kind, bucket, expected_fs_name, expected_fs_kind, expected_fs_type):
+    if skip_gcs[fs_kind]:
+        print("skipped")
+        return
+    init(fs_kind, bucket_=bucket)
+    assert not TransparentPath.unset
+    if expected_fs_kind == "local":
+        assert len(TransparentPath.fss) == 1
+    else:
+        assert len(TransparentPath.fss) == 2
+    assert expected_fs_kind == TransparentPath.fs_kind
+    assert expected_fs_name in list(TransparentPath.fss.keys())[-1]
+    assert isinstance(TransparentPath.fss[list(TransparentPath.fss.keys())[-1]], expected_fs_type)
+
+
+def test_init_gcs_fail():  # Do not use clean : we do NOT want to execute the rm for fs will have FAILED to setup
+    if skip_gcs["gcs"]:
+        print("skipped")
+        return
+
+    reinit()
+    with pytest.raises(NotADirectoryError):
+        TransparentPath.set_global_fs("gcs", bucket="coucou")
+    reinit()
