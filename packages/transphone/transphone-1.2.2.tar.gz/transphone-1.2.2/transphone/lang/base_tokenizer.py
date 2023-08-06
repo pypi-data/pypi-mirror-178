22518121fbff8842e04a2235cb109e2f6f13f444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:06 +0000

    Add package transphone/1.2.2/transphone-1.2.2.tar.gz

---
+++ packages/transphone/transphone-1.2.2.tar.gz/transphone-1.2.2/transphone/lang/base_tokenizer.py
@@ -0,0 +1,30 @@
+from phonepiece.inventory import read_inventory
+from transphone.g2p import read_g2p
+from transphone.config import TransphoneConfig
+
+class BaseTokenizer:
+
+    def __init__(self, lang_id, g2p_model='latest'):
+        self.lang_id = lang_id
+        self.inventory = read_inventory(lang_id)
+        self.g2p = read_g2p(g2p_model)
+        self.cache = {}
+        self.punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+        self.logger = TransphoneConfig.logger
+
+    def tokenize(self, text: str):
+        raise NotImplementedError
+
+    def tokenize_words(self, text:str):
+        text = text.translate(str.maketrans('', '', self.punctuation)).lower()
+        result = []
+
+        return text.split()
+
+    def convert_tokens_to_ids(self, lst):
+        lst = list(filter(lambda s: s!='<SPACE>', lst))
+
+        return self.inventory.phoneme.atoi(lst)
+
+    def convert_ids_to_tokens(self, lst):
+        return self.inventory.phoneme.itoa(lst)
