68615a17b6b68d8776c0dda0d20acbda311e6a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:22 +0000

    Add package librair/2022.11.26/librair-2022.11.26.tar.gz

---
+++ packages/librair/librair-2022.11.26.tar.gz/librair-2022.11.26/librair/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2021–2022  Donatus Herre <git@herre.io>
+
+This module offers tools to harvest data from different library catalogs.
+"""
+__author__ = "Donatus Herre <git@herre.io>"
+__version__ = "2022.11.26"
+__license__ = "MIT"
+
+from . import interfaces
+from . import protocols
+from . import schemas
+from . import services
+from . import endpoints
+from . import parsers
