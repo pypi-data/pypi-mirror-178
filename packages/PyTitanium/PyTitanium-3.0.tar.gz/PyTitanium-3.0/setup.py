95368e38bed432cc9b71aee3ed8abcc388b50355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:06 +0000

    Add package PyTitanium/3.0/PyTitanium-3.0.tar.gz

---
+++ packages/PyTitanium/PyTitanium-3.0.tar.gz/PyTitanium-3.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="PyTitanium",
+    version=3.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"]))
