247b95a5504f84fc6e110cb0b775183d028d908d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:43 +0000

    Add package bridgeobjects/0.1.16/bridgeobjects-0.1.16.tar.gz

---
+++ packages/bridgeobjects/bridgeobjects-0.1.16.tar.gz/bridgeobjects-0.1.16/bridgeobjects/_version.py
@@ -0,0 +1,8 @@
+__version__ = '0.1.16'
+
+"""
+0.1.16 24 Nov 2022
+        change output format of create_pbn_board
+        fix trick winner in _parse pbn_string
+        allow "" in contract name
+"""
