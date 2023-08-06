6aef4e8e1d10bd4a0274985844d57903b722c050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:19 +0000

    Add package PowerSchoolPy/0.0.2/powerschoolpy-0.0.2.tar.gz

---
+++ packages/PowerSchoolPy/powerschoolpy-0.0.2.tar.gz/powerschoolpy-0.0.2/setup.py
@@ -0,0 +1,52 @@
+from os import read, system
+import os
+import pathlib
+import re
+import sys
+from setuptools import setup, find_packages
+
+def get_version():
+    """Get current version from code."""
+    regex = r"__version__\s=\s\"(?P<version>[\d\.]+?)\""
+    path = ("PowerSchoolPy", "__version__.py")
+    return re.search(regex, read(*path)).group("version")
+
+def read(*parts):
+    """Read file."""
+    filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts)
+    sys.stdout.write(filename)
+    with open(filename, encoding="utf-8", mode="rt") as fp:
+        return fp.read()
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    author="Brian Lich",
+    author_email="blich29@hotmail.com",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    description="An async library for Powerschool",
+    include_package_data=True,
+    install_requires=["aiohttp>=3.0.0"],
+    keywords=["powerschool", "grades", "async", "client"],
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    name="powerschoolpy",
+    packages=find_packages(include=["powerschoolpy"]),
+    url="https://github.com/brianlich/powerschoolpy",
+    version=get_version(),
+    zip_safe=False,
+)
