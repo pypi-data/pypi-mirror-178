97d072f9f9d30cd26430a3f5735607045c7fb536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:55 +0000

    Add package plynx/1.8.2/plynx-1.8.2-py3.9.egg

---
+++ packages/plynx/plynx-1.8.2-py3.9.egg/plynx/constants/resource_enums.py
@@ -0,0 +1,11 @@
+"""Resource enums"""
+
+
+class NodeResources:
+    """Internal node elements"""
+    INPUT: str = 'inputs'
+    OUTPUT: str = 'outputs'
+    CLOUD_INPUT: str = 'cloud_inputs'
+    CLOUD_OUTPUT: str = 'cloud_outputs'
+    PARAM: str = 'params'
+    LOG: str = 'logs'
