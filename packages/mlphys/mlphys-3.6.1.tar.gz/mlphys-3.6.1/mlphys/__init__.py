7fcb7b5e63fd485fb5e6c64dc32fd4e53a7c696f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:53 +0000

    Add package mlphys/3.6.1/mlphys-3.6.1.tar.gz

---
+++ packages/mlphys/mlphys-3.6.1.tar.gz/mlphys-3.6.1/mlphys/__init__.py
@@ -0,0 +1,13 @@
+"""
+Please keep these lines. Thank you!
+"""
+__author__ = 'Sanjaya Lohani'
+__email__ = 'slohani@mlphys_nightly.com'
+__licence__ = 'Apache 2.0'
+__website__ = "sanjayalohani.com"
+
+
+import mlphys.deepqis.simulator.distributions as distributions
+import mlphys.deepqis.simulator.measurements as measurements
+from mlphys.deepqis.utils import Fidelity_Measure, Purity_Measure, Concurrence_Measure, Positive_Partial_Trace
+import mlphys_nightly.fso.lgoam.source.OAM_Intensity_Phase as OAM_Intensity_Phase
---
+++ packages/mlphys/mlphys-3.6.1.tar.gz/mlphys-3.6.1/mlphys/fso/lgoam/__init__.py
@@ -0,0 +1,4 @@
+__author__ = 'Sanjaya Lohani'
+__email__ = 'slohani@mlphys_nightly.com'
+__licence__ = 'Apache 2.0'
+__website__ = "sanjayalohani.com"
