f3e68213667f0760a8fe2c88db0a224409659f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:21 +0000

    Add package random-caps/0.0.1/random_caps-0.0.1-py2.py3-none-any.whl

---
+++ packages/random-caps/random_caps-0.0.1-py2.py3-none-any.whl/random_caps/__init__.py
@@ -0,0 +1,11 @@
+"""
+    A module for randomly applying uppercase to strings.
+
+    To use it simply use the function random_caps(string, chance),
+    string being the string to randomly apply uppercase and chance
+    being the chance of applying uppercase to a letter.
+
+    Example:
+        random_caps("random", 100)
+        Each letter would have a 1 in 100 chance of turning uppercase
+"""
---
+++ packages/random-caps/random_caps-0.0.1-py2.py3-none-any.whl/random_caps/random_caps.py
@@ -0,0 +1,55 @@
+import random
+
+
+def random_caps(string: str | list, chance: int) -> str | list:
+    """
+    Randomly applies uppercase to letters in a string.
+
+        Parameters:
+            string (str | list): the string to apply to.
+            chance (int): the chance of applying uppercase to a letter.
+
+        Returns:
+            The string or a list of strings with random uppercase and lowercase letters.
+
+        Example:
+            random_caps("random", 100) > each letter has a 1 in a 100 chance of turning uppercase
+
+        Raises:
+            TypeError: either a element in a list is not a string or
+            the argument "string" is not a string.
+    """
+
+    if type(string) == int:
+        raise TypeError
+
+    if type(string) == str:
+        result = []
+        for l in string:
+            rand = random.randint(0, chance)
+            if rand == 1:
+                result.append(l.capitalize())
+            else:
+                result.append(l)
+
+        return "".join(result)
+
+    elif type(string) == list:
+        strings = []
+        for s in string:
+            result = []
+            if type(s) == int:
+                raise TypeError
+            for l in s:
+
+                rand = random.randint(0, chance)
+                if rand == 1:
+                    result.append(l.capitalize())
+                else:
+                    result.append(l)
+            strings.append(result)
+
+        return strings
+
+    else:
+        raise TypeError
