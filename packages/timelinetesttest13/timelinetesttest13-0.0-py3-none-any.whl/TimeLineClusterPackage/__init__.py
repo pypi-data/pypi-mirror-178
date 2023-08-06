f90c506f6996cf5ab8ca8110bf94ca83a1db6788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:17 +0000

    Add package timelinetesttest13/0.0/timelinetesttest13-0.0-py3-none-any.whl

---
+++ packages/timelinetesttest13/timelinetesttest13-0.0-py3-none-any.whl/TimeLineClusterPackage/__init__.py
@@ -0,0 +1,4 @@
+import config
+import GenericController
+import LogController
+import UtailController
---
+++ packages/timelinetesttest13/timelinetesttest13-0.0-py3-none-any.whl/TimeLineClusterPackage/config.py
@@ -0,0 +1,14 @@
+import os
+
+class ConfigGenericApp:
+
+    root_path = os.path.dirname(os.path.abspath(__file__))
+    path_generic_config = root_path + "/generic/generic_config.json"
+
+    path_app = root_path + "/generic/Larva/config.json"
+
+    # path_generic_folder = root_path + "/generic"
+    # path_generic_app = path_generic_folder + "/app"
+    # path_generic_config = root_path + "/generic/generic_config.json"
+
+    
