6fcdad9ffd01eef843450c3ea980a5ffe5eac4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:23:04 +0000

    Add package pytometry/0.1.3/pytometry-0.1.3.tar.gz

---
+++ packages/pytometry/pytometry-0.1.3.tar.gz/pytometry-0.1.3/pytometry/__init__.py
@@ -0,0 +1,28 @@
+"""Flow & mass cytometry analytics.
+
+Import the package::
+
+   import pytometry
+
+This is the complete API reference:
+
+.. autosummary::
+   :recursive:
+   :toctree: .
+
+   read_write.read_fcs
+   preprocessing.split_signal
+   preprocessing.compensate
+   preprocessing.find_indexes
+   tools.normalize_arcsinh
+   tools.normalize_logicle
+   tools.normalize_biExp
+
+"""
+
+__version__ = "0.1.3"  # denote a pre-release for 0.1.0 with 0.1a1
+
+from . import plotting as pl
+from . import preprocessing as pp
+from . import read_write as io
+from . import tools as tl
