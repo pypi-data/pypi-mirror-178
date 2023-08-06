be215ee058bbb989f2ddadd1639637dcc6ee8e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:18 +0000

    Add package yonlu/1.1.22/yonlu-1.1.22-py3-none-any.whl

---
+++ packages/yonlu/yonlu-1.1.22-py3-none-any.whl/yonlu/examples/word2vec_tester.py
@@ -0,0 +1,13 @@
+
+from yonlu.word_embeddings.word_embeddings import Word2Vec
+
+word2vec = Word2Vec()
+binary=True
+#model_file = 'D:\\python_workspace\\yonlu\\yonlu\\models\\word2vec\\korean_wiki_w2v.bin'
+model_file = "word2vec_1.bin"
+word2vec.load_model(model_file, binary)
+
+print(word2vec.most_similar(positives=['이재명', '경제'], negatives=['정치인'], topn=10))
+print('-----------------------------------')
+
+print(word2vec.similar_by_word('이재명'))
