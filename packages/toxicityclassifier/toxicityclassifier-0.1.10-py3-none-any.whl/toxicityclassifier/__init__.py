2e5b0da4b8a35fbf1959af19361459d636f8c5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:15 +0000

    Add package toxicityclassifier/0.1.10/toxicityclassifier-0.1.10-py3-none-any.whl

---
+++ packages/toxicityclassifier/toxicityclassifier-0.1.10-py3-none-any.whl/toxicityclassifier/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+"""
+Python Encoder
+~~~~~~~~~~~~~~~~~~~
+A basic wrapper for the Discord API.
+:copyright: (c) 2022-present D1ffic00lt
+:license: MIT, see LICENSE for more details.
+"""
+
+try:
+    from sklearn.metrics import accuracy_score
+except ImportError:
+    raise ImportError("sklearn is not installed. Try \"pip install scikit-learn\"")
+
+try:
+    import nltk
+except ImportError:
+    raise ImportError("nltk is not installed. Try \"pip install nltk\"")
+
+from toxicityclassifier.toxicityclassifier import *
+
+__version__ = "0.1.10"
+__title__ = 'ToxicityClassificator'
+__author__ = "D1ffic00lt"
+__copyright__ = "Copyright 2013-2022 {}".format(__author__)
+__all__ = (
+    "ToxicityClassificator",
+)
