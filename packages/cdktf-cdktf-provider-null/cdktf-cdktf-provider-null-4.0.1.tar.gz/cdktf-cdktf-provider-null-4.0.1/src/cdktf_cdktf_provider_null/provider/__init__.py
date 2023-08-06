f2f4db9a08b2b2fb4ae4f4ffc636805054df6224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:11 +0000

    Add package cdktf-cdktf-provider-null/4.0.1/cdktf-cdktf-provider-null-4.0.1.tar.gz

---
+++ packages/cdktf-cdktf-provider-null/cdktf-cdktf-provider-null-4.0.1.tar.gz/cdktf-cdktf-provider-null-4.0.1/src/cdktf_cdktf_provider_null/provider/__init__.py
@@ -0,0 +1,137 @@
+'''
+# `provider`
+
+Refer to the Terraform Registory for docs: [`null`](https://www.terraform.io/docs/providers/null).
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+from .._jsii import *
+
+import cdktf
+import constructs
+
+
+class NullProvider(
+    cdktf.TerraformProvider,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@cdktf/provider-null.provider.NullProvider",
+):
+    '''Represents a {@link https://www.terraform.io/docs/providers/null null}.'''
+
+    def __init__(
+        self,
+        scope: constructs.Construct,
+        id: builtins.str,
+        *,
+        alias: typing.Optional[builtins.str] = None,
+    ) -> None:
+        '''Create a new {@link https://www.terraform.io/docs/providers/null null} Resource.
+
+        :param scope: The scope in which to define this construct.
+        :param id: The scoped construct ID. Must be unique amongst siblings in the same scope
+        :param alias: Alias name. Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/null#alias NullProvider#alias}
+        '''
+        if __debug__:
+            def stub(
+                scope: constructs.Construct,
+                id: builtins.str,
+                *,
+                alias: typing.Optional[builtins.str] = None,
+            ) -> None:
+                ...
+            type_hints = typing.get_type_hints(stub)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument id", value=id, expected_type=type_hints["id"])
+        config = NullProviderConfig(alias=alias)
+
+        jsii.create(self.__class__, self, [scope, id, config])
+
+    @jsii.member(jsii_name="resetAlias")
+    def reset_alias(self) -> None:
+        return typing.cast(None, jsii.invoke(self, "resetAlias", []))
+
+    @jsii.member(jsii_name="synthesizeAttributes")
+    def _synthesize_attributes(self) -> typing.Mapping[builtins.str, typing.Any]:
+        return typing.cast(typing.Mapping[builtins.str, typing.Any], jsii.invoke(self, "synthesizeAttributes", []))
+
+    @jsii.python.classproperty
+    @jsii.member(jsii_name="tfResourceType")
+    def TF_RESOURCE_TYPE(cls) -> builtins.str:
+        return typing.cast(builtins.str, jsii.sget(cls, "tfResourceType"))
+
+    @builtins.property
+    @jsii.member(jsii_name="aliasInput")
+    def alias_input(self) -> typing.Optional[builtins.str]:
+        return typing.cast(typing.Optional[builtins.str], jsii.get(self, "aliasInput"))
+
+    @builtins.property
+    @jsii.member(jsii_name="alias")
+    def alias(self) -> typing.Optional[builtins.str]:
+        return typing.cast(typing.Optional[builtins.str], jsii.get(self, "alias"))
+
+    @alias.setter
+    def alias(self, value: typing.Optional[builtins.str]) -> None:
+        if __debug__:
+            def stub(value: typing.Optional[builtins.str]) -> None:
+                ...
+            type_hints = typing.get_type_hints(stub)
+            check_type(argname="argument value", value=value, expected_type=type_hints["value"])
+        jsii.set(self, "alias", value)
+
+
+@jsii.data_type(
+    jsii_type="@cdktf/provider-null.provider.NullProviderConfig",
+    jsii_struct_bases=[],
+    name_mapping={"alias": "alias"},
+)
+class NullProviderConfig:
+    def __init__(self, *, alias: typing.Optional[builtins.str] = None) -> None:
+        '''
+        :param alias: Alias name. Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/null#alias NullProvider#alias}
+        '''
+        if __debug__:
+            def stub(*, alias: typing.Optional[builtins.str] = None) -> None:
+                ...
+            type_hints = typing.get_type_hints(stub)
+            check_type(argname="argument alias", value=alias, expected_type=type_hints["alias"])
+        self._values: typing.Dict[str, typing.Any] = {}
+        if alias is not None:
+            self._values["alias"] = alias
+
+    @builtins.property
+    def alias(self) -> typing.Optional[builtins.str]:
+        '''Alias name.
+
+        Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/null#alias NullProvider#alias}
+        '''
+        result = self._values.get("alias")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "NullProviderConfig(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "NullProvider",
+    "NullProviderConfig",
+]
+
+publication.publish()
