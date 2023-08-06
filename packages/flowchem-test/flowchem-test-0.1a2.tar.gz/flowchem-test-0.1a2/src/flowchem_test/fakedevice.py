6f367d110f3dcd4ad841ee81c840d79683ab1b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:47 +0000

    Add package flowchem-test/0.1a2/flowchem-test-0.1a2.tar.gz

---
+++ packages/flowchem-test/flowchem-test-0.1a2.tar.gz/flowchem-test-0.1a2/src/flowchem_test/fakedevice.py
@@ -0,0 +1,30 @@
+"""Fake device for testing purposes. No parameters needed."""
+from collections.abc import Iterable
+
+from flowchem.components.base_component import FlowchemComponent
+from flowchem.devices.flowchem_device import DeviceInfo
+from flowchem.devices.flowchem_device import FlowchemDevice
+from flowchem.people import dario
+
+from .test_component import TestComponent
+
+
+__all__ = ["FakeDevice"]
+
+
+class FakeDevice(FlowchemDevice):
+
+    metadata = DeviceInfo(
+        authors=[dario],
+        maintainers=[dario],
+        manufacturer="virtual-device",
+        model="FakeDevice",
+        serial_number=42,
+        version="v1.0",
+    )
+
+    def components(self) -> Iterable[FlowchemComponent]:
+        """Returns a test Component."""
+        component = TestComponent(name="test-component", hw_device=self)
+        component.add_api_route("/test", lambda: True, methods=["GET"])
+        return (component,)
