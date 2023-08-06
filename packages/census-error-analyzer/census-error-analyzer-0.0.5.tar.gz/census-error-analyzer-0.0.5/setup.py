ee89ded24e05318e4673f458ad8715b676c2f858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:02:16 +0000

    Add package census-error-analyzer/0.0.5/census-error-analyzer-0.0.5.tar.gz

---
+++ packages/census-error-analyzer/census-error-analyzer-0.0.5.tar.gz/census-error-analyzer-0.0.5/setup.py
@@ -0,0 +1,66 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+def version_scheme(version):
+    """
+    Version scheme hack for setuptools_scm.
+
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+
+    If that issue is resolved, this method can be removed.
+    """
+    import time
+
+    from setuptools_scm.version import guess_next_version
+
+    if version.exact:
+        return version.format_with("{tag}")
+    else:
+        _super_value = version.format_next_version(guess_next_version)
+        now = int(time.time())
+        return _super_value + str(now)
+
+
+def local_version(version):
+    """
+    Local version scheme hack for setuptools_scm.
+
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+
+    If that issue is resolved, this method can be removed.
+    """
+    return ""
+
+
+setup(
+    name="census-error-analyzer",
+    description="Analyze the margin of error in U.S. census data",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=("census_error_analyzer",),
+    use_scm_version={"version_scheme": version_scheme, "local_scheme": local_version},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    project_urls={
+        "Maintainer": "https://github.com/datadesk",
+        "Source": "https://github.com/datadesk/census-error-analyzer",
+        "Tracker": "https://github.com/datadesk/census-error-analyzer/issues",
+    },
+)
