337b1c258779598369acf51f017b373bb25a6fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:34 +0000

    Add package python-coiote/1.11.0/python_coiote-1.11.0-py3-none-any.whl

---
+++ packages/python-coiote/python_coiote-1.11.0-py3-none-any.whl/coiote/v3/model/device_monitoring.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional, Dict, List
+
+
+@dataclass
+class MonitoringStatus:
+    enabled: bool
+
+
+@dataclass
+class Datapoint:
+    date: datetime
+    value: str
+
+
+@dataclass
+class MonitoringDataResponse:
+    hasNext: bool
+    lastPointTimestamp: Optional[datetime] = field(default=None)
+    metadata: Dict[str, str] = field(default_factory=dict)
+    data: List[Datapoint] = field(default_factory=list)
