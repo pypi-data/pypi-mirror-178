600b158fedcdc79bea94f955d33ea83d091a26be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:24:07 +0000

    Add package anynet/0.0.19/anynet-0.0.19.tar.gz

---
+++ packages/anynet/anynet-0.0.19.tar.gz/anynet-0.0.19/setup.py
@@ -0,0 +1,26 @@
+
+import setuptools
+
+long_description = \
+	"This is a simple asynchronous networking library that " \
+	"I'm using for hobby projects. It's based on anyio, so " \
+	"it can be used together with either asyncio or trio."
+
+setuptools.setup(
+	name = "anynet",
+	version = "0.0.19",
+	description = "Networking library based on anyio",
+	long_description = long_description,
+	author = "Yannik Marchand",
+	author_email = "ymarchand@me.com",
+	url = "https://github.com/kinnay/anynet",
+	packages = ["anynet"],
+	license = "MIT",
+	
+	install_requires=[
+		"anyio ~= 3.1",
+		"pyopenssl",
+		"netifaces",
+		"multidict"
+	]
+)
