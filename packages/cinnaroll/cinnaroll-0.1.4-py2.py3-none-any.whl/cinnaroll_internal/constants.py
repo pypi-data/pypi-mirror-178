83caa73c22360637cc2cf1c9b39c39120eefaed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:14 +0000

    Add package cinnaroll/0.1.4/cinnaroll-0.1.4-py2.py3-none-any.whl

---
+++ packages/cinnaroll/cinnaroll-0.1.4-py2.py3-none-any.whl/cinnaroll_internal/constants.py
@@ -0,0 +1,12 @@
+KERAS = "keras"
+KERAS_PACKAGE_NAME = "tensorflow"
+TENSORFLOW = "tensorflow"
+TENSORFLOW_PACKAGE_NAME = "tensorflow"
+PYTORCH = "pytorch"
+PYTORCH_PACKAGE_NAME = "torch"
+UNKNOWN_FRAMEWORK = "unknown"
+
+VALID_FRAMEWORKS = (KERAS, TENSORFLOW, PYTORCH)
+
+BACKEND_BASE_URL = "https://api.prod.cluster.cinnaroll.ai"
+API_KEY_HEADER_KEY = "X-API-KEY"
