d959f8d06a87e599a90b4efec355331df31e7e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:22 +0000

    Add package RelevanceAI-dev/3.2.21.2022.11.24.23.0.15.372063/RelevanceAI_dev-3.2.21.2022.11.24.23.0.15.372063-py3-none-any.whl

---
+++ packages/RelevanceAI-dev/RelevanceAI_dev-3.2.21.2022.11.24.23.0.15.372063-py3-none-any.whl/relevanceai/constants/links.py
@@ -0,0 +1,9 @@
+SEARCH_APP_LINK = "https://cloud.relevance.ai/dataset/{}/deploy/recent/explore"
+PROJECTOR_APP_LINK = "https://cloud.relevance.ai/dataset/{}/deploy/recent/projector"
+CLUSTER_APP_LINK = "https://cloud.relevance.ai/dataset/{}/deploy/recent/explore/"
+EXPLORER_APP_LINK = "https://cloud.relevance.ai/dataset/{}/deploy/recent/explore"
+
+SIGNUP_URL = "https://cloud.relevance.ai/sdk/api"
+AUSTRALIA_URL = "https://gateway-api-aueast.relevance.ai/latest"
+WIDER_URL = "https://api.{}.relevance.ai/latest"
+DEV_URL = "https://api-dev.{}.relevance.ai/latest"
