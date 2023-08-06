dc4653c57073b4ac37ae22a88e3d2e9a6cb5c20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:24 +0000

    Add package szl-gui/0.0.1/szl_gui-0.0.1.tar.gz

---
+++ packages/szl-gui/szl_gui-0.0.1.tar.gz/szl_gui-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="szl_gui",
+    version="0.0.1",
+    author="szl",
+    author_email="1249268237@qq.com",
+    description="",  #介绍
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
