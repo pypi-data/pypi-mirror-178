c0c23ea901772226fc2de458a5c280c9a3e6f8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:07 +0000

    Add package inteltk/1.0.1/inteltk-1.0.1-py3-none-any.whl

---
+++ packages/inteltk/inteltk-1.0.1-py3-none-any.whl/inteltk/__init__.py
@@ -0,0 +1,7 @@
+from inteltk.colors import *
+from inteltk.inteltk import IntelTk
+from inteltk.inteltk import calculate_remaining_time
+from inteltk.inteltk import create_parser
+from inteltk.inteltk import set_exit_program
+from inteltk.inteltk import set_readline
+from inteltk.inteltk import startup
