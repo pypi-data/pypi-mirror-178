fdb4bd2cc5ef032e1c3d687cbc86dca7a6efa6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:00 +0000

    Add package erkr-semver-test3/0.1.0/erkr_semver_test3-0.1.0.tar.gz

---
+++ packages/erkr-semver-test3/erkr_semver_test3-0.1.0.tar.gz/erkr_semver_test3-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['erkr_semver_test3']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Flask>=2.2.2,<3.0.0', 'fastapi>=0.85.1,<0.86.0']
+
+setup_kwargs = {
+    'name': 'erkr-semver-test3',
+    'version': '0.1.0',
+    'description': 'This is the decription',
+    'long_description': 'This is a readme',
+    'author': 'Eran Kricheli',
+    'author_email': 'eran.kricheli@frontegg.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
