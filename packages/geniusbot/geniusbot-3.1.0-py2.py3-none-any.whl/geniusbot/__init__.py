c504cd738a242792bab0c6c853b7a42be4a593c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:51 +0000

    Add package geniusbot/3.1.0/geniusbot-3.1.0-py2.py3-none-any.whl

---
+++ packages/geniusbot/geniusbot-3.1.0-py2.py3-none-any.whl/geniusbot/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+from geniusbot.version import __version__, __author__, __credits__
+from geniusbot.geniusbot_chat import ChatBot
+from geniusbot.geniusbot import geniusbot
+"""
+geniusbot
+
+A tool that does it all!
+"""
+
+__version__ = __version__
+__author__ = __author__
+__credits__ = __credits__
