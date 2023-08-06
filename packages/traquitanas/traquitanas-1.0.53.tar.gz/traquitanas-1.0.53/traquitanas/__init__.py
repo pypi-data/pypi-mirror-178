d91e806d362f59d28a309529ff1a90ac3c62ff1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:22 +0000

    Add package traquitanas/1.0.53/traquitanas-1.0.53.tar.gz

---
+++ packages/traquitanas/traquitanas-1.0.53.tar.gz/traquitanas-1.0.53/traquitanas/__init__.py
@@ -0,0 +1,13 @@
+"""
+Used when import all
+Example: from traquitanas import *
+
+https://stackoverflow.com/questions/4902974/python-import-subpackage-from-a-package-not-working
+
+"""
+
+from traquitanas.net import downloads
+from traquitanas.scrapping import Driver, adds, gecko, scrapping
+
+# Net
+__all__ = ['downloads', 'scrapping', 'gecko', 'adds', 'Driver']
