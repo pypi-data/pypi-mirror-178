4b4fe3524c458735802ba7dc2cec43f51725a3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:11 +0000

    Add package graphql2python/0.0.1/graphql2python-0.0.1-py3-none-any.whl

---
+++ packages/graphql2python/graphql2python-0.0.1-py3-none-any.whl/graphql2python/__init__.py
@@ -0,0 +1,27 @@
+"""GRAPHQL2PYTHON
+
+Tools for GraphQL client in python:
+
+    - A simple GraphQL client;
+    - Auto generated pydantic classes from a GraphQL schema;
+    - Auto generated GraphQL queries;
+    - e.t.c
+
+The sub-packages:
+
+    - `graphql2python.client`:
+    - `graphql2python.model`:
+    - `graphql2python.query`:
+
+"""
+
+from .__info__ import __author__, __email__, __license__, __maintainer__
+from .__version__ import __version__
+
+__all__ = [
+    "__version__",
+    "__email__",
+    "__author__",
+    "__license__",
+    "__maintainer__",
+]
