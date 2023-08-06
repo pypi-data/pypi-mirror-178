f502b25e1c3aea7e0754f673ebd9de9b0081a0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:21 +0000

    Add package Fern2/1.2.2/Fern2-1.2.2.tar.gz

---
+++ packages/Fern2/Fern2-1.2.2.tar.gz/Fern2-1.2.2/fern/data/data_clean.py
@@ -0,0 +1,38 @@
+# Fern
+#
+# Author: Jason Lin
+# Email: jason.m.lin@outlook.com
+#
+# =============================================================================
+"""data cleaning"""
+
+
+def full_width_to_half_width(string: str) -> str:
+    """
+    字符串全角转半角，例如：`１２３４５６７８９０ -> 1234567890`
+
+    Args:
+        string: 待转换字符串
+
+    Returns:
+        半角字符串
+    """
+    table = {ord(f): ord(t) for f, t in zip(
+        u'＊・，。！？【】（）％＃＠＆１２３４５６７８９０、；—：■　ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ',
+        u'*·,.!?[]()%#@&1234567890.;-:￭ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')}
+
+    new_string = ''
+    for char in string:
+        char_ord = ord(char)
+        if char_ord in table:
+            char_ord = table[char_ord]
+            char = chr(char_ord)
+        new_string += char
+    return new_string
+
+
+def string_to_camel(string: str) -> str:
+    """
+    把下划线字符串转为驼峰模式
+    """
+    return ''.join([_string.capitalize() for _string in string.split('_')])
