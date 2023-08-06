e5c1570cc30b8a0a35b3dfdd3c82d1e280838f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:21 +0000

    Add package ntcore/0.1.5/ntcore-0.1.5.tar.gz

---
+++ packages/ntcore/ntcore-0.1.5.tar.gz/ntcore-0.1.5/setup.py
@@ -0,0 +1,48 @@
+import os
+
+from setuptools import find_packages, setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+about = {}
+with open(os.path.join(HERE, "ntcore", "__about__.py"), "r") as f:
+    exec(f.read(), about)
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    maintainer=about["__maintainer__"],
+    maintainer_email=about["__maintainer_email__"],
+    description=about["__description__"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license=about["__license__"],
+    url=about["__url__"],
+    packages=find_packages(),
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    install_requires=[
+        "cloudpickle",
+        "requests>=2.17.3",
+        "packaging",
+        "importlib_metadata>=3.7.0,!=4.7.0",
+        "numpy",
+        "pandas",
+        "gorilla",
+        "jwcrypto",
+        "six",
+        "python-jose",
+        "requests_toolbelt",
+        "requests-futures",
+        "psutil",
+        "click"
+    ],
+    entry_points={
+        "console_scripts": [
+            'ntcore = ntcore.cli.workflow:cli'
+        ],
+    },
+)
