95f90a41c8b0ea43a836200809cabdf3611717b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:27 +0000

    Add package delfino/0.20.2/delfino-0.20.2-py3-none-any.whl

---
+++ packages/delfino/delfino-0.20.2-py3-none-any.whl/delfino/contexts.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from click import make_pass_decorator
+from pydantic import BaseModel
+
+from delfino.constants import COMMANDS_DIRECTORY_NAME, PackageManager
+from delfino.models.pyproject_toml import PyprojectToml
+
+
+class AppContext(BaseModel):
+    project_root: Path
+    pyproject_toml: PyprojectToml
+    package_manager: PackageManager
+    commands_directory: Path = Path(COMMANDS_DIRECTORY_NAME)  # deprecated, remove once core package is created
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+pass_app_context = make_pass_decorator(AppContext)
