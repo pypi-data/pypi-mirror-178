821c1a51a3832d44cbdff5c0dc34be17d7cbfa6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:54 +0000

    Add package niagara/0.0.0.8/niagara-0.0.0.8.tar.gz

---
+++ packages/niagara/niagara-0.0.0.8.tar.gz/niagara-0.0.0.8/niagara/generic.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+import pygit2
+import json
+
+class Generic():
+    def __init__(self, file):
+        self.file = file
+
+    def operate(self):
+        with open(self.file) as f:
+            _data = f.read()
+            if "source" in json.loads(_data):
+                for cmd in json.loads(_data)["source"]:
+                    if "link" in cmd:
+                        pygit2.clone_repository(cmd["link"], str(cmd["link"].rsplit('/', 1).pop()))
+                    if "cmds" in cmd:
+                            cmds = cmd["cmds"]
+                            for x in cmds:
+                                if "cmd" in x:
+                                    if "dir" in x:
+                                        subprocess.call(x["cmd"].split(), cwd=x["dir"])
+                                    else:
+                                        subprocess.call(x["cmd"].split())
+            if "xinitrc" in json.loads(_data):
+                lines = []
+                for cmd in json.loads(_data)["xinitrc"]:
+                    lines.append(cmd)
+                with open(os.environ['HOME'] + '/.xinitrc', 'w+') as f:
+                    for line in lines:
+                        f.writelines([line])
+                f.close()
+
