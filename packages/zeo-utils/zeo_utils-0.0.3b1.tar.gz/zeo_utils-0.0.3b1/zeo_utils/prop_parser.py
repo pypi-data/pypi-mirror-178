5355d918a4bb5ef932ce38eaad81f5529fbbfa43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:05 +0000

    Add package zeo-utils/0.0.3b1/zeo_utils-0.0.3b1.tar.gz

---
+++ packages/zeo-utils/zeo_utils-0.0.3b1.tar.gz/zeo_utils-0.0.3b1/zeo_utils/prop_parser.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 11/24/2022 10:18 AM
+# @Author  : zhangbc0315@outlook.com
+# @File    : prop_parser.py
+# @Software: PyCharm
+
+
+class PropParser:
+
+    @classmethod
+    def _parse_tuple(cls, data, line):
+        key, values = line.split(': ')
+        # keys = keys.split(',')
+        values = values.split(',')
+        if data is None:
+            data = {key: []}
+        data[key].append(values)
+        return data
+
+    @classmethod
+    def parse(cls, pdm_str: str):
+        props = {}
+        logs = []
+        data = None
+        for line in pdm_str.split('\n'):
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            logs.append(line)
+            if line.startswith('@@'):
+                line = line[2:]
+                if line.startswith('TUPLE='):
+                    data = cls._parse_tuple(data, line[6:])
+                else:
+                    key, value = line.split(': ')
+                    props[key] = value
+        if data is not None:
+            props['data'] = data
+        props['logs'] = logs
+        return props
+
+
+if __name__ == "__main__":
+    pass
