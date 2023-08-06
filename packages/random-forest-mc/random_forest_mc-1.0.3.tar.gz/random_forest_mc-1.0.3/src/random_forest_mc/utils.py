4c9f8f1eb792d3d1e9c99ab98af2734006cf25fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:44 +0000

    Add package random-forest-mc/1.0.3/random_forest_mc-1.0.3.tar.gz

---
+++ packages/random-forest-mc/random_forest_mc-1.0.3.tar.gz/random_forest_mc-1.0.3/src/random_forest_mc/utils.py
@@ -0,0 +1,46 @@
+import json
+import numpy as np
+from glob import glob
+from typing import Any
+
+# For backward compatibility with 3.7
+# from typing import TypeAlias
+
+
+def np_encoder(object):
+    if isinstance(object, np.generic):
+        # Coverage trick!
+        _ = None
+        return object.item()
+
+
+# DictsPathType: TypeAlias = str
+DictsPathType = str
+
+
+def load_file_json(path: DictsPathType):
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def dump_file_json(path: DictsPathType, var: Any):
+    with open(path, "w") as f:
+        return json.dump(var, f, indent=4, default=np_encoder)
+
+
+class LoadDicts:
+    def __init__(self, dict_path: DictsPathType = "./data"):
+        Dicts_glob = glob(f"{dict_path}/*.json")
+        self.List = []
+        self.Dict = {}
+        for path_json in Dicts_glob:
+            name = path_json.split("/")[-1].replace(".json", "")
+            self.List.append(name)
+            self.Dict[name] = load_file_json(path_json)
+            setattr(self, name, self.Dict[name])
+
+    def __repr__(self) -> str:
+        return "LoadDicts: {}".format(", ".join(self.List))
+
+
+# EOF
