b5ddef864cfd5ca3944206057e42ca3c932afd20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:30 +0000

    Add package giphon/0.1.4/giphon-0.1.4-py2.py3-none-any.whl

---
+++ packages/giphon/giphon-0.1.4-py2.py3-none-any.whl/giphon/__init__.py
@@ -0,0 +1,21 @@
+__title__ = "Gitlab Siphon"
+__version__ = "0.1.4"
+__author__ = "Gabriel Creti"
+__license__ = "Apache"
+__copywrite__ = "Copyright (C) 2022 Gabriel Creti <gabrielcreti@gmail.com>"
+__email__ = "gabrielcreti@gmail.com"
+__status__ = "Development"
+
+from .siphon import siphon
+
+
+__all__ = [
+    "__title__",
+    "__version__",
+    "__author__",
+    "__license__",
+    "__copywrite__",
+    "__email__",
+    "__status__",
+    "siphon",
+]
---
+++ packages/giphon/giphon-0.1.4-py2.py3-none-any.whl/giphon/__main__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from typer import Typer
+
+from .siphon import siphon
+
+
+# TODO: add CI
+# TODO: add tests
+
+
+if __name__ == "__main__":
+
+    app = Typer(no_args_is_help=True, add_completion=False)
+
+    app.command()(siphon)
+    app()
