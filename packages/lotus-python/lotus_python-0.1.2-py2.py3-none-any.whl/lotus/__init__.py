00c85cd32a35f41c4298984da4a8544ac202ea34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:16 +0000

    Add package lotus-python/0.1.2/lotus_python-0.1.2-py2.py3-none-any.whl

---
+++ packages/lotus-python/lotus_python-0.1.2-py2.py3-none-any.whl/lotus/__init__.py
@@ -0,0 +1,92 @@
+from lotus.client import Client
+from lotus.version import VERSION
+
+__version__ = VERSION
+
+"""Settings."""
+api_key = None
+host = None
+on_error = None
+debug = False
+send = True
+sync_mode = False
+
+default_client = None
+
+
+def track_event(*args, **kwargs):
+    return _proxy("track_event", *args, **kwargs)
+
+
+def get_all_customers(*args, **kwargs):
+    return _proxy("get_all_customers", *args, **kwargs)
+
+
+def get_customer_detail(*args, **kwargs):
+    return _proxy("get_customer_detail", *args, **kwargs)
+
+
+def create_customer(*args, **kwargs):
+    return _proxy("create_customer", *args, **kwargs)
+
+
+def create_subscription(*args, **kwargs):
+    return _proxy("create_subscription", *args, **kwargs)
+
+
+def cancel_subscription(*args, **kwargs):
+    return _proxy("cancel_subscription", *args, **kwargs)
+
+
+def get_all_subscriptions(*args, **kwargs):
+    return _proxy("get_all_subscriptions", *args, **kwargs)
+
+
+def get_single_subscription(*args, **kwargs):
+    return _proxy("get_single_subscription", *args, **kwargs)
+
+
+def change_subscription_plan(*args, **kwargs):
+    return _proxy("change_subscription_plan", *args, **kwargs)
+
+
+def get_customer_access(*args, **kwargs):
+    return _proxy("get_customer_access", *args, **kwargs)
+
+
+def get_all_plans(*args, **kwargs):
+    return _proxy("get_subscriptions", *args, **kwargs)
+
+
+def flush():
+    """Tell the client to flush."""
+    _proxy("flush")
+
+
+def join():
+    """Block program until the client clears the queue"""
+    _proxy("join")
+
+
+def shutdown():
+    """Flush all messages and cleanly shutdown the client"""
+    _proxy("flush")
+    _proxy("join")
+
+
+def _proxy(method, *args, **kwargs):
+    """Create an analytics client if one doesn't exist and send to it."""
+    global default_client
+    if not default_client:
+        default_client = Client(
+            api_key,
+            host=host,
+            debug=debug,
+            on_error=on_error,
+            send=send,
+            sync_mode=sync_mode,
+        )
+
+    fn = getattr(default_client, method)
+    ret = fn(*args, **kwargs)
+    return ret
