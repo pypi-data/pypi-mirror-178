d9736055064baa3ac6fd040b4c74e9ad50f74132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:11 +0000

    Add package simauth/0.0.3/simauth-0.0.3.tar.gz

---
+++ packages/simauth/simauth-0.0.3.tar.gz/simauth-0.0.3/simauth/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import os
+
+from cryptography.fernet import Fernet
+
+
+class Accounts(object):
+
+    def __init__(self, accounts: dict, paths: list) -> None:
+        self.accounts = accounts
+        self.paths = paths
+
+    def get_key(self) -> str:
+        for p in self.paths:
+            if p.startswith('~'):
+                p = os.path.expanduser(p)
+            try:
+                return open(p).read().strip().encode()
+            except FileNotFoundError:
+                pass
+        raise Exception('Fernet key not found')
+
+    def initialize(self) -> None:
+
+        class Attrs(object):
+            pass
+
+        key = self.get_key()
+        fernet = Fernet(key)
+
+        for k, v in self.accounts.items():
+            key = fernet.decrypt(k.encode()).decode()
+            val = fernet.decrypt(v.encode()).decode()
+            try:
+                val = eval(val)
+            except Exception:
+                pass
+            setattr(Attrs, key, val)
+        return Attrs
+
+
+def simauth(accounts:dict, paths:list) -> 'Attrs':
+    return Accounts(accounts, paths).initialize()
