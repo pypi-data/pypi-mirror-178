45e811995a5928ad9f85869936783d455d9d447d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:18 +0000

    Add package pyperf-test/0.0.3/pyperf-test-0.0.3.tar.gz

---
+++ packages/pyperf-test/pyperf-test-0.0.3.tar.gz/pyperf-test-0.0.3/pyperf/classes/classes_slots.py
@@ -0,0 +1,27 @@
+from pyperf.base import profile, start
+
+class NoSlots:
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+        self.c = (1, 2, 34)
+
+class Slots:
+    __slots__ = ['a', 'b', 'c']
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+        self.c = (1, 2, 34)
+
+@profile()
+def class_slots():
+    xs = [Slots() for _ in range(0, 100_000)]
+    return xs
+
+@profile()
+def class_no_slots():
+    xs = [NoSlots() for _ in range(0, 100_000)]
+    return xs
+
+if __name__ == '__main__':
+    start(__file__, __name__, locals())
---
+++ packages/pyperf-test/pyperf-test-0.0.3.tar.gz/pyperf-test-0.0.3/pyperf/main.py
@@ -0,0 +1,11 @@
+from pyperf.classes import class_slots, class_no_slots
+from pyperf.lists import list_func1, list_func2, list_func3, list_func4, list_func5
+from pyperf.base import start
+
+def test_all():
+	start('classes', ['classes.classes_slots'], globals())
+	start('lists', ['lists.lists'], globals())
+
+if __name__ == '__main__':
+	test_all()
+
