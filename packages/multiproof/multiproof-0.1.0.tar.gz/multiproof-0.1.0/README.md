2016977de369128b29e4adfd2db6d5439aa8bcae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:41 +0000

    Add package multiproof/0.1.0/multiproof-0.1.0.tar.gz

---
+++ packages/multiproof/multiproof-0.1.0.tar.gz/multiproof-0.1.0/README.md
@@ -0,0 +1,34 @@
+# Stakewise python realization of `@openzeppelin/merkle-tree`
+
+## NB! Library is not well tested and not ready for the production use
+
+**A Python library to generate merkle trees and merkle proofs.**
+
+Well suited for airdrops and similar mechanisms in combination with OpenZeppelin Contracts [`MerkleProof`] utilities.
+
+[`MerkleProof`]: https://docs.openzeppelin.com/contracts/4.x/api/utils#MerkleProof
+
+## Quick Start
+
+``` shell
+poetry install
+```
+
+### Building a Tree
+
+```python
+from multiproof import StandardMerkleTree
+
+values = [
+    ["0x1111111111111111111111111111111111111111", 5000000000000000000],
+    ["0x2222222222222222222222222222222222222222", 2500000000000000000]
+]
+
+tree = StandardMerkleTree.of(values, ["address", "uint256"])
+
+print('Merkle Root:', tree.root)
+```
+
+# todos
+- extend tests
+- add docs
---
+++ packages/multiproof/multiproof-0.1.0.tar.gz/multiproof-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['multiproof', 'multiproof.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['eth-abi>=3.0.1,<4.0.0', 'web3==6.0.0b7']
+
+setup_kwargs = {
+    'name': 'multiproof',
+    'version': '0.1.0',
+    'description': 'A Python library to generate merkle trees and merkle proofs.',
+    'long_description': '# Stakewise python realization of `@openzeppelin/merkle-tree`\n\n## NB! Library is not well tested and not ready for the production use\n\n**A Python library to generate merkle trees and merkle proofs.**\n\nWell suited for airdrops and similar mechanisms in combination with OpenZeppelin Contracts [`MerkleProof`] utilities.\n\n[`MerkleProof`]: https://docs.openzeppelin.com/contracts/4.x/api/utils#MerkleProof\n\n## Quick Start\n\n``` shell\npoetry install\n```\n\n### Building a Tree\n\n```python\nfrom multiproof import StandardMerkleTree\n\nvalues = [\n    ["0x1111111111111111111111111111111111111111", 5000000000000000000],\n    ["0x2222222222222222222222222222222222222222", 2500000000000000000]\n]\n\ntree = StandardMerkleTree.of(values, ["address", "uint256"])\n\nprint(\'Merkle Root:\', tree.root)\n```\n\n# todos\n- extend tests\n- add docs\n',
+    'author': 'StakeWise Labs',
+    'author_email': 'info@stakewise.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
