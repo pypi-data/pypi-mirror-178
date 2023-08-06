fda1a2c14e3f2cc4afb80335e7a8b37f9c65052a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:05 +0000

    Add package cmdcheatsheet/0.0.32/cmdcheatsheet-0.0.32.tar.gz

---
+++ packages/cmdcheatsheet/cmdcheatsheet-0.0.32.tar.gz/cmdcheatsheet-0.0.32/cmdcheatsheet/alt_store/update_alt_store.py
@@ -0,0 +1,24 @@
+from cmdcheatsheet.alt_store.core import is_existing_store_name, update_alt_store
+from cmdcheatsheet.alt_store.messages import show_store_with_name_not_exists
+from cmdcheatsheet.shared.messages import show_invalid_store_location_message
+from cmdcheatsheet.shared.models import CommandArgument, CommandDetails
+from cmdcheatsheet.alt_store.models import AlternativeStore
+from cmdcheatsheet.config.validators import is_valid_custom_commands_location
+
+
+class UpdateAltStore(CommandDetails):
+    def __init__(self):
+       super().__init__(
+        ['--update-alternative-store', '-uas'],
+        "Update alternative commands store (JSON file).",
+        [CommandArgument('store_name'), CommandArgument('store_location')])
+
+    def handler(self, args):
+        alt_store = AlternativeStore(args[0], args[1])
+        if not is_existing_store_name(alt_store.name):
+            show_store_with_name_not_exists(alt_store.name)            
+        elif not is_valid_custom_commands_location(alt_store.location):
+            show_invalid_store_location_message()
+        else: 
+            update_alt_store(alt_store)
+
